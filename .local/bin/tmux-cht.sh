28cf00c838fdb8a3327f400bf0dec4d9b906e4e2
Rahsheen Porter <rahsheen.porter@gmail.com>
2022-01-17 23:13:36 -0500

    update tmux stuff

---
+++ .local/bin/tmux-cht.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+selected=`cat ~/.tmux-cht-languages ~/.tmux-cht-command | fzf`
+if [[ -z $selected ]]; then
+    exit 0
+fi
+
+read -p "Enter Query: " query
+
+if grep -qs "$selected" ~/.tmux-cht-languages; then
+    query=`echo $query | tr ' ' '+'`
+    tmux neww bash -c "echo \"curl cht.sh/$selected/$query/\" & curl cht.sh/$selected/$query & while [ : ]; do sleep 1; done"
+else
+    tmux neww bash -c "curl -s cht.sh/$selected~$query | less"
+fi
+
