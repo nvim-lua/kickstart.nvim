510ca59a6c2307283ecf27699cc3a3cdadf50260
Ash <ashisbitt@icloud.com>
2022-09-01 17:38:11 +0100

    Added cpp to treesitter and ordered TS imports

--- init.lua
+++ init.lua
@@ -187,7 +187,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'lua', 'typescript', 'rust', 'go', 'python' },
+  ensure_installed = {'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript'},
 
   highlight = { enable = true },
   indent = { enable = true },
