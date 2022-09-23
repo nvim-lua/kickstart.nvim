25362a0669bf759cd2e180019214cf7218a4e114
Andrew Ferrier <andrewferrier@users.noreply.github.com>
2022-09-23 18:44:20 +0100

    fix: Mistakes in merging

--- init.lua
+++ init.lua
@@ -8,7 +8,7 @@ if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
 end
 
 -- stylua: ignore start
-require('packer').startup(function(use
+require('packer').startup(function(use)
   use 'wbthomason/packer.nvim'                                           -- Package manager
   use 'tpope/vim-fugitive'                                               -- Git commands in nvim
   use 'tpope/vim-rhubarb'                                                -- Fugitive-companion to interact with github
@@ -27,7 +27,7 @@ require('packer').startup(function(use
   use 'tpope/vim-sleuth'                                                 -- Detect tabstop and shiftwidth automatically
   
   -- Fuzzy Finder (files, lsp, etc)
-  use { 'nvim-telescope/telescope.nvim', requires = { 'nvim-lua/plenary.nvim' } }
+  use { 'nvim-telescope/telescope.nvim', branch = '0.1.x', requires = { 'nvim-lua/plenary.nvim' } }
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable "make" == 1 }
