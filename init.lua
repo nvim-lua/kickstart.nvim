c73aefcb210125677aea9e62366c24beb0211b79
TJ DeVries <devries.timothyj@gmail.com>
2022-12-17 21:40:20 -0500

    Closes #46

--- init.lua
+++ init.lua
@@ -57,7 +57,7 @@ require('packer').startup(function(use)
   -- Fuzzy Finder Algorithm which requires local dependencies to be built. Only load if `make` is available
   use { 'nvim-telescope/telescope-fzf-native.nvim', run = 'make', cond = vim.fn.executable 'make' == 1 }
 
-  -- Add custom plugins to packer from /nvim/lua/custom/plugins.lua
+  -- Add custom plugins to packer from ~/.config/nvim/lua/custom/plugins.lua
   local has_plugins, plugins = pcall(require, 'custom.plugins')
   if has_plugins then
     plugins(use)
