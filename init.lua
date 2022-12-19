99df0d8f66ac499e949109883792ddb22ddba025
Debashis Biswas <37234687+debashisbiswas@users.noreply.github.com>
2022-12-19 15:57:28 -0700

    Fix error on Windows when space in install path (#64)

--- init.lua
+++ init.lua
@@ -3,7 +3,7 @@ local install_path = vim.fn.stdpath 'data' .. '/site/pack/packer/start/packer.nv
 local is_bootstrap = false
 if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
   is_bootstrap = true
-  vim.fn.execute('!git clone https://github.com/wbthomason/packer.nvim ' .. install_path)
+  vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
   vim.cmd [[packadd packer.nvim]]
 end
 
