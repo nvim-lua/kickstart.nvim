c4d7212de3d309a20435333f42a0473cee81e29a
amalgame21 <93383215+amalgame21@users.noreply.github.com>
2022-12-28 06:33:44 +0000

    Prevent multiple language servers from being spawned after every file save of init.lua (#96)
    * Prevent multiple language servers from being spawned
    
    Prevent multiple language servers from being spawned after every save of init.lua, which eventually leads to high RAM usage and system freeze.
    
    * Supress error messages
    
    add `silent!` in case of error message when nvim-lspconfig is not installed

--- init.lua
+++ init.lua
@@ -87,7 +87,7 @@ end
 -- Automatically source and re-compile packer whenever you save this init.lua
 local packer_group = vim.api.nvim_create_augroup('Packer', { clear = true })
 vim.api.nvim_create_autocmd('BufWritePost', {
-  command = 'source <afile> | PackerCompile',
+  command = 'source <afile> | silent! LspStop | silent! LspStart | PackerCompile',
   group = packer_group,
   pattern = vim.fn.expand '$MYVIMRC',
 })
