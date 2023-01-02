4916072854d01d0503821b7f3061daeb381f0441
willsmanic <57890520+nesper8@users.noreply.github.com>
2023-01-02 07:48:13 +0530

    added vim to tree-sitter's ensured installed list (#110)
    please refer to https://github.com/nvim-treesitter/nvim-treesitter/issues/3092

--- init.lua
+++ init.lua
@@ -221,7 +221,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'typescript', 'help', 'vim' },
 
   highlight = { enable = true },
   indent = { enable = true, disable = { 'python' } },
