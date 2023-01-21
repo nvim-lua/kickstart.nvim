ae402994afef9cca9223ea96a1dbee9b58d8dff5
Daniel Brittain Jr <66364685+DanielBrittainJr@users.noreply.github.com>
2023-01-21 09:09:13 -0500

    Added friendly snippets plugin to LuaSnip

--- init.lua
+++ init.lua
@@ -30,6 +30,11 @@ require('packer').startup(function(use)
     'hrsh7th/nvim-cmp',
     requires = { 'hrsh7th/cmp-nvim-lsp', 'L3MON4D3/LuaSnip', 'saadparwaiz1/cmp_luasnip' },
   }
+  
+  -- add friendly snippets to LuaSnip
+  use 'rafamadriz/friendly-snippets'
+  require("luasnip.loaders.from_vscode").lazy_load()
+
 
   use { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
