775ff6ac89d6a3fdf812f2fe79855f4a71ec1195
TJ DeVries <devries.timothyj@gmail.com>
2023-02-16 17:11:01 -0500

    more docs

--- init.lua
+++ init.lua
@@ -224,7 +224,7 @@ require('nvim-treesitter.configs').setup {
       init_selection = '<c-space>',
       node_incremental = '<c-space>',
       scope_incremental = '<c-s>',
-      node_decremental = '<c-backspace>',
+      node_decremental = '<M-space>',
     },
   },
   textobjects = {
