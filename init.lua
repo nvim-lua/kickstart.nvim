4b92837a7dfdc461d89a7b0c29f6f4d4179c0ea6
Tudor <59114560+tudorjnu@users.noreply.github.com>
2023-02-17 21:37:36 +0000

    feat: Add clipboard sync by default (#166)

--- init.lua
+++ init.lua
@@ -201,6 +201,11 @@ vim.wo.number = true
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
+-- Sync clipboard between OS and Neovim.
+--  Remove this option if you want your OS clipboard to remain independent.
+--  See `:help 'clipboard'`
+vim.o.clipboard = 'unnamedplus'
+
 -- Enable break indent
 vim.o.breakindent = true
 
