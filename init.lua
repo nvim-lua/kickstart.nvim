863c3619a87843d1c9c9691e707fbaf5f6bf4ddf
Tod Hansmann <githubpub@todandlorna.com>
2023-02-20 15:21:34 -0700

    docs: add comment about WSL clipboarding

--- init.lua
+++ init.lua
@@ -203,6 +203,7 @@ vim.o.mouse = 'a'
 
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
+--  If you are on WSL2, you should remove this line or nvim may hang on paste
 --  See `:help 'clipboard'`
 vim.o.clipboard = 'unnamedplus'
 
