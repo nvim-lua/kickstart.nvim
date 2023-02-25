a35bc79b5f59bd48c55415b5c6a116b83f7e7838
Ting Lu <ting-lu@student.kirkwood.edu>
2023-02-25 15:26:01 -0600

    Neotree close on only window open

--- lua/custom/plugins/filtree.lua
+++ lua/custom/plugins/filtree.lua
@@ -10,6 +10,8 @@ return {
     -- Unless you are still migrating, remove the deprecated commands from v1.x
     vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
-    require('neo-tree').setup {}
+    require('neo-tree').setup {
+      close_if_last_window = true,
+    }
   end,
 }
