187bebcbf93a82c29f12d946136b8d35ab695c64
Luiz Antonio Calliari Filho <luizcalliari.filho@protonmail.com>
2023-02-26 23:03:34 -0300

    Adding [+c and ]+c to move to diffs and leader+ph to show a pop up with the preview diff value.

--- init.lua
+++ init.lua
@@ -109,6 +109,19 @@ require('lazy').setup({
         topdelete = { text = '‾' },
         changedelete = { text = '~' },
       },
+      on_attach = function(bufnr)
+        local function map(mode, lhs, rhs, opts)
+            opts = vim.tbl_extend('force', {noremap = true, silent = true}, opts or {})
+            vim.api.nvim_buf_set_keymap(bufnr, mode, lhs, rhs, opts)
+        end
+
+        -- Navigation
+        map('n', ']c', "&diff ? ']c' : '<cmd>Gitsigns next_hunk<CR>'", {expr=true})
+        map('n', '[c', "&diff ? '[c' : '<cmd>Gitsigns prev_hunk<CR>'", {expr=true})
+
+        -- Actions
+        map('n', '<leader>ph', '<cmd>Gitsigns preview_hunk<CR>')
+      end
     },
   },
 
