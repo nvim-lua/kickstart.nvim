d14757cd4d575fa343093f0c03e4d7f67da22c1e
Luke Johnson <lukesj2011@gmail.com>
2023-02-28 11:19:09 -0700

    add better escape plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -22,5 +22,22 @@ return {
 				cmp_autopairs.on_confirm_done()
 			)
 		end
+	},
+
+	-- Better escape: press jj or jk to get out of insert mode
+	{
+		"max397574/better-escape.nvim",
+		config = function()
+			require("better_escape").setup {
+				mapping = { "jk", "jj" }, -- a table with mappings to use
+				timeout = vim.o.timeoutlen, -- the time in which the keys must be hit in ms. Use option timeoutlen by default
+				clear_empty_lines = false, -- clear line after escaping if there is only whitespace
+				keys = "<Esc>", -- keys used for escaping, if it is a function will use the result everytime
+				-- example(recommended)
+				-- keys = function()
+				--   return vim.api.nvim_win_get_cursor(0)[2] > 1 and '<esc>l' or '<esc>'
+				-- end,
+			}
+		end,
 	}
 }
