4449e70b05a3c614f4bcfe79663c4fb801a277be
Alexander <ahansen1335@gmail.com>
2023-03-13 16:21:16 +0100

    add ZenMode keymapping

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,6 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+vim.keymap.set("n", "<leader>zz", function()
+    require("zen-mode").toggle()
+end)
+
 return {
     "folke/zen-mode.nvim",
 }
