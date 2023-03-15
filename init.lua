b079304a9c912c24b9498fe0fe14ae19802ae816
Kyle Andelin <andelinkyle@gmail.com>
2023-03-15 09:39:01 -0700

    something

--- init.lua
+++ init.lua
@@ -120,18 +120,58 @@ require('lazy').setup({
     end,
   },
 
+--   { -- Set lualine as statusline
+--     'nvim-lualine/lualine.nvim',
+--     -- See `:help lualine.txt`
+--     opts = {
+--       options = {
+--         icons_enabled = false,
+--         theme = 'onedark',
+--         component_separators = '|',
+--         section_separators = '',
+--       },
+--     },
+--   },
+
+-- https://github.com/nvim-lualine/lualine.nvim
+-- https://github.com/bashbunni/dotfiles/blob/90b3f14be7691e06ad45b6e4f81bfe7bdefd7234/.config/nvim/lua/me/lualine.lua
   { -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
     opts = {
       options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
+        icons_enabled = true,
+        theme = 'auto',
+--         component_separators = '|',
+--         section_separators = '',
+      },
+      sections = {
+        lualine_a = {'mode'},
+        lualine_b = {'branch', 'diff'},
+        lualine_c = {'buffers'},
+        lualine_x = {'tabs'},
+        lualine_y = {'progress'},
+        lualine_z = {
+          { 'diagnostics',
+            sources = {'nvim_diagnostic', 'nvim_lsp'},
+            sections = {'error', 'warn', 'info', 'hint'},
+            diagnostics_color = {
+              -- Same values as the general color option can be used here.
+              error = 'DiagnosticError', -- Changes diagnostics' error color.
+              warn  = 'DiagnosticWarn',  -- Changes diagnostics' warn color.
+              info  = 'DiagnosticInfo',  -- Changes diagnostics' info color.
+              hint  = 'DiagnosticHint',  -- Changes diagnostics' hint color.
+            },
+            symbols = {error = 'E', warn = 'W', info = 'I', hint = 'H'},
+            colored = true,           -- Displays diagnostics status in color if set to true.
+            update_in_insert = false, -- Update diagnostics in insert mode.
+            always_visible = false,   -- Show diagnostics even if there are none.
+          },
+        },
       },
     },
   },
+  'kyazdani42/nvim-web-devicons',
 
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
@@ -410,7 +450,7 @@ local servers = {
   -- clangd = {},
   -- gopls = {},
   -- pyright = {},
-  -- rust_analyzer = {},
+  rust_analyzer = {},
   -- tsserver = {},
 
   lua_ls = {
