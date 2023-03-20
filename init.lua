b28ed339dfb18c54237bf68aaf01428c83015c0e
DexMech <alex.junior.carlos23@gmail.com>
2023-03-20 00:36:50 -0300

    prevent error if you dont have a custom plugin

--- init.lua
+++ init.lua
@@ -186,7 +186,7 @@ require('lazy').setup({
   --
   --    An additional note is that if you only copied in the `init.lua`, you can just comment this line
   --    to get rid of the warning telling you that there are not plugins in `lua/custom/plugins/`.
-  { import = 'custom.plugins' },
+  -- { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
