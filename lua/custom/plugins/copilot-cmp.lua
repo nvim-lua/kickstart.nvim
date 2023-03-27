c32565a48bd7582b947b3456c58efd9a6ff3a1b7
Thomas Alcala Schneider <thomas@lockrmail.com>
2023-03-27 19:16:35 +0200

    wip: make cmp use copilot

--- lua/custom/plugins/copilot-cmp.lua
+++ lua/custom/plugins/copilot-cmp.lua
@@ -1,11 +1,12 @@
 return {
-  'hrsh7th/cmp-copilot',
+  'zbirenbaum/copilot-cmp',
+  dependencies = { 'copilot.lua' },
   config = function()
+    require('copilot_cmp').setup()
     local cmp = require 'cmp'
     local config = cmp.get_config()
     table.insert(config.sources, {
       name = 'copilot',
-      option = {},
     })
     cmp.setup(config)
   end,
