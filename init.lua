f47c0365bdbdc2f70a00b4dac8db561d0f9e3d49
HieuTranTH <hieutran.th.358@gmail.com>
2023-04-02 11:59:52 +0300

    nvim-treesitter.configs: Rename help to vimdoc
    To reflect change 93fa5df from 'nvim-treesitter/nvim-treesitter'

--- init.lua
+++ init.lua
@@ -288,7 +288,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'help', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
