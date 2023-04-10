42b6d5f72e7bb66800a02f7adf912eb96fe9fc2a
SamPosh <samselvaprabu@gmail.com>
2023-04-10 15:36:56 +0530

    Update debug.lua

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -46,11 +46,12 @@ return {
     require('mason-nvim-dap').setup_handlers()
 
     -- Basic debugging keymaps, feel free to change to your liking!
+    
     vim.keymap.set('n', '<F5>', dap.continue)
-    vim.keymap.set('n', '<F1>', dap.step_into)
-    vim.keymap.set('n', '<F2>', dap.step_over)
-    vim.keymap.set('n', '<F3>', dap.step_out)
-    vim.keymap.set('n', '<leader>b', dap.toggle_breakpoint)
+    vim.keymap.set('n', '<F11>', dap.step_into)
+    vim.keymap.set('n', '<F10>', dap.step_over)
+    vim.keymap.set('n', '<S-F5>', dap.step_out)
+    vim.keymap.set('n', 'F9', dap.toggle_breakpoint)
     vim.keymap.set('n', '<leader>B', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
     end)
