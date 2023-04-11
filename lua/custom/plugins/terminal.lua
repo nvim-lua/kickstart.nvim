5eec133e0b4d536823826ea5c4d441174de02dde
SamPosh <samselvaprabu@gmail.com>
2023-04-11 19:15:14 +0530

    Terminal key is added

--- lua/custom/plugins/terminal.lua
+++ lua/custom/plugins/terminal.lua
@@ -1,3 +1,5 @@
 return {
-  'akinsho/toggleterm.nvim', version = "*", config = true
+  'akinsho/toggleterm.nvim', version = "*", config = true,
+
+  vim.keymap.set('n', '<C-j>', '<Cmd>ToggleTerm<CR>')
 }
