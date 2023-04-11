e2c244a3c489f7957e6d4517d3c52dbd4df3ea0f
SamPosh <samselvaprabu@gmail.com>
2023-04-11 19:04:49 +0530

    Update filetree.lua

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,6 +10,7 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup {}
+    require('neo-tree').setup{}
   end,
+  vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 }
