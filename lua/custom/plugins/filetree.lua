c049aadcd4ff216f0d354a03d8d438c6c6f29ed0
SamPosh <samselvaprabu@gmail.com>
2023-04-13 14:39:37 +0530

    Hide .git and .github folders

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,3 +1,5 @@
+    
+    
 -- Unless you are still migrating, remove the deprecated commands from v1.x
 vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
 
@@ -10,7 +12,24 @@ return {
     "MunifTanjim/nui.nvim",
   },
   config = function ()
-    require('neo-tree').setup{}
+    require('neo-tree').setup{
+       filesystem = {
+          filtered_items = {
+            visible = true, -- when true, they will just be displayed differently than normal items
+            hide_dotfiles = true,
+            hide_hidden = true,
+            hide_gitignored = true,
+            always_show = { -- remains visible even if other settings would normally hide it
+              ".vscode",
+              ".jenkinsfiles"
+            },
+            never_show = {
+              ".git",
+              ".github"
+            }
+          }
+      }
+    }
   end,
   vim.keymap.set('n', '<C-b>', '<Cmd>Neotree toggle<CR>')
 }
