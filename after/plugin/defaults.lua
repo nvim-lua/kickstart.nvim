1fbf3b32f8ecbffb61f21fbe87c642ce9cb64a04
Matthew Rowe <mrowe.w@gmail.com>
2023-04-13 18:45:19 -0400

    update

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -1,3 +1,13 @@
 vim.opt.relativenumber = true
 
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
+
+vim.keymap.set('n', '<leader>cc', function() require('dap').continue() end)
+vim.keymap.set('n', '<leader>so', function() require('dap').step_over() end)
+vim.keymap.set('n', '<leader>si', function() require('dap').step_into() end)
+vim.keymap.set('n', '<leader>sO', function() require('dap').step_out() end)
+vim.keymap.set('n', '<Leader>b', function() require('dap').toggle_breakpoint() end)
+vim.keymap.set('n', '<Leader>B', function() require('dap').set_breakpoint() end)
+vim.keymap.set('n', '<Leader>dr', function() require('dap').repl.open() end)
+vim.keymap.set('n', '<Leader>dl', function() require('dap').run_last() end)
+
