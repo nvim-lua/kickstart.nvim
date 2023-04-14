a292c1390fb0e0a861c25a569d095de1a37abee2
SamPosh <samselvaprabu@gmail.com>
2023-04-14 08:59:36 +0530

    Update debug.lua

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -32,6 +32,10 @@ return {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
       automatic_setup = true,
+      
+      -- You can provide additional configuration to the handlers,
+      -- see mason-nvim-dap README for more information
+      -- handlers = {},
 
       -- You'll need to check that you have the required things installed
       -- online, please don't ask me how to install them :)
