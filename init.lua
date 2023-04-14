2c41f65d1e47a7fd27cb52539affed1c322f1104
SamPosh <samselvaprabu@gmail.com>
2023-04-14 10:16:04 +0530

    disable netrw

--- init.lua
+++ init.lua
@@ -42,6 +42,10 @@ P.S. You can delete this when you're done too. It's your config now :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- disable netrw in favor of neo-tree
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
 --    `:help lazy.nvim.txt` for more info
