83de07ad02d63aa2a69020a157d8e2bdc05246a8
vladstojna <vlad.leic@gmail.com>
2023-04-18 21:06:43 +0100

    add additional plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,15 +7,102 @@ return {
   "nvim-treesitter/nvim-treesitter-context",
   "theprimeagen/harpoon",
   "mbbill/undotree",
-  "windwp/nvim-autopairs",
   "RRethy/vim-illuminate",
   "ahmedkhalf/project.nvim",
   "famiu/bufdelete.nvim",
-
+  "tpope/vim-repeat",
+  "JoosepAlviste/nvim-ts-context-commentstring",
+  {
+    "dstein64/vim-startuptime",
+    cmd = "StartupTime",
+    config = function()
+      vim.g.startuptime_tries = 10
+    end,
+  },
+  {
+    "windwp/nvim-spectre",
+    keys = {
+      {
+        "<leader>sr",
+        function() require("spectre").open() end,
+        desc = "[R]eplace in files (Spectre)"
+      },
+    },
+  },
+  {
+    "folke/persistence.nvim",
+    event = "BufReadPre",
+    opts = { options = { "buffers", "curdir", "tabpages", "winsize", "help", "globals" } },
+    keys = {
+      { "<leader>ps", function() require("persistence").load() end,                desc = "Restore Session" },
+      { "<leader>pl", function() require("persistence").load({ last = true }) end, desc = "Restore Last Session" },
+      { "<leader>pd", function() require("persistence").stop() end,                desc = "Don't Save Current Session" },
+    },
+  },
   {
     "folke/trouble.nvim",
     dependencies = {
       "nvim-tree/nvim-web-devicons",
     },
-  }
+    opts = {
+      use_diagnostic_signs = true,
+    },
+  },
+  {
+    "rcarriga/nvim-notify",
+    keys = {
+      {
+        "<leader>dn",
+        function()
+          require("notify").dismiss({ silent = true, pending = true })
+        end,
+        desc = "[D]elete all [N]otifications",
+      },
+    },
+  },
+  {
+    "folke/todo-comments.nvim",
+    cmd = { "TodoTrouble", "TodoTelescope" },
+    config = true,
+    keys = {
+      { "]t",         function() require("todo-comments").jump_next() end, desc = "Next todo comment" },
+      { "[t",         function() require("todo-comments").jump_prev() end, desc = "Previous todo comment" },
+      { "<leader>xt", "<cmd>TodoTrouble<cr>",                              desc = "Todo (Trouble)" },
+      { "<leader>xT", "<cmd>TodoTrouble keywords=TODO,FIX,FIXME<cr>",      desc = "Todo/Fix/Fixme (Trouble)" },
+      { "<leader>st", "<cmd>TodoTelescope<cr>",                            desc = "Todo" },
+      { "<leader>sT", "<cmd>TodoTelescope keywords=TODO,FIX,FIXME<cr>",    desc = "Todo/Fix/Fixme" },
+    },
+  },
+  {
+    'echasnovski/mini.pairs',
+    version = false,
+    config = function()
+      require('mini.pairs').setup()
+    end
+  },
+  {
+    'echasnovski/mini.trailspace',
+    version = false,
+    config = function()
+      require('mini.trailspace').setup()
+    end
+  },
+  {
+    'echasnovski/mini.surround',
+    version = false,
+    opts = {
+      mappings = {
+        add = "gsa",            -- Add surrounding in Normal and Visual modes
+        delete = "gsd",         -- Delete surrounding
+        find = "gsf",           -- Find surrounding (to the right)
+        find_left = "gsF",      -- Find surrounding (to the left)
+        highlight = "gsh",      -- Highlight surrounding
+        replace = "gsr",        -- Replace surrounding
+        update_n_lines = "gsn", -- Update `n_lines`
+      },
+    },
+    config = function(_, opts)
+      require('mini.surround').setup(opts)
+    end
+  },
 }
