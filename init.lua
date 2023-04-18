c3fddb21bc26ed84f845484f182581e0e574f06b
vladstojna <vlad.leic@gmail.com>
2023-04-18 02:07:01 +0100

    comment clipboard sharing option

--- init.lua
+++ init.lua
@@ -240,7 +240,7 @@ vim.o.mouse = 'a'
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.o.clipboard = 'unnamedplus'
+-- vim.o.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.o.breakindent = true
