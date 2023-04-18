fcbb6c3ad436d89bb0183b5042ebc135c1349126
vladstojna <vlad.leic@gmail.com>
2023-04-18 14:06:51 +0100

    change cmp confirm mapping

--- init.lua
+++ init.lua
@@ -506,7 +506,7 @@ cmp.setup {
     ['<C-d>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
-    ['<CR>'] = cmp.mapping.confirm {
+    ['<C-s>'] = cmp.mapping.confirm {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
