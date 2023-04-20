2d5130db44fb5d8c91b834783acf45646ced2bae
SamPosh <samselvaprabu@gmail.com>
2023-04-20 14:59:53 +0530

    Python dap support sample

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -32,13 +32,18 @@ return {
 
       -- You can provide additional configuration to the handlers,
       -- see mason-nvim-dap README for more information
-      handlers = {},
+      handlers = {
+        -- python debug configuration is available in this file. Uncomment to add python support
+        -- python = require('kickstart.plugins.dap.handler.python'),
+
+      },
 
       -- You'll need to check that you have the required things installed
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
         'delve',
+        'python'
       },
     }
 
