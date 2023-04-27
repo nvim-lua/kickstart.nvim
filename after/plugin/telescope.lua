504564146294b80c47a0915bb84d6d9dc8e73e89
vladstojna <vlad.leic@gmail.com>
2023-04-27 16:52:08 +0100

    add telescope keymap

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,6 +1,8 @@
+local telescope_builtin = require('telescope.builtin')
+
 vim.keymap.set('n', '<leader>sa',
     function()
-        require('telescope.builtin').find_files({
+        telescope_builtin.find_files({
             hidden = true,
             no_ignore = false,
         })
@@ -10,10 +12,14 @@ vim.keymap.set('n', '<leader>sa',
 
 vim.keymap.set('n', '<leader>sA',
     function()
-        require('telescope.builtin').find_files({
+        telescope_builtin.find_files({
             hidden = true,
             no_ignore = true,
         })
     end,
     { desc = '[S]earch [A]ll Files (+ hidden & ignored)' }
+)
+
+vim.keymap.set('n', '<leader>sk', telescope_builtin.keymaps,
+    { desc = '[S]earch [K]eymaps' }
 )
