ecff79bb3a90a3b80f1cb7e65251874c21b1a6db
nathan <nathanj758@gmail.com>
2023-04-30 23:57:01 -0400

    Add ToggleTerm plugin

---
+++ lua/custom/plugins/toggle_term.lua
@@ -0,0 +1,9 @@
+return {
+        'akinsho/toggleterm.nvim',
+        version = "*",
+        opts = {
+                direction = 'float',
+                open_mapping = [[<c-\>]],
+                shell = '/usr/bin/fish',
+        }
+}
