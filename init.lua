8aea776281be3d6487b31680da314700c0751ebc
SamPosh <samselvaprabu@gmail.com>
2023-05-02 14:05:03 +0530

    removed friendly snippet from snippet

--- init.lua
+++ init.lua
@@ -100,13 +100,6 @@ require('lazy').setup({
     dependencies = { 
         'hrsh7th/cmp-nvim-lsp', 
         'L3MON4D3/LuaSnip', 
-        dependencies = {
-        {
-          "rafamadriz/friendly-snippets",
-          config = function()
-            require("luasnip.loaders.from_vscode").lazy_load()
-          end,
-        },
         'saadparwaiz1/cmp_luasnip' 
       },
   },
