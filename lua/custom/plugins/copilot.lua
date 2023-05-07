0e3f8240b27607151994384f6c3ce0d0551f54f6
Thomas Alcala Schneider <thomas@lockrmail.com>
2023-05-07 17:23:00 +0200

    fix: setup copilot, fix feline lazy config

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -23,5 +23,6 @@ return {
     -- vim.g.copilot_no_tab_map = true
     vim.g.copilot_assume_mapped = true
     vim.keymap.set('i', '<C-J>', 'copilot#Accept("<CR>")', { silent = true, expr = true })
+    require('copilot').setup()
   end,
 }
--- lua/custom/plugins/feline.lua
+++ lua/custom/plugins/feline.lua
@@ -1,7 +1,7 @@
 return {
-            'feline-nvim/feline.nvim',
-            requires = { 'kyazdani42/nvim-web-devicons', opt = true },
-            config = function()
-                require('feline').setup()
-            end
-        }
+  'feline-nvim/feline.nvim',
+  dependencies = { 'kyazdani42/nvim-web-devicons', opt = true },
+  config = function()
+    require('feline').setup()
+  end,
+}
