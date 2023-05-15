37808a6f6e89abd05e20c0f5aa7bd82eaf7880bd
Jacob Hoopes <jake.hoopes@gmail.com>
2023-05-15 14:51:52 -0600

    Add Github Copilot plugin for neovim

--- init.lua
+++ init.lua
@@ -298,13 +298,13 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
+  ensure_installed = { 'c', 'cpp', 'go', 'java', 'javascript', 'lua', 'python', 'ruby', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
 
   highlight = { enable = true },
-  indent = { enable = true, disable = { 'python' } },
+  indent = { enable = true, disable = { 'python', 'ruby' } },
   incremental_selection = {
     enable = true,
     keymaps = {
