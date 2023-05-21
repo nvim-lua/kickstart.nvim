2dc76c90e216b19f9895f271b2e121fb7e8148b7
jsphwllng <63716060+jsphwllng@users.noreply.github.com>
2023-05-21 11:27:20 +0200

    Update init.lua
    Adding a small autocomplete plugin to the plugin list. The functionality is described succinctly [here](https://github.com/cohama/lexima.vim#basic-rules).
    
    This input rule is a common feature in modern IDEs and I think it would make the transition from other editors to NVIM more smooth.

--- init.lua
+++ init.lua
@@ -71,6 +71,9 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+    
+  -- Automatically append closing quotes and brackets when typing
+  'cohama/lexima.vim'
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
