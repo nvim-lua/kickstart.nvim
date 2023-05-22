ca75729c7a4d7faa35ebe5ade287140f1c1e0a7a
Gustavo Silva <Gustavo Silva (es00679121_prosegur)>
2023-05-22 17:21:00 +0200

    add neo-tree

---
+++ lua/custom/plugins/neo-tree.lua
@@ -0,0 +1,17 @@
+-- You can add your own plugins here or in other files in this directory!
+--  I promise not to create any merge conflicts in this directory :)
+--
+-- See the kickstart.nvim README for more information
+
+return {
+	"nvim-neo-tree/neo-tree.nvim",
+	version = "*",
+	dependencies= {
+		"nvim-lua/plenary.nvim",
+		"nvim-tree/nvim-web-devicons",
+		"MunifTanjim/nui.nvim",
+	},
+	config = function ()
+		require('neo-tree').setup {}
+	end
+}
