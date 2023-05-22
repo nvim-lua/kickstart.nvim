1e6a8e904962679667956584690955259f445893
Garrett Dawson <killtheliterate@gmail.com>
2023-05-22 16:34:41 -0600

    fix: syntax issue

--- init.lua
+++ init.lua
@@ -108,8 +108,8 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',          opts = {} }
-    
+  { 'folke/which-key.nvim',          opts = {} },
+
   {
     -- Adds git releated signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
