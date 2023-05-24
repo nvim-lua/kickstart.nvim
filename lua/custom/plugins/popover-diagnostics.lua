770822730e1632a856973dc055ddba30a2373e0b
Justin Prather <justinprather94@gmail.com>
2023-05-24 00:21:26 -0600

    better popover error messages

---
+++ lua/custom/plugins/popover-diagnostics.lua
@@ -0,0 +1,6 @@
+-- You will likely want to reduce updatetime which affects CursorHold
+-- note: this setting is global and should be set only once
+vim.o.updatetime = 250
+vim.cmd [[autocmd! CursorHold,CursorHoldI * lua vim.diagnostic.open_float(nil, {focus=false})]]
+
+return {}
