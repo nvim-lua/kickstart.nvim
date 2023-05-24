0a7631595fcecc2f1c90329c81ef4ef61a1f59f6
vladstojna <vlad.leic@gmail.com>
2023-05-24 18:35:59 +0100

    change and add some keymaps

--- after/plugin/remap.lua
+++ after/plugin/remap.lua
@@ -3,7 +3,7 @@ vim.keymap.set("v", "K", ":m '<-2<CR>gv=gv")
 
 vim.keymap.set("n", "J", "mzJ`z")
 vim.keymap.set("n", "<C-d>", "<C-d>zz")
-vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "<C-s>", "<C-u>zz")
 vim.keymap.set("n", "<PageDown>", "<C-d>zz")
 vim.keymap.set("n", "<PageUp>", "<C-u>zz")
 vim.keymap.set("n", "{", "{zz")
@@ -22,6 +22,10 @@ vim.keymap.set("n", "<leader>Y", [["+Y]])
 vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 
 vim.keymap.set("i", "<C-c>", "<Esc>")
+vim.keymap.set({ "i", "x" }, "kj", "<Esc>")
+vim.keymap.set("i", "<C-d>", "<C-o>de")
+vim.keymap.set("i", "<C-h>", "<Backspace>")
+vim.keymap.set("i", "<C-l>", "<Del>")
 
 -- vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<leader>f", vim.lsp.buf.format,
