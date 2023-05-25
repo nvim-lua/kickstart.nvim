eb34e6276b9fbac368bbd20462c74f57ab1d9b11
vladstojna <vlad.leic@gmail.com>
2023-05-25 19:02:11 +0100

    add some lualine separator options

--- after/plugin/lualine.lua
+++ after/plugin/lualine.lua
@@ -1,6 +1,8 @@
 require('lualine').setup {
     options = {
         theme = 'tokyonight',
+        component_separators = { left = '', right = '' },
+        section_separators = { left = '', right = '' },
     },
     extensions = {
         'toggleterm',
