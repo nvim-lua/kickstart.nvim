44a4303cd354f4229fc3b7bb302a6d7b7c387a6f
Chris Patti <feoh@feoh.org>
2023-06-02 18:15:33 +0000

    This commit refactors the theme & status line cfg
    This commit refactors the theme & status line configuration
    into their own lua files so they can be more readily
    customized by the user. As kickstart has grown, it's becoming
    harder and harder to keep up with changes to the main init.lua
    file if you're maintaining local customizations. We should still
    encourage users to modify init.lua to suit their needs, but also
    make it possible to leave it unmodified if that's their choice.

--- init.lua
+++ init.lua
@@ -129,28 +129,11 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Theme inspired by Atom
-    'navarasu/onedark.nvim',
-    priority = 1000,
-    config = function()
-      vim.cmd.colorscheme 'onedark'
-    end,
-  },
+  -- Theme related configs go here
+  require 'kickstart.plugins.theme',
 
-  {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
-    },
-  },
+  -- Status line related configs go here
+  require 'kickstart.plugins.statusline',
 
   {
     -- Add indentation guides even on blank lines
---
+++ lua/kickstart/plugins/theme.lua
@@ -0,0 +1,9 @@
+return {
+  -- Theme inspired by Atom
+  'navarasu/onedark.nvim',
+  priority = 1000,
+  config = function()
+    vim.cmd.colorscheme 'onedark'
+  end,
+}
+
