a0d2b224cdb057a72bbb87de2c2a48a31afcf34e
Micah Effiong <micaiah.effiong@gmail.com>
2023-06-06 11:48:05 +0100

    added custom settings for one dark theme

---
+++ lua/custom/plugins/onedark-theme.lua
@@ -0,0 +1,13 @@
+return {
+	-- Theme inspired by Atom
+	'navarasu/onedark.nvim',
+	priority = 1000,
+	config = function()
+		vim.cmd.colorscheme 'onedark'
+	end,
+	require('onedark').setup {
+		style = 'deep',
+		toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
+		toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'},
+	}
+}
