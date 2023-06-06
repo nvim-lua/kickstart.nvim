57a4adc3bf5bb1353817576b6d5b42bcafa4ff51
Micah Effiong <micaiah.effiong@gmail.com>
2023-06-06 11:49:01 +0100

    added custom vim settings

---
+++ lua/custom/options/init.lua
@@ -0,0 +1,14 @@
+-- custom vim options
+
+-- set fat cursor
+vim.opt.guicursor = ''
+
+-- set relative-number
+vim.wo.relativenumber = true
+
+vim.o.termguicolors = true
+vim.opt.smartindent = true
+vim.opt.tabstop = 2
+vim.opt.shiftwidth = 2
+vim.opt.expandtab = true
+
