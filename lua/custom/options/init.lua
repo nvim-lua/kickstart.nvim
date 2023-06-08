7e03113c497d7d2c0f6950c9fba62a9c9a5eaf3a
Micah Effiong <micaiah.effiong@gmail.com>
2023-06-08 01:22:25 +0100

    added horizontal cursorline

--- lua/custom/options/init.lua
+++ lua/custom/options/init.lua
@@ -16,6 +16,8 @@ vim.opt.colorcolumn = '80'
 -- always have a set number of lines
 -- below the screen except at the end of the screen
 vim.opt.scrolloff = 8
-
 vim.opt.splitright = true
 vim.opt.splitbelow = true
+
+-- horizontal cursorline
+vim.opt.cursorline = true
