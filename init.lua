df8b6d79e7a137de96b808d65b5b05c3389f5296
Andres <andrsbtrg@gmail.com>
2023-06-12 19:27:09 +0200

    Fix annoying telescope depr warning

--- init.lua
+++ init.lua
@@ -155,7 +155,7 @@ require('lazy').setup({
   { 'numToStr/Comment.nvim',         opts = {} },
 
   -- Fuzzy Finder (files, lsp, etc)
-  { 'nvim-telescope/telescope.nvim', version = '*', dependencies = { 'nvim-lua/plenary.nvim' } },
+  { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
 
   -- Fuzzy Finder Algorithm which requires local dependencies to be built.
   -- Only load if `make` is available. Make sure you have the system
