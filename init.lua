91bdea975ef78688f35b51b017e09f7d9dd80fee
Andres <andrsbtrg@gmail.com>
2023-06-13 23:50:09 +0200

    Tag fidget to legacy

--- init.lua
+++ init.lua
@@ -84,7 +84,8 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      -- tag to legacy to avoid message startup
+      { 'j-hui/fidget.nvim', opts = {}, tag = 'legacy' },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
