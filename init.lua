f8c20d648b9e8515a259fd071ae66da48881b35c
Gavin <gavin@rohrer.org>
2023-06-14 17:21:15 -0400

    Add legacy tag to fidget to avoid deprecation warning

--- init.lua
+++ init.lua
@@ -84,7 +84,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim', tag = 'legacy', opts = {} },
 
       -- Additional lua configuration, makes nvim stuff amazing!
       'folke/neodev.nvim',
