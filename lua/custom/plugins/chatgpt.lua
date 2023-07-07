656828dc730b5c65876fd023fa9f2ebab588f2c2
Andreas Wachs <andreas@wachs.dk>
2023-07-07 06:56:44 +0200

    Add chatgpt

---
+++ lua/custom/plugins/chatgpt.lua
@@ -0,0 +1,13 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
+}
