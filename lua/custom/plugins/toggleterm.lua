9bb018d2295711dc629489a34c27c5aa70e4a4a6
Andreas Wachs <andreas@wachs.dk>
2023-07-07 07:45:27 +0200

    Remove keymap setup from toggleterm

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,9 +1,4 @@
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
 
-function _lazygit_toggle()
-  lazygit:toggle()
-end
 
 return {
   {
