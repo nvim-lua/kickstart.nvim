5eb1daec00bab2b7f851cd670ec6b8f977926de6
Andreas Wachs <andreas@wachs.dk>
2023-07-10 06:45:46 +0200

    Fix terminal

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -5,7 +5,7 @@
 
 
 -- Neotree
-vim.keymap.set('n', '<space>e', function() vim.cmd.Neotree("toggle") end, { desc = 'Toggle Neotree' })
+vim.keymap.set('n', '<space>f', function() vim.cmd.Neotree("toggle") end, { desc = 'Toggle Neotree' })
 
 -- Movement in the editor
 vim.keymap.set('n', '<C-h>', function() vim.cmd.wincmd("h") end, { desc = 'Terminal left window navigation' })
@@ -18,33 +18,6 @@ vim.keymap.set('n', '<S-h>', function() vim.cmd.tabprevious() end, { desc = 'Mov
 vim.keymap.set('n', '<S-l>', function() vim.cmd.tabnext() end, { desc = 'Move to next tab'})
 vim.keymap.set('n', '<space>tn', function() vim.cmd.tabnew() end, { desc = 'Create new tab' })
 vim.keymap.set('n', '<space>tc', function() vim.cmd.tabclose() end, { desc = 'Close tab'})
-
-
--- ToggleTerm
-vim.keymap.set('n', '<space>tt', function() vim.cmd('ToggleTerm direction=float')  end, { desc = 'Open ToggleTerm'})
-vim.keymap.set('n', '<F7>', function() vim.cmd('ToggleTerm')  end, { desc = 'Toggle ToggleTerm'})
-
--- Lazygit
-local Terminal  = require('toggleterm.terminal').Terminal
-local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
-local pythonREPL = Terminal:new({ cmd = "python3", hidden = true })
-local haskellREPL = Terminal:new({ cmd = "ghci", hidden = true })
-
-function _lazygit_toggle()
-  lazygit:toggle()
-end
-
-function _pythonREPL_toggle()
-  pythonREPL:toggle()
-end
-
-function _haskellREPL_toggle()
-  haskellREPL:toggle()
-end
-
-vim.keymap.set('n', '<space>tl', "<cmd>lua _lazygit_toggle()<CR>", { desc = 'Toggle Lazygit', noremap = true, silent = true })
-vim.keymap.set('n', '<space>tp', "<cmd>lua _pythonREPL_toggle()<CR>", { desc = 'Toggle Python3 REPL', noremap = true, silent = true })
-vim.keymap.set('n', '<space>th', "<cmd>lua _haskellREPL_toggle()<CR>", { desc = 'Toggle GHCI', noremap = true, silent = true })
 
 return {
 }
--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -5,6 +5,35 @@ return {
     "akinsho/toggleterm.nvim",
     event = "VeryLazy",
     cmd = { "ToggleTerm", "TermExec" },
+    config = function()
+      -- Lazygit
+      local Terminal  = require('toggleterm.terminal').Terminal
+      local defaultTerm = Terminal:new({ cmd = "zsh", hidden = true, orientation = "float" })
+      local lazygit = Terminal:new({ cmd = "lazygit", hidden = true })
+      local pythonREPL = Terminal:new({ cmd = "python3", hidden = true })
+      local haskellREPL = Terminal:new({ cmd = "ghci", hidden = true })
+
+      function _defaultterm_toggle()
+        defaultTerm:toggle()
+      end
+      function _lazygit_toggle()
+        lazygit:toggle()
+      end
+
+      function _pythonREPL_toggle()
+        pythonREPL:toggle()
+      end
+
+      function _haskellREPL_toggle()
+        haskellREPL:toggle()
+      end
+
+      vim.keymap.set('n', '<space>tt', "<cmd>lua _defaultterm_toggle()<CR>", { desc = 'Toggle Lazygit', noremap = true, silent = true })
+      vim.keymap.set('n', '<F7>', "<cmd>lua _defaultterm_toggle()<CR>", { desc = 'Toggle Lazygit', noremap = true, silent = true })
+      vim.keymap.set('n', '<space>tl', "<cmd>lua _lazygit_toggle()<CR>", { desc = 'Toggle Lazygit', noremap = true, silent = true })
+      vim.keymap.set('n', '<space>tp', "<cmd>lua _pythonREPL_toggle()<CR>", { desc = 'Toggle Python3 REPL', noremap = true, silent = true })
+      vim.keymap.set('n', '<space>th', "<cmd>lua _haskellREPL_toggle()<CR>", { desc = 'Toggle GHCI', noremap = true, silent = true })
+    end,
     opts = {
       size = 10,
       on_create = function()
