734cc4e94aec64c8cb3504711978c51b092f2542
Merel Jossart <merel.jossart@outlook.com>
2023-07-25 17:06:04 +0200

    Fix bug when server not explicitely defined in configuration but installed

--- init.lua
+++ init.lua
@@ -463,7 +463,7 @@ mason_lspconfig.setup_handlers {
       capabilities = capabilities,
       on_attach = on_attach,
       settings = servers[server_name],
-      filetypes = servers[server_name].filetypes,
+      filetypes = (servers[server_name] or {}).filetypes,
     }
   end
 }
