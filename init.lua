f5e6162f9aef92c93d5e7fbb64599fb7d173411a
Markus Ullmann <morjgus@protonmail.com>
2023-08-02 00:09:37 +0200

    Enable import of custom plugins

--- init.lua
+++ init.lua
@@ -282,7 +282,7 @@ require('lazy').setup({
   --    Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --
   --    For additional information see: https://github.com/folke/lazy.nvim#-structuring-your-plugins
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {})
 
 -- [[ Setting options ]]
