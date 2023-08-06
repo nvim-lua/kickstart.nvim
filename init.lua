a5a263d3364b9ff6bbd2c20ab9c8bd99461cb645
daan <daanrongen@Hotmail.com>
2023-08-06 16:27:05 +0100

    fix: missing comma

--- init.lua
+++ init.lua
@@ -171,7 +171,7 @@ require('lazy').setup({
     'nvim-telescope/telescope.nvim',
     branch = '0.1.x',
     dependencies = {
-      'nvim-lua/plenary.nvim'
+      'nvim-lua/plenary.nvim',
       -- Fuzzy Finder Algorithm which requires local dependencies to be built.
       -- Only load if `make` is available. Make sure you have the system
       -- requirements installed.
