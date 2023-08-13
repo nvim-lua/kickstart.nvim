c96aae75722f7929b62bead8847571fd806c4344
juanmagalhaes <contato.juanmagalhaes@gmail.com>
2023-08-13 18:57:15 -0300

    Changes

--- init.lua
+++ init.lua
@@ -95,7 +95,6 @@ pcall(require('telescope').load_extension, 'fzf')
 
 -- See `:help telescope.builtin`
 vim.keymap.set('n', '<leader>?', require('telescope.builtin').oldfiles, { desc = '[?] Find recently opened files' })
-vim.keymap.set('n', '<leader><space>', require('telescope.builtin').buffers, { desc = '[ ] Find existing buffers' })
 vim.keymap.set('n', '<leader>/', function()
   -- You can pass additional configuration to telescope to change theme, layout, etc.
   require('telescope.builtin').current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
@@ -110,6 +109,12 @@ vim.keymap.set('n', '<leader>sh', require('telescope.builtin').help_tags, { desc
 vim.keymap.set('n', '<leader>sw', require('telescope.builtin').grep_string, { desc = '[S]earch current [W]ord' })
 vim.keymap.set('n', '<leader>sg', require('telescope.builtin').live_grep, { desc = '[S]earch by [G]rep' })
 vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { desc = '[S]earch [D]iagnostics' })
+
+-- Neotree
+
+-- local nvim_tree = require("nvim-tree")
+
+-- vim.keymap.set('n', '<leader><space>', nvim_tree.toggle, { desc = '(nvim-tree) File tree' })
 
 -- [[ Configure Treesitter ]]
 -- See `:help nvim-treesitter`
--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -10,6 +10,15 @@ return {
 		"MunifTanjim/nui.nvim",
 	},
 	config = function()
-		require('neo-tree').setup {}
+		require('neo-tree').setup {
+			filesystem = {
+				filtered_items = {
+					visible = false,
+					hide_dotfiles = false,
+				}
+			}
+		}
+
+		vim.keymap.set('n', '<leader><space>', ':Neotree toggle<cr>', { desc = '(nvim-tree) File tree' })
 	end,
 }
