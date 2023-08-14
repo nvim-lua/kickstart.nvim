f761a740a47d92a01fc163453192c88f55b57bce
Lorne Stalker <lorne.stalker@gmail.com>
2023-08-14 10:52:20 +0100

    chaning fugitive keybindings to make push more conscious

--- lua/lornest/plugins/fugitive.lua
+++ lua/lornest/plugins/fugitive.lua
@@ -16,12 +16,12 @@ return {
 
                 local bufnr = vim.api.nvim_get_current_buf()
                 local opts = {buffer = bufnr, remap = false}
-                vim.keymap.set("n", "<leader>p", function()
+                vim.keymap.set("n", "<leader>P", function()
                     vim.cmd.Git('push')
                 end, opts)
 
                 -- rebase always
-                vim.keymap.set("n", "<leader>P", function()
+                vim.keymap.set("n", "<leader>p", function()
                     vim.cmd.Git({'pull',  '--rebase'})
                 end, opts)
 
