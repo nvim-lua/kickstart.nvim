965b392ace8c7dcc846a4deb37cdce46b03e9a4c
Yevhenii Moruhyi <ymoruhyi@cisco.com>
2023-08-16 19:49:08 -0400

    add rose-pine colorscheme

---
+++ lua/plugins/theme.lua
@@ -0,0 +1,26 @@
+return {
+  {
+    "rose-pine/neovim",
+    name = "rose-pine",
+    opts = {
+      variant = "moon",
+      dark_variant = "moon",
+      -- highlight_groups = {
+      --   Normal = { bg = "none" },
+      --   NormalFloat = { bg = "none" },
+      --   TelescopeBorder = { fg = "highlight_high", bg = "none" },
+      --   TelescopeNormal = { bg = "none" },
+      --   TelescopePromptNormal = { bg = "base" },
+      --   TelescopeResultsNormal = { fg = "subtle", bg = "none" },
+      --   TelescopeSelection = { fg = "text", bg = "base" },
+      --   TelescopeSelectionCaret = { fg = "rose", bg = "rose" },
+      -- },
+    },
+  },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "rose-pine",
+    },
+  },
+}
