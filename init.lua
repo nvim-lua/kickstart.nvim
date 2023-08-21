1283a0bbe7424b44a38dc983f1ba0fbe33e80978
Chris Patti <feoh@feoh.org>
2023-08-21 17:19:13 -0400

    Update init.lua
    Fix typo in original.
    
    Co-authored-by: Luis G Estrades <luisgarciaestrades@gmail.com>

--- init.lua
+++ init.lua
@@ -403,7 +403,7 @@ local on_attach = function(_, bufnr)
 
   nmap('gd', vim.lsp.buf.definition, '[G]oto [D]efinition')
   nmap('gr', require('telescope.builtin').lsp_references, '[G]oto [R]eferences')
-  nmap('gI', require('telescope.builtin').lsp_implementaitons, '[G]oto [I]mplementation')
+  nmap('gI', require('telescope.builtin').lsp_implementations, '[G]oto [I]mplementation')
   nmap('<leader>D', vim.lsp.buf.type_definition, 'Type [D]efinition')
   nmap('<leader>ds', require('telescope.builtin').lsp_document_symbols, '[D]ocument [S]ymbols')
   nmap('<leader>ws', require('telescope.builtin').lsp_dynamic_workspace_symbols, '[W]orkspace [S]ymbols')
