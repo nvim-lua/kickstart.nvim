9c984cc1a0363a01976c9f1c02bcd7733a153d0c
mjhika <1zzt6ovh@mojica.anonaddy.com>
2023-08-24 18:03:00 -0400

    fix lisp

--- init.lua
+++ init.lua
@@ -282,7 +282,7 @@ vim.keymap.set('n', '<leader>sd', require('telescope.builtin').diagnostics, { de
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'vimdoc', 'vim', 'clojure', 'lisp' },
+  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'vimdoc', 'vim', 'clojure', 'commonlisp' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = true,
