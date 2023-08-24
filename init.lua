ced2253c6bf38c8fa38f2fba1f0a3faa64633758
mjhika <1zzt6ovh@mojica.anonaddy.com>
2023-08-24 17:57:44 -0400

    add vim-surround

--- init.lua
+++ init.lua
@@ -27,6 +27,9 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: First, some plugins that don't require any configuration
 
+  -- surround support
+  'tpope/vim-surround',
+
   -- Git related plugins
   'tpope/vim-fugitive',
   'tpope/vim-rhubarb',
