b2ea6eb142dc40a42af3ee4d5d5ba1957b4630bc
vladstojna <vlad.leic@gmail.com>
2023-08-30 21:10:16 +0300

    vimtex: get editor based on OS

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -1,6 +1,18 @@
+local function get_editor(sys)
+  local editor
+  if sys == "Darwin" then
+    editor = "skim"
+  elseif sys == "Linux" then
+    editor = "zathura"
+  else -- windows?
+    editor = "zathura"
+  end
+  return editor
+end
+
 return {
   "lervag/vimtex",
   config = function()
-    vim.g.vimtex_view_method = "zathura"
+    vim.g.vimtex_view_method = get_editor(vim.loop.os_uname().sysname)
   end
 }
