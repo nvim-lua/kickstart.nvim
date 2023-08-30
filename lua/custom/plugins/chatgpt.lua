dbaeb81cd1a25c88fa0cd48ce83d7da554604585
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2023-08-30 08:51:02 -0300

    feat(nvim): added chatgpt.lua

---
+++ lua/custom/plugins/chatgpt.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    "jackMort/ChatGPT.nvim",
+    config = function()
+      require("chatgpt").setup()
+    end,
+    dependencies = {
+      "MunifTanjim/nui.nvim",
+      "nvim-lua/plenary.nvim",
+      "nvim-telescope/telescope.nvim"
+    }
+  }
+}
+-- The line beneath this is called `modeline`. See `:help modeline`
+-- vim: ts=2 sts=2 sw=2 et
