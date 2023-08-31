b46c2aa0525b11685891aa38946b872ba09d250a
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2023-08-30 22:02:48 -0300

    feat(gpt): added chatgpt config for custom prompt

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -2,7 +2,13 @@ return {
   {
     "jackMort/ChatGPT.nvim",
     config = function()
-      require("chatgpt").setup()
+      require("chatgpt").setup(
+        {
+          api_key_cmd = "pass show azure/hypera/oai/token",
+          predefined_chat_gpt_prompts =
+          "https://raw.githubusercontent.com/julianobarbosa/custom-gpt-prompts/main/prompt.csv"
+        }
+      )
     end,
     dependencies = {
       "MunifTanjim/nui.nvim",
