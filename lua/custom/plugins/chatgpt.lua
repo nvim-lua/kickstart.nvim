c01a93caee63d13a901827308d247734bb746465
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2023-08-31 10:46:15 -0300

    fix: chatgpt.lua

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -6,12 +6,9 @@ return {
       require("chatgpt").setup(
         {
           api_key_cmd = "pass show azure/hypera/oai/token",
-<<<<<<< HEAD
-=======
           api_host_cmd = "pass show azure/hypera/oai/url",
           predefined_chat_gpt_prompts =
           "https://raw.githubusercontent.com/julianobarbosa/custom-gpt-prompts/main/prompt.csv"
->>>>>>> 6e7efe1fd708281c9c1e215c554461ac76236ee5
         }
       )
     end,
