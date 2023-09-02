772a2e6999e840bc9c38fa14ca39ad14e305d070
Vinit Neogi <20491952+vneogi199@users.noreply.github.com>
2023-09-02 20:51:32 +0530

    add nvim devicons for lualine

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -18,9 +18,12 @@ return {
     -- Set lualine as statusline
     'nvim-lualine/lualine.nvim',
     -- See `:help lualine.txt`
+    dependencies = {
+      'nvim-tree/nvim-web-devicons'
+    },
     opts = {
       options = {
-        icons_enabled = false,
+        icons_enabled = true,
         theme = 'auto',
         component_separators = '|',
         section_separators = '',
