ecd94a4686d47c989159e38b596f9a4042614d79
patrickmao <patrickmao93@gmail.com>
2023-09-06 15:01:13 +0800

    remove unused code

--- lua/custom/plugins/filetree.lua
+++ lua/custom/plugins/filetree.lua
@@ -1,17 +1,3 @@
--- vim.cmd([[ let g:neo_tree_remove_legacy_commands = 1 ]])
--- return {
---   "nvim-neo-tree/neo-tree.nvim",
---   version = "*",
---   dependencies = {
---     "nvim-lua/plenary.nvim",
---     "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
---     "MunifTanjim/nui.nvim",
---   },
---   config = function ()
---     require('neo-tree').setup {}
---   end,
--- }
-
 vim.g.loaded_netrw = 1
 vim.g.loaded_netrwPlugin = 1
 
