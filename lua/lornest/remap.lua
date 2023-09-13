d61fc1f55cd3f312e586dea7db67f0a46330a0a8
Lorne Stalker <lorne.stalker@gmail.com>
2023-09-13 18:26:08 +0100

    adding some nice remaps

--- lua/lornest/remap.lua
+++ lua/lornest/remap.lua
@@ -6,6 +6,11 @@ vim.g.maplocalleader = ' '
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
 vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+vim.keymap.set("x", "<leader>p", [["_dP]])
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
 
 -- Remap for dealing with word wrap
 vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
