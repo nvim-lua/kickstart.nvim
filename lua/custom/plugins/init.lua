bf0db95c1943d1d9b064e124832770afca040181
Adang23 <122211611+Adang23@users.noreply.github.com>
2023-09-14 09:11:06 -0400

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,5 +1,17 @@
 -- You can add your own plugins here or in other files in this directory!
 --  I promise not to create any merge conflicts in this directory :)
---
+
+vim.cmd('set autochdir')
+-- custom/plugins/keymappings.lua
+
+-- Function to set up your custom keymappings
+local function setup_keymappings()
+  -- Map <Leader>t to open a terminal in a vertical split on the right
+  vim.api.nvim_set_keymap('n', '<Leader>t', ':vsplit | wincmd l | terminal<CR>', { noremap = true, silent = true })
+end
+
+-- Call the function to set up your keymappings
+setup_keymappings()
+
 -- See the kickstart.nvim README for more information
 return {}
