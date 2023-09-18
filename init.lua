4f160cfe775abd2e44e45bce17cced56f66b4ef0
Jeremie Fraeys <13443851+jfraeys@users.noreply.github.com>
2023-09-18 00:36:16 -0400

    fix git hunk conficts

--- init.lua
+++ init.lua
@@ -85,9 +85,6 @@ require('lazy').setup({
         changedelete = { text = '~' },
       },
       on_attach = function(bufnr)
-        vim.keymap.set('n', '<leader>gp', require('gitsigns').prev_hunk,
-          { buffer = bufnr, desc = '[G]o to [P]revious Hunk' })
-        vim.keymap.set('n', '<leader>gn', require('gitsigns').next_hunk, { buffer = bufnr, desc = '[G]o to [N]ext Hunk' })
         vim.keymap.set('n', '<leader>hp', require('gitsigns').preview_hunk, { buffer = bufnr, desc = 'Preview git hunk' })
 
         -- don't override the built-in and fugitive keymaps
