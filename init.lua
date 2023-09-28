006aabe333555a68d01df81eafcaff12d318f70b
Emmanuel Chucks <31349069+emmanuelchucks@users.noreply.github.com>
2023-09-28 05:57:53 +0000

    fix(init.lua): indent blankline v3 setup

--- init.lua
+++ init.lua
@@ -170,10 +170,12 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
-    opts = {
-      char = '┊',
-      show_trailing_blankline_indent = false,
-    },
+    config = function()
+      require('ibl').setup {
+        char = '┊',
+        show_trailing_blankline_indent = false,
+      }
+    end,
   },
 
   -- "gc" to comment visual regions/lines
