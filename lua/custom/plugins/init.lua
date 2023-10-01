75e4932e68e6ccd56e11ca1356dc49bbc021c03c
Toth Alex <tothalex@Toths-MacBook-Pro.local>
2023-10-02 00:45:58 +0200

    fix dropdown

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -38,5 +38,32 @@ return {
 
     -- save
     keymap.set('n', 'sw', ':w<CR>', { silent = true })
+
+    require 'cmp'.setup {
+      window = {
+        completion = {
+          border = "rounded",
+          winhighlight = "Normal:CmpNormal,CursorLine:MyPmenuSel",
+        }
+      }
+    }
+
+    vim.api.nvim_set_hl(0, 'MyPmenuSel', { bg = 'NONE', fg = '#d55fde' })
+    -- gray
+    vim.api.nvim_set_hl(0, 'CmpItemAbbrDeprecated', { bg = 'NONE', strikethrough = true, fg = '#808080' })
+    -- blue
+    vim.api.nvim_set_hl(0, 'CmpItemAbbrMatch', { bg = 'NONE', fg = '#569CD6' })
+    vim.api.nvim_set_hl(0, 'CmpItemAbbrMatchFuzzy', { link = 'CmpIntemAbbrMatch' })
+    -- light blue
+    vim.api.nvim_set_hl(0, 'CmpItemKindVariable', { bg = 'NONE', fg = '#9CDCFE' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindInterface', { link = 'CmpItemKindVariable' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindText', { link = 'CmpItemKindVariable' })
+    -- pink
+    vim.api.nvim_set_hl(0, 'CmpItemKindFunction', { bg = 'NONE', fg = '#C586C0' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindMethod', { link = 'CmpItemKindFunction' })
+    -- front
+    vim.api.nvim_set_hl(0, 'CmpItemKindKeyword', { bg = 'NONE', fg = '#D4D4D4' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindProperty', { link = 'CmpItemKindKeyword' })
+    vim.api.nvim_set_hl(0, 'CmpItemKindUnit', { link = 'CmpItemKindKeyword' })
   end
 }
