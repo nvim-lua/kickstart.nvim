976fe3dd7acdd2d50e0a67bce129122844ee2b21
Your Name <you@example.com>
2023-10-01 16:25:05 +0200

    Update

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,1 +1,8 @@
-return {'akinsho/toggleterm.nvim', version = "*", config = true}
+return {
+    'akinsho/toggleterm.nvim',
+    version = "*",
+    config = true,
+    opts = {buffer = 0},
+    vim.keymap.set('t', '<esc>', [[<C-\><C-n>]]),
+    vim.keymap.set('n', '<space>v', '<Cmd>ToggleTerm<CR>'),
+}
