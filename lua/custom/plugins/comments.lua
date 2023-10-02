b5decabae0ec539fe197d32a2202acf802fdd5c1
Your Name <you@example.com>
2023-10-02 19:20:02 +0200

    Cmd line and comments

---
+++ lua/custom/plugins/comments.lua
@@ -0,0 +1,9 @@
+return
+-- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+{
+    'numToStr/Comment.nvim',
+    opts = {
+        -- add any options here
+    },
+    lazy = false,
+}
--- lua/custom/plugins/overseer.lua
+++ lua/custom/plugins/overseer.lua
@@ -3,4 +3,6 @@ return {
   opts = {
     templates = { "builtin", "user.run_script" },
   },
+  vim.keymap.set('n', '<space>or', '<Cmd>OverseerRun<CR>'),
+  vim.keymap.set('n', '<C-O>', '<Cmd>OverseerToggle<CR>'),
 }
