edd450ce2f173999e90a0f3d73be5072ab0f69db
Gregory Power <gregorywpower@gmail.com>
2023-10-04 01:55:10 -0400

    lock indent_blankline to version 2.20.8

--- init.lua
+++ init.lua
@@ -170,6 +170,7 @@ require('lazy').setup({
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
+    version = '2.20.8',
     opts = {
       char = '┊',
       show_trailing_blankline_indent = false,
