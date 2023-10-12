65fcf31c63dfda5acd7d5f143dd9dd44837e42b9
Arthur Bailão <arthur.bailao@dito.com.br>
2023-10-12 11:49:54 -0300

    disabling ibl scope by default

--- init.lua
+++ init.lua
@@ -179,7 +179,9 @@ require('lazy').setup({
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help indent_blankline.txt`
     main = 'ibl',
-    opts = {},
+    opts = {
+      scope = { enabled = false },
+    },
   },
 
   -- "gc" to comment visual regions/lines
