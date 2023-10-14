ed2410d4e86f9dfff736948d75dd5ad9381fb068
Lorne Stalker <lorne.stalker@gmail.com>
2023-10-14 17:04:16 +0100

    updating dependencies and config

--- lua/lornest/plugins/indent-blankline.lua
+++ lua/lornest/plugins/indent-blankline.lua
@@ -1,9 +1,11 @@
 return {
-  -- Add indentation guides even on blank lines
   'lukas-reineke/indent-blankline.nvim',
-  -- See `:help indent_blankline.txt`
+  main = 'ibl',
   opts = {
-    char = '┊',
-    show_trailing_blankline_indent = false,
+    indent = { char = '┊' },
+    scope = {
+      enabled = true,
+      highlight = { "Function", "Label" },
+    },
   },
 }
