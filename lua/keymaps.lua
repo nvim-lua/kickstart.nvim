b9b81b8aa769ca2ef8afd7f05f3ca15cbbbe04cf
Markus Ullmann <markus.ullmann@umsemi.com>
2023-10-18 08:51:23 +0200

    Add keymap for ESC in terminal mode
    Use ESC to retrun to normal mode in terminal

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -46,6 +46,5 @@ map('n', '<Space>bd', '<Cmd>BufferOrderByDirectory<CR>', opts)
 map('n', '<Space>bl', '<Cmd>BufferOrderByLanguage<CR>', opts)
 map('n', '<Space>bw', '<Cmd>BufferOrderByWindowNumber<CR>', opts)
 
--- Other:
--- :BarbarEnable - enables barbar (enabled by default)
--- :BarbarDisable - very bad command, should never be used
+-- map <Esc> to exit terminal-mode
+map('t', '<Esc>', '<C-\\><C-n>', opts)
