ec6470a68b8c030e739a75d5b83e305ddb140058
Micah Effiong <micaiah.effiong@gmail.com>
2023-10-20 12:35:15 +0100

    added markdown support

---
+++ lua/custom/plugins/markdown-preview.lua
@@ -0,0 +1,13 @@
+return {
+	"iamcco/markdown-preview.nvim",
+	cmd = { "MarkdownPreviewToggle", "MarkdownPreview", "MarkdownPreviewStop" },
+	build = "cd app && npm install",
+	init = function()
+		vim.g.mkdp_filetypes = { "markdown" }
+	end,
+	config = function()
+		vim.fn["mkdp#util#install"]()
+	end,
+	ft = { "markdown" },
+
+}
