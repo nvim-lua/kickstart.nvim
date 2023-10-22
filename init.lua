4e2623e557ed36e515f227ae278168cb2920a144
Damjan 9000 <damjan.9000@gmail.com>
2023-10-22 12:13:07 +0200

    Added lua/options.lua

--- init.lua
+++ init.lua
@@ -168,6 +168,8 @@ vim.o.scrolloff = 10
 -- instead raise a dialog asking if you wish to save the current file(s)
 -- See `:help 'confirm'`
 vim.o.confirm = true
+-- Set options
+require('options')
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
