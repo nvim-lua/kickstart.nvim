511dae83403888c9a8b48a7478829d4ad516d12b
Damjan 9000 <damjan.9000@gmail.com>
2023-10-22 11:44:31 +0200

    Added lua/lazy-bootstrap.lua

--- init.lua
+++ init.lua
@@ -37,27 +37,15 @@ I hope you enjoy your Neovim journey,
 
 P.S. You can delete this when you're done too. It's your config now :)
 --]]
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Install package manager
---    https://github.com/folke/lazy.nvim
---    `:help lazy.nvim.txt` for more info
-local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
-if not vim.loop.fs_stat(lazypath) then
-  vim.fn.system {
-    'git',
-    'clone',
-    '--filter=blob:none',
-    'https://github.com/folke/lazy.nvim.git',
-    '--branch=stable', -- latest stable release
-    lazypath,
-  }
-end
-vim.opt.rtp:prepend(lazypath)
+-- Install lazy plugin manager
+require('lazy-bootstrap')
 
 -- NOTE: Here is where you install your plugins.
 --  You can configure plugins using the `config` key.
