bbf96f56b8cf72a14a2d18eefe8ce0beaa480449
Gustavo Silva <silvagustavo@yandex.ru>
2023-10-25 21:56:32 +0200

    add dadbod

--- lua/custom/plugins/null_ls.lua
+++ lua/custom/plugins/null_ls.lua
@@ -7,8 +7,8 @@ return {
 
 		null_ls.setup {
 			sources = {
-				null_ls.builtins.formatting.goimports_reviser,
-				-- null_ls.builtins.formatting.gofumpt,
+				-- null_ls.builtins.formatting.goimports_reviser,
+				null_ls.builtins.formatting.gofumpt,
 				-- null_ls.builtins.formatting.golines,
 			},
 			on_attach = function(client, bufnr)
