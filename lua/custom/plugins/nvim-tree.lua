c030ba8f5d552830acce1a4d82c8d12ce6709bf0
ZachHunn <zach.hunn@trongov.us>
2023-10-25 21:52:21 -1000

    added nvim-tree, autosave, and autopair plugins

---
+++ lua/custom/plugins/nvim-tree.lua
@@ -0,0 +1,13 @@
+return {
+  "nvim-tree/nvim-tree.lua",
+  version = "*",
+  lazy = false,
+  dependencies = {
+    "nvim-tree/nvim-web-devicons",
+  },
+
+  config = function()
+    require("nvim-tree").setup {
+    }
+  end,
+}
