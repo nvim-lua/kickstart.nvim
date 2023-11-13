8cca88e4e2606e9942fe7c78602e9708d2c67411
Thomas Alcala Schneider <thomas@lockrmail.com>
2023-11-13 15:06:54 +0100

    feat: check OS and disable tabnine on linux aarch64

--- lua/custom/plugins/cmp-tabnine.lua
+++ lua/custom/plugins/cmp-tabnine.lua
@@ -1,5 +1,18 @@
+local uname = vim.loop.os_uname()
+local sysname = uname.sysname
+local machine = uname.machine
+
+local enableCmpTabnine = true
+
+-- Check if the system is Linux and the machine is aarch64
+if sysname == "Linux" and machine == "aarch64" then
+  -- Disable cmp-tabnine plugin
+  enableCmpTabnine = false
+end
+
 return {
   'tzachar/cmp-tabnine',
+  enabled = enableCmpTabnine,
   build = './install.sh',
   dependencies = { 'hrsh7th/nvim-cmp' },
   config = function()
