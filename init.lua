f9fdb2242207e6e77e88edf4da5ad49693d2af9e
akashmjain <akashmjain.official@gmail.com>
2023-11-13 21:52:17 +0530

    Basic configs

--- init.lua
+++ init.lua
@@ -242,6 +242,9 @@ vim.o.hlsearch = false
 -- Make line numbers default
 vim.wo.number = true
 
+-- Enable relative line numbers
+vim.wo.relativenumber = true
+
 -- Enable mouse mode
 vim.o.mouse = 'a'
 
