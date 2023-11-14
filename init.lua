c2fd11bd66fbd42b663582f7f636a1df654e4393
jannawro <jan.nawrocki@idemia.com>
2023-11-14 09:27:53 +0100

    retry lualine reconfig

--- init.lua
+++ init.lua
@@ -179,7 +179,18 @@ require('lazy').setup({
         component_separators = '|',
         section_separators = '',
       },
+      sections = {
+        lualine_a = { 'mode' },
+        lualine_b = { 'branch', 'diff', 'diagnostics' },
+        lualine_c = { 'filename' },
+        lualine_x = { 'filetype' },
+        lualine_y = {},
+        lualine_z = {}
+      },
       inactive_sections = {
+        lualine_a = {},
+        lualine_b = {},
+        lualine_c = {},
         lualine_x = { 'encoding', 'fileformat' },
         lualine_y = { 'progress' },
         lualine_z = { 'location' },
