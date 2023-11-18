65e81d3c0b96dead3f75b38aafa24b99fec0dd25
frumsy <frumsy@users.noreply.github.com>
2023-11-18 13:30:11 -0500

    xget copilot working in neovim

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,11 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	"github/copilot.vim",
+	cmd = "Copilot",
+	event = "InsertEnter",
+	config = function()
+		require("copilot").setup({})
+	end,
+}
