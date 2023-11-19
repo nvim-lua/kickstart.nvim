5411e0fccba50a8ad0a48fd6c7751818155ab56a
Micah Effiong <micaiah.effiong@gmail.com>
2023-11-19 13:09:18 +0100

    code formatting

--- lua/custom/plugins/onedark-theme.lua
+++ lua/custom/plugins/onedark-theme.lua
@@ -6,8 +6,8 @@ return {
 		vim.cmd.colorscheme 'onedark'
 	end,
 	require('onedark').setup {
-		style = 'deep',
+		style = 'darker',
 		toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-		toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'},
+		toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
 	}
 }
