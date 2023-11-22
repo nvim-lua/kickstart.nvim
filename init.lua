83a369891b2ea54bc9afbbec60f8781a7261fb4e
Tommy Williams <wommy@users.noreply.github.com>
2023-11-22 17:18:42 -0500

    missed a nexted object

--- init.lua
+++ init.lua
@@ -523,7 +523,7 @@ local servers = {
       workspace = { checkThirdParty = false },
       telemetry = { enable = false },
       -- NOTE: toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-      -- diagnostics = { disable = 'missing-fields' },
+      -- diagnostics = { disable = { 'missing-fields' } },
     },
   },
 }
