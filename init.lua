21c4cd26a0f4a7ad534fee036e3dec9b9ac648cf
fine2006 <animeshpathak.2006@gmail.com>
2023-11-27 20:59:15 +0530

    remove firenvim

--- init.lua
+++ init.lua
@@ -95,17 +95,6 @@ require('lazy').setup({
       'folke/neodev.nvim',
     },
   },
-  {
-    'glacambre/firenvim',
-
-    -- Lazy load firenvim
-    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-    lazy = not vim.g.started_by_firenvim,
-    build = function()
-        vim.fn["firenvim#install"](0)
-    end
-  },
-
 
   {
     -- Autocompletion
