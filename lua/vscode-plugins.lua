7e31e79d6ad8963620ec9ac34d20e3464c0b6018
Eric Tiedemann <etiedem01@users.noreply.github.com>
2023-11-28 10:54:52 -0500

    Add treesitter and incremental selection to vscode

--- lua/vscode-plugins.lua
+++ lua/vscode-plugins.lua
@@ -12,7 +12,29 @@ end
 vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   {
+    'nvim-treesitter',
+    dependencies = {
+      'nvim-treesitter-textobjects',
+    },
+    build = ':TSUpdate',
+  },
+  {
     'kylechui/nvim-surround',
     opts = { config = {} }
+  }
+})
+
+require('nvim-treesitter.configs').setup({
+  ensure_installed = { 'go', 'python', 'rust', 'javascript', 'typescript' },
+  auto_install = true,
+  highlight = { enable = true },
+  incremental_selection = {
+    enable = true,
+    keymaps = {
+      init_selection = '<CR>',
+      node_incremental = '<CR>',
+      scope_incremental = '<TAB>',
+      node_decremental = '<BS>',
+    }
   }
 })
