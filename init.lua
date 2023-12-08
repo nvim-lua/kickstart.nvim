76c5b1ec57f40d17ac787feb018817a802e24bb6
Shashwat Agrawal <72117025+ShashwatAgrawal20@users.noreply.github.com>
2023-12-08 18:44:06 +0530

    feat(cmp): path completion feature (#536)

--- init.lua
+++ init.lua
@@ -105,6 +105,7 @@ require('lazy').setup({
 
       -- Adds LSP completion capabilities
       'hrsh7th/cmp-nvim-lsp',
+      'hrsh7th/cmp-path',
 
       -- Adds a number of user-friendly snippets
       'rafamadriz/friendly-snippets',
@@ -652,6 +653,7 @@ cmp.setup {
   sources = {
     { name = 'nvim_lsp' },
     { name = 'luasnip' },
+    { name = 'path' },
   },
 }
 
