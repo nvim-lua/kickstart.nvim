f9775b92f04b4c355c6951d7f354dbdd683526ad
David Papp <d.papp@outlook.com>
2023-12-10 20:05:49 +0100

    fix: lualine theme to `auto`
    Fixed remnant `lualine.options.theme` from `onedark` to `auto`.

--- init.lua
+++ init.lua
@@ -212,7 +212,7 @@ require('lazy').setup({
     opts = {
       options = {
         icons_enabled = false,
-        theme = 'onedark',
+        theme = 'auto',
         component_separators = '|',
         section_separators = '',
       },
