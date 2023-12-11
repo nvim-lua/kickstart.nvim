bffc38cfb91b2683084729a823893e4306de666f
rajvatsal <rajvatsal2004@gmail.com>
2023-12-11 15:36:34 +0530

    Revert "Add new font and some colorschemes"
    This reverts commit 14e5f340d44aee4cc6163cf9316ec4b55f8050ca.

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,9 +3,3 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
-
--- Colorscheme
-vim.cmd.colorscheme 'gruvbox'
-
--- Nerd fonts
---vim.opt.guifont = ''
