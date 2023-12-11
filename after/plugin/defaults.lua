14e5f340d44aee4cc6163cf9316ec4b55f8050ca
rajvatsal <rajvatsal2004@gmail.com>
2023-12-11 15:35:35 +0530

    Add new font and some colorschemes

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -3,3 +3,9 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
+
+-- Colorscheme
+vim.cmd.colorscheme 'gruvbox'
+
+-- Nerd fonts
+--vim.opt.guifont = ''
