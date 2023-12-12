8baa1144ab2df7aefbdd2ed400644fb409e33f78
lobneroO <Tare.noN@web.de>
2023-12-12 15:47:45 +0100

    added code folding via ufo plugin. enabled usage of *.lua files through the init.lua file.

---
+++ lua/custom/plugins/ufo.lua
@@ -0,0 +1,27 @@
+-- install plugin for code folding
+
+return {
+    "kevinhwang91/nvim-ufo",
+    event = "BufRead",
+    dependencies = { "kevinhwang91/promise-async" },
+	config = function()
+		-- setup folding source: first lsp, then indent as fallback
+		require("ufo").setup({
+			provider_selector = function(bufnr, filetype, buftype)
+				return { 'lsp', 'indent' }
+			end
+		})
+		
+		-- default settings to enable
+		vim.o.foldcolumn = '1'
+		vim.o.foldlevel = 99
+		vim.o.foldlevelstart = 99
+		vim.o.foldenable = true
+
+		-- remap keys for "fold all" and "unfold all"
+		vim.keymap.set('n', 'zR', require('ufo').openAllFolds, { desc = "Open all folds" })
+		vim.keymap.set('n', 'zM', require('ufo').closeAllFolds, { desc = "Close all folds" })
+
+	end
+}
+
