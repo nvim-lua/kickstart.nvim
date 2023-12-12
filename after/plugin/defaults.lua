35d7773232470f8d6ce034d1465c056ab682bd99
rajvatsal <rajvatsal2004@gmail.com>
2023-12-12 08:46:51 +0530

    Set theme to gruvbox

--- after/plugin/defaults.lua
+++ after/plugin/defaults.lua
@@ -2,6 +2,7 @@ vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.bo.softtabstop = 2
+vim.opt.relativenumber = true
 
--- Set custom colorscheme [[ NOTE: Never set it to onedark cuz it breaks the editor ]]
-vim.cmd.colorscheme 'tokyonight-night'
+-- Set custom colorscheme [[ NOTE: Never set it to "onedark" cuz it breaks the editor ]]
+vim.cmd.colorscheme 'gruvbox'
