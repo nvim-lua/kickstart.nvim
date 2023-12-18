866e97064b53c0ff581c3fccef35e6c341ce8323
joe <ratracejoe@hotmail.com>
2023-12-18 22:36:32 +0000

    fixing the treesitter issue

--- init.lua
+++ init.lua
@@ -672,4 +672,6 @@ vim.wo.relativenumber = true
 require("oil").setup()
 vim.keymap.set("n", "-", "<CMD>Oil<CR>", { desc = "Open parent directory" })
 
+-- Setup treesitter
+require("nvim-treesitter.install").prefer_git = true
 -- Setup Java Debugging
