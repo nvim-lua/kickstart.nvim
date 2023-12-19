9eb34622d761e687c06b756aa2bf3180699da816
Nick Burt <nbur4556@gmail.com>
2023-12-18 19:20:55 -0600

    include rust autoformatting on save

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -1,6 +1,16 @@
 return {
 	-- File Browser
 	{ 'stevearc/oil.nvim' },
+
+	-- Rust Format on Save
+	{
+		'rust-lang/rust.vim',
+		ft = "rust",
+		init = function ()
+			vim.g.rustfmt_autosave = 1
+		end
+	},
+
 	-- Theme
 	{ 'catppuccin/nvim', name='catppuccin', priority = 1000 },
 }
