e310632994e62bc9091bf35c400940010e9b4067
Steven Moon <smoon@presagegroup.com>
2023-12-20 11:46:34 -0500

    added nvim-tree

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,14 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	"nvim-tree/nvim-tree.lua",
+	version = "*",
+	lazy = true,
+	dependencies = {
+		"nvim-tree/nvim-web-devicons",
+	},
+	config = function()
+		require("nvim-tree").setup {}
+	end,
+}
