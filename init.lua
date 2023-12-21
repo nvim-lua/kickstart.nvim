629994d2b4ac95c10a2873eb7a1d811ce562dd0f
Hörmet Yiltiz <yiltiz@apple.com>
2023-12-21 16:39:03 -0500

    update style

--- init.lua
+++ init.lua
@@ -113,7 +113,7 @@ require('lazy').setup({
   },
 
   -- Useful plugin to show you pending keybinds.
-  { 'folke/which-key.nvim',  opts = {} },
+  { 'folke/which-key.nvim', opts = {} },
   {
     -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
@@ -423,9 +423,11 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python',
-      'rust', 'tsx', 'javascript', 'typescript',
-      'vimdoc', 'vim', 'bash' },
+    ensure_installed = { 
+      'c', 'cpp', 'go', 'lua', 'python', 'rust', 
+      'tsx', 'javascript', 'typescript', 
+      'vimdoc', 'vim', 'bash'
+    },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
