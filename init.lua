872d4b1a87047c656d944a052b9882600d9e5e11
Ari Pollak <aripollak@users.noreply.github.com>
2023-12-21 15:35:28 -0500

    Switch cmp up mapping from C-d to C-b to match regular vim up key (#549)

--- init.lua
+++ init.lua
@@ -624,7 +624,7 @@ cmp.setup {
   mapping = cmp.mapping.preset.insert {
     ['<C-n>'] = cmp.mapping.select_next_item(),
     ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
+    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
     ['<C-f>'] = cmp.mapping.scroll_docs(4),
     ['<C-Space>'] = cmp.mapping.complete {},
     ['<CR>'] = cmp.mapping.confirm {
