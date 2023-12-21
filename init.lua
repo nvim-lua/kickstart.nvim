e5081f274beb31fd58f0e497a55469ac433e6b9b
Hörmet Yiltiz <yiltiz@apple.com>
2023-12-21 16:40:04 -0500

    align on their own lines

--- init.lua
+++ init.lua
@@ -423,10 +423,19 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 vim.defer_fn(function()
   require('nvim-treesitter.configs').setup {
     -- Add languages to be installed here that you want installed for treesitter
-    ensure_installed = { 
-      'c', 'cpp', 'go', 'lua', 'python', 'rust', 
-      'tsx', 'javascript', 'typescript', 
-      'vimdoc', 'vim', 'bash'
+    ensure_installed = {
+      'c',
+      'cpp',
+      'go',
+      'lua',
+      'python',
+      'rust',
+      'tsx',
+      'javascript',
+      'typescript',
+      'vimdoc',
+      'vim',
+      'bash',
     },
 
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
