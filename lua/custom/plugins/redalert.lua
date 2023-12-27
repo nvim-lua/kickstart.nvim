fe489cc9bb933d91b3437cf50b2264c48140517b
Thomas Alcala Schneider <thomas@lockrmail.com>
2023-12-27 19:57:57 +0100

    feat: add redalert

---
+++ lua/custom/plugins/redalert.lua
@@ -0,0 +1,9 @@
+return { 
+  "waltr-fr/redalert.nvim",
+  dependencies = { 'nvim-lua/plenary.nvim' },
+  config = function()
+      require("redalert").setup({
+      cutoff_days = 5, 
+    })
+  end,
+}
