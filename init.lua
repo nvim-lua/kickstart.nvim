b7035c2140c1ce0342291b1725114ae91b8183f4
Prajakt Shastry <prajakt.shastry@gmail.com>
2023-12-28 22:25:20 -0500

    Moved lsp helm ls config

--- init.lua
+++ init.lua
@@ -269,26 +269,5 @@ require('litee.gh').setup({
   }
 })
 
-local configs = require('lspconfig.configs')
-local lspconfig = require('lspconfig')
-local util = require('lspconfig.util')
-
-if not configs.helm_ls then
-  configs.helm_ls = {
-    default_config = {
-      cmd = { "helm_ls", "serve" },
-      filetypes = { 'helm' },
-      root_dir = function(fname)
-        return util.root_pattern('Chart.yaml')(fname)
-      end,
-    },
-  }
-end
-
-lspconfig.helm_ls.setup {
-  filetypes = { "helm" },
-  cmd = { "helm_ls", "serve" },
-}
-
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/lsp.lua
+++ lua/custom/lsp.lua
@@ -88,3 +88,23 @@ vim.api.nvim_create_autocmd({ "BufWritePre" }, {
   pattern = { "*.tf", "*.tfvars" },
 })
 
+local configs = require('lspconfig.configs')
+local lspconfig = require('lspconfig')
+local util = require('lspconfig.util')
+
+if not configs.helm_ls then
+  configs.helm_ls = {
+    default_config = {
+      cmd = { "helm_ls", "serve" },
+      filetypes = { 'helm' },
+      root_dir = function(fname)
+        return util.root_pattern('Chart.yaml')(fname)
+      end,
+    },
+  }
+end
+
+lspconfig.helm_ls.setup {
+  filetypes = { "helm" },
+  cmd = { "helm_ls", "serve" },
+}
