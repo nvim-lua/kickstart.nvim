e10dc4e9a9d783167fe93b2f4722a7fe3ed8384c
rajvatsal <rajvatsal2004@gmail.com>
2024-01-06 11:04:56 +0530

    feat(keymaps): Add :bprev and :bnext
    On <leader>p open previous buffer
    On <leader>n open next buffer

--- after/plugin/settings.lua
+++ after/plugin/settings.lua
@@ -4,4 +4,6 @@ vim.opt.expandtab = true
 vim.bo.softtabstop = 2
 vim.opt.relativenumber = true
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true }) -- Set keymap to exit terminal using 'exit'
-vim.cmd.colorscheme 'rose-pine-main'                             -- Set custom colorscheme [[ NOTE: Never set it to "onedark" cuz it breaks the editor ]]
+vim.keymap.set('n', '<leader>p', ':bprev<CR>', { noremap = true, silent = true, desc = 'Open previous buffer' })
+vim.keymap.set('n', '<leader>n', ':bnext<CR>', { noremap = true, silent = true, desc = 'Open next buffer' })
+vim.cmd.colorscheme 'rose-pine-main' -- Set custom colorscheme [[ NOTE: Never set it to "onedark" cuz it breaks the editor ]]
