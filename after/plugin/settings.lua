63b2024feb62782c7db74ad390c33a85138e1289
rajvatsal <rajvatsal2004@gmail.com>
2024-01-06 12:38:57 +0530

    feat(keymaps): Add :cd %:p
    If press <leader>p then move to current buffer location

--- after/plugin/settings.lua
+++ after/plugin/settings.lua
@@ -6,4 +6,5 @@ vim.opt.relativenumber = true
 vim.keymap.set('t', '<Esc>', [[<C-\><C-n>]], { noremap = true }) -- Set keymap to exit terminal using 'exit'
 vim.keymap.set('n', '<leader>p', ':bprev<CR>', { noremap = true, silent = true, desc = 'Open previous buffer' })
 vim.keymap.set('n', '<leader>n', ':bnext<CR>', { noremap = true, silent = true, desc = 'Open next buffer' })
+vim.keymap.set('n', '<leader>m', ':cd %:p:h<CR>', { noremap = true, silent = true, desc = 'Move to current buffer' })
 vim.cmd.colorscheme 'rose-pine-main' -- Set custom colorscheme [[ NOTE: Never set it to "onedark" cuz it breaks the editor ]]
