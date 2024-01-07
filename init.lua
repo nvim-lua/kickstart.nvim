97f5a8b1e418ad3a4d135e3705b46dba03283fc3
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-01-07 11:08:10 -0500

    change local leader

--- init.lua
+++ init.lua
@@ -2,7 +2,7 @@
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are required (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ','
+vim.g.maplocalleader = ';'
 
 -- Install package manager
 --    https://github.com/folke/lazy.nvim
