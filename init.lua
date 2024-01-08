3b7c1e883ed960d98b945666611e052def0ce123
Mohamad Alamin Yassin <ameen.yaseen.98@gmail.com>
2024-01-08 14:38:11 +0300

    Add Missing Fields to Treesitter Config to Resolve Warnings  Description: This commit introduces three additional fields - `sync_install`, `ignore_install`, and `modules` - to the Treesitter configuration. This update is aimed at resolving warnings that were previously displayed, potentially causing confusion or frustration for new users of Neovim. By explicitly defining these fields, the configuration aligns better with the latest `nvim-treesitter` requirements, ensuring a smoother and more intuitive setup experience.
    This commit introduces three additional fields - `sync_install`, `ignore_install`, and `modules` - to the Treesitter configuration. This update is aimed at resolving warnings that were previously displayed, potentially causing confusion or frustration for new users of Neovim. By explicitly defining these fields, the configuration aligns better with the latest `nvim-treesitter` requirements.

--- init.lua
+++ init.lua
@@ -427,9 +427,9 @@ vim.defer_fn(function()
     -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
     auto_install = false,
     -- Install languages synchronously (only applied to `ensure_installed`)
-    sync_install = false, 
+    sync_install = false,
     -- List of parsers to ignore installing
-    ignore_install = {}, 
+    ignore_install = {},
     -- You can specify additional Treesitter modules here: -- For example: -- playground = {--enable = true,-- },
     modules = {},
     highlight = { enable = true },
