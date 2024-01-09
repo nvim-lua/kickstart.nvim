257425247f726acc8f320f089587270544f0db02
mjhika <1zzt6ovh@mojica.anonaddy.com>
2024-01-09 09:17:16 -0500

    add clangd

--- init.lua
+++ init.lua
@@ -294,7 +294,7 @@ end
 --  If you want to override the default filetypes that your language server will attach to you can
 --  define the property 'filetypes' to the map in question.
 local servers = {
-  -- clangd = {},
+  clangd = {},
   gopls = {},
   -- pyright = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
