684fcff874d2938f9254e86f75bc219b0a0e6bb6
Nick Burt <nbur4556@gmail.com>
2024-01-08 19:42:29 -0600

    handle autocomplete setup from autocomplete module

--- init.lua
+++ init.lua
@@ -17,38 +17,4 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup('plugins');
 require('lua/settings');
 
--- [[ Configure nvim-cmp ]]
-local cmp = require 'cmp'
-local luasnip = require 'luasnip'
-require('luasnip.loaders.from_vscode').lazy_load()
-luasnip.config.setup {}
-
-cmp.setup {
-  snippet = {
-    expand = function(args)
-      luasnip.lsp_expand(args.body)
-    end,
-  },
-  completion = {
-    completeopt = 'menu,menuone,noinsert',
-  },
-  mapping = cmp.mapping.preset.insert {
-    ['<C-n>'] = cmp.mapping.select_next_item(),
-    ['<C-p>'] = cmp.mapping.select_prev_item(),
-    ['<C-d>'] = cmp.mapping.scroll_docs(-4),
-    ['<C-f>'] = cmp.mapping.scroll_docs(4),
-    ['<C-Space>'] = cmp.mapping.complete {},
-    ['<Tab>'] = cmp.mapping.confirm {
-      behavior = cmp.ConfirmBehavior.Replace,
-      select = true,
-    },
-  },
-  sources = {
-    { name = 'nvim_lsp' },
-    { name = 'luasnip' },
-    { name = 'path' },
-  },
-}
-
-
 -- vim: ts=2 sts=2 sw=2 et
--- lua/plugins/autocompletion.lua
+++ lua/plugins/autocompletion.lua
@@ -12,5 +12,38 @@ return {
 
 		-- Adds a number of user-friendly snippets
 		'rafamadriz/friendly-snippets',
-	}
+	},
+	config = function()
+		local cmp = require 'cmp'
+		local luasnip = require 'luasnip'
+		require('luasnip.loaders.from_vscode').lazy_load()
+		luasnip.config.setup {}
+
+		cmp.setup {
+		  snippet = {
+			expand = function(args)
+			  luasnip.lsp_expand(args.body)
+			end,
+		  },
+		  completion = {
+			completeopt = 'menu,menuone,noinsert',
+		  },
+		  mapping = cmp.mapping.preset.insert {
+			['<C-n>'] = cmp.mapping.select_next_item(),
+			['<C-p>'] = cmp.mapping.select_prev_item(),
+			['<C-d>'] = cmp.mapping.scroll_docs(-4),
+			['<C-f>'] = cmp.mapping.scroll_docs(4),
+			['<C-Space>'] = cmp.mapping.complete {},
+			['<Tab>'] = cmp.mapping.confirm {
+			  behavior = cmp.ConfirmBehavior.Replace,
+			  select = true,
+			},
+		  },
+		  sources = {
+			{ name = 'nvim_lsp' },
+			{ name = 'luasnip' },
+			{ name = 'path' },
+		  },
+		}
+	end
 }
