b2700b93fa027d035c672ea1587a5123ae6dc663
Micah Effiong <micaiah.effiong@gmail.com>
2024-01-10 14:16:42 +0100

    remove duplicate conflict

--- lua/custom/plugins/onedark-theme.lua
+++ lua/custom/plugins/onedark-theme.lua
@@ -1,13 +1,10 @@
 return {
-	-- Theme inspired by Atom
-	'navarasu/onedark.nvim',
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme 'onedark'
-	end,
+	-- Extend onedark config
+	-- prevent override conflict
+	--
 	require('onedark').setup {
 		style = 'deep',
 		toggle_style_key = '<leader>ts', -- keybind to toggle theme style. Leave it nil to disable it, or set it to a string, for example "<leader>ts"
-		toggle_style_list = {'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light'},
+		toggle_style_list = { 'dark', 'darker', 'cool', 'deep', 'warm', 'warmer', 'light' },
 	}
 }
