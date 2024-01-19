50a0ab51b64accda4aaab49da1cd88e986305046
gregladden <greg.ladden@g.austincc.edu>
2024-01-19 00:55:35 -0600

    adding additional plugins

--- lua/plugins/autoformat.lua
+++ lua/plugins/autoformat.lua
@@ -3,8 +3,7 @@
 -- Use your language server to automatically format your code on save.
 -- Adds additional commands as well to manage the behavior
 
-return {
-  'neovim/nvim-lspconfig',
+return { 'neovim/nvim-lspconfig',
   config = function()
     -- Switch for controlling whether you want autoformatting.
     --  Use :KickstartFormatToggle to toggle autoformatting on or off
