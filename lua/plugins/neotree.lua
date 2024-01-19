66c760bf9ab02d252f25d2eab67e820ceafa8c63
gregladden <greg.ladden@g.austincc.edu>
2024-01-19 00:34:32 -0600

    adding additional plugins

---
+++ lua/plugins/neotree.lua
@@ -0,0 +1,16 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    },
+    config = function()
+      -- NeoTree Settings
+      vim.keymap.set('n', '<C-n>', ':Neotree filesystem reveal left<cr>')
+    end
+  }
+
+
---
+++ lua/plugins/utils.lua
@@ -0,0 +1,10 @@
+return {
+  -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-sleuth',
+
+  -- Useful plugin to show you pending keybinds.
+  { 'folke/which-key.nvim',  opts = {} },
+ -- "gc" to comment visual regions/lines
+  { 'numToStr/Comment.nvim', opts = {} },
+}
+
