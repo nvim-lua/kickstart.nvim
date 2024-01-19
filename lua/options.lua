3b2c65de0d8d079c8f1a1438817ed50524bcdda9
gregladden <greg.ladden@g.austincc.edu>
2024-01-19 17:24:43 -0600

    adding additional plugins

--- lua/options.lua
+++ lua/options.lua
@@ -1,3 +1,15 @@
+local opt = vim.opt
+
+-- line numbers 
+opt.relativenumber = true
+opt.number = true
+
+-- tabs & indentation 
+opt.tabstop = 2
+opt.shiftwidth = 2
+opt.expandtab = true
+opt.autoindent = true
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
