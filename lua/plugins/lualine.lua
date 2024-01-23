59ab26def4b7f07263bcb966489bd39a47283ba0
gregladden <greg.ladden@g.austincc.edu>
2024-01-22 22:09:56 -0600

    update lualine

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -1,15 +1,14 @@
 return {
-    -- Set lualine as statusline
-    'nvim-lualine/lualine.nvim',
-    -- See `:help lualine.txt`
-    opts = {
-      options = {
-        icons_enabled = false,
-        theme = 'onedark',
-        component_separators = '|',
-        section_separators = '',
-      },
+  -- Set lualine as statusline
+  'nvim-lualine/lualine.nvim',
+  -- See `:help lualine.txt`
+  opts = {
+    options = {
+      icons_enabled = true,
+      theme = 'everforest',
+      component_separators = '|',
+      section_separators = '',
     },
-  }
-
-
+  },
+  dependencies = { 'nvim-tree/nvim-web-devicons'}
+}
