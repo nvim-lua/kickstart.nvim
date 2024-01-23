50b1303debd2131fdb81fc779ee50be7db26bb80
gregladden <greg.ladden@g.austincc.edu>
2024-01-22 18:39:08 -0600

    update to theme

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -2,17 +2,39 @@
 
 -- Keymaps for better default experience
 -- See `:help vim.keymap.set()`
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+
+local keymap = vim.keymap
+
+-- removes highlight on / 
+keymap.set("n", "<leader>nh", ":nohl<CR>")
+
+-- deletes character but does not copy into a registar 
+keymap.set("n", "x", '"_x"')
+
+keymap.set("n", "<leader>sv", "<C-w>v") -- split window vertically 
+keymap.set("n", "<leader>sh", "<C-w>s") -- split window horizontally 
+keymap.set("n", "<leader>se", "<C-w>=") -- make split windows equal width  
+keymap.set("n", "<leader>sx", ":close<CR>") -- close current split window 
+
+keymap.set("n", "<leader>to", ":tabnew<CR>") -- open new tab
+keymap.set("n", "<leader>tx", ":tabclose<CR>") -- close current tab
+keymap.set("n", "<leader>tn", ":tabn<CR>") -- go to next tab
+keymap.set("n", "<leader>tp", ":tabp<CR>") -- go to previous tab
+
+-- vim maximizer 
+keymap.set("n", "<leader>sm", ":MaximizerToggle<CR>")
+
+keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
 
 -- Remap for dealing with word wrap
-vim.keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
-vim.keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
+keymap.set('n', 'k', "v:count == 0 ? 'gk' : 'k'", { expr = true, silent = true })
+keymap.set('n', 'j', "v:count == 0 ? 'gj' : 'j'", { expr = true, silent = true })
 
 -- Diagnostic keymaps
-vim.keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
-vim.keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
-vim.keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
-vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+keymap.set('n', '[d', vim.diagnostic.goto_prev, { desc = 'Go to previous diagnostic message' })
+keymap.set('n', ']d', vim.diagnostic.goto_next, { desc = 'Go to next diagnostic message' })
+keymap.set('n', '<leader>e', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
+keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
 
 -- [[ Highlight on yank ]]
 -- See `:help vim.highlight.on_yank()`
--- lua/options.lua
+++ lua/options.lua
@@ -10,14 +10,35 @@ opt.shiftwidth = 2
 opt.expandtab = true
 opt.autoindent = true
 
+-- line wrap 
+opt.wrap = false
+
+-- search settings
+opt.ignorecase = true
+opt.smartcase = true
+
+-- cursor line
+opt.cursorline = true
+
+-- appearance
+opt.termguicolors = true
+opt.background = "dark"
+opt.signcolumn = "yes"
+
+-- backspace 
+opt.backspace = "indent,eol,start"
+
+-- split windows 
+opt.splitright = true
+opt.splitbelow = true
+
+opt.iskeyword:append("-")
+
 -- [[ Setting options ]]
 -- See `:help vim.o`
 
 -- Set highlight on search
-vim.o.hlsearch = false
-
--- Make line numbers default
-vim.wo.number = true
+vim.o.hlsearch = true
 
 -- Enable mouse mode
 vim.o.mouse = 'a'
@@ -33,19 +54,9 @@ vim.o.breakindent = true
 -- Save undo history
 vim.o.undofile = true
 
--- Case-insensitive searching UNLESS \C or capital in search
-vim.o.ignorecase = true
-vim.o.smartcase = true
-
--- Keep signcolumn on by default
-vim.wo.signcolumn = 'yes'
-
 -- Decrease update time
 vim.o.updatetime = 250
 vim.o.timeoutlen = 300
 
 -- Set completeopt to have a better completion experience
 vim.o.completeopt = 'menuone,noselect'
-
--- NOTE: You should make sure your terminal supports this
-vim.o.termguicolors = true
