dbc8995622b84ed9891ad63a1021e3254ed04c40
zwergius <webmaster@zwergius.com>
2024-01-23 14:59:01 +0100

    Fixes indentation

--- lua/custom/plugins/conform.lua
+++ lua/custom/plugins/conform.lua
@@ -1,39 +1,41 @@
 return {
-	"stevearc/conform.nvim",
-	lazy = true,
-	-- event = { "BufReadPre", "BufNewFile" }, -- to disable comment this out
-	config = function()
-		local conform = require("conform")
+  "stevearc/conform.nvim",
+  lazy = true,
+  -- event = { "BufReadPre", "BufNewFile" }, -- to disable comment this out
+  config = function()
+    local conform = require("conform")
 
-		conform.setup({
-			formatters_by_ft = {
-				css = { { --[[ "prettierd", ]] "prettier" } },
-				graphql = { { --[[ "prettierd", ]] "prettier" } },
-				html = { { --[[ "prettierd", ]] "prettier" } },
-				javascript = { { --[[ "prettierd", ]] "prettier" } },
-				javascriptreact = { { --[[ "prettierd", ]] "prettier" } },
-				json = { { --[[ "prettierd", ]] "prettier" } },
-				less = { { --[[ "prettierd", ]] "prettier" } },
-				markdown = { { --[[ "prettierd", ]] "prettier" } },
-				scss = { { --[[ "prettierd", ]] "prettier" } },
-				svelte = { { --[[ "prettierd", ]] "prettier" } },
-				typescript = { { --[[ "prettierd", ]] "prettier" } },
-				typescriptreact = { { --[[ "prettierd", ]] "prettier" } },
-				yaml = { { --[[ "prettierd", ]] "prettier" } },
-			},
-			format_on_save = {
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 500
-			}
-		})
+    conform.setup({
 
-		vim.keymap.set({ "n", "v" }, "<leader>f", function()
-			conform.format({
-				lsp_fallback = true,
-				async = false,
-				timeout_ms = 1000,
-			})
-		end, { desc = "Format file or range (in visual mode)" })
-	end
+      formatters_by_ft = {
+        css = { { --[[ "prettierd", ]] "prettier" } },
+        graphql = { { --[[ "prettierd", ]] "prettier" } },
+        html = { { --[[ "prettierd", ]] "prettier" } },
+        javascript = { { --[[ "prettierd", ]] "prettier" } },
+        javascriptreact = { { --[[ "prettierd", ]] "prettier" } },
+        json = { { --[[ "prettierd", ]] "prettier" } },
+        less = { { --[[ "prettierd", ]] "prettier" } },
+        markdown = { { --[[ "prettierd", ]] "prettier" } },
+        scss = { { --[[ "prettierd", ]] "prettier" } },
+        svelte = { { --[[ "prettierd", ]] "prettier" } },
+        typescript = { { --[[ "prettierd", ]] "prettier" } },
+        typescriptreact = { { --[[ "prettierd", ]] "prettier" } },
+        yaml = { { --[[ "prettierd", ]] "prettier" } },
+      },
+
+      format_on_save = {
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 500
+      }
+    })
+
+    vim.keymap.set({ "n", "v" }, "<leader>f", function()
+      conform.format({
+        lsp_fallback = true,
+        async = false,
+        timeout_ms = 1000,
+      })
+    end, { desc = "Format file or range (in visual mode)" })
+  end
 }
