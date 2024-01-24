82a8ea9330c2704c6b32439b6895575f913fd359
zwergius <webmaster@zwergius.com>
2024-01-24 16:03:22 +0100

    Adds a couple of settings and remaps

--- lua/custom/zwergius/remap.lua
+++ lua/custom/zwergius/remap.lua
@@ -7,3 +7,14 @@ vim.keymap.set('n', '<C-h>', '<C-w>h', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-j>', '<C-w>j', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-k>', '<C-w>k', { noremap = true, silent = true })
 vim.keymap.set('n', '<C-l>', '<C-w>l', { noremap = true, silent = true })
+
+-- Jump and keep cursor in the middle
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+
+-- Keep searchterms in the middle
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+-- Paste over visually selected maintaining clipboard
+vim.keymap.set('x', '<leader>p', [["_dP]])
--- lua/custom/zwergius/set.lua
+++ lua/custom/zwergius/set.lua
@@ -2,6 +2,9 @@
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+-- no line wrap
+vim.opt.wrap = false
+
 -- Indents
 vim.opt.tabstop = 2
 vim.opt.softtabstop = 2
@@ -9,9 +12,19 @@ vim.opt.shiftwidth = 2
 vim.opt.expandtab = true
 vim.opt.smartindent = true
 
+-- Infinite undo
+vim.opt.undodir = os.getenv 'HOME' .. '/.vim/undodir'
+vim.opt.undofile = true
+
 -- Set highlight on search
-vim.o.hlsearch = false
+vim.o.hlsearch = true
 vim.opt.incsearch = true
+
+-- Scroll buffer
+vim.opt.scrolloff = 8
+
+-- not sure
+vim.opt.colorcolumn = '80'
 
 -- Filetype overrides
 vim.filetype.add {
