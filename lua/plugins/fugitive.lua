f047e7db9ca26b6e092c0f6b30f73ba4f3a10a6a
Nick Burt <nbur4556@gmail.com>
2024-01-25 13:00:36 -0600

    show branch names in git graph view

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,7 +3,7 @@ return { 'tpope/vim-fugitive',
 		vim.keymap.set('n', '<leader>gs', '<CMD>Git<CR>', { desc = '[G]it [S]tatus' });
 		vim.keymap.set('n', '<leader>ga', '<CMD>Git add -A<CR>', { desc = 'Stage [A]ll [G]it files' });
 		vim.keymap.set('n', '<leader>gc', '<CMD>Git commit<CR>', { desc = '[C]ommit staged [G]it files' });
-		vim.keymap.set('n', '<leader>gg', '<CMD>Git log --graph --oneline<CR>', { desc = '[G]it log [G]raph' });
+		vim.keymap.set('n', '<leader>gg', '<CMD>Git log --graph --oneline --decorate=short<CR>', { desc = '[G]it log [G]raph' });
 		vim.keymap.set('n', '<leader>gb', '<CDM>Git blame<CR>', { desc =  '[G]it [B]lame' });
 	end
 };
