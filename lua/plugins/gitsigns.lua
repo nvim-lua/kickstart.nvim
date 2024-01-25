5eb3d923c14913d2cd586ada5359878811049a0f
Nick Burt <nbur4556@gmail.com>
2024-01-25 01:51:39 -0600

    fix labeling and remove unused git hunk shortcuts

--- lua/plugins/gitsigns.lua
+++ lua/plugins/gitsigns.lua
@@ -43,31 +43,26 @@ return {
 			-- visual mode
 			map('v', '<leader>ghs', function()
 				gs.stage_hunk { vim.fn.line '.', vim.fn.line 'v' }
-			end, { desc = 'stage git hunk' })
+			end, { desc = '[S]tage [G]it [H]unk' })
 			map('v', '<leader>ghr', function()
 				gs.reset_hunk { vim.fn.line '.', vim.fn.line 'v' }
-			end, { desc = 'reset git hunk' })
+			end, { desc = '[R]eset [G]it [H]unk' })
 			-- normal mode
-			map('n', '<leader>ghs', gs.stage_hunk, { desc = 'git stage hunk' })
-			map('n', '<leader>ghr', gs.reset_hunk, { desc = 'git reset hunk' })
-			map('n', '<leader>ghS', gs.stage_buffer, { desc = 'git Stage buffer' })
-			map('n', '<leader>ghu', gs.undo_stage_hunk, { desc = 'undo stage hunk' })
-			map('n', '<leader>ghR', gs.reset_buffer, { desc = 'git Reset buffer' })
-			map('n', '<leader>ghp', gs.preview_hunk, { desc = 'preview git hunk' })
+			map('n', '<leader>ghs', gs.stage_hunk, { desc = '[G]it [S]tage [H]unk' })
+			map('n', '<leader>ghr', gs.reset_hunk, { desc = '[G]it [R]eset [H]unk' })
+			map('n', '<leader>ghS', gs.stage_buffer, { desc = '[G]it [S]tage buffer' })
+			map('n', '<leader>ghu', gs.undo_stage_hunk, { desc = '[U]ndo stage [H]unk' })
+			map('n', '<leader>ghR', gs.reset_buffer, { desc = '[G]it [R]eset buffer' })
+			map('n', '<leader>ghp', gs.preview_hunk, { desc = '[P]review [G]it [H]unk' })
 			map('n', '<leader>ghb', function()
 				gs.blame_line { full = false }
-			end, { desc = 'git blame line' })
-			map('n', '<leader>ghd', gs.diffthis, { desc = 'git diff against index' })
-			map('n', '<leader>ghD', function()
-				gs.diffthis '~'
-			end, { desc = 'git diff against last commit' })
+			end, { desc = '[G]it blame line' })
 
 			-- Toggles
-			map('n', '<leader>tb', gs.toggle_current_line_blame, { desc = 'toggle git blame line' })
-			map('n', '<leader>td', gs.toggle_deleted, { desc = 'toggle git show deleted' })
+			map('n', '<leader>ghx', gs.toggle_deleted, { desc = 'toggle [G]it show deleted' })
 
 			-- Text object
-			map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select git hunk' })
+			map({ 'o', 'x' }, 'ih', ':<C-U>Gitsigns select_hunk<CR>', { desc = 'select [G]it [H]unk' })
 		end
 	}
 }
--- lua/plugins/whichkey.lua
+++ lua/plugins/whichkey.lua
@@ -8,7 +8,6 @@ return { 'folke/which-key.nvim', opts = {},
 			['<leader>gh'] = { name = '[G]it [H]unk', _ = 'which_key_ignore' },
 			['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
 			['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
-			['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
 			['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
 		}
 		-- register which-key VISUAL mode
