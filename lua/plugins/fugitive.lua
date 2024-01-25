95532ea93b99c38f99b5a4abd4f09885f7f29e94
Nick Burt <nbur4556@gmail.com>
2024-01-25 01:29:15 -0600

    add shortcut to git log graph oneline

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,5 +3,6 @@ return { 'tpope/vim-fugitive',
 		vim.keymap.set('n', '<leader>gs', '<CMD>Git<CR>', { desc = '[G]it [S]tatus' });
 		vim.keymap.set('n', '<leader>ga', '<CMD>Git add -A<CR>', { desc = 'Stage [A]ll [G]it files' });
 		vim.keymap.set('n', '<leader>gc', '<CMD>Git commit<CR>', { desc = '[C]ommit staged [G]it files' });
+		vim.keymap.set('n', '<leader>gg', '<CMD>Git log --graph --oneline<CR>', { desc = '[G]it log [G]raph' });
 	end
 };
