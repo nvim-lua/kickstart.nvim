a5e4878030f13537597cd9921b4190368d08c130
Nick Burt <nbur4556@gmail.com>
2024-01-25 01:57:33 -0600

    add git blame shortcut

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,5 +4,6 @@ return { 'tpope/vim-fugitive',
 		vim.keymap.set('n', '<leader>ga', '<CMD>Git add -A<CR>', { desc = 'Stage [A]ll [G]it files' });
 		vim.keymap.set('n', '<leader>gc', '<CMD>Git commit<CR>', { desc = '[C]ommit staged [G]it files' });
 		vim.keymap.set('n', '<leader>gg', '<CMD>Git log --graph --oneline<CR>', { desc = '[G]it log [G]raph' });
+		vim.keymap.set('n', '<leader>gb', '<CDM>Git blame<CR>', { desc =  '[G]it [B]lame' });
 	end
 };
