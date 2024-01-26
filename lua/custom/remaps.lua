bb64844c2005bbef9e827d15efacba5c83d73837
Henry Denny <henry.denny12@gmail.com>
2024-01-26 17:46:49 +0000

    Add orientation remaps.

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -1,3 +1,10 @@
+-- Where am I?
+vim.keymap.set("n", "J", "mzJ`z")
+vim.keymap.set("n", "<C-d>", "<C-d>zz")
+vim.keymap.set("n", "<C-u>", "<C-u>zz")
+vim.keymap.set("n", "n", "nzzzv")
+vim.keymap.set("n", "N", "Nzzzv")
+
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>cD', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
