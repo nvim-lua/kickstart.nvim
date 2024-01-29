2868d5e65cbe17cc34cc48e8c654522174a03952
Nazar <63452145+Tokarak@users.noreply.github.com>
2024-01-29 18:52:50 +0000

    Add Onedark Style (#590)
    * Add style options to Onedark setup (init.lua)
    
    * stylua init.lua
    
    * Load onedark through `require`
    
    * Improve commenting

--- init.lua
+++ init.lua
@@ -193,8 +193,13 @@ require('lazy').setup({
     -- Theme inspired by Atom
     'navarasu/onedark.nvim',
     priority = 1000,
+    lazy = false,
     config = function()
-      vim.cmd.colorscheme 'onedark'
+      require('onedark').setup {
+        -- Set a style preset. 'dark' is default.
+        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+      }
+      require('onedark').load()
     end,
   },
 
