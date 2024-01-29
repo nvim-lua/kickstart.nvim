9438f78264f2733fddbd9fd7216a5ab493475db8
Tokarak <63452145+Tokarak@users.noreply.github.com>
2024-01-29 11:15:17 +0000

    stylua init.lua

--- init.lua
+++ init.lua
@@ -195,9 +195,9 @@ require('lazy').setup({
     priority = 1000,
     lazy = false,
     config = function()
-      require('onedark').setup({
-        style = 'dark' -- dark, darker, cool, deep, warm, warmer, light
-      })
+      require('onedark').setup {
+        style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
+      }
       vim.cmd.colorscheme 'onedark'
     end,
   },
