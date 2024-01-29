bdb455ce6d95b50c2c562b760839c0008e03ce0d
Tokarak <63452145+Tokarak@users.noreply.github.com>
2024-01-29 11:29:01 +0000

    Improve commenting

--- init.lua
+++ init.lua
@@ -196,6 +196,7 @@ require('lazy').setup({
     lazy = false,
     config = function()
       require('onedark').setup {
+        -- Set a style preset. 'dark' is default.
         style = 'dark', -- dark, darker, cool, deep, warm, warmer, light
       }
       require('onedark').load()
