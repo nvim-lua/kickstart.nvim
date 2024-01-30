22f8695b13b7f623ae4f69567bc39e6179f7aa15
Henry Denny <henry.denny12@gmail.com>
2024-01-30 17:13:16 +0000

    Update ui icons.

---
+++ lua/custom/plugins/ui.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    "nvim-tree/nvim-web-devicons",
+    enabled = vim.g.icons_enabled,
+    lazy = true,
+    opts = {
+      override = {
+        toml = {icon = "", name = "TOML" },
+        deb = { icon = "", name = "Deb" },
+        lock = { icon = "󰌾", name = "Lock" },
+        mp3 = { icon = "󰎆", name = "Mp3" },
+        mp4 = { icon = "", name = "Mp4" },
+        out = { icon = "", name = "Out" },
+        ["robots.txt"] = { icon = "󰚩", name = "Robots" },
+        ttf = { icon = "", name = "TrueTypeFont" },
+        rpm = { icon = "", name = "Rpm" },
+        woff = { icon = "", name = "WebOpenFontFormat" },
+        woff2 = { icon = "", name = "WebOpenFontFormat2" },
+        xz = { icon = "", name = "Xz" },
+        zip = { icon = "", name = "Zip" },
+      },
+    },
+  },
+}
