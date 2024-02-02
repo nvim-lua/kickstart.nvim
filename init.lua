f39c6fdf51ffa996f15911e061f0890d7f0d114e
Neeraj A <neeraj.a4353@gmail.com>
2024-02-02 11:25:43 +0530

    update 02/02/2024

--- init.lua
+++ init.lua
@@ -251,7 +251,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
 
 -- [[ Configure Telescope ]]
 -- See `:help telescope` and `:help telescope.setup()`
-require('telescope').setup {defaults = {mappings = {i = {['<C-u>'] = false, ['<C-d>'] = false}}}}
+-- require('telescope').setup {defaults = {mappings = {i = {['<C-u>'] = false, ['<C-d>'] = false}}}}
 
 -- Enable telescope fzf native, if installed
 pcall(require('telescope').load_extension, 'fzf')
