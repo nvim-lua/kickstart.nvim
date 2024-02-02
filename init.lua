cf4850937250578ba481efed22b357f424d4d4d8
Richard Garber <richard.garber@ufl.edu>
2024-02-02 18:15:40 -0500

    Added Build Step to LuaSnip

--- init.lua
+++ init.lua
@@ -100,7 +100,10 @@ require('lazy').setup({
     'hrsh7th/nvim-cmp',
     dependencies = {
       -- Snippet Engine & its associated nvim-cmp source
-      'L3MON4D3/LuaSnip',
+      {
+        'L3MON4D3/LuaSnip',
+        build = 'make install_jsregexp'
+      },
       'saadparwaiz1/cmp_luasnip',
 
       -- Adds LSP completion capabilities
