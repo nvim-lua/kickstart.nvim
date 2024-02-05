46496f572d32a1768180b0335c04e29b1c706b53
Nick Burt <nbur4556@gmail.com>
2024-02-05 12:26:48 -0600

    install and configure vim surround plugin

--- lua/plugins/oil.lua
+++ lua/plugins/oil.lua
@@ -1,5 +1,6 @@
 -- file browser
-return { 'stevearc/oil.nvim',
+return {
+	'stevearc/oil.nvim',
 	config = function()
 		require("oil").setup();
 		vim.keymap.set('n', '<leader>-', '<CMD>Oil --float<CR>', { desc = 'Open current directory' })
