dbda320fd8fce3e11ba867e090cdfb03bb1d6337
naman <namanddev7@gmail.com>
2024-02-08 00:29:34 +0530

    enable python lsp

--- init.lua
+++ init.lua
@@ -516,7 +516,7 @@ require('mason-lspconfig').setup()
 local servers = {
   -- clangd = {},
   -- gopls = {},
-  -- pyright = {},
+  pyright = {},
   -- rust_analyzer = {},
   -- tsserver = {},
   -- html = { filetypes = { 'html', 'twig', 'hbs'} },
