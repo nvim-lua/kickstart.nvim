51548cf6bfac42bd30ce5b4ee092692398a6a3bc
Noah Håkansson <noah.hakansson@protonmail.com>
2024-02-07 11:22:02 +0100

    remove which key CODE

--- init.lua
+++ init.lua
@@ -588,7 +588,7 @@ end
 
 -- document existing key chains
 require('which-key').register({
-  ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+  -- ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
   -- ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
   ['<leader>g'] = { name = '[G]it', _ = 'which_key_ignore' },
   -- ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
