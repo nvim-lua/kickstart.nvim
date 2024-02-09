5f0e8a6a292e0450bd9c7d034732e2e3b2a4f986
Ryan <ryan@rtrichter.com>
2024-02-08 20:07:59 -0500

    updated init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.expandtab = true
+
 return {}
