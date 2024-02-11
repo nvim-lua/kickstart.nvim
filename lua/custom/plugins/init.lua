b677522f35499e97498ebe83ccdd6a7783f6a66d
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2024-02-11 15:39:19 -0500

    Run eslint conditionally in ES files only

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -7,8 +7,21 @@ vim.opt.colorcolumn = '80,100'
 vim.opt.number = true
 vim.opt.relativenumber = true
 
+local function lint_and_format()
+  local filetype = vim.bo.filetype
+  if
+    filetype == 'javascript'
+    or filetype == 'typescript'
+    or filetype == 'javascriptreact'
+    or filetype == 'typescriptreact'
+  then
+    vim.cmd('EslintFixAll')
+  end
+  vim.cmd('Neoformat')
+end
+
 require('which-key').register({
-  f = { ':Neoformat<CR>:EslintFixAll<CR>', 'Prettier then eslint' }
+  f = { lint_and_format, 'Lint and format current buffer' }
 }, { prefix = '<leader>c' })
 
 return {}
