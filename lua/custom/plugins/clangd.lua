be35cd5a14239849baabc732118effd27f5e2581
flea <fleashiro@gmail.com>
2024-02-19 17:26:00 +0800

    Add keybind for cmake debug

--- lua/custom/plugins/clangd.lua
+++ lua/custom/plugins/clangd.lua
@@ -2,6 +2,11 @@ return {
   { 'p00f/clangd_extensions.nvim', lazy = true },
   {
     'Civitasv/cmake-tools.nvim',
+    lazy = true,
+    ft = { 'cpp' },
     opts = {},
+    keys = {
+      { '<leader>db', "<cmd> CMakeDebug <cr>", desc = "CMake debug" }
+    }
   },
 }
