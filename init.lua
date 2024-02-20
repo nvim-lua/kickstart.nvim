d955e57ce8ef325a23356e13487a8bf5eed8e59b
Frederic Houdemer <f.houdemer@gmail.com>
2024-02-20 01:57:49 +0100

    :sparkles: added copilot + change tab to ctrl-tab for some existing shortcut

--- init.lua
+++ init.lua
@@ -75,6 +75,7 @@ require('lazy').setup({
 
   -- Detect tabstop and shiftwidth automatically
   'tpope/vim-sleuth',
+  'github/copilot.vim',
 
   -- NOTE: This is where your plugins related to LSP can be installed.
   --  The configuration is done below. Search for lspconfig to find it below.
@@ -641,7 +642,7 @@ cmp.setup {
       behavior = cmp.ConfirmBehavior.Replace,
       select = true,
     },
-    ['<Tab>'] = cmp.mapping(function(fallback)
+    ['<C-Tab>'] = cmp.mapping(function(fallback)
       if cmp.visible() then
         cmp.select_next_item()
       elseif luasnip.expand_or_locally_jumpable() then
