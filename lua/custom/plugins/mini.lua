c25d5147874c55b69760a1b9fb5fcb173a730913
Henry Denny <henry.denny12@gmail.com>
2024-02-22 15:15:52 +0000

    Change cursor animation.

--- lua/custom/plugins/mini.lua
+++ lua/custom/plugins/mini.lua
@@ -1,9 +1,13 @@
 return {
   "echasnovski/mini.nvim",
   version = "*",
-  config = function ()
+  config = function()
     local animate = require('mini.animate')
     animate.setup({
+      cursor = {
+        timing = animate.gen_timing.exponential({ duration = 250, unit = 'total' }),
+        path = animate.gen_path.angle(),
+      },
       scroll = {
         timing = animate.gen_timing.linear({ duration = 100, unit = 'total' }),
       },
