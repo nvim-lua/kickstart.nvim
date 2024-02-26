95a334f4323518447da5c227fd23b4833ab04cc2
Henry Denny <henry.denny12@gmail.com>
2024-02-26 13:12:32 +0000

    Add window switching keymaps.

--- lua/custom/remaps.lua
+++ lua/custom/remaps.lua
@@ -8,3 +8,18 @@ vim.keymap.set("n", "N", "Nzzzv")
 -- Diagnostic keymaps
 vim.keymap.set('n', '<leader>cd', vim.diagnostic.open_float, { desc = 'Open floating diagnostic message' })
 vim.keymap.set('n', '<leader>cD', vim.diagnostic.setloclist, { desc = 'Open diagnostics list' })
+
+-- Moving between windows
+vim.cmd.tnoremap('<A-h', [[<C-\><C-N><C-w>h]])
+vim.cmd.tnoremap('<A-h>', [[<C-\><C-N><C-w>h]])
+vim.cmd.tnoremap('<A-j>', [[<C-\><C-N><C-w>j]])
+vim.cmd.tnoremap('<A-k>', [[<C-\><C-N><C-w>k]])
+vim.cmd.tnoremap('<A-l>', [[<C-\><C-N><C-w>l]])
+vim.cmd.inoremap('<A-h>', [[<C-\><C-N><C-w>h]])
+vim.cmd.inoremap('<A-j>', [[<C-\><C-N><C-w>j]])
+vim.cmd.inoremap('<A-k>', [[<C-\><C-N><C-w>k]])
+vim.cmd.inoremap('<A-l>', [[<C-\><C-N><C-w>l]])
+vim.cmd.nnoremap('<A-h>', [[<C-w>h]])
+vim.cmd.nnoremap('<A-j>', [[<C-w>j]])
+vim.cmd.nnoremap('<A-k>', [[<C-w>k]])
+vim.cmd.nnoremap('<A-l>', [[<C-w>l]])
