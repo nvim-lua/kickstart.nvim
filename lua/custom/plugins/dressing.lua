71d25adebe9c2ec5fef8d562870339a0a24a8067
chilborne <christopher.hilborne@zerocopy.be>
2024-02-26 12:10:56 +0100

    added dressings for nicer pop-up inputs

---
+++ lua/custom/plugins/dressing.lua
@@ -0,0 +1,15 @@
+return {
+  "stevearc/dressing.nvim",
+  init = function()
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.select = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.select(...)
+    end
+    ---@diagnostic disable-next-line: duplicate-set-field
+    vim.ui.input = function(...)
+      require("lazy").load({ plugins = { "dressing.nvim" } })
+      return vim.ui.input(...)
+    end
+  end,
+}
