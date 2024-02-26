af4fd2355f211d4d19ec08ea6d57cbea483e2ee7
TJ DeVries <devries.timothyj@gmail.com>
2024-02-26 10:46:31 -0500

    fixup: change comment to reflect auto_install

--- init.lua
+++ init.lua
@@ -775,7 +775,7 @@ require('lazy').setup({
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
         ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
-        -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
+        -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
         indent = { enable = true },
