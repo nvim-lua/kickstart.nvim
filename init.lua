579d50ea1b3790b8bb6e25e1431a677a297c5f1e
Lucas Nicolas <lucas.nicolas@epfl.ch>
2024-02-26 17:10:41 +0100

    quiet theme

--- init.lua
+++ init.lua
@@ -728,7 +728,7 @@ require('lazy').setup({
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'quiet'
 
       -- You can configure highlights by doing something like
       vim.cmd.hi 'Comment gui=none'
