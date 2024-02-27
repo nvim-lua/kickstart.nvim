c1a40e7571cb099afc49ad4f758ced01d63a0151
brxxlstxrs <92815065+brxxlstxrs@users.noreply.github.com>
2024-02-27 19:51:27 +0300

    added space

--- init.lua
+++ init.lua
@@ -219,6 +219,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+
   -- [[ Plugin Specs list ]]
 
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
