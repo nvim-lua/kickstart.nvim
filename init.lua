c9ada989f30509ab58cf4caecd70c131b7f1d41d
Trevor Griggs <trevor.griggs@caliola.com>
2024-02-27 16:37:15 -0700

    revert accidental deletion

--- init.lua
+++ init.lua
@@ -277,6 +277,8 @@ require('lazy').setup({
       -- Document existing key chains
       require('which-key').register {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
+        ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
       }
