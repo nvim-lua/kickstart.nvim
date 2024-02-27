ee02f5e450004d6da527b7e43aeb8951cdc5cc84
Zach Zolton <zach.zolton@sproutsocial.com>
2024-02-27 17:11:39 -0500

    Turn off formatting on save of buffer

--- init.lua
+++ init.lua
@@ -583,10 +583,7 @@ require('lazy').setup({
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = false,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
