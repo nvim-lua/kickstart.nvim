77c5695b5cc2e0f518efcd98819546c31627dd88
lucas nicolas <lucas.nicolas@epfl.ch>
2024-02-27 02:54:49 +0100

    x

--- init.lua
+++ init.lua
@@ -536,10 +536,11 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
+        typoslsp = {},
         clangd = {},
         gopls = {},
         -- pylyzer = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
