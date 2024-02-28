8abc7ad6a1fe1f97ef8be46bdb637ec7efb00f34
Tschierske, Nikki (NE-GI/F) <nikki.tschierske@volkswagen.de>
2024-02-28 12:43:36 +0100

    add refactor plugin

---
+++ lua/custom/plugins/refactor.lua
@@ -0,0 +1,10 @@
+return {
+  "ThePrimeagen/refactoring.nvim",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-treesitter/nvim-treesitter",
+  },
+  config = function()
+    require("refactoring").setup()
+  end,
+}
