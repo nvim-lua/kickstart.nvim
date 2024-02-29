3f4a6cd6e7b4b079858b7d54209041b516726d0a
Henry Denny <henry.denny12@gmail.com>
2024-02-29 12:19:31 +0000

    Add Tabnine client.

---
+++ lua/custom/plugins/tabnine.lua
@@ -0,0 +1,15 @@
+return {
+  "codota/tabnine-nvim",
+  build = "./dl_binaries.sh",
+  config = function()
+    require('tabnine').setup({
+      disable_auto_comment = true,
+      accept_keymap = "<A-]>",
+      dismiss_keymap = "<A-[>",
+      debounce_ms = 800,
+      suggestion_color = { gui = "#808080", cterm = 244 },
+      exclude_filetypes = { "TelescopePrompt", "NvimTree" },
+      log_file_path = nil, -- absolute path to Tabnine log file
+    })
+  end
+}
