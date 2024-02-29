2d4812d283093ea794bfbdab5a10ae4d33eeb777
Gilad Sher <gilad.sher@zoominsoftware.com>
2024-02-29 15:42:07 -0500

    add astro support in treesitter

--- init.lua
+++ init.lua
@@ -426,7 +426,7 @@ vim.keymap.set('n', '<leader>sr', require('telescope.builtin').resume, { desc = 
 -- See `:help nvim-treesitter`
 require('nvim-treesitter.configs').setup {
   -- Add languages to be installed here that you want installed for treesitter
-  ensure_installed = { 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'css', 'scss' },
+  ensure_installed = { 'astro', 'c', 'cpp', 'go', 'lua', 'python', 'rust', 'tsx', 'typescript', 'vimdoc', 'vim', 'css', 'scss' },
 
   -- Autoinstall languages that are not installed. Defaults to false (but you can change for yourself!)
   auto_install = false,
