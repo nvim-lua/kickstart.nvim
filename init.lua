e272951042e4ac7362f43d9aed7e7c6a88a83a19
Rahsheen Porter <rahsheen.porter@thirtymadison.com>
2024-03-01 14:07:35 -0500

    update nvim config and junk

--- init.lua
+++ init.lua
@@ -228,14 +228,14 @@ require('lazy').setup({
     },
   },
 
-  {
-    -- Add indentation guides even on blank lines
-    'lukas-reineke/indent-blankline.nvim',
-    -- Enable `lukas-reineke/indent-blankline.nvim`
-    -- See `:help ibl`
-    main = 'ibl',
-    opts = {},
-  },
+--  {
+--    -- Add indentation guides even on blank lines
+--    'lukas-reineke/indent-blankline.nvim',
+--    -- Enable `lukas-reineke/indent-blankline.nvim`
+--    -- See `:help ibl`
+--    main = 'ibl',
+--    opts = {},
+--  },
 
   -- "gc" to comment visual regions/lines
   { 'numToStr/Comment.nvim', opts = {} },
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -51,4 +51,18 @@ return {
 			require("luasnip.loaders.from_vscode").lazy_load()
 		end,
 	},
+	{
+		"nvim-telescope/telescope.nvim",
+		dependencies = {
+			{
+				"nvim-telescope/telescope-live-grep-args.nvim",
+				-- This will not install any breaking changes.
+				-- For major updates, this must be adjusted manually.
+				version = "^1.0.0",
+			},
+		},
+		config = function()
+			require("telescope").load_extension("live_grep_args")
+		end
+	}
 }
