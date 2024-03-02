1f1673571ba69348983039c8aac463c6c815c8d4
Noah Håkansson <noah.hakansson@protonmail.com>
2024-03-02 14:32:31 +0100

    add paren-hint.nvim plugin

---
+++ lua/custom/plugins/paren-hint.lua
@@ -0,0 +1,29 @@
+local M = {
+  'briangwaltney/paren-hint.nvim',
+  lazy = false,
+  dependencies = {
+    'nvim-treesitter/nvim-treesitter',
+  },
+}
+
+function M.config()
+  -- you can create a custom highlight group for the ghost text with the below command.
+  -- change the `highlight` option to `parenhint` if you use this method.
+  vim.api.nvim_exec([[ highlight parenhint guifg='#495659' ]], false)
+  require('paren-hint').setup({
+    -- Include the opening paren in the ghost text
+    include_paren = true,
+
+    -- Show ghost text when cursor is anywhere on the line that includes the close paren rather than just when the cursor is on the close paren
+    anywhere_on_line = true,
+
+    -- show the ghost text when the opening paren is on the same line as the close paren
+    show_same_line_opening = false,
+
+    -- style of the ghost text using highlight group
+    -- :Telescope highlights to see the available highlight groups if you have telescope installed
+    highlight = 'parenhint',
+  })
+end
+
+return M
