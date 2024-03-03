e0f7c4d772cf86521256ded7bd2760f8abd1deaa
Wellinton Aracena <thewillyvegetta@gmail.com>
2024-03-03 12:01:54 -0500

    added some plugins from nvchad

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,6 +3,47 @@
 --
 -- See the kickstart.nvim README for more information
 --
-local M = {}
+local plugins = {
+  {
+    'ellisonleao/gruvbox.nvim',
+    priority = 1000,
+    config = true,
+    opts = function()
+      -- Default options:
+      require('gruvbox').setup {
+        terminal_colors = true, -- add neovim terminal colors
+        undercurl = true,
+        underline = true,
+        bold = true,
+        italic = {
+          strings = true,
+          emphasis = true,
+          comments = true,
+          operators = false,
+          folds = true,
+        },
+        strikethrough = true,
+        invert_selection = false,
+        invert_signs = false,
+        invert_tabline = false,
+        invert_intend_guides = false,
+        inverse = true, -- invert background for search, diffs, statuslines and errors
+        contrast = '', -- can be "hard", "soft" or empty string
+        palette_overrides = {},
+        overrides = {},
+        dim_inactive = false,
+        transparent_mode = false,
+      }
+      vim.cmd.colorscheme 'gruvbox'
+    end,
+  },
+  {
+    'jubnzv/mdeval.nvim',
+    event = 'VeryLazy',
+    opt = function()
+      return vim.g.markdown_fenced_languages == { 'python', 'cpp' }
+    end,
+  },
+}
 
-return M
+return plugins
