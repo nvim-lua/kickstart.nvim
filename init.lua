c3127f1226df227e436a62105a1e33ea052f5e56
Damjan 9000 <damjan.9000@gmail.com>
2024-03-04 14:16:50 +0100

    Change statusline location to LINE:COLUMN (#689)

--- init.lua
+++ init.lua
@@ -761,11 +761,11 @@ require('lazy').setup {
       statusline.setup()
 
       -- You can configure sections in the statusline by overriding their
-      -- default behavior. For example, here we disable the section for
-      -- cursor information because line numbers are already enabled
+      -- default behavior. For example, here we set the section for
+      -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
       statusline.section_location = function()
-        return ''
+        return '%2l:%-2v'
       end
 
       -- ... and there is more!
