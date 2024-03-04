ae770cabde5a2626caa9c16f3d85067bee805c26
Damjan 9000 <damjan.9000@gmail.com>
2024-03-04 22:45:44 +0100

    conform: disable autoformat on save for specified filetypes
    Provide a method to disable autoformat on save lsp fallback for
    specified filetypes. By default disable for C/C++ as an example,
    because it does not have a well standardized coding style.
    
    Based on conform recipe:
    https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md

--- init.lua
+++ init.lua
@@ -603,10 +603,16 @@ require('lazy').setup({
     'stevearc/conform.nvim',
     opts = {
       notify_on_error = false,
-      format_on_save = {
-        timeout_ms = 500,
-        lsp_fallback = true,
-      },
+      format_on_save = function(bufnr)
+        -- Disable "format_on_save lsp_fallback" for languages that don't
+        -- have a well standardized coding style. You can add additional
+        -- languages here or re-enable it for the disabled ones.
+        local disable_filetypes = { c = true, cpp = true }
+        return {
+          timeout_ms = 500,
+          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+        }
+      end,
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
