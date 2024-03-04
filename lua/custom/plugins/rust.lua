245105e8f32f3ee5769e6eaed1a210b20062ce0d
Vladislav <vgrechannik@gmail.com>
2024-03-04 03:06:44 +0100

    Remove unneded comma

--- lua/custom/plugins/rust.lua
+++ lua/custom/plugins/rust.lua
@@ -1,9 +1,9 @@
 return {
-    -- Managing crates.io dependencies --
-    'saecki/crates.nvim',
-    tag = 'stable',
-    event = { "BufRead Cargo.toml" },
-    config = function()
-      require('crates').setup()
-    end,
-  },
+  -- Managing crates.io dependencies --
+  'saecki/crates.nvim',
+  tag = 'stable',
+  event = { 'BufRead Cargo.toml' },
+  config = function()
+    require('crates').setup()
+  end,
+}
