a3f35e35087e1760153da4e504d12d6977c4e3c9
Ryan Winchester <ryan@winchester.dev>
2024-03-05 19:32:45 -0400

    Add more detail to colorscheme comment

--- init.lua
+++ init.lua
@@ -725,7 +725,9 @@ require('lazy').setup {
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      -- Load the colorscheme here
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       vim.cmd.colorscheme 'tokyonight-night'
 
       -- You can configure highlights by doing something like
