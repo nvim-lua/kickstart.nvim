9fc61fb23a0c6d2ae8fc663b58e3280b16d3c1b8
Xander Johnson <xander@metasyn.pw>
2024-03-04 19:35:18 -0800

    Replace conform's `formatters_by_ft` with just `formatters`

--- init.lua
+++ init.lua
@@ -611,7 +611,7 @@ require('lazy').setup {
         timeout_ms = 500,
         lsp_fallback = true,
       },
-      formatters_by_ft = {
+      formatters = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
