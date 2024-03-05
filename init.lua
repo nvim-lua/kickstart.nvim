fa57fe149003a69b7a55410d8ca1ba24b28f3fe4
David Drottson <dosdro@gmail.com>
2024-03-05 13:48:48 +0100

    feat: enable loading lua/custom/plugins/*.lua

--- init.lua
+++ init.lua
@@ -814,7 +814,7 @@ require('lazy').setup {
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }
 
 -- The line beneath this is called `modeline`. See `:help modeline`
