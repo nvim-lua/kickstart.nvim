d9e77802d967b6a630e3fb9e2dbbe7917b9f624b
Joel Lau <joel.lau@coinhako.com>
2024-03-07 10:30:21 +0800

    search hidden files

--- init.lua
+++ init.lua
@@ -521,6 +521,21 @@ require('telescope').setup {
       },
     },
   },
+  pickers = {
+    find_files = {
+      hidden = true,
+    },
+    live_grep = {
+      additional_args = function()
+        return { '--hidden', '--glob', '!**/.git/*' }
+      end,
+    },
+    grep_string = {
+      additional_args = function()
+        return { '--hidden', '--glob', '!**/.git/*' }
+      end,
+    },
+  },
 }
 
 -- Enable telescope fzf native, if installed
