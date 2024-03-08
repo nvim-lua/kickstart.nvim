50b37a91cfaeb7b23d5b9ead345025d5650e6122
dezlymacauley <dezlymacauley@proton.me>
2024-03-08 22:46:51 +0200

    Fixed stylua formatting

--- init.lua
+++ init.lua
@@ -197,7 +197,7 @@ vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper win
 --  See `:help vim.highlight.on_yank()`
 
 -- Choose your own highlight color: value in hexidecimals
-local highlight_color = "#40005b"
+local highlight_color = '#40005b'
 
 -- Define how long the highlight should appear: value is in milliseconds
 local highlight_timeout = 300
@@ -208,8 +208,8 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
     -- Set highlight color and timeout
-    vim.cmd(string.format("highlight YankHighlight guibg=%s ctermbg=0", highlight_color))
-    vim.highlight.on_yank({ higroup = "YankHighlight", timeout = highlight_timeout })
+    vim.cmd(string.format('highlight YankHighlight guibg=%s ctermbg=0', highlight_color))
+    vim.highlight.on_yank { higroup = 'YankHighlight', timeout = highlight_timeout }
   end,
 })
 
@@ -247,7 +247,7 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  { 'numToStr/Comment.nvim',    opts = {} },
+  { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`. This is equivalent to the following lua:
@@ -282,7 +282,7 @@ require('lazy').setup({
   -- after the plugin has been loaded:
   --  config = function() ... end
 
-  {                     -- Useful plugin to show you pending keybinds.
+  { -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VimEnter', -- Sets the loading event to 'VimEnter'
     config = function() -- This is the function that runs, AFTER loading
@@ -328,7 +328,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -735,7 +735,7 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
-    lazy = false,    -- make sure we load this during startup if it is your main colorscheme
+    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
       -- Load the colorscheme here.
