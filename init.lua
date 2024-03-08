4a791de93e82bb3345f3d2e1cb4f1a6b153e3550
Kasper Sternberg <kaspersternberg@gmail.com>
2024-03-08 18:58:45 +0100

    Using Omnisharp instead of csharp_ls as LSP

--- init.lua
+++ init.lua
@@ -533,7 +533,7 @@ require('lazy').setup {
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        csharp_ls = {}, -- clangd = {},
+        omnisharp = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
