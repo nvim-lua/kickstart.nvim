1efbc15b317aacc625c7a1c6d41b890581158d40
Moritz Wilksch <moritz.wilksch@quantco.com>
2024-03-09 21:40:22 +0100

    dont default to sys clipboard

--- init.lua
+++ init.lua
@@ -27,7 +27,7 @@ vim.opt.showmode = false
 -- Sync clipboard between OS and Neovim.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+-- vim.opt.clipboard = 'unnamedplus'
 
 -- Enable break indent
 vim.opt.breakindent = true
