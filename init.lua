3cfccc01be47259613e39196e6a6901ceecc3a0f
name.tar.xz <54463026+name-tar-xz@users.noreply.github.com>
2024-03-10 01:55:08 +0530

    use init for colorscheme (#715)

--- init.lua
+++ init.lua
@@ -723,9 +723,8 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`
     'folke/tokyonight.nvim',
-    lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
-    config = function()
+    init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
