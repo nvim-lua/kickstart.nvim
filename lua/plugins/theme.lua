39e9fb0a241b1022829c4d4cdaab2565b0266d3f
gregladden <greg.ladden@g.austincc.edu>
2024-03-08 22:46:13 -0600

    update theme to everforest

--- lua/plugins/theme.lua
+++ lua/plugins/theme.lua
@@ -1,7 +1,21 @@
-return {
-	'catppuccin/nvim',
-	priority = 1000,
-	config = function()
-		vim.cmd.colorscheme 'catppuccin'
-	end,
+local M = {
+  "neanias/everforest-nvim",
+  lazy = false,
+  priority = 1000, -- make sure to load this before all the other start plugins
 }
+
+function M.config()
+  local everforest = require("everforest")
+  everforest.setup({
+    background = "hard",
+    transparent_background_level = 0,
+    italics = true,
+    disable_italic_comments = false,
+    on_highlights = function(hl, _)
+      hl["@string.special.symbol.ruby"] = { link = "@field" }
+    end,
+  })
+  everforest.load()
+end
+
+return M
