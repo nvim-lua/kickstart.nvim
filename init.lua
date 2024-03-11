cdc22761ace8b9b757d68f1735904a78d300c502
rapzy <sarjyant@gmail.com>
2024-03-11 15:18:22 +0545

    telescope prefix $

--- init.lua
+++ init.lua
@@ -375,11 +375,12 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          --   mappings = {
+          --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
+          --   },
+          prompt_prefix = '$ ',
+        },
         -- pickers = {}
         extensions = {
           ['ui-select'] = {
