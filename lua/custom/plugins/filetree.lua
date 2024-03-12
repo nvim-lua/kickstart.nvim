26829fc9923b018cf8d7ae582ed67763140cdd2d
rapzy <sarjyant@gmail.com>
2024-03-12 09:02:53 +0545

    autopairs and neotree added

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,12 @@
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+}
