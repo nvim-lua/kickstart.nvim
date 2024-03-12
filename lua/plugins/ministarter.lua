d69f78eac1987a173a87a4a2964fc1bb7fbb95a9
Nick Burt <nbur4556@gmail.com>
2024-03-12 11:56:53 -0500

    initial mini starter configuration

--- lua/plugins/ministarter.lua
+++ lua/plugins/ministarter.lua
@@ -2,6 +2,19 @@
 return {
 	'echasnovski/mini.nvim',
 	config = function()
-		require('mini.starter').setup();
+		local mini = require('mini.starter');
+		mini.setup({
+			items = {
+				-- FIX: either install telescope browser extension, or remove browser from telescope options
+				mini.sections.telescope(),
+				mini.sections.recent_files(5, true, true),
+				mini.sections.builtin_actions(),
+			},
+			content_hooks = {
+				mini.gen_hook.adding_bullet('-'),
+				mini.gen_hook.aligning('center', 'center'),
+			},
+			footer = '',
+		})
 	end
 }
