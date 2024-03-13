ef053487d0cca8cfa4177bbff9a13cb0225bd358
gregladden <greg.ladden@g.austincc.edu>
2024-03-12 21:13:00 -0500

    adding git func

---
+++ lua/plugins/git.lua
@@ -0,0 +1,15 @@
+return {
+  {
+    'tpope/vim-fugitive',
+  },
+  {
+    'lewis6991/gitsigns.nvim',
+    config = function()
+      require('gitsigns').setup {}
+
+      -- Keymaps
+      vim.keymap.set("n", "<leader>gp", ":Gitsigns preview_hunk<CR>", {})
+      vim.keymap.set("n", "<leader>gt", ":Gitsigns toggle_current_line_blame<CR>", {})
+    end
+  }
+}
