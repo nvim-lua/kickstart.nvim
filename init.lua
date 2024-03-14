4f9baf1ea3c065b774b51191c7ad5c7a0d89d669
geo <geo@Debian-VM.debian-vm.localdomain>
2024-03-14 23:08:15 +0200

    relative numbers

--- init.lua
+++ init.lua
@@ -99,7 +99,7 @@ vim.g.maplocalleader = ' '
 vim.opt.number = true
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
