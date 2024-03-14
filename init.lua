a6c04ac8dd765a8a88ab407fe1272cf47a0a574d
Vladislav <vgrechannik@gmail.com>
2024-03-14 05:07:40 +0100

    move friendly snippets to dependencies of LuaSnip, as suggested by the docs

--- init.lua
+++ init.lua
@@ -656,6 +656,13 @@ require('lazy').setup({
           end
           return 'make install_jsregexp'
         end)(),
+        dependencies = {
+          -- If you want to add a bunch of pre-configured snippets,
+          --    you can use this plugin to help you. It even has snippets
+          --    for various frameworks/libraries/etc. but you will have to
+          --    set up the ones that are useful for you.
+          'rafamadriz/friendly-snippets',
+        },
         config = function()
           require('luasnip.loaders.from_vscode').lazy_load()
         end,
@@ -667,12 +674,6 @@ require('lazy').setup({
       --  into multiple repos for maintenance purposes.
       'hrsh7th/cmp-nvim-lsp',
       'hrsh7th/cmp-path',
-
-      -- If you want to add a bunch of pre-configured snippets,
-      --    you can use this plugin to help you. It even has snippets
-      --    for various frameworks/libraries/etc. but you will have to
-      --    set up the ones that are useful for you.
-      'rafamadriz/friendly-snippets',
     },
     config = function()
       -- See `:help cmp`
