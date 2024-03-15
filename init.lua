7892c0c354639985b1cf36f11d175201590e267b
TJ DeVries <devries.timothyj@gmail.com>
2024-03-15 11:35:07 -0400

    fix: disable ts indenting for Ruby
    Tree-sitter indenting for ruby is pretty terrible.
    But the fix requires a few steps, so showed those
    and documented how you could do that for other languages
    as well (with the tricky part being the
    additional_vim_regex_highlighting trick)

--- init.lua
+++ init.lua
@@ -790,8 +790,14 @@ require('lazy').setup({
       ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
-      highlight = { enable = true },
-      indent = { enable = true },
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
