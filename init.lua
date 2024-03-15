ed32b88ff319deac536eac98655c750c5d2b8202
Rafael Zasas <42390827+RafaelZasas@users.noreply.github.com>
2024-03-15 16:12:41 +0200

    Add <C-b>/<C-f> cmp mapping to scroll cmp docs (#750)

--- init.lua
+++ init.lua
@@ -674,6 +674,10 @@ require('lazy').setup({
           -- Select the [p]revious item
           ['<C-p>'] = cmp.mapping.select_prev_item(),
 
+          -- scroll the documentation window [b]ack / [f]orward
+          ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+          ['<C-f>'] = cmp.mapping.scroll_docs(4),
+
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
