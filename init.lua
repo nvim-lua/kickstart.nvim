add28b9fc4cca92444e5c2f582755844a0b76415
Kasper Sternberg <kst@nemlig.com>
2024-03-15 14:34:36 +0100

    Changed keybinds for running tests

--- init.lua
+++ init.lua
@@ -278,10 +278,12 @@ require('lazy').setup {
         ['<leader>c'] = { name = '[C]ode', _ = 'which_key_ignore' },
         ['<leader>cp'] = { name = '[C]o[P]ilot', _ = 'which_key_ignore' },
         ['<leader>d'] = { name = '[D]ocument', _ = 'which_key_ignore' },
-        ['<leader>r'] = { name = '[R]ename', _ = 'which_key_ignore' },
+        ['<leader>R'] = { name = '[R]ename', _ = 'which_key_ignore' },
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
+        ['<leader>r'] = { name = '[Run]', _ = 'which_key_ignore' },
+        ['<leader>rt'] = { name = '[R]un [T]est', _ = 'which_key_ignore' },
       }
     end,
   },
--- lua/custom/plugins/neotest.lua
+++ lua/custom/plugins/neotest.lua
@@ -16,5 +16,6 @@ return {
     }
   end,
   vim.keymap.set('n', '<leader>te', ':Neotest summary toggle<CR>', { desc = '[T]est [E]xplorer' }),
-  vim.keymap.set('n', '<leader>rt', ':Neotest run suite=true<CR>', { desc = '[R]un [T]est suite' }),
+  vim.keymap.set('n', '<leader>rts', ':Neotest run suite=true<CR>', { desc = '[R]un [T]est [S]uite' }),
+  vim.keymap.set('n', '<leader>rtc', ':Neotest run suite=true<CR>', { desc = '[R]un [T]est [C]ase' }),
 }
