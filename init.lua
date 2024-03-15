d0ddb50c328f50c7dc643b7ae53a3faf22a38cda
Zach Zolton <zach.zolton@sproutsocial.com>
2024-03-15 10:17:14 -0400

    Fix git commit highlighting
    As per: https://neovim.discourse.group/t/git-diff-highlighting-are-not-working-anymore-in-gitcommit-filetype/3547/4

--- init.lua
+++ init.lua
@@ -745,7 +745,17 @@ require('lazy').setup({
 
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup {
-        ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+        ensure_installed = {
+          'bash',
+          'c',
+          'diff',
+          'git_rebase',
+          'html',
+          'lua',
+          'markdown',
+          'vim',
+          'vimdoc',
+        },
         -- Autoinstall languages that are not installed
         auto_install = true,
         highlight = { enable = true },
