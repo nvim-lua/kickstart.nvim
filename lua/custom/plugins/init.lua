a87fdc2396dbaa4c986c82f8c251b7fc62cc9f99
tsorabel-pc <tsorabel-pc@tsorabel-pc.tsorabel-pc>
2024-03-17 16:17:06 +0100

    feat: add git shortcut

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -14,7 +14,7 @@ return {
       vim.keymap.set({ 'n' }, '<leader>a', function()
         harpoon:list():append()
       end, { desc = '[A]ppend File to harpoon' })
-      vim.keymap.set('n', '<leader>h', function()
+      vim.keymap.set('n', '<C-h>', function()
         harpoon.ui:toggle_quick_menu(harpoon:list())
       end, { desc = '[H]arpoon quick menu' })
 
