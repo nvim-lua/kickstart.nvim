2970ccd315cf96137094bc0f7f5eb8024a47b62a
Henry Denny <henry.denny12@gmail.com>
2024-03-18 21:40:00 +0000

    Change Tabnine keymaps.

--- lua/custom/plugins/tabnine.lua
+++ lua/custom/plugins/tabnine.lua
@@ -4,8 +4,8 @@ return {
   config = function()
     require('tabnine').setup({
       disable_auto_comment = true,
-      accept_keymap = "<A-]>",
-      dismiss_keymap = "<A-[>",
+      accept_keymap = "<C-]>",
+      dismiss_keymap = "<C-[>",
       debounce_ms = 800,
       suggestion_color = { gui = "#808080", cterm = 244 },
       exclude_filetypes = { "TelescopePrompt", "NvimTree" },
