cba095942d007e64f06ee40434da6ca3805664e4
Joshua Sun <joshuapsun@gmail.com>
2024-03-18 10:58:25 -0400

    tiny fix

--- init.lua
+++ init.lua
@@ -838,7 +838,7 @@ require('lazy').setup({
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information see: :help lazy.nvim-lazy.nvim-structuring-your-plugins
   { import = 'custom.plugins' },
-}
+})
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
