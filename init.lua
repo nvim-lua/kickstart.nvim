e58ca75bf4d32adf85278598c093bf795f33af6b
Fredrik Averpil <fredrik.averpil@gmail.com>
2024-03-18 15:00:48 +0100

    feat: add linter plugin (#699)

--- init.lua
+++ init.lua
@@ -831,6 +831,7 @@ require('lazy').setup({
   --
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
+  -- require 'kickstart.plugins.lint',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
