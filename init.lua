3ac923a4247483d2a08c300c8f921f5eebbd95b3
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2024-03-19 17:59:50 -0700

    enable autoformatting for ts, js and ruby

--- init.lua
+++ init.lua
@@ -663,7 +663,9 @@ require('lazy').setup({
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
-        -- javascript = { { "prettierd", "prettier" } },
+        javascript = { { 'prettierd', 'prettier' } },
+        typescript = { { 'prettierd', 'prettier' } },
+        ruby = { { 'rubocop' } },
       },
     },
   },
