f4ee26d9b833b96410a37d18bcafd131866faa80
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2024-03-19 17:21:55 -0700

    update colorscheme

--- init.lua
+++ init.lua
@@ -779,14 +779,15 @@ require('lazy').setup({
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
-    -- 'catppuccin/nvim',
-    'olivercederborg/poimandres.nvim',
+    'catppuccin/nvim',
+    -- 'olivercederborg/poimandres.nvim',
+    name = 'catpuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'poimandres'
+      vim.cmd.colorscheme 'catppuccin-macchiato'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
