df1ccefc7ae2c68f407e201d72e96eaf0178901b
Kerry Staley <35789718+kgstaley@users.noreply.github.com>
2024-03-21 10:34:26 -0700

    add linters, enable plugins

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,10 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        ruby = { 'rubocop' },
+        terraform = { 'tlint' },
+        json = { 'jsonlint' },
+        javascript = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
