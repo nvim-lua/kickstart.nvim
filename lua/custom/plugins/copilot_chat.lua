2e73061f082c9d5acb960341b723d1a3285a0eb5
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2024-03-22 22:16:37 -0400

    Add copilot chat

---
+++ lua/custom/plugins/copilot_chat.lua
@@ -0,0 +1,11 @@
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    branch = "canary",
+    dependencies = {
+      "zbirenbaum/copilot.lua",
+      "nvim-lua/plenary.nvim",
+    },
+    opts = {},
+  },
+}
