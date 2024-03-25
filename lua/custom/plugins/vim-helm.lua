4c8dd5cc90c34c3196d632a072c3e1b271aaa983
Tim Redband <tim.redband@dunnhumby.com>
2024-03-25 16:11:33 -0400

    add custom plugins

--- lua/custom/plugins/vim-helm.lua
+++ lua/custom/plugins/vim-helm.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'towolf/vim-helm',
+    ft = 'helm',
+  },
+}
