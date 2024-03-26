fec2cc485ed5017dfea8e4f6d151d4b6b0515907
Sylvain Maret <sylvain.maret@hotmail.fr>
2024-03-26 23:38:28 +0100

    adding plugins

--- lua/custom/plugins/ferris.lua
+++ lua/custom/plugins/ferris.lua
@@ -1,10 +1,10 @@
 return {
-    'vxpm/ferris.nvim',
-    opts = {
-        -- your options here
-        -- If true, will automatically create commands for each LSP method
-	create_commands = true, -- bool
-	-- Handler for URL's (used for opening documentation)
-	url_handler = "xdg-open", -- string | function(string)
-    }
+	'vxpm/ferris.nvim',
+	opts = {
+		-- your options here
+		-- if true, will automatically create commands for each lsp method
+		create_commands = true, -- bool
+		-- handler for url's (used for opening documentation)
+		url_handler = "xdg-open", -- string | function(string)
+	}
 }
---
+++ lua/custom/plugins/ollama-chat.lua
@@ -0,0 +1,8 @@
+return {
+	"gerazov/ollama-chat.nvim",
+	dependencies = {
+		"nvim-lua/plenary.nvim",
+		"stevearc/dressing.nvim",
+		"nvim-telescope/telescope.nvim",
+	},
+}
