1981a87ae82c78c43cb9b3eb871e679ed3d20d21
Liu Qisheng <81770798+Saplyn@users.noreply.github.com>
2024-03-27 21:19:41 +0800

    fix #799
    Add `'luadoc'`, to the `ensure_installed` of `nvim-treesitter/nvim-treesitter`

--- init.lua
+++ init.lua
@@ -791,7 +791,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'html', 'lua', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
