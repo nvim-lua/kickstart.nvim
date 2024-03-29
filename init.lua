53dbdf9dc0d085baeeab69c49d9ca2ebf2dd0512
Jonas Hagberg <jonas.hagberg@edgeware.tv>
2024-03-29 14:51:08 +0100

    Add vim-delve plugin

--- init.lua
+++ init.lua
@@ -817,6 +817,9 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+      'sebdah/vim-delve',
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
