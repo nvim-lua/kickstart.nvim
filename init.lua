1175f6d25a84146fda2eb16236d2a21ae30cc7b1
Damjan 9000 <damjan.9000@gmail.com>
2024-03-31 19:36:43 +0200

    Add a keymap space-f to format buffer using conform (#817)
    This works also for visual range selection
    Copied from conform recipe:
    https://github.com/stevearc/conform.nvim/blob/master/doc/recipes.md

--- init.lua
+++ init.lua
@@ -600,6 +600,16 @@ require('lazy').setup({
 
   { -- Autoformat
     'stevearc/conform.nvim',
+    keys = {
+      {
+        '<leader>f',
+        function()
+          require('conform').format { async = true, lsp_fallback = true }
+        end,
+        mode = '',
+        desc = '[F]ormat buffer',
+      },
+    },
     opts = {
       notify_on_error = false,
       format_on_save = function(bufnr)
