08dc9f4ca57087209a7a2a1d0637529b6855b1a1
Christoph Wagner <christoph.c.wagner@bmw.de>
2024-03-31 22:42:57 +0200

    back to null ls

--- lua/linter/plugins/nonels.lua
+++ lua/linter/plugins/nonels.lua
@@ -1,13 +1,13 @@
 return {
   {
-    'nvimtools/none-ls.nvim',
+    'jose-elias-alvarez/null-ls.nvim',
     config = function()
       local null_ls = require 'null-ls'
       null_ls.setup {
         sources = {
-          null_ls.builtins.diagnostics.pylint,
+          null_ls.builtins.diagnostics.flake8,
           null_ls.builtins.diagnostics.markdownlint,
-          null_ls.builtins.diagnostics.spectral,
+          null_ls.builtins.diagnostics.jsonlint,
           -- null_ls.builtins.completion.spell,
         },
       }
