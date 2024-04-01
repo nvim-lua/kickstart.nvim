aa4afdfbed08842edc28202c49da340b2a71692e
tsorabel <tom.sorabella@gmail.com>
2024-04-01 19:55:40 +0200

    feat: rm mdlint

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,7 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
+        -- markdown = { 'markdownlint' },
         typescript = { 'eslint_d' },
         javascript = { 'eslint_d' },
         typescriptreact = { 'eslint_d' },
