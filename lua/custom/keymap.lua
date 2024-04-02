e0ae1668c1a586eff9f6db65b39f49b267ee18e9
k-knosala <k.knosala@fz-juelich.de>
2024-04-02 10:13:23 +0200

    Add compile markdown

--- lua/custom/keymap.lua
+++ lua/custom/keymap.lua
@@ -1,25 +1,38 @@
 -- Custom keychains
 require('which-key').register {
-    ['<leader>p'] = { name = '[P]ython', _ = 'which_key_ignore' },
+  ['<leader>p'] = { name = '[P]ython', _ = 'which_key_ignore' },
 }
 -- Undo
-vim.keymap.set("n", "<leader>su", require("telescope").extensions.undo.undo, { desc = '[S]earch [U]ndo' })
+vim.keymap.set('n', '<leader>su', require('telescope').extensions.undo.undo, { desc = '[S]earch [U]ndo' })
 
 -- Lazygit
-vim.keymap.set("n", "<leader>gg", ":LazyGit<CR>", { desc = "[G]it Lazy[G]it" })
+vim.keymap.set('n', '<leader>gg', ':LazyGit<CR>', { desc = '[G]it Lazy[G]it' })
 
 -- Debug
-vim.keymap.set({ "n", "v" }, "<Leader>pm", function() require("dap-python").test_method() end,
-    { desc = '[P]ython Debug [M]ethod' })
-vim.keymap.set({ "n", "v" }, "<Leader>pc", function() require("dap-python").test_class() end,
-    { desc = '[P]ython Debug [C]lass' })
-vim.keymap.set("v", "<Leader>k", function() require("dapui").eval() end, { desc = "Debug: Eval" })
+vim.keymap.set({ 'n', 'v' }, '<Leader>pm', function()
+  require('dap-python').test_method()
+end, { desc = '[P]ython Debug [M]ethod' })
+vim.keymap.set({ 'n', 'v' }, '<Leader>pc', function()
+  require('dap-python').test_class()
+end, { desc = '[P]ython Debug [C]lass' })
+vim.keymap.set('v', '<Leader>k', function()
+  require('dapui').eval()
+end, { desc = 'Debug: Eval' })
 
 -- Navigate buffers
-vim.keymap.set("n", "<leader>l", ":ls<CR>:b<space>", { desc = "Select buffer" })
+vim.keymap.set('n', '<leader>l', ':ls<CR>:b<space>', { desc = 'Select buffer' })
 
 -- Make
-vim.keymap.set("n", "<leader>m", ":make <CR>", { desc = "[M]ake" })
+vim.keymap.set('n', '<leader>m', ':make <CR>', { desc = '[M]ake' })
 
 -- Telescope undo
-vim.keymap.set("n", "<leader>su", require("telescope").extensions.undo.undo, { desc = '[S]earch [U]ndo' })
+vim.keymap.set('n', '<leader>su', require('telescope').extensions.undo.undo, { desc = '[S]earch [U]ndo' })
+
+-- Compile markdown to html with pandoc
+vim.keymap.set(
+  'n',
+  '<C-H>',
+  ":! pandoc '%:p' -o /tmp/'%:p:t'.html --template=easy_template.html --toc && /mnt/c/Program\\  Files/Mozilla\\ Firefox/ /tmp/'%:p:t'.html &<CR><CR>",
+  { desc = 'Build [M]arkdown to HTML' }
+)
+--
