b6c59cba05b7558b32385843a90ff4dfe421babc
pvilje <patrik.viljehav@gmail.com>
2024-04-03 22:14:36 +0200

    start of kali branch

---
+++ lua/custom/plugins/filetree.lua
@@ -0,0 +1,14 @@
+-- File: lua/custom/plugins/filetree.lua
+
+return {
+  'nvim-neo-tree/neo-tree.nvim',
+  version = '*',
+  dependencies = {
+    'nvim-lua/plenary.nvim',
+    'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
+    'MunifTanjim/nui.nvim',
+  },
+  config = function()
+    require('neo-tree').setup {}
+  end,
+}
