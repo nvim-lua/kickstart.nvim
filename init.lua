66f931e9343eac0aa2fe5bc3f4d8233cd6b3060a
Phillip Nguyen <philmein23@Phil-Nguyen-V3C6QT57TT.local>
2024-04-04 04:00:31 +0700

    update config

--- init.lua
+++ init.lua
@@ -1024,6 +1024,35 @@ require('lazy').setup({
       { '<leader>5', '<cmd>Grapple select index=5<cr>', desc = 'Grapple select 5' },
     },
   },
+  {
+    'folke/trouble.nvim',
+    dependencies = { 'nvim-tree/nvim-web-devicons' },
+    config = function()
+      vim.keymap.set('n', '<leader>xx', function()
+        require('trouble').toggle()
+      end)
+      vim.keymap.set('n', '<leader>xw', function()
+        require('trouble').toggle 'workspace_diagnostics'
+      end)
+      vim.keymap.set('n', '<leader>xd', function()
+        require('trouble').toggle 'document_diagnostics'
+      end)
+      vim.keymap.set('n', '<leader>xq', function()
+        require('trouble').toggle 'quickfix'
+      end)
+      vim.keymap.set('n', '<leader>xl', function()
+        require('trouble').toggle 'loclist'
+      end)
+      vim.keymap.set('n', 'gR', function()
+        require('trouble').toggle 'lsp_references'
+      end)
+    end,
+    opts = {
+      -- your configuration comes here
+      -- or leave it empty to use the default settings
+      -- refer to the configuration section below
+    },
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
