bb3d6b9bfb604cde3ba98ced464812a34f94eb68
Max <55395652+kontr0x@users.noreply.github.com>
2024-04-05 18:28:46 +0000

    Change lsp confirm key to <cr>

--- init.lua
+++ init.lua
@@ -616,7 +616,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<cr>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
