e44652ec12f68f7f620873142a696dceef83a1d6
trevor <trevor@trevg.xyz>
2024-04-07 08:54:31 -0600

    add clanformat to compe

--- init.lua
+++ init.lua
@@ -625,6 +625,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        cpp = { 'clang-format' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
         --
