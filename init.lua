f851ea5ff21fbea62e4a37da1f445930d6c5b262
Hendra <hendra2392@gmail.com>
2024-04-10 14:07:33 +0700

    Relative num

--- init.lua
+++ init.lua
@@ -72,7 +72,7 @@ vim.opt.number = true
 
 -- You can also add relative line numbers, for help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
