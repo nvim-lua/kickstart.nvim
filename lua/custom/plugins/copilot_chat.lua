3ec08eea458c022f4ec728c18b8ae1c75b89daf3
Mark Gladkikh <17348049+sockcrates@users.noreply.github.com>
2024-04-14 13:30:00 -0400

    Update lazy-lock

--- lua/custom/plugins/copilot_chat.lua
+++ lua/custom/plugins/copilot_chat.lua
@@ -8,4 +8,22 @@ return {
     },
     opts = {},
   },
+  keys = {
+    {
+      "<leader>cch",
+      function()
+        local actions = require("CopilotChat.actions")
+        require("CopilotChat.integrations.telescope").pick(actions.help_actions())
+      end,
+      desc = "CopilotChat - Help actions",
+    },
+    {
+      "<leader>ccp",
+      function()
+        local actions = require("CopilotChat.actions")
+        require("CopilotChat.integrations.telescope").pick(actions.prompt_actions())
+      end,
+      desc = "CopilotChat - Prompt actions",
+    },
+  }
 }
