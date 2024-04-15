5ca867e7e50931af3b3ac02bb94386773662b6de
MysterieDev <josef.slezak@huk-coburg.de>
2024-04-15 10:02:01 +0200

    update terminal settings

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -3,9 +3,7 @@ return {
   version = '*',
   init = function()
     -- Toggleterm Plugin
-    vim.keymap.set('n', '<leader>t', '<Cmd>1ToggleTerm name="floater" direction="float"<CR>', { desc = '[T]oggle [T]erminal' })
-    vim.keymap.set('n', '<leader>tt', '<Cmd>2ToggleTerm name="horizont" direction="horizontal"<CR>', { desc = '[T]oggle [T]erminal' })
-    vim.keymap.set('n', '<leader>tl', '<Cmd>TermSelect<CR>', { desc = '[T]oggle [T]erminal' })
+    vim.keymap.set('n', '<leader>tt', '<Cmd>1ToggleTerm name="floater" direction="float"<CR>', { desc = '[T]oggle [T]erminal' })
   end,
   config = true,
 }
