c1383b1fe60aa8e45920f5c197e01a01c72efc8d
Jonas Hagberg <jonas.hagberg@agilecontent.com>
2024-04-16 13:49:00 +0200

    Disable displaying whitespace

--- init.lua
+++ init.lua
@@ -142,8 +142,8 @@ vim.opt.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
-vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.list = true
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
