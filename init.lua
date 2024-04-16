9400c355f4046d64a4c7194ed1de56d0a9ab1fa3
luba <luciano.eq@outlook.com>
2024-04-16 08:08:11 -0300

    changes

--- init.lua
+++ init.lua
@@ -903,6 +903,7 @@ require('lazy').setup({
     end,
   },
   { 'christoomey/vim-tmux-navigator' },
+  -- { 'lukas-reineke/indent-blankline.nvim', main = 'ibl', opts = {} },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
