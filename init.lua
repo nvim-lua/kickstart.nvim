70e24df473d9aaed30564db3205f7c396848e4c8
Jonas Hagberg <jonas.hagberg@agilecontent.com>
2024-04-17 11:43:16 +0200

    Add tpope/vim-fugitive

--- init.lua
+++ init.lua
@@ -839,6 +839,9 @@ require('lazy').setup({
   {
     'github/copilot.vim',
   },
+  {
+    'tpope/vim-fugitive',
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
