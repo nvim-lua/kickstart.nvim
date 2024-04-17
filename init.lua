9ddac5a2f168dd9d5839d9ef2195e8748fa86ad2
Chris Patti <feoh@feoh.org>
2024-04-17 11:25:13 -0400

    Fix: #847
    This PR configures Treesitter to prefer git rather than expecting curl
    to be installed. This prevevents a failure mode on WSL where curl is
    optional.

--- init.lua
+++ init.lua
@@ -817,6 +817,7 @@ require('lazy').setup({
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
+      require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
 
