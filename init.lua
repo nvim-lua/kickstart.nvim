dfa17c7189137344eeb809b8bcca7ff0f310e8d7
Brad <flex.strongo@gmail.com>
2024-04-17 07:03:14 -0400

    Add oilnvim

--- init.lua
+++ init.lua
@@ -175,6 +175,9 @@ vim.opt.scrolloff = 10
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
+-- oil.nvim keyman
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+
 -- Set highlight on search, but clear on pressing <Esc> in normal mode
 vim.opt.hlsearch = true
 vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
