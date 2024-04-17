6d4dabe8f6dcec0af1509e0cf5ef5fbb95779d43
Chris Patti <feoh@feoh.org>
2024-04-17 14:02:24 -0400

    Fix: #847 - add prefer_git to treesitter config (#856)

--- init.lua
+++ init.lua
@@ -817,6 +817,8 @@ require('lazy').setup({
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
 
+      -- Prefer git instead of curl in order to improve connectivity in some environments
+      require('nvim-treesitter.install').prefer_git = true
       ---@diagnostic disable-next-line: missing-fields
       require('nvim-treesitter.configs').setup(opts)
 
