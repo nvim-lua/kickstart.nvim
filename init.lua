5e258d276fef52cc45a17021dc83a95748a0bc7f
Damjan 9000 <damjan.9000@gmail.com>
2024-04-17 21:25:54 +0200

    Move plugin examples from README to optional plugin files (#831)
    * Move autopairs example from README to an optional plugin
    
    * Move neo-tree example from README to an optional plugin

--- init.lua
+++ init.lua
@@ -854,6 +854,8 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
+  -- require 'kickstart.plugins.autopairs',
+  -- require 'kickstart.plugins.neo-tree',
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
