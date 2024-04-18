f5c963316d26212f6d728a760b6670f606241b27
Jonas Hagberg <jonas.hagberg@agilecontent.com>
2024-04-18 14:58:55 +0200

    Bind § to <Esc> in insert mode

--- init.lua
+++ init.lua
@@ -194,6 +194,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Bind § to <Esc> in insert mode
+vim.keymap.set('i', '§', '<Esc>')
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
