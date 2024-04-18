644ac32da0bcfb3667be444d753e0a23c58f1291
Brad <flex.strongo@gmail.com>
2024-04-18 08:07:46 -0400

    rose pine

--- init.lua
+++ init.lua
@@ -772,7 +772,8 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'rose-pine'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
