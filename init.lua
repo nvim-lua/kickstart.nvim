da00a1000dc444e7fee263af625ca217ccddd06c
GameFuzzy <mail@gamefuzzy.dev>
2024-04-19 19:50:42 +0200

    fix: restore Mason config timing for DAP startup (again) (#865)

--- init.lua
+++ init.lua
@@ -412,7 +412,7 @@ require('lazy').setup({
     'neovim/nvim-lspconfig',
     dependencies = {
       -- Automatically install LSPs and related tools to stdpath for Neovim
-      'williamboman/mason.nvim',
+      { 'williamboman/mason.nvim', config = true }, -- NOTE: Must be loaded before dependants
       'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
