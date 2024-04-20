cf28db3e22d40026ef41aecc52545fd56a254dbe
Damjan 9000 <damjan.9000@gmail.com>
2024-04-20 19:14:49 +0200

    Add a commented out example of the classic complete keymaps. Fixes #866 (#868)

--- init.lua
+++ init.lua
@@ -720,6 +720,12 @@ require('lazy').setup({
           --  This will expand snippets if the LSP sent a snippet.
           ['<C-y>'] = cmp.mapping.confirm { select = true },
 
+          -- If you prefer more traditional completion keymaps,
+          -- you can uncomment the following lines
+          --['<CR>'] = cmp.mapping.confirm { select = true },
+          --['<Tab>'] = cmp.mapping.select_next_item(),
+          --['<S-Tab>'] = cmp.mapping.select_prev_item(),
+
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
           --  completions whenever it has completion options available.
