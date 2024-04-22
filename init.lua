7464adef9beba4b16ceb885f2d2ee2b9c65281fa
Francis Belanger <francis.belanger@gmail.com>
2024-04-22 15:53:45 -0400

    fix: highlight group clear on each attach (#874)

--- init.lua
+++ init.lua
@@ -514,7 +514,7 @@ require('lazy').setup({
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.server_capabilities.documentHighlightProvider then
-            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = true })
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = false })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
               group = highlight_augroup,
