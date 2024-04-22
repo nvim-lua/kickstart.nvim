c4383c4ce944cb86eea2cb8875e21185910897ba
Vladislav Grechannik <52157081+VlaDexa@users.noreply.github.com>
2024-04-22 13:27:13 +0200

    Fix deprecation notice of inlay hints (#873)

--- init.lua
+++ init.lua
@@ -531,7 +531,7 @@ require('lazy').setup({
           -- This may be unwanted, since they displace some of your code
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
             map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(0, not vim.lsp.inlay_hint.is_enabled())
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
             end, '[T]oggle Inlay [H]ints')
           end
         end,
