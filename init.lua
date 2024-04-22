8f6249401118c20ec992898e6193a6cb21116a7e
Francis Belanger <francis.belanger@gmail.com>
2024-04-22 11:43:10 -0400

    Fix highlight errors when lsp crash or stop (#864)
    * Fix highlight errors when lsp crash or stop
    
    It adds a check wether the client is still available before
    highlighting.
    
    If the client is not there anymore it returns `true` to unregister the
    autocommand
    
    This fix the
    `method textDocument/documentHighlight is not supported by any of the servers registered for the current buffer`
    errors when doing a LspRestart or the server crashes
    
    * Delete the highlight autocommands in the LspDetatch event
    
    * Only delete autocmds for the current buffer with the group name
    
    * Simplify clearing the autocommands
    
    ---------
    
    Co-authored-by: Francis Belanger <francis.belanger@ubisoft.com>

--- init.lua
+++ init.lua
@@ -514,13 +514,16 @@ require('lazy').setup({
           -- When you move your cursor, the highlights will be cleared (the second autocommand).
           local client = vim.lsp.get_client_by_id(event.data.client_id)
           if client and client.server_capabilities.documentHighlightProvider then
+            local highlight_augroup = vim.api.nvim_create_augroup('kickstart-lsp-highlight', { clear = true })
             vim.api.nvim_create_autocmd({ 'CursorHold', 'CursorHoldI' }, {
               buffer = event.buf,
+              group = highlight_augroup,
               callback = vim.lsp.buf.document_highlight,
             })
 
             vim.api.nvim_create_autocmd({ 'CursorMoved', 'CursorMovedI' }, {
               buffer = event.buf,
+              group = highlight_augroup,
               callback = vim.lsp.buf.clear_references,
             })
           end
@@ -534,6 +537,14 @@ require('lazy').setup({
               vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
             end, '[T]oggle Inlay [H]ints')
           end
+        end,
+      })
+
+      vim.api.nvim_create_autocmd('LspDetach', {
+        group = vim.api.nvim_create_augroup('kickstart-lsp-detach', { clear = true }),
+        callback = function(event)
+          vim.lsp.buf.clear_references()
+          vim.api.nvim_clear_autocmds { group = 'kickstart-lsp-highlight', buffer = event.buf }
         end,
       })
 
