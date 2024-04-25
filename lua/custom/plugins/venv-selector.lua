8ff9c94f8f96bf58d12450e53cb87c11fe95b8c6
Fernando Muñoz Paredes <fernando.munoz@madisonmk.com>
2024-04-25 16:35:26 +0200

    Add virtualenv ui selector

---
+++ lua/custom/plugins/venv-selector.lua
@@ -0,0 +1,16 @@
+return {
+  'linux-cultist/venv-selector.nvim',
+  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+  opts = {
+    -- Your options go here
+    -- name = "venv",
+    -- auto_refresh = false
+  },
+  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    -- Keymap to open VenvSelector to pick a venv.
+    { '<leader>vs', '<cmd>VenvSelect<cr>', desc = "[V]irtualenv [S]elect" },
+    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+    { '<leader>vc', '<cmd>VenvSelectCached<cr>', desc = "[V]irtualenv select [C]ached" },
+  },
+}
