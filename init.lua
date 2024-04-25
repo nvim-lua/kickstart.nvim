882c6233855518258ec7f88341503d26623ba5db
Fernando Muñoz Paredes <fernando.munoz@madisonmk.com>
2024-04-25 16:35:10 +0200

    Add pyright and taplo to install automatic

--- init.lua
+++ init.lua
@@ -557,7 +557,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
+        taplo = {}, -- LSP for toml files
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
