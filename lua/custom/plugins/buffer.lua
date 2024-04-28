45bb16f283f719d0f10be578887150129b388470
ZaneFerns360 <zane.ferns360@gmail.com>
2024-04-28 10:03:05 +0530

    changes

--- lua/custom/plugins/buffer.lua
+++ lua/custom/plugins/buffer.lua
@@ -21,11 +21,6 @@ local buffers = {
       right_mouse_command = function(n) require("mini.bufremove").delete(n, false) end,
         diagnostics = 'nvim_lsp',
         always_show_bufferline = false,
-        diagnostics_indicator = function(_, _, diag)
-          local icons = require('lazyvim.config').icons.diagnostics
-          local ret = (diag.error and icons.Error .. diag.error .. ' ' or '') .. (diag.warning and icons.Warn .. diag.warning or '')
-          return vim.trim(ret)
-        end,
         offsets = {
           {
             filetype = 'neo-tree',
---
+++ lua/custom/plugins/dashboard.lua
@@ -0,0 +1,53 @@
+local dash = {
+  {
+    'nvimdev/dashboard-nvim',
+    event = 'VimEnter',
+    opts = function()
+      local logo = [[
+         ██╗      █████╗ ███████╗██╗   ██╗██╗   ██╗██╗███╗   ███╗          Z
+         ██║     ██╔══██╗╚══███╔╝╚██╗ ██╔╝██║   ██║██║████╗ ████║      Z    
+         ██║     ███████║  ███╔╝  ╚████╔╝ ██║   ██║██║██╔████╔██║   z       
+         ██║     ██╔══██║ ███╔╝    ╚██╔╝  ╚██╗ ██╔╝██║██║╚██╔╝██║ z         
+         ███████╗██║  ██║███████╗   ██║    ╚████╔╝ ██║██║ ╚═╝ ██║           
+         ╚══════╝╚═╝  ╚═╝╚══════╝   ╚═╝     ╚═══╝  ╚═╝╚═╝     ╚═╝           
+    ]]
+
+      logo = string.rep('\n', 8) .. logo .. '\n\n'
+
+      local opts = {
+        theme = 'doom',
+        hide = {
+          -- this is taken care of by lualine
+          -- enabling this messes up the actual laststatus setting after loading a file
+          statusline = false,
+        },
+        config = {
+          header = vim.split(logo, '\n'),
+        -- stylua: ignore
+        center = {
+          { action = "Telescope find_files",                                    desc = " Find File",       icon = " ", key = "f" },
+          { action = "ene | startinsert",                                        desc = " New File",        icon = " ", key = "n" },
+          { action = "Telescope oldfiles",                                       desc = " Recent Files",    icon = " ", key = "r" },
+          { action = "Telescope live_grep",                                      desc = " Find Text",       icon = " ", key = "g" },
+          { action = "Lazy",                                                     desc = " Lazy",            icon = "󰒲 ", key = "l" },
+          { action = "qa",                                                       desc = " Quit",            icon = " ", key = "q" },
+        },
+          footer = function()
+            local stats = require('lazy').stats()
+            local ms = (math.floor(stats.startuptime * 100 + 0.5) / 100)
+            return { '⚡ Neovim loaded ' .. stats.loaded .. '/' .. stats.count .. ' plugins in ' .. ms .. 'ms' }
+          end,
+        },
+      }
+
+      for _, button in ipairs(opts.config.center) do
+        button.desc = button.desc .. string.rep(' ', 43 - #button.desc)
+        button.key_format = '  %s'
+      end
+
+      return opts
+    end,
+  },
+}
+
+return dash
