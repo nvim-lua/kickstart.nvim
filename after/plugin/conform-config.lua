f07fb6cced175463424b0d85406b2babb28d1617
Oluwatobi <bellotobiloba01@gmail.com>
2024-04-30 10:19:08 +0100

    added xml formatter

--- after/plugin/conform-config.lua
+++ after/plugin/conform-config.lua
@@ -8,10 +8,22 @@ require('conform').setup {
     javascriptreact = { { 'prettier' } },
     typescriptreact = { { 'prettier' } },
     typescript = { { 'prettier' } },
+    xml = { 'prettier', 'xmlformat' },
     html = { { 'prettier' } },
     go = { { 'gofumpt', 'goimports-reviser', 'golines' } },
     java = { 'google-java-format' },
     templ = { 'templ' },
+  },
+  format_on_save = {
+    -- I recommend these options. See :help conform.format for details.
+    lsp_fallback = true,
+    timeout_ms = 500,
+  },
+  -- If this is set, Conform will run the formatter asynchronously after save.
+  -- It will pass the table to conform.format().
+  -- This can also be a function that returns the table.
+  format_after_save = {
+    lsp_fallback = true,
   },
 }
 
