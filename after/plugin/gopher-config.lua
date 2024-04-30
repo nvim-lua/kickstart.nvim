b4d295e1e4fb220f5595776ac7223cbad93b0528
Oluwatobi <bellotobiloba01@gmail.com>
2024-04-30 10:19:27 +0100

    updated key bind for go

--- after/plugin/gopher-config.lua
+++ after/plugin/gopher-config.lua
@@ -8,5 +8,8 @@ require('gopher').setup {
   },
 }
 
+vim.keymap.set('n', '<leader>gsv', ':GoTagAdd validate:required', { desc = 'Add validation to struct' })
 vim.keymap.set('n', 'gaj', ':GoTagAdd json<CR>', { desc = 'Add JSON Tag TO Go Struct' })
 vim.keymap.set('n', 'gax', ':GoTagAdd xml<CR>', { desc = 'Add XML Tag TO Go Struct' })
+vim.keymap.set('n', 'gadb', ':GoTagAdd db<CR>', { desc = 'Add DB Tag TO Go Struct' })
+vim.keymap.set('n', '<leader>gmt', ':GoMod tidy', { desc = 'go mod tidy' })
