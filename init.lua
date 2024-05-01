dc8b2f78e06eb65b9e4624d53edbc624aff600a8
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2024-04-30 18:46:17 -0600

    feat: disable mouse

--- init.lua
+++ init.lua
@@ -90,6 +90,9 @@ P.S. You can delete this when you're done too. It's your config now! :)
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
+-- Disable mouse mode
+vim.o.mouse = ''
+
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
 
