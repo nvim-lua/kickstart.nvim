11205d11639f5e4ba939d51b023c469dc189a363
Carlos Hurtado <52082645+carlosahs@users.noreply.github.com>
2024-05-01 12:40:43 -0600

    fix: disable tree-sitter indenting for HTML files

--- init.lua
+++ init.lua
@@ -881,7 +881,7 @@ require('lazy').setup({
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', 'html' } },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
