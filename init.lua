9ebd1fa9c63430cc2aeeefd3c71e9cfe6dfcba90
Juan Miguel Rúa <juanmiguel.rua@wallbox.com>
2024-05-02 17:07:15 +0200

    Changin the indent highlight format

--- init.lua
+++ init.lua
@@ -143,7 +143,8 @@ vim.opt.splitbelow = true
 --  See `:help 'list'`
 --  and `:help 'listchars'`
 vim.opt.list = true
-vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+-- vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
+vim.opt.listchars = { tab = '  ', trail = '·', nbsp = '␣' }
 
 -- Preview substitutions live, as you type!
 vim.opt.inccommand = 'split'
--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -1,9 +1,16 @@
+local highlight = {
+  'ColorColumn',
+  'Whitespace',
+}
+
 return {
   { -- Add indentation guides even on blank lines
     'lukas-reineke/indent-blankline.nvim',
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = { highlight = highlight, char = '' },
+    },
   },
 }
