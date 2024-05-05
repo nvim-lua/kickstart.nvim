59684f664512b3cc4e1b80645a4b3eeebfb55faa
Jorge Benavente <jorge.benavente2003@gmail.com>
2024-05-05 13:07:08 +0200

    Add  autotag

--- init.lua
+++ init.lua
@@ -228,19 +228,19 @@ require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
   'tpope/vim-surround',
-  -- {
-  --   'windwp/nvim-autopairs',
-  --   event = 'InsertEnter',
-  --   config = function()
-  --     require('nvim-autopairs').setup {}
-  --   end,
-  -- },
-  -- {
-  --   'windwp/nvim-ts-autotag',
-  --   config = function()
-  --     require('nvim-ts-autotag').setup {}
-  --   end,
-  -- },
+  {
+    'windwp/nvim-autopairs',
+    event = 'InsertEnter',
+    config = function()
+      require('nvim-autopairs').setup {}
+    end,
+  },
+  {
+    'windwp/nvim-ts-autotag',
+    config = function()
+      require('nvim-ts-autotag').setup {}
+    end,
+  },
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
