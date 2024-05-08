62f99c5a3f25824b680eb588913e0401f7e76330
Smig <89040888+smiggiddy@users.noreply.github.com>
2024-05-08 10:55:49 -0400

    fix: debug.lua (#918)

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -31,7 +31,7 @@ return {
     require('mason-nvim-dap').setup {
       -- Makes a best effort to setup the various debuggers with
       -- reasonable debug configurations
-      automatic_setup = true,
+      automatic_installation = true,
 
       -- You can provide additional configuration to the handlers,
       -- see mason-nvim-dap README for more information
