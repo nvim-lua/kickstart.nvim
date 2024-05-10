74322d795c35c0e5af6d5e3d2f8e0809b0a75567
Per Malmberg <PerMalmberg@users.noreply.github.com>
2024-05-10 19:43:22 +0200

    Automatically set detached state as needed. (#925)
    * Automatically set detached state as needed.
    
    * Use vim.fn.has instead.
    
    * Fix int vs bool.

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -85,6 +85,12 @@ return {
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
 
     -- Install golang specific config
-    require('dap-go').setup()
+    require('dap-go').setup {
+      delve = {
+        -- On Windows delve must be run attached or it crashes.
+        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
+        detached = vim.fn.has 'win32' == 0,
+      },
+    }
   end,
 }
