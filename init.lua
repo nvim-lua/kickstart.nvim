037c895f512cc582d09671747e55c5fe6cd3b8cf
Le Viet Dat <lvdat.roy@gmail.com>
2024-05-12 09:01:31 +0700

    Install Codeium plugin

--- init.lua
+++ init.lua
@@ -737,6 +737,16 @@ require('lazy').setup({
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
   },
+  {
+    'Exafunction/codeium.nvim',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'hrsh7th/nvim-cmp',
+    },
+    config = function()
+      require('codeium').setup {}
+    end,
+  },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
