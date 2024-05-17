ce44ec83447834f5f2e7b02af00c1d22cfd5d0be
smashblu <smashblu@gmail.com>
2024-05-17 14:04:24 -0700

    Configure tokyonight theme with transparency

--- init.lua
+++ init.lua
@@ -714,6 +714,9 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     'folke/tokyonight.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
+    opts = {
+      transparent = true,
+    },
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
