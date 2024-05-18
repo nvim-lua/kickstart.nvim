7fc2f7b0ca2b25b1ba0d4b1ecffc369efd3bfb1d
Noah <73550955+noahapursell@users.noreply.github.com>
2024-05-18 15:06:38 -0500

    Update init.lua to make esc jj

--- init.lua
+++ init.lua
@@ -189,6 +189,10 @@ vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left wind
 vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- NOAH
+-- Remap 'jj' to 'Esc' in insert mode
+vim.api.nvim_set_keymap('i', 'jj', '<Esc>', { noremap = true, silent = true })
+
 
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
