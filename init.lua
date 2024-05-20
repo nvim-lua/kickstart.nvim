22b422fc5e4cb620a786e710c47880ac984493bf
ramintheredmn <redcrossprog@gmail.com>
2024-05-20 14:56:38 +0330

    new changes

--- init.lua
+++ init.lua
@@ -557,8 +557,8 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = { capabilities = capabilities },
 
+        pyright = { capabilities = capabilities },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
---
+++ lua/custom/plugins/venvselector.lua
@@ -0,0 +1,17 @@
+return {
+  'linux-cultist/venv-selector.nvim',
+  dependencies = { 'neovim/nvim-lspconfig', 'nvim-telescope/telescope.nvim', 'mfussenegger/nvim-dap-python' },
+  opts = {
+    -- Your options go here
+    -- name = "venv",
+    -- auto_refresh = false
+    path = '/home/raminrafiee/Desktop/personal/cheminfo/cheminfosoftware/python',
+  },
+  event = 'VeryLazy', -- Optional: needed only if you want to type `:VenvSelect` without a keymapping
+  keys = {
+    -- Keymap to open VenvSelector to pick a venv.
+    { '<leader>vs', '<cmd>VenvSelect<cr>' },
+    -- Keymap to retrieve the venv from a cache (the one previously used for the same project directory).
+    { '<leader>vc', '<cmd>VenvSelectCached<cr>' },
+  },
+}
