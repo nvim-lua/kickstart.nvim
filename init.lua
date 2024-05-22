1ff791946aee76dfc86a77b7d6fbfa11d97865c4
jhe <joergen.henriksen@appear.net>
2024-05-22 16:25:58 +0200

    signcolumn number

--- init.lua
+++ init.lua
@@ -126,7 +126,7 @@ vim.opt.ignorecase = true
 vim.opt.smartcase = true
 
 -- Keep signcolumn on by default
-vim.opt.signcolumn = 'yes'
+vim.opt.signcolumn = 'number'
 
 -- Decrease update time
 vim.opt.updatetime = 250
