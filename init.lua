482e301abad5c0ae44b930e6799c516aa6485d47
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2024-05-24 10:27:58 +0200

    Deactivate mini.surround

--- init.lua
+++ init.lua
@@ -881,7 +881,7 @@ require('lazy').setup({
       -- - saiw) - [S]urround [A]dd [I]nner [W]ord [)]Paren
       -- - sd'   - [S]urround [D]elete [']quotes
       -- - sr)'  - [S]urround [R]eplace [)] [']
-      require('mini.surround').setup()
+      -- require('mini.surround').setup()
 
       -- Simple and easy statusline.
       --  You could remove this setup call if you don't like it,
