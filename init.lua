38faede3c14f96edc49049b4611b9edfa66f6c6c
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2024-05-24 10:45:01 +0200

    Remove gitsigns signs override

--- init.lua
+++ init.lua
@@ -254,13 +254,13 @@ require('lazy').setup({
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
     opts = {
-      signs = {
-        add = { text = '+' },
-        change = { text = '~' },
-        delete = { text = '_' },
-        topdelete = { text = '‾' },
-        changedelete = { text = '~' },
-      },
+      -- signs = {
+      --   add = { text = '+' },
+      --   change = { text = '~' },
+      --   delete = { text = '_' },
+      --   topdelete = { text = '‾' },
+      --   changedelete = { text = '~' },
+      -- },
     },
   },
 
