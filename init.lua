bd6b5905909465afe48e6eb6d1863055c2ab5cd6
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2024-05-24 10:38:49 +0200

    Change leader to ','

--- init.lua
+++ init.lua
@@ -87,8 +87,8 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.mapleader = ','
+vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
