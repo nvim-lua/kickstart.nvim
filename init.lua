8a11022330d608c34456bf34a99cc7bb387be3dd
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2024-05-24 10:30:54 +0200

    Add tpope plugins with default config

--- init.lua
+++ init.lua
@@ -235,6 +235,9 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
+  'tpope/vim-rails', -- Rails integration
+  'tpope/vim-fugitive', -- Support for :Git command
+  'tpope/vim-surround', -- Support for :Git command
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
