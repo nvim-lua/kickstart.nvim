a5458dfb8cab49d880d95892cbb0609f5994ac68
lorran.thierry.david <lorran.thierry.david@accenture.com>
2024-05-26 21:03:52 -0300

    add custom config to kickstarter

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -175,10 +175,10 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
@@ -889,6 +889,15 @@ require('lazy').setup({
         return '%2l:%-2v'
       end
 
+      -- Starter screen
+      require('mini.starter').setup()
+
+      -- Jump to next/previous single character
+      require('mini.jump').setup()
+
+      -- Move any selection in any direction
+      require('mini.move').setup()
+
       -- ... and there is more!
       --  Check out: https://github.com/echasnovski/mini.nvim
     end,
@@ -931,9 +940,9 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,85 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+	{
+		"ThePrimeagen/harpoon",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+		},
+		opts = {
+			global_settings = {
+				save_on_toggle = true,
+				enter_on_sendcmd = true,
+			},
+		},
+		config = function ()
+			local harpoon_ui = require("harpoon.ui")
+			local harpoon_mark = require("harpoon.mark")
+
+			-- Harpoon keybinds --
+			-- Open harpoon ui
+			vim.keymap.set("n", "<C-e>", function()
+				harpoon_ui.toggle_quick_menu()
+			end)
+
+			-- Add current file to harpoon
+			vim.keymap.set("n", "<leader>a", function()
+				harpoon_mark.add_file()
+			end)
+
+			-- Quickly jump to harpooned files
+			vim.keymap.set("n", "<leader>1", function()
+				harpoon_ui.nav_file(1)
+			end)
+
+			vim.keymap.set("n", "<leader>2", function()
+				harpoon_ui.nav_file(2)
+			end)
+
+			vim.keymap.set("n", "<leader>3", function()
+				harpoon_ui.nav_file(3)
+			end)
+
+			vim.keymap.set("n", "<leader>4", function()
+				harpoon_ui.nav_file(4)
+			end)
+
+			vim.keymap.set("n", "<leader>5", function()
+				harpoon_ui.nav_file(5)
+			end)
+		end
+	},
+
+	{
+		'stevearc/oil.nvim',
+		opts = {},
+		dependencies = { "nvim-tree/nvim-web-devicons" },
+		config = function()
+			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<M-h>"] = "actions.select_split",
+				},
+				view_options = {
+					show_hidden = true,
+				},
+			})
+
+			-- Open parent directory in current window
+			vim.keymap.set("n", "-", "<CMD>Oil<CR>",{ desc = "Open parent directory" })
+
+			-- Open parent directory in floating window
+			vim.keymap.set("n", "<space>-", require("oil").toggle_float)
+		end,
+	},
+
+	{
+		'm4xshen/hardtime.nvim',
+		dependecies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+		opts = {
+			restriction_mode = 'hint',
+		},
+	},
+}
