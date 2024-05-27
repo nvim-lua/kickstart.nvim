6150159f1b0a6fd232d25cf23279f33c2d5f926b
Madison <madisonkanna@gmail.com>
2024-05-27 15:19:07 -0700

    add tabstop, shiftwidth and autoindent

--- init.lua
+++ init.lua
@@ -154,6 +154,10 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+vim.opt.autoindent = true
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
