007b51c01774d06cdb39e22fc70eb19818ba61c9
NikitaT <ng.telitsyn@outlook.com>
2024-06-02 00:52:51 +0200

    added remaps from primeagen

--- lua/regularnick/remaps.lua
+++ lua/regularnick/remaps.lua
@@ -19,3 +19,25 @@ vim.keymap.set('n', '<M-/>', ':bn<cr>', { desc = 'Go to the next buffer' })
 vim.keymap.set('n', '<M-,>', ':bp<cr>', { desc = 'Go to the previous buffer' })
 vim.keymap.set('n', '<M-n>', ':enew<cr>', { desc = 'Go to the previous buffer' })
 vim.keymap.set('n', '<M-w>', ':bd<cr>', { desc = 'Go to the previous buffer' })
+
+vim.keymap.set('v', 'J', "m: '>+1<cr>gv=gv")
+vim.keymap.set('v', 'K', "m: '<-2<cr>gv=gv")
+
+vim.keymap.set('n', 'Y', 'yg$')
+vim.keymap.set('n', 'J', 'mzJ`z')
+vim.keymap.set('n', '<C-d>', '<C-d>zz')
+vim.keymap.set('n', '<C-u>', '<C-u>zz')
+vim.keymap.set('n', 'n', 'nzzzv')
+vim.keymap.set('n', 'N', 'Nzzzv')
+
+vim.keymap.set('n', '<leader>p', '"_dP')
+
+vim.keymap.set('n', '<leader>y', '"+y')
+vim.keymap.set('v', '<leader>y', '"+y')
+vim.keymap.set('n', '<leader>Y', '"+Y')
+
+vim.keymap.set('n', '<leader>d', '"_d')
+vim.keymap.set('v', '<leader>d', '"_d')
+
+vim.keymap.set('n', '<leader>s', [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><Left>]])
+vim.keymap.set('n', '<leader>x', '<cmd>!chmod +x %<CR>', { silent = true })
