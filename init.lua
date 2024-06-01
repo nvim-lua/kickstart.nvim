747a9c1832a82ba7b6d8d9c231dab9974aeb4587
Mohammad Moosazadeh <mohammad@jewlr.com>
2024-06-01 01:30:15 -0400

    Add Ruby treesitter

--- init.lua
+++ init.lua
@@ -838,7 +838,7 @@ require('lazy').setup({
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'vim', 'vimdoc', 'ruby' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
