60912931f47365640e9671256018f6c2b86b5772
Mohammad Moosazadeh <mohammad@jewlr.com>
2024-06-01 00:57:53 -0400

    Add Rubocop Autoformatter

--- init.lua
+++ init.lua
@@ -660,6 +660,7 @@ require('lazy').setup({
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
         -- javascript = { { "prettierd", "prettier" } },
+        ruby = { 'rubocop' },
       },
     },
   },
