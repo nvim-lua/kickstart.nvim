ff670efde07e8809636cb602b08b8148ede2646c
Alex Bolotskov <mamoruuu@proton.me>
2024-06-02 20:55:33 +0500

    indents

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,9 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
+
+vim.bo.tabstop = 4
+vim.bo.shiftwidth = 4
+vim.bo.expandtab = false
+
 return {}
