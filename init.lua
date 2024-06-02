fdd4a945e2077472bdce8fd71cdcd5968c0a5bb1
Seb Tomasini <sebt@qgates.com>
2024-06-02 02:56:44 +0100

    Upgrade folke/neodev (sunsetting) to folke/lazydev

--- init.lua
+++ init.lua
@@ -420,9 +420,9 @@ require('lazy').setup({
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
 
-      -- `neodev` configures Lua LSP for your Neovim config, runtime and plugins
+      -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/neodev.nvim', opts = {} },
+      { 'folke/lazydev.nvim', ft = 'lua', opts = {} },
     },
     config = function()
       -- Brief aside: **What is LSP?**
