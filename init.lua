296a271905fd4231c5a3728f7c459e29ef97864e
Mohammad Moosazadeh <mohammad@jewlr.com>
2024-06-02 13:35:32 -0400

    Remove Treesitter autotag (deprecated)

--- init.lua
+++ init.lua
@@ -886,10 +886,6 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
       },
       indent = { enable = true, disable = { 'ruby' } },
-      autotag = {
-        enable = true,
-        filetypes = { 'html', 'xml', 'javascriptreact', 'typescriptreact' },
-      },
     },
     config = function(_, opts)
       -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
