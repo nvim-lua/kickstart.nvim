6a885bd906f37f2fdf6f3e1611e3257980e3d881
Mohammad Moosazadeh <mohammad@jewlr.com>
2024-06-02 02:36:10 -0400

    Change color schema to catppuccin and transparent

--- init.lua
+++ init.lua
@@ -788,24 +788,49 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   -- init = function()
+  --   --   -- Load the colorscheme here.
+  --   --   -- Like many other themes, this one has different styles, and you could load
+  --   --   -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --   --   vim.cmd.colorscheme 'tokyonight-moon'
+  --   --
+  --   --   -- You can configure highlights by doing something like:
+  --   --   vim.cmd.hi 'Comment gui=none'
+  --   -- end,
+  --   config = function()
+  --     require('tokyonight').setup {
+  --       -- your configuration comes here
+  --       -- or leave it empty to use the default settings
+  --       style = 'storm', -- The theme comes in three styles, `storm`, `moon`, a darker variant `night` and `day`
+  --       light_style = 'day', -- The theme is used when the background is set to light
+  --       transparent = true,
+  --     }
+  --
+  --     vim.cmd.colorscheme 'tokyonight'
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
+  {
+    'catppuccin/nvim',
+    name = 'catppuccin',
+    priority = 1000,
+    config = function()
+      require('catppuccin').setup {
+        flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        transparent_background = true,
+      }
 
-      -- You can configure highlights by doing something like:
+      vim.cmd.colorscheme 'catppuccin'
       vim.cmd.hi 'Comment gui=none'
     end,
   },
-
   -- Highlight todo, notes, etc in comments
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
