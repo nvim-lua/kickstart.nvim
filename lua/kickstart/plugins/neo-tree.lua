380d10f747574816d3b0532b8252fa424f974bfa
Sean Villars <sean@setpoint.io>
2024-06-04 15:33:06 -0500

    comment out neo tree instant start

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -14,25 +14,25 @@ return {
     { '\\', ':Neotree reveal<CR>', { desc = 'NeoTree reveal' } },
   },
   -- sourced from here https://github.com/LazyVim/LazyVim/blob/4d706f1bdc687f1d4d4cd962ec166c65c453633e/lua/lazyvim/plugins/editor.lua#L46-L64
-  init = function()
-    -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
-    -- because `cwd` is not set up properly.
-    vim.api.nvim_create_autocmd('BufEnter', {
-      group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
-      desc = 'Start Neo-tree with directory',
-      once = true,
-      callback = function()
-        if package.loaded['neo-tree'] then
-          return
-        else
-          local stats = vim.uv.fs_stat(vim.fn.argv(0))
-          if stats and stats.type == 'directory' then
-            require 'neo-tree'
-          end
-        end
-      end,
-    })
-  end,
+  -- init = function()
+  --   -- FIX: use `autocmd` for lazy-loading neo-tree instead of directly requiring it,
+  --   -- because `cwd` is not set up properly.
+  --   vim.api.nvim_create_autocmd('BufEnter', {
+  --     group = vim.api.nvim_create_augroup('Neotree_start_directory', { clear = true }),
+  --     desc = 'Start Neo-tree with directory',
+  --     once = true,
+  --     callback = function()
+  --       if package.loaded['neo-tree'] then
+  --         return
+  --       else
+  --         local stats = vim.uv.fs_stat(vim.fn.argv(0))
+  --         if stats and stats.type == 'directory' then
+  --           require 'neo-tree'
+  --         end
+  --       end
+  --     end,
+  --   })
+  -- end,
   opts = {
     filesystem = {
       filtered_items = { hide_dotfiles = false },
