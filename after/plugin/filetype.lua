fc3cdeaad9100af2c1216c038a49b3f68d33310c
Gilad Sher <gilad.sher@zoominsoftware.com>
2024-06-11 15:41:47 -0400

    refactor filetype function + add docker compose fix

--- after/plugin/filetype.lua
+++ after/plugin/filetype.lua
@@ -1,6 +1,10 @@
-vim.cmd([[
-  augroup filetypedetect
-    autocmd! BufRead,BufNewFile .swcrc       setfiletype json
-    autocmd! BufRead,BufNewFile .prettierrc  setfiletype json
-  augroup END
-]])
+local function set_filetype(pattern, filetype)
+  vim.api.nvim_create_autocmd({ 'BufRead', 'BufNewFile' }, {
+    pattern = pattern,
+    command = 'set filetype=' .. filetype,
+  })
+end
+
+set_filetype('.swcrc', 'json')
+set_filetype('.prettierrc', 'json')
+set_filetype('docker-compose.yml', 'yaml.docker-compose')
