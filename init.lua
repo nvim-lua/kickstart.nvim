208221eea7d15e9090fc8d1d3474ba6938b99715
syhner <71605633+syhner@users.noreply.github.com>
2024-06-12 11:41:08 +0200

    fix mini.ai example command

--- init.lua
+++ init.lua
@@ -801,7 +801,7 @@ require('lazy').setup({
       --
       -- Examples:
       --  - va)  - [V]isually select [A]round [)]paren
-      --  - yinq - [Y]ank [I]nside [N]ext [']quote
+      --  - yin' - [Y]ank [I]nside [N]ext [']quote
       --  - ci'  - [C]hange [I]nside [']quote
       require('mini.ai').setup { n_lines = 500 }
 
