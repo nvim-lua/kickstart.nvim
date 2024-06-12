ff93f931905a082ef835c26ec18b7045b0cef39e
Mohammad Moosazadeh <mohammad@jewlr.com>
2024-06-12 12:13:46 -0400

    Add barbar plugin

---
+++ lua/custom/plugins/barbar.lua
@@ -0,0 +1,19 @@
+return {
+  {
+    'romgrk/barbar.nvim',
+    dependencies = {
+      'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
+      'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
+    },
+    init = function()
+      vim.g.barbar_auto_setup = false
+    end,
+    opts = {
+      -- lazy.nvim will automatically call setup for you. put your options here, anything missing will use the default:
+      -- animation = true,
+      -- insert_at_start = true,
+      -- …etc.
+    },
+    version = '^1.0.0', -- optional: only update when a new 1.x version is released
+  },
+}
