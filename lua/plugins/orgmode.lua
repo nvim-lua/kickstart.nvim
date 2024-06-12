a6be72da5117e1ae219a83109969b229c4db9f62
marctalcott <marcus.talcott@inmar.com>
2024-06-12 07:02:47 -0400

    Added more Orgmode templates

--- lua/plugins/orgmode.lua
+++ lua/plugins/orgmode.lua
@@ -8,14 +8,62 @@ return {
       org_agenda_files = '~/orgfiles/**/*',
       org_default_notes_file = '~/orgfiles/refile.org',
       org_capture_templates = {
-        t = { description = 'Task', template = '* TODO %?\n  %u' },
         j = {
           description = 'Journal',
           datetree = {
             tree_type = 'day',
           },
-          template = '\n**** %?\n%U\n',
-          target = '~/orgfiles/journal.org',
+          template = '\n**** %?\nEntered on %U\n',
+          target = '~/OrgMode-Inmar/OrgRoamRoot/002-Areas/journal.org',
+        },
+        -- Setup 1:1 capture templates
+        s = {
+          description = 'Sandeep 1:1',
+          datetree = {
+            tree_type = 'day',
+          },
+          template = '**** %?\nEntered on %U\n',
+          target = '~/OrgMode-Inmar/OrgRoamRoot/002-Areas/OneOnOne/sandeep1on1.org',
+        },
+        d = {
+          description = 'Debbie 1:1',
+          datetree = {
+            tree_type = 'day',
+          },
+          template = '**** %?\nEntered on %U\n',
+          target = '~/OrgMode-Inmar/OrgRoamRoot/002-Areas/OneOnOne/debbie1on1.org',
+        },
+        b = {
+          description = 'Billy 1:1',
+          datetree = {
+            tree_type = 'day',
+          },
+          template = '**** %?\nEntered on %U\n',
+          target = '~/OrgMode-Inmar/OrgRoamRoot/002-Areas/OneOnOne/billy1on1.org',
+        },
+        t = {
+          description = 'Taylor 1:1',
+          datetree = {
+            tree_type = 'day',
+          },
+          template = '**** %?\nEntered on %U\n',
+          target = '~/OrgMode-Inmar/OrgRoamRoot/002-Areas/OneOnOne/taylor1on1.org',
+        },
+        a = {
+          description = 'Asha 1:1',
+          datetree = {
+            tree_type = 'day',
+          },
+          template = '**** %?\nEntered on %U\n',
+          target = '~/OrgMode-Inmar/OrgRoamRoot/002-Areas/OneOnOne/asha1on1.org',
+        },
+        g = {
+          description = 'Gerald 1:1',
+          datetree = {
+            tree_type = 'day',
+          },
+          template = '**** %?\nEntered on %U\n',
+          target = '~/OrgMode-Inmar/OrgRoamRoot/002-Areas/OneOnOne/gerald1on1.org',
         },
       },
     }
