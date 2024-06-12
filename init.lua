07193009544b4fbc999084830c998a48cd0c3362
Mohammad Moosazadeh <mohammad@jewlr.com>
2024-06-12 07:17:57 -0400

    Disable transparent background

--- init.lua
+++ init.lua
@@ -823,8 +823,8 @@ require('lazy').setup({
     priority = 1000,
     config = function()
       require('catppuccin').setup {
-        flavour = 'mocha', -- latte, frappe, macchiato, mocha
-        transparent_background = true,
+        -- flavour = 'mocha', -- latte, frappe, macchiato, mocha
+        -- transparent_background = true,
       }
 
       vim.cmd.colorscheme 'catppuccin'
