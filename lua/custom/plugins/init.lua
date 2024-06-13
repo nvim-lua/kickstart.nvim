b6ce30ed1e88ad3598792cf04c5a37c8a989e465
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-06-13 21:47:03 +0530

    fix: descriptions

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -15,13 +15,13 @@ return {
       function _lazygit_toggle()
         lazygit:toggle()
       end
-      vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>gg', '<cmd>lua _lazygit_toggle()<CR>', { noremap = true, silent = true, desc = 'Toggle Lazygit' })
 
       local terminal = Terminal:new { hidden = true }
       function _terminal_toggle()
         terminal:toggle()
       end
-      vim.api.nvim_set_keymap('n', '<leader>gt', '<cmd>lua _terminal_toggle()<CR>', { noremap = true, silent = true })
+      vim.api.nvim_set_keymap('n', '<leader>gt', '<cmd>lua _terminal_toggle()<CR>', { noremap = true, silent = true, desc = 'Toggle Terminal' })
     end,
   },
 }
