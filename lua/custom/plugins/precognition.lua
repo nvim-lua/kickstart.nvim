a3646ee1d56371f58f6db334ef8373e212774387
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-06-12 22:10:28 -0400

    configure precognition

---
+++ lua/custom/plugins/precognition.lua
@@ -0,0 +1,27 @@
+return {
+  'tris203/precognition.nvim',
+  --event = "VeryLazy",
+  config = {
+    -- startVisible = true,
+    -- showBlankVirtLine = true,
+    -- highlightColor = { link = "Comment" },
+    -- hints = {
+    --      Caret = { text = "^", prio = 2 },
+    --      Dollar = { text = "$", prio = 1 },
+    --      MatchingPair = { text = "%", prio = 5 },
+    --      Zero = { text = "0", prio = 1 },
+    --      w = { text = "w", prio = 10 },
+    --      b = { text = "b", prio = 9 },
+    --      e = { text = "e", prio = 8 },
+    --      W = { text = "W", prio = 7 },
+    --      B = { text = "B", prio = 6 },
+    --      E = { text = "E", prio = 5 },
+    -- },
+    -- gutterHints = {
+    --     G = { text = "G", prio = 10 },
+    --     gg = { text = "gg", prio = 9 },
+    --     PrevParagraph = { text = "{", prio = 8 },
+    --     NextParagraph = { text = "}", prio = 8 },
+    -- },
+  },
+}
