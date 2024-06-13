4d9e54d6d68a5d1a100a4be35485bc517317acf7
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-06-13 18:32:22 -0400

    add obsidian md (to be configured)

--- init.lua
+++ init.lua
@@ -825,5 +825,7 @@ require('lazy').setup({
   },
 })
 
+vim.opt_global.conceallevel = 1
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/markdown.lua
+++ lua/custom/plugins/markdown.lua
@@ -1,8 +1,8 @@
 return {
-  -- 'MeanderingProgrammer/markdown.nvim',
-  -- name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
-  -- dependencies = { 'nvim-treesitter/nvim-treesitter' },
-  -- config = function()
-  --   require('render-markdown').setup {}
-  -- end,
+  'MeanderingProgrammer/markdown.nvim',
+  name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  config = function()
+    require('render-markdown').setup {}
+  end,
 }
