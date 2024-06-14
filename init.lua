14c04fc93b670e26cf09a4dbef6c41ec15fb258c
Abhinav Robinson <abhinavrobinson@gmail.com>
2024-06-14 17:47:37 +0530

    fix: errors

--- init.lua
+++ init.lua
@@ -546,7 +546,7 @@ require('lazy').setup({
           -- This may be unwanted, since they displace some of your code
           if client and client.server_capabilities.inlayHintProvider and vim.lsp.inlay_hint then
             map('<leader>th', function()
-              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled())
+              vim.lsp.inlay_hint.enable(not vim.lsp.inlay_hint.is_enabled {})
             end, '[T]oggle Inlay [H]ints')
           end
         end,
--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,6 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
-        markdown = { 'markdownlint' },
         typescript = { 'eslint' },
         javascript = { 'eslint' },
       }
