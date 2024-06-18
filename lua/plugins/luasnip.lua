397bdb7f24be8325d3579dd2d8a85d43ad0d9446
jhe <joergen.henriksen@appear.net>
2024-06-18 09:51:23 +0200

    luasnip added

---
+++ lua/plugins/luasnip.lua
@@ -0,0 +1,7 @@
+return {
+  'L3MON4D3/LuaSnip',
+  -- follow latest release.
+  version = 'v2.*', -- Replace <CurrentMajor> by the latest released major (first number of latest release)
+  -- install jsregexp (optional!).
+  build = 'make install_jsregexp',
+}
