9caa98cf08b25acb33fe2cc5a04441cbd7b5fb6d
Oséias K. Rocha <orocha@ael.com.br>
2024-06-18 17:06:31 -0300

    Adding hardtime.nvim
    Signed-off-by: Oséias K. Rocha <orocha@ael.com.br>

--- init.lua
+++ init.lua
@@ -885,6 +885,11 @@ require('lazy').setup({
       --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
+  {
+    'm4xshen/hardtime.nvim',
+    dependencies = { 'MunifTanjim/nui.nvim', 'nvim-lua/plenary.nvim' },
+    opts = {},
+  },
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
