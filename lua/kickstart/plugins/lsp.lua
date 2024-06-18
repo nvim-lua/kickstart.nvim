db15bab6927023c5acd3660f2e2f9832991c3e6a
peturparkur <peter@nagymathe.xyz>
2024-06-18 09:43:44 +0100

    whoops forgot the separator

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -171,7 +171,7 @@ return { -- LSP Configuration & Plugins
                     },
                 },
             },
-        }
+        },
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
