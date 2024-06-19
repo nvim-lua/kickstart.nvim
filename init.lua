6aecaa4403c599a39653e2867c76b7a7e5f58566
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-06-19 14:52:03 -0400

    remove git hunk

--- init.lua
+++ init.lua
@@ -199,12 +199,7 @@ require('lazy').setup({
         ['<leader>s'] = { name = '[S]earch', _ = 'which_key_ignore' },
         ['<leader>w'] = { name = '[W]orkspace', _ = 'which_key_ignore' },
         ['<leader>t'] = { name = '[T]oggle', _ = 'which_key_ignore' },
-        ['<leader>h'] = { name = 'Git [H]unk', _ = 'which_key_ignore' },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
