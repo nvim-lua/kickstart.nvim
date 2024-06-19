d5aaa05aca33da76c742bea1a9a3cb5231b8130e
Jack Koskie <65452167+jackkoskie@users.noreply.github.com>
2024-06-19 14:52:30 -0400

    fix oil hidden files

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -4,7 +4,19 @@ return {
   -- Optional dependencies
   dependencies = { 'nvim-tree/nvim-web-devicons' },
   config = function()
-    require('oil').setup()
+    require('oil').setup {
+      skip_confirm_for_simple_edits = true,
+      delete_to_trash = true,
+      view_options = {
+        show_hidden = true,
+        is_always_hidden = function(name)
+          if name == '.git' then
+            return true
+          end
+          return false
+        end,
+      },
+    }
     vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
   end,
 }
