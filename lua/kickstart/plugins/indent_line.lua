5ca470d28e3a603f3d9a511a018c8f135c590a17
Aaron Weinberger <aw9994@cs.ship.edu>
2024-06-20 18:38:34 -0400

    Prevent indent-blankline from being applied unnecessarily to UI elements

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -5,5 +5,16 @@ return {
     -- See `:help ibl`
     main = 'ibl',
     opts = {},
+    config = function()
+      require('ibl').setup({
+        exclude = {
+          buftypes = {'NvimTree', 'terminal', 'term', 'dashboard', 'gitcommit', 'fugitive'},
+          filetypes = {'NvimTree', 'terminal', 'term', 'dashboard', 'gitcommit', 'fugitive'}
+        },
+      -- context_patterns = {'class', 'function', 'method', 'block', 'list_literal', 'selector', '^if', '^table', 'if_statement', 'while', 'for', 'object', 'start_tag', 'open_tag', 'element'},
+      -- show_first_indent_level = true,
+      -- show_trailing_blankline_indent = false,
+      })
+    end,
   },
 }
