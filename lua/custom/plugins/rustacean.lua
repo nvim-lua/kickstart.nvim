bee3e36677ec17dfdc16548f5e8928a41dedd71c
Aaron Weinberger <aw9994@cs.ship.edu>
2024-06-20 18:17:23 -0400

    Add rustacean.nvim and dap plugins

---
+++ lua/custom/plugins/rustacean.lua
@@ -0,0 +1,9 @@
+return {
+  'mrcjkb/rustaceanvim',
+  version = '^4', -- Recommended
+  lazy = false, -- This plugin is already lazy
+}
+
+-- nvim-lspconfig.rust_analyzer has been setup.
+-- This will likely lead to conflicts with the rustaceanvim LSP client.
+-- See ':h rustaceanvim.mason'
