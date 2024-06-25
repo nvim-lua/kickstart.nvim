7c958943130468ad7e51c1a5241fbb5fdd29c147
TheSoeren <imhofabian@gmail.com>
2024-06-25 22:35:43 +0200

    remove clashing key binds

--- lua/remap.lua
+++ lua/remap.lua
@@ -47,8 +47,6 @@ vim.keymap.set('x', '<leader>p', '"_dP')
 vim.keymap.set('n', '<leader>d', '"_d')
 vim.keymap.set('v', '<leader>d', '"_d')
 
-vim.keymap.set('n', '<C-k>', '<cmd>cnext<CR>zz')
-vim.keymap.set('n', '<C-j>', '<cmd>cprev<CR>zz')
 vim.keymap.set('n', '<leader>k', '<cmd>lnext<CR>zz')
 vim.keymap.set('n', '<leader>j', '<cmd>lprev<CR>zz')
 
