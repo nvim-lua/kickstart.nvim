fa69facd52e82e9c9db01ac0d70f73003e0fc7f6
Yaser Alraddadi <gam.phon@gmail.com>
2024-06-25 09:10:51 +0300

    Add Neogit

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,24 @@
+return {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      'nvim-telescope/telescope.nvim', -- optional
+    },
+    config = true,
+  },
+
+  --{
+  --	"simrat39/rust-tools.nvim",
+  --	ft = "rust",
+  --	dependencies = "neovim/nvim-lspconfig",
+  --	opts = function()
+  --		return require "custom.configs.rust-tools"
+  --	end,
+  --	config = function(_, opts)
+  --		require("rust-tools").setup(opts)
+  --	end,
+  --},
+}
