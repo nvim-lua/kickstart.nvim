649367e357bc8e9f8ee25a8d559bc4763712e7b9
Juan Miguel Rúa <juanmiguel.rua@wallbox.com>
2024-06-27 21:43:27 +0200

    Refactor the init.lua file

---
+++ lua/custom/plugins/gitsigns.lua
@@ -0,0 +1,19 @@
+return {
+  -- Here is a more advanced example where we pass configuration
+  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
+  --    require('gitsigns').setup({ ... })
+  --
+  -- See `:help gitsigns` to understand what the configuration keys do
+  { -- Adds git related signs to the gutter, as well as utilities for managing changes
+    'lewis6991/gitsigns.nvim',
+    opts = {
+      signs = {
+        add = { text = '+' },
+        change = { text = '~' },
+        delete = { text = '_' },
+        topdelete = { text = '‾' },
+        changedelete = { text = '~' },
+      },
+    },
+  },
+}
