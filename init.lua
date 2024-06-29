e93d4458ed6c501b08be77d3fead53715a84d2ca
anbit <antreasmptzls5@gmail.com>
2024-06-29 18:20:27 +0200

    Add shortcuts

--- init.lua
+++ init.lua
@@ -473,6 +473,8 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<S-f>', builtin.find_files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<S-s>', builtin.live_grep, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
