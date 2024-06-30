03affc6b7f1f67a89ca0575a28a8ae296aa71b7c
jbates35 <jimmyb@apexmotion.com>
2024-06-30 13:53:37 -0700

    Indent lines and debug stop added

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -52,7 +52,7 @@ return {
     vim.keymap.set('n', '<F1>', dap.step_into, { desc = 'Debug: Step Into' })
     vim.keymap.set('n', '<F2>', dap.step_over, { desc = 'Debug: Step Over' })
     vim.keymap.set('n', '<F3>', dap.step_out, { desc = 'Debug: Step Out' })
-    vim.keymap.set('n', '<C-F5>', dap.stop, { desc = 'Debug: Stop' })
+    vim.keymap.set('n', '<F9>', dap.terminate, { desc = 'Debug: Stop' })
     vim.keymap.set('n', '<C-b>', dap.toggle_breakpoint, { desc = 'Debug: Toggle Breakpoint' })
     vim.keymap.set('n', '<C-S-b>', function()
       dap.set_breakpoint(vim.fn.input 'Breakpoint condition: ')
--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,20 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
+    init = function()
+      local highlight = {
+        'CursorColumn',
+        'Whitespace',
+      }
+      require('ibl').setup {
+        indent = { highlight = highlight, char = '' },
+        whitespace = {
+          highlight = highlight,
+          remove_blankline_trail = false,
+        },
+        scope = { enabled = false },
+      }
+    end,
     opts = {},
   },
 }
