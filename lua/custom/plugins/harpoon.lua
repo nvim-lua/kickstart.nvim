98a1570fc3d5593e5c862fe72043d9faa3e2eade
Jimmy Bates <jbates.productions@gmail.com>
2024-06-30 15:50:49 -0700

    Updated harpoon to harpoon v2

--- lua/custom/plugins/harpoon.lua
+++ lua/custom/plugins/harpoon.lua
@@ -1,18 +1,75 @@
 return {
   'ThePrimeagen/harpoon',
   lazy = false,
+  branch = 'harpoon2',
   dependencies = {
     'nvim-lua/plenary.nvim',
   },
-  config = true,
+  config = function()
+    require('harpoon'):setup()
+  end,
   keys = {
-    { '<C-m>', "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = 'Mark file with harpoon' },
-    { '<C-]>', "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = 'Go to next harpoon mark' },
-    { '<C-[>', "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = 'Go to previous harpoon mark' },
-    { '<C-o>', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = 'Show harpoon marks' },
-    { '<leader>om', "<cmd>lua require('harpoon.mark').add_file()<cr>", desc = 'Mark file with harpoon' },
-    { '<leader>o[', "<cmd>lua require('harpoon.ui').nav_next()<cr>", desc = 'Go to next harpoon mark' },
-    { '<leader>o]', "<cmd>lua require('harpoon.ui').nav_prev()<cr>", desc = 'Go to previous harpoon mark' },
-    { '<leader>oo', "<cmd>lua require('harpoon.ui').toggle_quick_menu()<cr>", desc = 'Show harpoon marks' },
+    {
+      '<C-m>',
+      function()
+        require('harpoon'):list():add()
+      end,
+      desc = 'Mark file with harpoon',
+    },
+    {
+      '<C-]>',
+      function()
+        require('harpoon'):list():next()
+      end,
+      desc = 'Go to next harpoon mark',
+    },
+    {
+      '<C-[>',
+      function()
+        require('harpoon'):list():prev()
+      end,
+      desc = 'Go to previous harpoon mark',
+    },
+    {
+      '<C-o>',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(require('harpoon'):list())
+      end,
+      desc = 'Show harpoon marks',
+    },
+    {
+      '<leader>om',
+      function()
+        require('harpoon'):list():add()
+      end,
+      desc = 'Mark file with harpoon',
+    },
+    {
+      '<leader>o[',
+      function()
+        require('harpoon'):list():next()
+      end,
+      desc = 'Go to next harpoon mark',
+    },
+    {
+      '<leader>o]',
+      function()
+        require('harpoon'):list():prev()
+      end,
+      desc = 'Go to previous harpoon mark',
+    },
+    {
+      '<leader>oo',
+      function()
+        local harpoon = require 'harpoon'
+        harpoon.ui:toggle_quick_menu(require('harpoon'):list())
+      end,
+      desc = 'Show harpoon marks',
+    },
   },
 }
+
+-- Good example of converting commands from normal VIM setup to lazy setup:
+--    https://github.com/ThePrimeagen/harpoon/tree/harpoon2
+--    https://www.reddit.com/r/neovim/comments/18as0nm/harpoon2_branch_lazy_vim_setup
