d9aba24302e2780895bdcc755005d87d642582d0
Fernando Bueno <fernando.bueno@equisoft.com>
2024-06-30 20:19:01 -0400

    restores

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.opt`
@@ -430,12 +430,20 @@ require('lazy').setup({
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
+          ['media_files'] = {
+            -- filetypes whitelist
+            -- defaults to {"png", "jpg", "mp4", "webm", "pdf"}
+            filetypes = {"png", "webp", "jpg", "jpeg"},
+            -- find command (defaults to `fd`)
+            find_cmd = "rg"
+          }
         },
       }
 
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'media-files')
 
       -- See `:help telescope.builtin`
       local builtin = require 'telescope.builtin'
@@ -952,7 +960,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,27 +2,27 @@ require("custom.core.keymaps")
 require("custom.core.options")
 
 return {
-  'nvim-lua/popup.nvim',
-  'christoomey/vim-tmux-navigator',
+  -- 'christoomey/vim-tmux-navigator',
+  'nvim-lua/popup.nvim', -- used by other plugins
   'szw/vim-maximizer',  -- maximizes and restores current window
   'tpope/vim-surround',  -- add, delete, change surroundings (it's awesome)
   'vim-scripts/ReplaceWithRegister',  -- replace with register contents using motion (gr + motion)
   'kyazdani42/nvim-web-devicons', -- vs-code like icons
-  'hrsh7th/cmp-buffer',  -- source for text in buffer,
-  'hrsh7th/cmp-path',  -- source for file system paths
-  'rafamadriz/friendly-snippets', -- useful snippets
-  { "glepnir/lspsaga.nvim", branch = "main" }, -- enhanced lsp uis
-  'jose-elias-alvarez/typescript.nvim', -- additional functionality for typescript server (e.g. rename file & update imports) 
-  'onsails/lspkind.nvim', -- vs-code like icons for autocompletion
-  'jose-elias-alvarez/null-ls.nvim',  -- configure formatters & linters
-  'jayp0521/mason-null-ls.nvim',  -- bridges gap b/w mason & null-ls
-  { "windwp/nvim-ts-autotag", after = "nvim-treesitter" }, -- autoclose tags
   'fedepujol/move.nvim', -- move line/block up/down
+  -- 'hrsh7th/cmp-buffer',  -- source for text in buffer,
+  -- 'hrsh7th/cmp-path',  -- source for file system paths
+  'rafamadriz/friendly-snippets', -- useful snippets
+  -- { "glepnir/lspsaga.nvim", branch = "main" }, -- enhanced lsp uis
+  -- 'jose-elias-alvarez/typescript.nvim', -- additional functionality for typescript server (e.g. rename file & update imports) 
+  -- 'onsails/lspkind.nvim', -- vs-code like icons for autocompletion
+  -- 'jose-elias-alvarez/null-ls.nvim',  -- configure formatters & linters
+  -- 'jayp0521/mason-null-ls.nvim',  -- bridges gap b/w mason & null-ls
+  { "windwp/nvim-ts-autotag", after = "nvim-treesitter" }, -- autoclose tags
   'ThePrimeagen/harpoon', -- the name is... ThePrimeagen
   'MattesGroeger/vim-bookmarks', -- vim-bookmarks
   'tom-anders/telescope-vim-bookmarks.nvim', -- telescope-vim-bookmarks
   'tpope/vim-unimpaired', -- vim-unimpared
-  -- 'nvim-telescope/telescope-media-files.nvim',
-  'nvim-telescope/telescope-ui-select.nvim',
+  -- 'nvim-telescope/telescope-media-files.nvim', -- doesnt work for windows
   'nvim-telescope/telescope-file-browser.nvim',
 }
+
