8963521384c7f725b0002ec7b8aedb236963ba04
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-07-02 17:43:52 -0500

    update mason defaults

--- init.lua
+++ init.lua
@@ -531,8 +531,10 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        pyright = {},
-        -- rust_analyzer = {},
+        -- pyright = {},
+        ruff = {},
+        ruff_lsp = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
@@ -624,7 +626,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        python = { 'isort', 'black' },
+        python = {},
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
