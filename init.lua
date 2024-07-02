b0f5e73a2353cec185597b357575e2df970d560f
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-07-02 17:48:46 -0500

    updates

--- init.lua
+++ init.lua
@@ -746,6 +746,7 @@ require('lazy').setup({
           { name = 'luasnip' },
           { name = 'path' },
           { name = 'copilot' },
+          { name = 'buffer' },
         },
       }
     end,
@@ -819,7 +820,7 @@ require('lazy').setup({
       { 'nushell/tree-sitter-nu' },
     },
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'nu' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
@@ -853,7 +854,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
