5121a0af02ab998253b064e368d2463b6828a4b7
Armando D. Galiati <armandogaliati@gmail.com>
2024-07-02 21:57:10 -0300

    .

--- init.lua
+++ init.lua
@@ -663,7 +663,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         -- Conform can also run multiple formatters sequentially
-        python = { "isort", "black" },
+        python = { 'black' },
         --
         -- You can use a sub-list to tell conform to run *until* a formatter
         -- is found.
