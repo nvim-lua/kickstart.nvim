f4d76df7bfa4d940ab64df6a6eba77da9560e00f
topper3418 <67675579+topper3418@users.noreply.github.com>
2024-07-04 00:25:49 -0500

    got it refactored and working, still plenty to split and organize

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -10,20 +10,20 @@ return {
   -- NOTE: Yes, you can install new plugins here!
   'mfussenegger/nvim-dap',
   -- NOTE: And you can specify dependencies as well
-  dependencies = {
-    -- Creates a beautiful debugger UI
-    'rcarriga/nvim-dap-ui',
-
-    -- Required dependency for nvim-dap-ui
-    'nvim-neotest/nvim-nio',
-
-    -- Installs the debug adapters for you
-    'williamboman/mason.nvim',
-    'jay-babu/mason-nvim-dap.nvim',
-
-    -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
-  },
+  -- dependencies = {
+  --   -- Creates a beautiful debugger UI
+  --   'rcarriga/nvim-dap-ui',
+  --
+  --   -- Required dependency for nvim-dap-ui
+  --   'nvim-neotest/nvim-nio',
+  --
+  --   -- Installs the debug adapters for you
+  --   'williamboman/mason.nvim',
+  --   'jay-babu/mason-nvim-dap.nvim',
+  --
+  --   -- Add your own debuggers here
+  --   'leoluz/nvim-dap-go',
+  -- },
   config = function()
     local dap = require 'dap'
     local dapui = require 'dapui'
