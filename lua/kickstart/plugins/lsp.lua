f2e977c94438c65764aaf01dd49919636d095049
peturparkur <peter@nagymathe.xyz>
2024-07-05 13:34:06 +0100

    remove on-attach print

--- lua/kickstart/plugins/lsp.lua
+++ lua/kickstart/plugins/lsp.lua
@@ -149,8 +149,6 @@ return {
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
-          print('LSP attach event for ', event.data, event.data.client_id)
-
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
