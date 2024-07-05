0773e7906c5923a7a38d4aaacab5c08e29164126
Yaser Alraddadi <yaseralraddadi@Yasers-MBP.localdomain>
2024-07-05 18:21:43 +0300

    update

--- lua/custom/plugins/neogit.lua
+++ lua/custom/plugins/neogit.lua
@@ -18,16 +18,4 @@ return {
     config = true,
     vim.keymap.set({ 'n' }, '<leader>n', '<cmd>Neogit kind=replace<CR>', {}),
   },
-
-  --{
-  --	"simrat39/rust-tools.nvim",
-  --	ft = "rust",
-  --	dependencies = "neovim/nvim-lspconfig",
-  --	opts = function()
-  --		return require "custom.configs.rust-tools"
-  --	end,
-  --	config = function(_, opts)
-  --		require("rust-tools").setup(opts)
-  --	end,
-  --},
 }
