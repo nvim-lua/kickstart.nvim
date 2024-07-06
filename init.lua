bb35751c474ad3912208792308df6ce4f8220c72
armandogaliati <armandogaliati@gmail.com>
2024-07-06 15:13:55 -0300

    fixed telescope window

--- init.lua
+++ init.lua
@@ -496,7 +496,7 @@ require('lazy').setup({
 			vim.keymap.set('n', '<leader>/', function()
 				-- You can pass additional configuration to Telescope to change the theme, layout, etc.
 				builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
-					winblend = 10,
+					--winblend = 10,
 					previewer = false,
 				})
 			end, { desc = '[/] Fuzzily search in current buffer' })
