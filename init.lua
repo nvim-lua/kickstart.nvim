17e35a2bd6415376b08fb50a53c3da1a5298c363
Oséias K. Rocha <orocha@ael.com.br>
2024-07-08 13:22:54 -0300

    Adding Nightfox theme
    Signed-off-by: Oséias K. Rocha <orocha@ael.com.br>

--- init.lua
+++ init.lua
@@ -805,6 +805,10 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'EdenEast/nightfox.nvim',
+    -- init = function()
+    --   vim.cmd 'colorscheme carbonfox'
+    -- end,
     'catppuccin/nvim',
     flavor = 'mocha',
     priority = 1000, -- Make sure to load this before all the other start plugins.
