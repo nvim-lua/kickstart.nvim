43dce4e2bd170dba0c12b5bea0b547234dec8ca2
armandogaliati <armandogaliati@gmail.com>
2024-07-07 22:48:33 -0300

    added obsidian nvim

--- init.lua
+++ init.lua
@@ -1196,6 +1196,44 @@ require('lazy').setup({
 	},
 	{ 'tpope/vim-fugitive' },
 	{ 'HiPhish/rainbow-delimiters.nvim' },
+	{
+		'epwalsh/obsidian.nvim',
+		version = '*', -- recommended, use latest release instead of latest commit
+		lazy = true,
+		ft = 'markdown',
+		-- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+		-- event = {
+		--   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+		--   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/**.md"
+		--   "BufReadPre path/to/my-vault/**.md",
+		--   "BufNewFile path/to/my-vault/**.md",
+		-- },
+		dependencies = {
+			-- Required.
+			'nvim-lua/plenary.nvim',
+
+			-- see below for full list of optional dependencies 👇
+		},
+		opts = {
+			workspaces = {
+				{
+					name = 'NotesWSL',
+					path = '/mnt/c/Documents and Settings/arman/Documents/Notes',
+				},
+				{
+					name = 'NotesUbuntu',
+					path = '~/Notes',
+				},
+				{
+					name = 'NotesWindows',
+					path = '',
+				},
+			},
+
+			-- see below for full list of options 👇
+		},
+	},
+
 	-- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
 	-- init.lua. If you want these files, they are in the repository, so you can just download them and
 	-- place them in the correct locations.
