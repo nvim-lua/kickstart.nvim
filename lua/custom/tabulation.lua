2d4e0274fdc21a3d738b9b6540d326caafc42e42
schistos <schistos@example.com>
2024-07-09 13:53:04 +0300

    adjust tabulation

---
+++ lua/custom/tabulation.lua
@@ -0,0 +1,4 @@
+vim.o.tabstop = 4 -- A TAB character looks like 4 spaces
+vim.o.expandtab = true -- Pressing the TAB key will insert spaces instead of a TAB character
+vim.o.softtabstop = 4 -- Number of spaces inserted instead of a TAB character
+vim.o.shiftwidth = 4 -- Number of spaces inserted when indenting
