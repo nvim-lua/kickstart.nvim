c1dbb3f7b388e03b95050bc063f33431b5768ced
Graham McMillan <30657475+gmcmillan82@users.noreply.github.com>
2024-07-11 15:22:38 +0200

    Remove some commented code

--- lua/custom/plugins/colorscheme.lua
+++ lua/custom/plugins/colorscheme.lua
@@ -4,25 +4,6 @@
 --     name = "catppuccin",
 --     lazy = false,
 --     priority = 1000,
---     -- opts = {
---     --   flavour = 'mocha',
---     -- },
---     config = function()
---       require("catppuccin").setup({
---         flavour = "mocha",
---         transparent_background = false,
---       })
---       vim.cmd.colorscheme 'catppuccin'
---     end,
---   },
--- }
-
--- return {
---   {
---     "catppuccin/nvim",
---     name = "catppuccin",
---     lazy = false,
---     priority = 1000,
 --
 --     config = function()
 --       require("catppuccin").setup({
@@ -36,14 +17,14 @@
 
 return {
   {
-    "folke/tokyonight.nvim",
+    'folke/tokyonight.nvim',
     lazy = false,
     priority = 1000,
     config = function()
-      require("tokyonight").setup({
-        style = "moon",
-      })
-      vim.cmd[[colorscheme tokyonight]]
-    end
-  }
+      require('tokyonight').setup {
+        style = 'moon',
+      }
+      vim.cmd [[colorscheme tokyonight]]
+    end,
+  },
 }
