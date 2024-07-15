fbafd84ebad9acf05d9c0c35a63cfc3d3c5226c7
Vladislav <vgrechannik@gmail.com>
2024-07-15 17:56:25 +0200

    remove unneeded brackets from which-key registration

--- init.lua
+++ init.lua
@@ -281,15 +281,13 @@ require('lazy').setup({
 
       -- Document existing key chains
       require('which-key').add {
-        {
-          { '<leader>c', group = '[C]ode' },
-          { '<leader>d', group = '[D]ocument' },
-          { '<leader>r', group = '[R]ename' },
-          { '<leader>s', group = '[S]earch' },
-          { '<leader>w', group = '[W]orkspace' },
-          { '<leader>t', group = '[T]oggle' },
-          { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
-        },
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
     end,
   },
