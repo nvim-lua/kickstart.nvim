30e2771875184221a233134d85167004fcfb4af1
Gilad Sher <gilad.sher@zoominsoftware.com>
2024-07-16 13:30:53 -0400

    no need

--- after/plugin/init.lua
+++ after/plugin/init.lua
@@ -35,12 +35,3 @@ vim.api.nvim_create_autocmd('FileType', {
     end
   end,
 })
-
-local format_sync_grp = vim.api.nvim_create_augroup('GoFormat', {})
-vim.api.nvim_create_autocmd('BufWritePre', {
-  pattern = '*.go',
-  callback = function()
-    require('go.format').goimports()
-  end,
-  group = format_sync_grp,
-})
