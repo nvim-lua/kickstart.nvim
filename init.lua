1d2514443755e3b4695995bd2d3122f53281ca60
Yaser Alraddadi <gam.phon@gmail.com>
2024-07-16 23:29:26 +0300

    update

--- init.lua
+++ init.lua
@@ -215,25 +215,25 @@ vim.keymap.set('n', '<leader>q', vim.diagnostic.setloclist, { desc = 'Open diagn
 vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
 
 -- TIP: Disable arrow keys in normal mode
--- vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
--- vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
--- vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
--- vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
+vim.keymap.set('n', '<left>', '<cmd>echo "Use h to move!!"<CR>')
+vim.keymap.set('n', '<right>', '<cmd>echo "Use l to move!!"<CR>')
+vim.keymap.set('n', '<up>', '<cmd>echo "Use k to move!!"<CR>')
+vim.keymap.set('n', '<down>', '<cmd>echo "Use j to move!!"<CR>')
 
 -- Disable arrow keys
-vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', '<Nop>', { silent = true })
-vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', '<Nop>', { silent = true })
-vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<Nop>', { silent = true })
-vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<Nop>', { silent = true })
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Up>', '<Nop>', { silent = true })
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Down>', '<Nop>', { silent = true })
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Left>', '<Nop>', { silent = true })
+-- vim.keymap.set({ 'n', 'i', 'v' }, '<Right>', '<Nop>', { silent = true })
 
 -- Keybinds to make split navigation easier.
 --  Use CTRL+<hjkl> to switch between windows
 --
 --  See `:help wincmd` for a list of all window commands
-vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
-vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
-vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
-vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
+-- vim.keymap.set('n', '<C-h>', '<C-w><C-h>', { desc = 'Move focus to the left window' })
+-- vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right window' })
+-- vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
+-- vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
 -- Buffer switch
 vim.keymap.set({ 'n' }, ']b', '<cmd>bnext<CR>', { silent = true })
