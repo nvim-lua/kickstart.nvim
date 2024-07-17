42f70ab8b413d4cecda6b4ba951ff3f9e2e881ea
Robin Ting <civ2boss@gmail.com>
2024-07-17 10:33:34 -0400

    Disable neodev which is now EOL

--- init.lua
+++ init.lua
@@ -424,25 +424,25 @@ require('lazy').setup({
       -- used for completion, annotations and signatures of Neovim apis
       -- { 'folke/neodev.nvim', opts = {} },
       -- NEODEV IS NOW EOL
-      { "folke/neodev.nvim", enabled = false }, -- disable neodev as it is EOL
+      { 'folke/neodev.nvim', enabled = false }, -- disable neodev as it is EOL
       {
-        "folke/lazydev.nvim",
-        ft = "lua", -- only load on lua files
+        'folke/lazydev.nvim',
+        ft = 'lua', -- only load on lua files
         opts = {
           library = {
             -- See the configuration section for more details
             -- Load luvit types when the `vim.uv` word is found
-            { path = "luvit-meta/library", words = { "vim%.uv" } },
+            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
           },
         },
       },
-      { "Bilal2453/luvit-meta", lazy = true }, -- optional `vim.uv` typings
+      { 'Bilal2453/luvit-meta', lazy = true }, -- optional `vim.uv` typings
       { -- optional completion source for require statements and module annotations
-        "hrsh7th/nvim-cmp",
+        'hrsh7th/nvim-cmp',
         opts = function(_, opts)
           opts.sources = opts.sources or {}
           table.insert(opts.sources, {
-            name = "lazydev",
+            name = 'lazydev',
             group_index = 0, -- set group index to 0 to skip loading LuaLS completions
           })
         end,
