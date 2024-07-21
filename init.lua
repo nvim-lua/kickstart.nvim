c6e7e426c38aa34746867a8828eeec94984b1aef
long-crypto <175532236+long-crypto@users.noreply.github.com>
2024-07-21 06:05:10 +0000

    enable friendly-snippets

--- init.lua
+++ init.lua
@@ -700,12 +700,12 @@ require('lazy').setup({
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+           {
+             'rafamadriz/friendly-snippets',
+             config = function()
+               require('luasnip.loaders.from_vscode').lazy_load()
+             end,
+           },
         },
       },
       'saadparwaiz1/cmp_luasnip',
