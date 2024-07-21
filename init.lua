787dfdd4f7ab3f45794420e1409b50b7539a3ee0
Carlo Teubner <carlo@cteubner.net>
2024-07-21 21:48:35 +0100

    Remove 'K' mapping
    This is now built into Neovim: https://neovim.io/doc/user/various.html#K-lsp-default

--- init.lua
+++ init.lua
@@ -498,10 +498,6 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
-          -- Opens a popup that displays documentation about the word under your cursor
-          --  See `:help K` for why this keymap.
-          map('K', vim.lsp.buf.hover, 'Hover Documentation')
-
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
