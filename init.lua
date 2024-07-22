4f8a0df5837b4478f917018741c42b820150d0a4
smashblu <smashblu@gmail.com>
2024-07-22 09:45:36 -0700

    Removed Firenvim plugin

--- init.lua
+++ init.lua
@@ -176,16 +176,6 @@ require('lazy').setup({
   --    require('Comment').setup({})
 
   -- "gc" to comment visual regions/lines
-  {
-    'glacambre/firenvim',
-
-    -- Lazy load firenvim
-    -- Explanation: https://github.com/folke/lazy.nvim/discussions/463#discussioncomment-4819297
-    lazy = not vim.g.started_by_firenvim,
-    build = function()
-      vim.fn['firenvim#install'](0)
-    end,
-  },
   { 'numToStr/Comment.nvim', opts = {} },
 
   -- Here is a more advanced example where we pass configuration
