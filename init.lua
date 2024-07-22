cabbfb833352277b1df38e6360dfc519ca40b1af
long-crypto <175532236+long-crypto@users.noreply.github.com>
2024-07-22 02:27:02 +0000

    remove toggleterm, :terminal is enough

--- init.lua
+++ init.lua
@@ -886,30 +886,6 @@ require('lazy').setup({
     end,
   },
 
-  {
-    'akinsho/toggleterm.nvim',
-    config = function()
-      require('toggleterm').setup {
-        size = 20,
-        open_mapping = [[<c-\>]],
-        direction = 'float',
-        float_opts = {
-          border = 'curved',
-        },
-      }
-
-      function _G.set_terminal_keymaps()
-        local opts = { buffer = 0 }
-        vim.keymap.set('t', 'jk', [[<C-\><C-n>]], opts)
-      end
-
-      -- if you only want these mappings for toggle term use term://*toggleterm#* instead
-      vim.cmd 'autocmd! TermOpen term://* lua set_terminal_keymaps()'
-    end,
-    keys = [[<c-\>]],
-    cmd = 'ToggleTerm',
-  },
-
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
