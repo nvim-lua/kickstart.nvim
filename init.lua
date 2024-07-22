a986882465144ca2031293e881346a396013b399
Richard Macklin <1863540+rmacklin@users.noreply.github.com>
2024-07-21 19:08:09 -0700

    Update lazydev config to fix "Undefined field `fs_stat`" LSP error (#1040)
    dc3dcc5a1cff1a3ea4b5b07d1407e90478884f0d switched from neodev to
    lazydev, but in the process it introduced an LSP error in `init.lua`,
    which degrades the desired "first timer" experience of kickstart.nvim.
    
    This commit follows the configuration suggested in
    https://github.com/folke/lazydev.nvim/tree/6184ebbbc8045d70077659b7d30c705a588dc62f#-installation
    which resolves the LSP error.

--- init.lua
+++ init.lua
@@ -413,7 +413,17 @@ require('lazy').setup({
 
       -- `lazydev` configures Lua LSP for your Neovim config, runtime and plugins
       -- used for completion, annotations and signatures of Neovim apis
-      { 'folke/lazydev.nvim', ft = 'lua', opts = {} },
+      {
+        'folke/lazydev.nvim',
+        ft = 'lua',
+        opts = {
+          library = {
+            -- Load luvit types when the `vim.uv` word is found
+            { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+          },
+        },
+      },
+      { 'Bilal2453/luvit-meta', lazy = true },
     },
     config = function()
       -- Brief aside: **What is LSP?**
