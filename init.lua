2ffbf2f8c9bdf22c8cafd2d9e386e64d544fe78c
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2024-07-24 11:07:22 +0200

    Add supermaven to nvim-cmp

--- init.lua
+++ init.lua
@@ -869,6 +869,7 @@ require('lazy').setup({
           { name = 'nvim_lsp' },
           { name = 'luasnip' },
           { name = 'path' },
+          { name = 'supermaven' },
         },
       }
     end,
