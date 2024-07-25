b9564a9dc3146f14c6e07dd123fbb3586db46e43
Aaron Weinberger <aw9994@cs.ship.edu>
2024-07-25 19:06:03 -0400

    Add markdown plugin

---
+++ lua/custom/plugins/markdown.lua
@@ -0,0 +1,7 @@
+return {
+    'MeanderingProgrammer/markdown.nvim',
+    main = "render-markdown",
+    opts = {},
+    name = 'render-markdown', -- Only needed if you have another plugin named markdown.nvim
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+}
