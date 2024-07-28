aa344b3b4775a099778474fbf4c3127890f3163e
abeldekat <58370433+abeldekat@users.noreply.github.com>
2024-07-28 21:41:34 +0000

    performance: defer clipboard because xsel and pbcopy can be slow (#1049)

--- init.lua
+++ init.lua
@@ -111,9 +111,12 @@ vim.opt.mouse = 'a'
 vim.opt.showmode = false
 
 -- Sync clipboard between OS and Neovim.
+--  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
 --  See `:help 'clipboard'`
-vim.opt.clipboard = 'unnamedplus'
+vim.schedule(function()
+  vim.opt.clipboard = 'unnamedplus'
+end)
 
 -- Enable break indent
 vim.opt.breakindent = true
