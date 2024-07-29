a74c09f95150977d43f7c5ca361197969ed06144
Felipe Magrassi <felipe.1magrassi@gmail.com>
2024-07-29 08:20:47 -0300

    Mon Jul 29 08:20:47 -03 2024

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -53,7 +53,18 @@ conform.formatters_by_ft = {
   javascript = { 'prettierd ' },
   typescript = { 'prettierd ' },
   ruby = { 'rubocop' },
+  markdown = { 'markdownlint' },
 }
+conform.format_on_save = function(bufnr)
+  -- Disable "format_on_save lsp_fallback" for languages that don't
+  -- have a well standardized coding style. You can add additional
+  -- languages here or re-enable it for the disabled ones.
+  local disable_filetypes = { c = true, cpp = true }
+  return {
+    timeout_ms = 5000,
+    lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+  }
+end
 
 return {
   {
