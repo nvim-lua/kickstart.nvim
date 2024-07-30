b5c9c637489b78111e9d9727f10adc3d4270737f
Pascal Popp <pascal.markus.popp@gmail.com>
2024-07-30 16:56:21 +0200

    update: add formatter for cpp and python
    clangd as formatter for cpp and isort, black, and autopep8 for python

--- init.lua
+++ init.lua
@@ -663,8 +663,9 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        cpp = { 'clangd' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black', 'autopep8' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
