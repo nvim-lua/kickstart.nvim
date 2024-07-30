e76c56156490c3b697fa23f411df4257c6976432
Pascal Popp <pascal.markus.popp@gmail.com>
2024-07-30 16:30:12 +0200

    update: add options for tabs vs spaces
    two empty spaces instead of tab are being inserted when pressing tab

--- init.lua
+++ init.lua
@@ -84,6 +84,12 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- Tabs vs spaces
+vim.opt.expandtab = true
+vim.opt.tabstop = 2
+vim.opt.softtabstop = 2
+vim.opt.shiftwidth = 2
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
