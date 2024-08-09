7413ca93dbb32cc1f07675a72f3847f55ac084c6
Jeremie Fraeys <jfaeys@gmail.com>
2024-08-09 17:04:04 -0400

    whitespace change

--- lua/custom/plugins/lsp-config.lua
+++ lua/custom/plugins/lsp-config.lua
@@ -214,7 +214,7 @@ return {
 
     vim.lsp.handlers['textDocument/hover'] = vim.lsp.with(vim.lsp.handlers.hover, { border = 'rounded' })
     vim.lsp.handlers['textDocument/signatureHelp'] =
-      vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
+        vim.lsp.with(vim.lsp.handlers.signature_help, { border = 'rounded' })
   end,
 }
 
