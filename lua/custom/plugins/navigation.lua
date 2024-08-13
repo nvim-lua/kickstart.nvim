6d384cb30ecc6958550fe2f7fa82922d769e9003
ZaneFerns360 <zane.ferns360@gmail.com>
2024-08-13 23:40:44 +0530

    small changes

--- lua/custom/plugins/navigation.lua
+++ lua/custom/plugins/navigation.lua
@@ -1,14 +1,14 @@
 local navigation = {
 
   'stevearc/dressing.nvim',
-  --[[   {
+  {
     'max397574/better-escape.nvim',
     event = 'VeryLazy',
     config = function()
       require('better_escape').setup()
     end,
   },
-  ]]
+
   {
     'christoomey/vim-tmux-navigator',
     cmd = {
