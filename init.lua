dce7f6ebe5781fec161dba8aa458335f14e5d263
Eric Olerud <eric@olerud.com>
2024-08-12 23:15:51 -0400

    add rust snippets and update autocomplete button

--- init.lua
+++ init.lua
@@ -618,6 +618,7 @@ require('lazy').setup({
           {
             'rafamadriz/friendly-snippets',
             config = function()
+              require('luasnip').filetype_extend('rust', { 'rust' })
               require('luasnip.loaders.from_vscode').lazy_load()
             end,
           },
@@ -665,7 +666,7 @@ require('lazy').setup({
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
           -- original: ['<C-y>'] = cmp.mapping.confirm { select = true },
-          ['<Return>'] = cmp.mapping.confirm { select = true },
+          ['<CR>'] = cmp.mapping.confirm { select = true },
 
           -- Manually trigger a completion from nvim-cmp.
           --  Generally you don't need this, because nvim-cmp will display
