4a882c7d6cc5ae57e7d8eac81ab3afee14c01e80
Michael Mroczka <mmroczka@salesforce.com>
2024-08-13 11:49:43 -0400

    Fix nvim setup and add custom plugins

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,35 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- underline the word your cursor is on
+  {
+    'echasnovski/mini.cursorword',
+    version = '*',
+    opts = {}
+  },
+  {
+    "aserowy/tmux.nvim",
+    config = function()
+      return require("tmux").setup({
+        copy_sync = {
+          enable = false,
+        },
+        resize = {
+          enable_default_keybindings = true,
+          resize_step_x = 50,
+          resize_step_y = 10,
+        }
+      })
+    end
+  },
+  {
+    'ThePrimeagen/harpoon', -- Enhance marks
+    dependencies = {
+      -- requires to work
+      'nvim-lua/plenary.nvim'
+    }
+  },
+  'ojroques/nvim-osc52',
+  'szw/vim-maximizer'
+}
