8c5945a038762aa53020ca7e8683ebb55c5bb0e9
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2024-08-15 09:36:54 -0300

    chore(copilot): added copilot.lua

---
+++ lua/custom/plugins/copilot.lua
@@ -0,0 +1,20 @@
+return {
+  'github/copilot.vim',
+  config = function()
+    vim.keymap.set('i', '<C-J>', 'copilot#Accept("\\<CR>")', {
+      expr = true,
+      replace_keycodes = false,
+    })
+    vim.g.copilot_no_tab_map = true
+  end,
+  opts = {
+    suggestion = { enabled = false },
+    panel = { enabled = false },
+    filetypes = {
+      gitcommit = true,
+      yaml = true,
+      markdown = true,
+      help = true,
+    },
+  },
+}
