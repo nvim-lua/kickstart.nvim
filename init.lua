db8a24cc2cc1756b9925edb951c25e7dc9fce4ca
Rui Xuan <ruixuantan@yahoo.com.sg>
2024-08-19 15:52:24 +0800

    Disable python format on save

--- init.lua
+++ init.lua
@@ -665,7 +665,7 @@ require('lazy').setup({
         -- Disable "format_on_save lsp_fallback" for languages that don't
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
-        local disable_filetypes = { c = true, cpp = true }
+        local disable_filetypes = { c = true, cpp = true, py = true, python = true }
         return {
           timeout_ms = 500,
           lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
