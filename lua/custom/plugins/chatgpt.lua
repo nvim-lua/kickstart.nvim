8aba6f8dc988b3588df99b78a297a9ea4f444813
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2024-08-23 08:51:14 -0300

    chore(chatgpt): update chatgpt config to use azure api

--- lua/custom/plugins/chatgpt.lua
+++ lua/custom/plugins/chatgpt.lua
@@ -1,23 +1,25 @@
 return {
   {
-    "jackMort/ChatGPT.nvim",
-    event = "VeryLazy",
+    'jackMort/ChatGPT.nvim',
+    event = 'VeryLazy',
     config = function()
-      require("chatgpt").setup(
-        {
-          api_key_cmd = "pass show azure/hypera/oai/token",
-          api_host_cmd = "pass show azure/hypera/oai/url",
-          predefined_chat_gpt_prompts =
-          "https://raw.githubusercontent.com/julianobarbosa/custom-gpt-prompts/main/prompt.csv"
-        }
-      )
+      require('chatgpt').setup {
+        api_key_cmd = 'pass show azure/hypera/oai/idg-dev/token',
+        api_host_cmd = 'echo -n ""',
+        api_type_cmd = 'echo azure',
+        azure_api_base_cmd = 'pass show azure/hypera/oai/idg-dev/base',
+        azure_api_engine_cmd = 'pass show azure/hypera/oai/idg-dev/engine',
+        azure_api_version_cmd = 'pass show azure/hypera/oai/idg-dev/api-version',
+        predefined_chat_gpt_prompts = 'https://raw.githubusercontent.com/julianobarbosa/custom-gpt-prompts/main/prompt.csv',
+      }
     end,
     dependencies = {
-      "MunifTanjim/nui.nvim",
-      "nvim-lua/plenary.nvim",
-      "nvim-telescope/telescope.nvim"
-    }
-  }
+      'MunifTanjim/nui.nvim',
+      'nvim-lua/plenary.nvim',
+      'nvim-telescope/telescope.nvim',
+    },
+  },
 }
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
