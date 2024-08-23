67a4497ae799c4f1f3a86495bd6269a04a7843f4
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2024-08-23 06:51:42 -0300

    chore(init): added lsp capabilities

--- init.lua
+++ init.lua
@@ -584,6 +584,7 @@ require('lazy').setup({
       --  So, we create new capabilities with nvim cmp, and then broadcast that to the servers.
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities = vim.tbl_deep_extend('force', capabilities, require('cmp_nvim_lsp').default_capabilities())
+      capabilities.textDocument.completion.completionItem.snippetSupport = true
 
       -- Enable the following language servers
       --  Feel free to add/remove any LSPs that you want here. They will automatically be installed.
