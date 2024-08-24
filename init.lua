3ca902e4905df82ea0fb9fb0576a201f98f9747d
mclilzee <70924991+Mclilzee@users.noreply.github.com>
2024-08-24 23:14:15 +0200

    Add back code diagnostic keymap

--- init.lua
+++ init.lua
@@ -544,6 +544,10 @@ require('lazy').setup({
           -- or a suggestion from your LSP for this to activate.
           map('<leader>ca', vim.lsp.buf.code_action, '[C]ode [A]ction')
 
+          -- Open Code diagnostic in float window. your cursor needs to be on top of an error
+          -- Alternatively a hotkey were added in nvim 0.10 <C-W>d to do this functionaly by default
+          map('<leader>cd', vim.diagnostic.open_float, '[C]ode [D]iagnostic')
+
           -- WARN: This is not Goto Definition, this is Goto Declaration.
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
