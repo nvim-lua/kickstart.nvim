300edd4025eb72ea68374f7000e9345cccff90d8
Lei Chen <lei.chen@smartx.com>
2024-08-25 22:20:25 -0400

    Add nvim-window-picker for neo-tree plugin

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -8,6 +8,7 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    's1n7ax/nvim-window-picker',
   },
   cmd = 'Neotree',
   keys = {
