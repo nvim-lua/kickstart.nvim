6f27eee3aff0d93d4a5b91b749d10a158d5fde30
Gilad Sher <gilad.sher@zoominsoftware.com>
2024-08-26 14:27:56 -0400

    add multi pattern for multi curstor package

--- lua/custom/plugins/multi-cursor.lua
+++ lua/custom/plugins/multi-cursor.lua
@@ -13,5 +13,11 @@ return {
       '<cmd>MCstart<cr>',
       desc = 'Create a [M]ulti [S]election for selected text or word under the cursor',
     },
+    {
+      mode = { 'v', 'n' },
+      '<Leader>mp',
+      '<cmd>MCpattern<cr>',
+      desc = 'Create a [M]ulti [P]attern selection in the current buffer',
+    },
   },
 }
