4e3ffc6fd9b918aa6aff535a786eba2cb6eda614
Ihsan Tonuzi <115842560+iton0@users.noreply.github.com>
2024-08-25 23:28:26 -0400

    fix: remove deprecated opt for conform.nvim (#1070)
    - changed lsp_fallback -> lsp_format
    - updated format_on_save function to reflect change above

--- init.lua
+++ init.lua
@@ -675,7 +675,7 @@ require('lazy').setup({
       {
         '<leader>f',
         function()
-          require('conform').format { async = true, lsp_fallback = true }
+          require('conform').format { async = true, lsp_format = 'fallback' }
         end,
         mode = '',
         desc = '[F]ormat buffer',
@@ -688,9 +688,15 @@ require('lazy').setup({
         -- have a well standardized coding style. You can add additional
         -- languages here or re-enable it for the disabled ones.
         local disable_filetypes = { c = true, cpp = true }
+        local lsp_format_opt
+        if disable_filetypes[vim.bo[bufnr].filetype] then
+          lsp_format_opt = 'never'
+        else
+          lsp_format_opt = 'fallback'
+        end
         return {
           timeout_ms = 500,
-          lsp_fallback = not disable_filetypes[vim.bo[bufnr].filetype],
+          lsp_format = lsp_format_opt,
         }
       end,
       formatters_by_ft = {
