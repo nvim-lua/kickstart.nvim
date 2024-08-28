b29962db75e75428a9461d4e91f07bac9446b876
jorgenwh <jorgenwhen@gmail.com>
2024-08-28 17:02:04 +0200

    line numbers

--- init.lua
+++ init.lua
@@ -11,6 +11,7 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+vim.opt.number = true
 vim.opt.relativenumber = true
 
 -- Tab settings
