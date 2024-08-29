14b8c50baea69bf6e9d9332285863a7e4cb2dbca
Levente Krizsán <lewalevi@gmail.com>
2024-08-29 12:50:36 +0200

    Tweak noice

--- lua/custom/plugins/noice.lua
+++ lua/custom/plugins/noice.lua
@@ -35,5 +35,28 @@ return {
         lsp_doc_border = false, -- add a border to hover docs and signature help
       },
     }
+    local search = vim.api.nvim_get_hl(0, { name = 'Search' })
+    vim.api.nvim_set_hl(0, 'TransparentSearch', { fg = search.foreground })
+
+    local help = vim.api.nvim_get_hl(0, { name = 'IncSearch' })
+    vim.api.nvim_set_hl(0, 'TransparentHelp', { fg = help.foreground })
+
+    local cmdGroup = 'DevIconLua'
+    local noice_cmd_types = {
+      CmdLine = cmdGroup,
+      Input = cmdGroup,
+      Lua = cmdGroup,
+      Filter = cmdGroup,
+      Rename = cmdGroup,
+      Substitute = 'Define',
+      Help = 'TransparentHelp',
+      Search = 'TransparentSearch',
+    }
+
+    for type, hl in pairs(noice_cmd_types) do
+      vim.api.nvim_set_hl(0, 'NoiceCmdlinePopupBorder' .. type, { link = hl })
+      vim.api.nvim_set_hl(0, 'NoiceCmdlineIcon' .. type, { link = hl })
+    end
+    vim.api.nvim_set_hl(0, 'NoiceConfirmBorder', { link = cmdGroup })
   end,
 }
