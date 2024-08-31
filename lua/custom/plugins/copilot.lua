fdf95f3674ca57a7d4e8f9f463f0b8e036423a5b
Yaser Alraddadi <gam.phon@gmail.com>
2024-08-31 23:01:32 +0300

    update

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -4,6 +4,7 @@ return {
     -- let g:copilot_enabled = v:false
     init = function()
       -- vim.g.copilot_enabled = false
+      vim.keymap.set('i', '<M-;>', '<Plug>(copilot-accept-word)')
     end,
     --
   },
