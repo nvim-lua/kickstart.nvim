ff81bb2317a60991322d1543b2ed9ae1206920b2
Lorran David <lorrantd1@gmail.com>
2024-08-31 12:50:30 -0300

    catppuccin & oil

--- init.lua
+++ init.lua
@@ -835,15 +835,39 @@ require('lazy').setup({
     end,
   },
 
-  -- You can easily change to a different colorscheme.
-  -- Change the name of the colorscheme plugin below, and then
-  -- change the command in the config to whatever the name of that colorscheme is.
-  --
-  -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  -- Colorscheme
   {
-    'vague2k/vague.nvim',
+    'catppuccin/nvim',
     config = function()
-      require('vague').setup {}
+      require('catppuccin').setup {
+        integrations = {
+          cmp = true,
+          gitsigns = true,
+          harpoon = true,
+          illuminate = true,
+          indent_blankline = {
+            enabled = false,
+            scope_color = 'sapphire',
+            colored_indent_levels = false,
+          },
+          mason = true,
+          native_lsp = { enabled = true },
+          notify = true,
+          nvimtree = true,
+          neotree = true,
+          symbols_outline = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+        },
+      }
+
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+
+      -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+      for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
+        vim.api.nvim_set_hl(0, group, {})
+      end
     end,
   },
 
