565fa0640f819ba187ffadb14ebf55e26cc6e5f8
dover_shen <dover_shen@gss.com.tw>
2024-09-01 15:27:26 +0800

    temp

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -8,6 +8,25 @@ return {
     'nvim-lua/plenary.nvim',
     'nvim-tree/nvim-web-devicons', -- not strictly required, but recommended
     'MunifTanjim/nui.nvim',
+    {
+      's1n7ax/nvim-window-picker',
+      version = '2.*',
+      config = function()
+        require('window-picker').setup {
+          filter_rules = {
+            include_current_win = false,
+            autoselect_one = true,
+            -- filter using buffer options
+            bo = {
+              -- if the file type is one of following, the window will be ignored
+              filetype = { 'neo-tree', 'neo-tree-popup', 'notify' },
+              -- if the buffer type is one of following, the window will be ignored
+              buftype = { 'terminal', 'quickfix' },
+            },
+          },
+        }
+      end,
+    },
   },
   cmd = 'Neotree',
   keys = {
@@ -20,6 +39,8 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      find_command = 'which', -- 這邊可能根據使用系統更換 fd / find / which
+      check_gitignore_in_search = false,
     },
   },
 }
