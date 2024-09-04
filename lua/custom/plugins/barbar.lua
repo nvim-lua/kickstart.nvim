435b652c8e6720434d43acd64914947cee0164fb
Levente Krizsán <lewalevi@gmail.com>
2024-09-04 13:09:35 +0200

    Use barbar.nvim instead of bufferline

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -1,7 +1,7 @@
 -- https://github.com/romgrk/barbar.nvim
 return {
   'romgrk/barbar.nvim',
-  enabled = false,
+  enabled = true,
   dependencies = {
     'lewis6991/gitsigns.nvim', -- OPTIONAL: for git status
     'nvim-tree/nvim-web-devicons', -- OPTIONAL: for file icons
@@ -9,5 +9,17 @@ return {
   init = function()
     vim.g.barbar_auto_setup = false
   end,
-  opts = {},
+  opts = {
+    sidebar_filetypes = {
+      ['neo-tree'] = { event = 'BufWipeout' },
+    },
+  },
+  config = function(_, opts)
+    require('barbar').setup(opts)
+
+    vim.keymap.set('n', '<Tab>', '<Cmd>BufferNext<CR>', { noremap = true, silent = true, desc = 'Move to next buffer' })
+    vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true, desc = 'Move to previous buffer' })
+
+    vim.keymap.set('n', '<leader>bc', '<Cmd>BufferClose<CR>', { noremap = true, silent = true, desc = 'Close current buffer' })
+  end,
 }
