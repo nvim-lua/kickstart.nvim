5d111e9b328c244a8b473f474f3782d85f2dd1cf
Andreas Bitzilis <andreas.bitzilis@redpill-linpro.com>
2024-09-05 15:22:10 +0200

    Update config

--- init.lua
+++ init.lua
@@ -191,7 +191,7 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
-vim.keymap.set('i', '{', '{<Enter>}<Esc><S-o>', { desc = 'Matching { }' })
+vim.keymap.set('i', '{', '{}<Esc>ha', { desc = 'Matching { }' })
 vim.keymap.set('i', '(', '()<Esc>ha', { desc = 'Matching ( )' })
 vim.keymap.set('i', '[', '[]<Esc>ha', { desc = 'Matching [ ]' })
 vim.keymap.set('i', "'", "''<Esc>ha", { desc = 'Matching quotes' })
