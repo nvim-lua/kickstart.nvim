d1d9a23309432ba5cf031af58ddcaf511e3d5f30
Levente Krizsán <lewalevi@gmail.com>
2024-09-06 13:46:56 +0200

    Add buffer shortcuts

--- lua/custom/plugins/barbar.lua
+++ lua/custom/plugins/barbar.lua
@@ -21,5 +21,15 @@ return {
     vim.keymap.set('n', '<S-Tab>', '<Cmd>BufferPrevious<CR>', { noremap = true, silent = true, desc = 'Move to previous buffer' })
 
     vim.keymap.set('n', '<leader>q', '<Cmd>BufferClose<CR>', { noremap = true, silent = true, desc = 'Close current buffer' })
+
+    vim.keymap.set('n', '<leader>1', '<Cmd>BufferGoto 1<CR>', { noremap = true, silent = true, desc = 'Go to buffer [1]' })
+    vim.keymap.set('n', '<leader>2', '<Cmd>BufferGoto 2<CR>', { noremap = true, silent = true, desc = 'Go to buffer [2]' })
+    vim.keymap.set('n', '<leader>3', '<Cmd>BufferGoto 3<CR>', { noremap = true, silent = true, desc = 'Go to buffer [3]' })
+    vim.keymap.set('n', '<leader>4', '<Cmd>BufferGoto 4<CR>', { noremap = true, silent = true, desc = 'Go to buffer [4]' })
+    vim.keymap.set('n', '<leader>5', '<Cmd>BufferGoto 5<CR>', { noremap = true, silent = true, desc = 'Go to buffer [5]' })
+    vim.keymap.set('n', '<leader>6', '<Cmd>BufferGoto 6<CR>', { noremap = true, silent = true, desc = 'Go to buffer [6]' })
+    vim.keymap.set('n', '<leader>7', '<Cmd>BufferGoto 7<CR>', { noremap = true, silent = true, desc = 'Go to buffer [7]' })
+    vim.keymap.set('n', '<leader>8', '<Cmd>BufferGoto 8<CR>', { noremap = true, silent = true, desc = 'Go to buffer [8]' })
+    vim.keymap.set('n', '<leader>9', '<Cmd>BufferGoto 9<CR>', { noremap = true, silent = true, desc = 'Go to buffer [9]' })
   end,
 }
