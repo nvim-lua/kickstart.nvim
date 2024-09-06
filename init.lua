7f0b387dc2030c1966d7dec9d7af6fe0ff1c16d2
joe-sinopoli <3653367+joe-sinopoli@users.noreply.github.com>
2024-09-05 22:32:50 -0400

    pyright

--- init.lua
+++ init.lua
@@ -608,7 +608,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        --pyright = {},
+        pyright = {},
         jdtls = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
