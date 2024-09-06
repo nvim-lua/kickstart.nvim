042b34cebf448d91cba6d7b1243a016422346129
Aaron Weinberger <aw9994@cs.ship.edu>
2024-09-06 16:47:19 -0400

    Use `eslint_d` for both javascript and typescript

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,8 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        javascript = { 'eslint_d' },
+        typescript = { 'eslint_d' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
