d96b004a2480e6452243fce6ca4dcccc7127a90b
Lorran David <lorrantd1@gmail.com>
2024-09-08 02:52:27 -0300

    set theme to carbonfox

--- init.lua
+++ init.lua
@@ -837,37 +837,11 @@ require('lazy').setup({
 
   -- Colorscheme
   {
-    'catppuccin/nvim',
+    'EdenEast/nightfox.nvim',
     config = function()
-      require('catppuccin').setup {
-        integrations = {
-          cmp = true,
-          gitsigns = true,
-          harpoon = true,
-          illuminate = true,
-          indent_blankline = {
-            enabled = false,
-            scope_color = 'sapphire',
-            colored_indent_levels = false,
-          },
-          mason = true,
-          native_lsp = { enabled = true },
-          notify = true,
-          nvimtree = true,
-          neotree = true,
-          symbols_outline = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
-        },
+      require('nightfox').setup {
+        vim.cmd.colorscheme 'carbonfox',
       }
-
-      vim.cmd.colorscheme 'catppuccin-macchiato'
-
-      -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-      for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
-        vim.api.nvim_set_hl(0, group, {})
-      end
     end,
   },
 
