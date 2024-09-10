827ea7ceecfd2613abdded538070ac7581b7dab3
joe-sinopoli <3653367+joe-sinopoli@users.noreply.github.com>
2024-09-10 18:44:59 -0400

    adding diagnostic message keymap for LSP

--- init.lua
+++ init.lua
@@ -548,6 +548,8 @@ require('lazy').setup({
           --  For example, in C this would take you to the header.
           map('gD', vim.lsp.buf.declaration, '[G]oto [D]eclaration')
 
+          -- Show LSP message
+          map('gM', vim.diagnostic.open_float, '[G]oto [M]essage')
           -- The following two autocommands are used to highlight references of the
           -- word under your cursor when your cursor rests there for a little while.
           --    See `:help CursorHold` for information about when this is executed
