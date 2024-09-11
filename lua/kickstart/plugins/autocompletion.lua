250be667a174c044761b35c9a466c408f89dcd23
Miguel Da Silva <miguel@dasilvaf.net>
2024-09-11 21:35:13 +0200

    Change confirm completion to <CR>

--- lua/kickstart/plugins/autocompletion.lua
+++ lua/kickstart/plugins/autocompletion.lua
@@ -65,7 +65,8 @@ return { -- Autocompletion
         -- Accept ([y]es) the completion.
         --  This will auto-import if your LSP supports it.
         --  This will expand snippets if the LSP sent a snippet.
-        ['<C-y>'] = cmp.mapping.confirm { select = true },
+        -- ['<C-y>'] = cmp.mapping.confirm { select = true },
+        ['<CR>'] = cmp.mapping.confirm { select = true },
 
         -- If you prefer more traditional completion keymaps,
         -- you can uncomment the following lines
