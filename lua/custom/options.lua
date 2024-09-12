df3391a4cfe9f12e6fabedb2a87a5319e4e145d6
dla <dla@axon.com>
2024-09-12 17:59:24 +0700

    Add vim log highlight

---
+++ lua/custom/options.lua
@@ -0,0 +1,6 @@
+vim.opt.tabstop = 4 -- Set tabstop to 4 spaces
+vim.opt.shiftwidth = 4 -- Set shiftwidth to 4 spaces
+vim.opt.smarttab = true -- Enable smarttab
+vim.opt.expandtab = true -- Convert tabs to spaces
+vim.opt.smartindent = true -- Enable smart indentation
+vim.opt.autoindent = true -- Enable auto indentation
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'mtdl9/vim-log-highlighting',
+    opts = {},
+    config = function() end,
+  },
+}
