5f242413743f915b715fd05cf3891a745be257d1
Zach Zolton <zach.zolton@sproutsocial.com>
2024-09-12 15:24:54 -0400

    Further configure Treesitter Context display

--- lua/custom/plugins/nvim_treesitter_context.lua
+++ lua/custom/plugins/nvim_treesitter_context.lua
@@ -9,7 +9,7 @@ return {
 			line_numbers = true,
 			multiline_threshold = 20, -- Maximum number of lines to show for a single context
 			trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
-			mode = 'cursor',    -- Line used to calculate context. Choices: 'cursor', 'topline'
+			mode = 'topline',    -- Line used to calculate context. Choices: 'cursor', 'topline'
 			-- Separator between context and content. Should be a single character string, like '-'.
 			-- When separator is set, the context will only show up when there are at least 2 lines above cursorline.
 			separator = nil,
