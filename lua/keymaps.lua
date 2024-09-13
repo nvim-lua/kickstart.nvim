4e435342a31e7a079f68478c18ec5542022d170d
Miguel Da Silva <miguel@dasilvaf.net>
2024-09-13 22:06:06 +0200

    change "Exit terminal mode" shortcut

--- lua/keymaps.lua
+++ lua/keymaps.lua
@@ -64,4 +64,5 @@ vim.keymap.set('n', '<Esc>', '<cmd>nohlsearch<CR>')
 --
 -- NOTE: This won't work in all terminal emulators/tmux/etc. Try your own mapping
 -- or just use <C-\><C-n> to exit terminal mode
-vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+-- vim.keymap.set('t', '<Esc><Esc>', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
+vim.keymap.set('t', '<C- >', '<C-\\><C-n>', { desc = 'Exit terminal mode' })
