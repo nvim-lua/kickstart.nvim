691eb9832e45f247f2e9ac91e3567c83c506eb1a
dla <dla@axon.com>
2024-09-13 16:55:28 +0700

    Fix osyank

--- lua/custom/keymaps.lua
+++ lua/custom/keymaps.lua
@@ -4,10 +4,10 @@ vim.keymap.set('n', '<leader>l', ':set hlsearch!<CR>', { noremap = true, silent 
 vim.keymap.set('n', '<C-s>', ':w<CR>', { noremap = true, silent = true })
 vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>', { noremap = true, silent = true })
 
-vim.keymap.set('v', '<C-c', ':OSCYankVisual<CR>', { noremap = true, silent = true })
+vim.keymap.set('v', '<C-c>', ':OSCYankVisual<CR>', { noremap = true, silent = true })
 
 -- Map Ctrl-c to Esc in normal mode
---vim.keymap.set('n', '<C-c>', '<Esc>', { noremap = true, silent = true })
+vim.keymap.set('n', '<C-c>', '<Esc>', { noremap = true, silent = true })
 
 -- Toggle comment
 vim.keymap.set('n', '<C-\\>', '<plug>NERDCommenterToggle', { noremap = true, silent = true })
