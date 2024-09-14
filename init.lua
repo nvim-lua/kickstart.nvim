84666b856a922a1130b7804769c4ecdf401195b3
Lorran David <lorrantd1@gmail.com>
2024-09-14 18:51:03 -0300

    set theme to catppuccin

--- init.lua
+++ init.lua
@@ -837,11 +837,38 @@ require('lazy').setup({
 
   -- Colorscheme
   {
-    'EdenEast/nightfox.nvim',
+    'catppuccin/nvim',
+    lazy = false,
+    priority = 1000,
     config = function()
-      require('nightfox').setup {
-        vim.cmd.colorscheme 'carbonfox',
+      require('catppuccin').setup {
+        integrations = {
+          cmp = true,
+          fidget = true,
+          gitsigns = true,
+          harpoon = true,
+          indent_blankline = {
+            enabled = false,
+            scope_color = 'sapphire',
+            colored_indent_levels = false,
+          },
+          mason = true,
+          native_lsp = { enabled = true },
+          noice = true,
+          notify = true,
+          symbols_outline = true,
+          telescope = true,
+          treesitter = true,
+          treesitter_context = true,
+        },
       }
+
+      vim.cmd.colorscheme 'catppuccin-macchiato'
+
+      -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
+      for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
+        vim.api.nvim_set_hl(0, group, {})
+      end
     end,
   },
 
