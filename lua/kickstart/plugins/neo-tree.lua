7aa5ffd29f576a915d4775db42594d4bcc8c65df
dover_shen <dover_shen@gss.com.tw>
2024-09-15 13:13:21 +0800

    根據源頭更新

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -30,7 +30,7 @@ return {
   },
   cmd = 'Neotree',
   keys = {
-    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal' },
+    { '\\', ':Neotree reveal<CR>', desc = 'NeoTree reveal', silent = true },
   },
   opts = {
     filesystem = {
