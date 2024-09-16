6ff988528ac237de69f2ce21b070529aa5731035
Sergey Sychugin <40148988+sychugin@users.noreply.github.com>
2024-09-16 17:10:18 +0300

    VimTeX: частичная тестовая настройка

--- init.lua
+++ init.lua
@@ -985,6 +985,7 @@ require('lazy').setup({
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
+        disable = { 'latex' },
       },
       indent = { enable = true, disable = { 'ruby' } },
     },
--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -4,7 +4,9 @@ return {
   init = function()
     vim.g.vimtex_view_method = 'skim'
 
-    vim.g.vimtex_syntax_enabled = 0
+    -- vim.g.vimtex_syntax_enabled = 0
     -- vim.g.vimtex_compiler_method = "latexmk"
+
+    vim.g.vimtex_quickfix_mode = 0
   end,
 }
