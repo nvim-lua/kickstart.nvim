8378e8069bdff779537d8ab9677cb4f36f7cfac5
Diego Villamil <dmvillamil@up.edu.ph>
2024-09-17 22:07:18 +0800

    uncommented LSP servers for c, golang, python, and rust

--- init.lua
+++ init.lua
@@ -605,10 +605,10 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
-        -- gopls = {},
-        -- pyright = {},
-        -- rust_analyzer = {},
+        clangd = {},
+        gopls = {},
+        pyright = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
