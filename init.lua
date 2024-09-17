b58add8fb616a4af4acd130eff376f6e7165ae78
lorran.thierry.david <lorran.thierry.david@accenture.com>
2024-09-16 22:52:24 -0300

    change theme to tokyonight

--- init.lua
+++ init.lua
@@ -835,40 +835,21 @@ require('lazy').setup({
     end,
   },
 
-  -- Colorscheme
-  {
-    'catppuccin/nvim',
-    lazy = false,
-    priority = 1000,
-    config = function()
-      require('catppuccin').setup {
-        integrations = {
-          cmp = true,
-          fidget = true,
-          gitsigns = true,
-          harpoon = true,
-          indent_blankline = {
-            enabled = false,
-            scope_color = 'sapphire',
-            colored_indent_levels = false,
-          },
-          mason = true,
-          native_lsp = { enabled = true },
-          noice = true,
-          notify = true,
-          symbols_outline = true,
-          telescope = true,
-          treesitter = true,
-          treesitter_context = true,
-        },
-      }
+  { -- You can easily change to a different colorscheme.
+    -- Change the name of the colorscheme plugin below, and then
+    -- change the command in the config to whatever the name of that colorscheme is.
+    --
+    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+    'folke/tokyonight.nvim',
+    priority = 1000, -- Make sure to load this before all the other start plugins.
+    init = function()
+      -- Load the colorscheme here.
+      -- Like many other themes, this one has different styles, and you could load
+      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+      vim.cmd.colorscheme 'tokyonight'
 
-      vim.cmd.colorscheme 'catppuccin-macchiato'
-
-      -- Hide all semantic highlights until upstream issues are resolved (https://github.com/catppuccin/nvim/issues/480)
-      for _, group in ipairs(vim.fn.getcompletion('@lsp', 'highlight')) do
-        vim.api.nvim_set_hl(0, group, {})
-      end
+      -- You can configure highlights by doing something like:
+      vim.cmd.hi 'Comment gui=none'
     end,
   },
 
