ad45b137cb706e351c511199510860a7626df30d
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2024-09-20 21:17:49 +0800

    uncommented python formatting tools

--- init.lua
+++ init.lua
@@ -700,7 +700,7 @@ require('lazy').setup({
       formatters_by_ft = {
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
+        python = { 'isort', 'black' },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
