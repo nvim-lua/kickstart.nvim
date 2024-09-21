fbc1918c69ce4f00cfde65d657a09095fdd7f927
lorran.thierry.david <lorran.thierry.david@accenture.com>
2024-09-21 04:13:07 -0300

    enable autopairs

--- init.lua
+++ init.lua
@@ -940,7 +940,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.debug',
   -- require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
