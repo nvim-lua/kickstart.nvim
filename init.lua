1a47b59d26b0f507c5b52f5f1b2c72e8ea994b37
MuskoM <61276807+MuskoM@users.noreply.github.com>
2024-09-22 20:03:26 +0200

    Update tsserver to ts_ls

--- init.lua
+++ init.lua
@@ -540,7 +540,7 @@ require('lazy').setup({
         -- Some languages (like typescript) have entire language plugins that can be useful:
         --    https://github.com/pmizio/typescript-tools.nvim
         --
-        -- But for many setups, the LSP (`tsserver`) will work just fine
+        -- But for many setups, the LSP (`ts_ls`) will work just fine
         volar = {
           init_options = {
             vue = {
@@ -551,7 +551,7 @@ require('lazy').setup({
             },
           },
         },
-        tsserver = {},
+        ts_ls = {},
         --
 
         lua_ls = {
