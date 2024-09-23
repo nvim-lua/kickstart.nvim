337e06bee86d6aae3347dcb5c467e99646d89bc9
smashblu <smashblu@gmail.com>
2024-09-23 15:13:57 -0700

    Disable Markview (issues disabling decorations)

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -49,7 +49,7 @@ require('lazy').setup({
 
   require 'custom/plugins/typescript-tools',
 
-  require 'custom/plugins/markview',
+  -- require 'custom/plugins/markview',
 
   -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
