a955cce2a9b5a2f5a1f18cb9c24593dd7e4f6751
Juliano Barbosa <juliano.barbosa@hypera.com.br>
2024-09-24 11:24:14 -0300

    chore(lint): add linters for dockerfile, json, python, terraform

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,11 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
+        dockerfile = { 'hadolint' },
+        json = { 'jsonlint' },
         markdown = { 'markdownlint' },
+        python = { 'pylint' },
+        terraform = { 'tflint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
