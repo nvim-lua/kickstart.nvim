038a12fbef85367d41e73dc9d7e8bb3a51260232
MimiValsi <miguel@dasilvaf.net>
2024-09-28 08:44:52 +0200

    Add path shortcuts

--- lua/kickstart/plugins/harpoon.lua
+++ lua/kickstart/plugins/harpoon.lua
@@ -22,17 +22,37 @@ return {
     map('n', '<leader>1', function()
       harpoon:list():select(1)
     end, { desc = '[Harpoon] Go to first buffer saved' })
+
     map('n', '<leader>2', function()
       harpoon:list():select(2)
     end, { desc = '[Harpoon] Go to second buffer saved' })
+
     map('n', '<leader>3', function()
       harpoon:list():select(3)
     end, { desc = '[Harpoon] Go to third buffer saved' })
+
     map('n', '<leader>4', function()
       harpoon:list():select(4)
     end, { desc = '[Harpoon] Go to forth buffer saved' })
+
     map('n', '<leader>5', function()
       harpoon:list():select(5)
+    end, { desc = '[Harpoon] Go to forth buffer saved' })
+
+    map('n', '<leader>6', function()
+      harpoon:list():select(6)
+    end, { desc = '[Harpoon] Go to forth buffer saved' })
+
+    map('n', '<leader>7', function()
+      harpoon:list():select(7)
+    end, { desc = '[Harpoon] Go to forth buffer saved' })
+
+    map('n', '<leader>8', function()
+      harpoon:list():select(8)
+    end, { desc = '[Harpoon] Go to forth buffer saved' })
+
+    map('n', '<leader>9', function()
+      harpoon:list():select(9)
     end, { desc = '[Harpoon] Go to forth buffer saved' })
   end,
 }
