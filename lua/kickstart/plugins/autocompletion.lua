aa22e410ec4e315ef35bc9075ce31c4e85d917ad
MimiValsi <miguel@dasilvaf.net>
2024-09-28 08:44:02 +0200

    Correction of <S-TAB> behavior

--- lua/kickstart/plugins/autocompletion.lua
+++ lua/kickstart/plugins/autocompletion.lua
@@ -98,8 +98,8 @@ return { -- Autocompletion
         end, { 'i', 's' }),
         ['<S-Tab>'] = cmp.mapping(function(fallback)
           if cmp.visible() then
-            cmp.select_next_item()
-          elseif luasnip.locally_jumpable(-1) then
+            cmp.select_prev_item()
+          elseif luasnip.jumpable(-1) then
             luasnip.jump(-1)
           else
             fallback()
