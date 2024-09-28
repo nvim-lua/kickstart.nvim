70a245c28c3a729980b4c42f02fd362c4f1d0caa
Miguel Da Silva <miguel@dasilvaf.net>
2024-09-28 09:43:42 +0200

    .

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -6,7 +6,12 @@ return {
     config = function()
       local lint = require 'lint'
       lint.linters_by_ft = {
+<<<<<<< HEAD
         markdown = { 'markdownlint' },
+=======
+        ['markdown'] = { 'markdownlint' },
+        ['python'] = { 'flake8' },
+>>>>>>> 373bf49 (typos)
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
