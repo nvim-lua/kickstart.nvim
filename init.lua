c372e473d2a57f320eeab0043d848e233c2a42e8
Sebastian <sebastianw.codes@gmail.com>
2024-09-29 21:56:55 +0200

    added snippets for csharp

--- init.lua
+++ init.lua
@@ -802,16 +802,21 @@ require('lazy').setup({
           end
           return 'make install_jsregexp'
         end)(),
+
+        config = function()
+          require('luasnip.loaders.from_lua').load { paths = { '~/.config/nvim/snippets/' } }
+        end,
         dependencies = {
+
           -- `friendly-snippets` contains a variety of premade snippets.
           --    See the README about individual language/framework/plugin snippets:
           --    https://github.com/rafamadriz/friendly-snippets
-          -- {
-          --   'rafamadriz/friendly-snippets',
-          --   config = function()
-          --     require('luasnip.loaders.from_vscode').lazy_load()
-          --   end,
-          -- },
+          --{
+          --  'rafamadriz/friendly-snippets',
+          --  config = function()
+          --    require('luasnip.loaders.from_vscode').lazy_load()
+          --  end,
+          --},
         },
       },
       'saadparwaiz1/cmp_luasnip',
---
+++ snippets/cs.lua
@@ -0,0 +1,172 @@
+local ls = require 'luasnip'
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local f = ls.function_node
+local c = ls.choice_node
+local d = ls.dynamic_node
+local r = ls.restore_node
+local fmt = require('luasnip.extras.fmt').fmt
+
+return {
+
+  s(
+    'cww',
+    fmt('Console.WriteLine({});{}', {
+      i(1, ''),
+      i(0),
+    })
+  ),
+
+  s(
+    'cw',
+    fmt('Console.Write({});{}', {
+      i(1, ''),
+      i(0),
+    })
+  ),
+
+  s(
+    'do',
+    fmt(
+      [[
+do
+{{
+{}}} while ({});
+{}]],
+      {
+        i(2),
+        i(1),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    'while',
+    fmt(
+      [[
+while ({})
+{{
+{}}}
+{}]],
+      {
+        i(1),
+        i(2),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    'fo',
+    fmt(
+      [[
+for (int {} = 0; {} < {}; {}++)
+{{
+{}}}
+{}]],
+      {
+        i(2, 'i'),
+        f(function(args)
+          return args[1][1]
+        end, { 2 }),
+        i(1, '1'),
+        f(function(args)
+          return args[1][1]
+        end, { 2 }),
+        i(3),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    'forr',
+    fmt(
+      [[
+for (int {} = {} - 1; {} >= 0; {}--)
+{{
+{}}}
+{}]],
+      {
+        i(2, 'i'),
+        i(1, 'length'),
+        f(function(args)
+          return args[1][1]
+        end, { 2 }),
+        f(function(args)
+          return args[1][1]
+        end, { 2 }),
+        i(3),
+        i(0),
+      }
+    )
+  ),
+
+  s('cc', t 'Console.Clear();'),
+
+  s('crk', t 'Console.ReadKey(true);'),
+
+  s('crl', t 'Console.ReadLine();'),
+
+  s(
+    'foreach',
+    fmt(
+      [[
+foreach ({})
+{{
+{}}}{}]],
+      {
+        i(1),
+        i(2),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    'cla',
+    fmt(
+      [[
+class {}
+{{
+{}}}
+  ]],
+      {
+        i(1, 'ClassName'),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    'inter',
+    fmt(
+      [[
+interface {}
+{{
+{}}}
+  ]],
+      {
+        i(1, 'IInterfaceName'),
+        i(0),
+      }
+    )
+  ),
+
+  s(
+    'enu',
+    fmt(
+      [[
+enum {}
+{{
+{}}}
+  ]],
+      {
+        i(1, 'EnumName'),
+        i(0),
+      }
+    )
+  ),
+}
