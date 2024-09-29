aea8575fc10c01b51d71d7b32144096e1d67b03c
Slayter Teal <slayterteal@gmail.com>
2024-09-29 08:28:54 -0500

    configure relative line numbers to true

--- init.lua
+++ init.lua
@@ -7,6 +7,9 @@ vim.g.maplocalleader = ' '
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 
+-- Set relative line #s
+vim.wo.relativenumber = true
+
 -- [[ Setting options ]]
 -- See `:help vim.opt`
 -- NOTE: You can change these options as you wish!
