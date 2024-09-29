0485d332d2f55e9be40d802dbaf373d25df35fd0
Sebastian <sebastianw.codes@gmail.com>
2024-09-29 19:46:28 +0200

    added autosave

--- init.lua
+++ init.lua
@@ -504,6 +504,17 @@ require('lazy').setup({
       end, { desc = 'Open harpoon window in telescope' })
     end,
   },
+
+  {
+    'okuuva/auto-save.nvim',
+    cmd = 'ASToggle', -- optional for lazy loading on command
+    event = { 'InsertLeave', 'TextChanged' }, -- optional for lazy loading on trigger events
+    opts = {
+      -- your config goes here
+      -- or just leave it empty :)
+    },
+  },
+
   -- LSP Plugins
   {
     -- `lazyev` configures Lua LSP for your Neovim config, runtime and plugins
