08814eaff906e49183ee9d395fb468019a8bbe77
Naphtali Odinakachi <naphtali.odinakachi@workday.com>
2024-09-30 12:13:17 +0100

    Added ES7 and React snippets from vscode

--- init.lua
+++ init.lua
@@ -926,10 +926,15 @@ require('lazy').setup({
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
-  --
+
+  -- Adding ES7+ React/Redux/React-Native snippets from vscode as custom plugin
+  {
+    'dsznajder/vscode-es7-javascript-react-snippets',
+    build = 'yarn install --frozen-lockfile && yarn compile'
+  },
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
