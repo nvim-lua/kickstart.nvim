766a88ad146aea731417c2c42c2bfb14fee028a1
chibs4 <47668526+chibs4@users.noreply.github.com>
2024-10-03 14:15:57 +0300

    Update toggleterm.lua

--- lua/custom/plugins/toggleterm.lua
+++ lua/custom/plugins/toggleterm.lua
@@ -1,7 +1,10 @@
 return {
 	'akinsho/toggleterm.nvim',
 	version = "*",
-	config=true,
-	keys = { {'<leader>t', ":ToggleTerm size=15<cr>"},}
+	config = true,
+	keys = { 
+		{ '<leader>t', ":ToggleTerm size=15<cr>" },
+		{ '<leader>t', mode = { "t" }, "<C-\\><C-n><CMD>ToggleTerm size=15<cr>" },
+		{ '<esc>',     mode = { "t" }, [[<C-\><C-n>]] }
+	}
 }
-
