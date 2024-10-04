0a09de49817358a3e1c9a55be4482af8c24d1665
nik alexandrov <nikolay.alexandrov@coremont.com>
2024-10-04 12:40:18 +0100

    add terraform

--- init.lua
+++ init.lua
@@ -523,6 +523,7 @@ require('lazy').setup({
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
+        terraformls = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
