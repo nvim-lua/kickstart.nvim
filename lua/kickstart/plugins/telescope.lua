c887281569562211b37549c0ff2552b3902b58e2
smashblu <98678476+smashblu@users.noreply.github.com>
2024-10-04 14:59:52 -0700

    Fixed not updating keys

--- lua/kickstart/plugins/telescope.lua
+++ lua/kickstart/plugins/telescope.lua
@@ -83,7 +83,7 @@ return {
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
-      vim.keymap.set('n', '<leader>sr', builtin.registers, { desc = '[S]earch r[E]gisters' })
+      vim.keymap.set('n', '<leader>se', builtin.registers, { desc = '[S]earch r[E]gisters' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
