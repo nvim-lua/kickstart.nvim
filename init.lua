ffac2052bdb019348995b6e55e46de3dcedb816f
Albert Yu <albert.yu@gmail.com>
2024-10-05 17:52:22 -0400

    Enable formatter for js/ts

--- init.lua
+++ init.lua
@@ -703,7 +703,8 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier', 'prettierd', stop_after_first = true },
+        typescript = { 'prettier', 'prettierd', stop_after_first = true },
       },
     },
   },
