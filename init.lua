4cabb9f2a2b41cc609cf7aa9c02ce8129adbd517
Albert Yu <albert.yu@gmail.com>
2024-10-05 17:58:19 -0400

    Use tokyo night storm

--- init.lua
+++ init.lua
@@ -836,7 +836,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'tokyonight-storm'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
