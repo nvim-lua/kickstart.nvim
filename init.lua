7380e890462dc0b6ab212d3ff6ee316fc718d8d5
tommconley <tom.conley@outreach.io>
2024-10-05 09:16:56 -0700

    Add some colorschemes

--- init.lua
+++ init.lua
@@ -799,21 +799,51 @@ require('lazy').setup({
     end,
   },
 
-  { -- You can easily change to a different colorscheme.
-    -- Change the name of the colorscheme plugin below, and then
-    -- change the command in the config to whatever the name of that colorscheme is.
-    --
-    -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'folke/tokyonight.nvim',
-    priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
-      -- Load the colorscheme here.
-      -- Like many other themes, this one has different styles, and you could load
-      -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-moon'
+  -- { -- You can easily change to a different colorscheme.
+  --   -- Change the name of the colorscheme plugin below, and then
+  --   -- change the command in the config to whatever the name of that colorscheme is.
+  --   --
+  --   -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
+  --   'folke/tokyonight.nvim',
+  --   priority = 1000, -- Make sure to load this before all the other start plugins.
+  --   init = function()
+  --     -- Load the colorscheme here.
+  --     -- Like many other themes, this one has different styles, and you could load
+  --     -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
+  --     vim.cmd.colorscheme 'tokyonight-moon'
+  --
+  --     -- You can configure highlights by doing something like:
+  --     vim.cmd.hi 'Comment gui=none'
+  --   end,
+  -- },
 
-      -- You can configure highlights by doing something like:
-      vim.cmd.hi 'Comment gui=none'
+  -- {
+  --   'morhetz/gruvbox',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.g.gruvbox_contrast_dark = 'hard'
+  --     vim.g.gruvbox_bold = 0
+  --     vim.cmd.colorscheme 'gruvbox'
+  --   end,
+  -- },
+
+  -- {
+  --   'sainnhe/everforest',
+  --   priority = 1000,
+  --   init = function()
+  --     vim.g.everforest_enable_italic = true
+  --     vim.g.everforest_background = 'hard'
+  --     vim.cmd.colorscheme 'everforest'
+  --   end,
+  -- },
+
+  {
+    'sainnhe/gruvbox-material',
+    priority = 1000,
+    init = function()
+      vim.g.gruvbox_material_enable_italic = true
+      vim.g.gruvbox_material_background = 'hard'
+      vim.cmd.colorscheme 'gruvbox-material'
     end,
   },
 
