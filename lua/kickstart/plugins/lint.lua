5b1484b0bf5e5b34db5c52a61b4a5cf1e88fc0d6
Pascal Popp <pascal.markus.popp@gmail.com>
2024-10-06 20:45:38 +0200

    update: add pyright as linter for python language

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,7 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        python = { 'pylint' },
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
