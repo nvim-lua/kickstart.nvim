37a8553df7d440507c3bc23a7cae1a3c8443205b
dpearre <dpearre@gmail.com>
2024-10-06 21:53:10 -0700

    Add keyboard mapping for nvimtreetoggle

--- init.lua
+++ init.lua
@@ -209,6 +209,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- Set keymap to toggle nvim-tree
+vim.keymap.set('n', '<leader>e', '<cmd>NvimTreeToggle<CR>', { desc = 'Toggle Nvim Tree' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
