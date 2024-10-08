dd7c6fc45c7d6781d09820a82e868f910108ef6b
smashblu <smashblu@gmail.com>
2024-10-08 13:35:25 -0700

    Configure oil.nvim ('-' to go up one dir)

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -1,3 +1,4 @@
+vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 return {
   {
     'stevearc/oil.nvim',
@@ -7,5 +8,21 @@ return {
     -- Optional dependencies
     dependencies = { { 'echasnovski/mini.icons', opts = {} } },
     -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if prefer nvim-web-devicons
+    config = function()
+      require('oil').setup {
+        -- default_file_explorer = true,
+        skip_confirm_for_simple_edits = true,
+        view_options = {
+          show_hidden = true,
+          natural_order = true,
+          is_hidden_file = function(name, _)
+            return name == '..' or name == '.git'
+          end,
+        },
+        win_options = {
+          wrap = true,
+        },
+      }
+    end,
   },
 }
