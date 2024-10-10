2193b6f007132080db7feb0e70f874989841bdb9
Zach Zolton <zach.zolton@sproutsocial.com>
2024-10-10 17:15:45 -0400

    Reduce context threshold

--- lua/custom/plugins/nvim_treesitter_context.lua
+++ lua/custom/plugins/nvim_treesitter_context.lua
@@ -7,7 +7,7 @@ return {
 			max_lines = 0,      -- How many lines the window should span. Values <= 0 mean no limit.
 			min_window_height = 0, -- Minimum editor window height to enable context. Values <= 0 mean no limit.
 			line_numbers = true,
-			multiline_threshold = 20, -- Maximum number of lines to show for a single context
+			multiline_threshold = 10, -- Maximum number of lines to show for a single context
 			trim_scope = 'outer', -- Which context lines to discard if `max_lines` is exceeded. Choices: 'inner', 'outer'
 			mode = 'topline',    -- Line used to calculate context. Choices: 'cursor', 'topline'
 			-- Separator between context and content. Should be a single character string, like '-'.
