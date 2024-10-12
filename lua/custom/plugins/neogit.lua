472b43c16edfb65d8ba497eac6c3919276dab5b0
ZaneFerns360 <zane.ferns360@gmail.com>
2024-10-12 11:42:21 +0530

    neogit

---
+++ lua/custom/plugins/neogit.lua
@@ -0,0 +1,16 @@
+local neogit = {
+  {
+    'NeogitOrg/neogit',
+    dependencies = {
+      'nvim-lua/plenary.nvim', -- required
+      'sindrets/diffview.nvim', -- optional - Diff integration
+
+      -- Only one of these is needed.
+      'nvim-telescope/telescope.nvim', -- optional
+      'ibhagwan/fzf-lua', -- optional
+      'echasnovski/mini.pick', -- optional
+    },
+    config = true,
+  },
+}
+return neogit
