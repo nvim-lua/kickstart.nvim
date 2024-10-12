1f46ee863c0e8857a55cb423ba2c4dcc9891a895
OscarSen <oscar.senlis@gmail.com>
2024-10-12 19:57:36 -0400

    Added ltex-extra

--- init.lua
+++ init.lua
@@ -893,6 +893,7 @@ require('lazy').setup({
       auto_install = true,
       highlight = {
         enable = true,
+        disable = { 'latex' },
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
@@ -929,7 +930,7 @@ require('lazy').setup({
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
   --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,53 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'skim'
+      vim.g.vimtex_view_skim_sync = 1
+      vim.g.vimtex_view_skim_activate = 1
+      vim.g.vimtex_compiler_latexmk = {
+        aux_dir = 'aux',
+        out_dir = '',
+        callback = 1,
+        continuous = 1,
+        executable = 'latexmk',
+        hooks = {},
+        options = {
+          '-verbose',
+          '-file-line-error',
+          '-synctex=1',
+          '-interaction=nonstopmode',
+          '-shell-escape',
+        },
+      }
+    end,
+  },
+  {
+    'barreiroleo/ltex_extra.nvim',
+    ft = { 'markdown', 'tex' },
+    dependencies = { 'neovim/nvim-lspconfig' },
+    -- yes, you can use the opts field, just I'm showing the setup explicitly
+    config = function()
+      require('ltex_extra').setup {
+        -- your_ltex_extra_opts,
+        server_opts = {
+          -- capabilities = your_capabilities,
+          on_attach = function(client, bufnr)
+            -- your on_attach process
+          end,
+          -- settings = {
+          --     ltex = {
+          --     your settings
+          --     }
+          -- }
+        },
+      }
+    end,
+  },
+}
