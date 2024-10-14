3af120d431f1ded160c75da2760626a6f30b3e5d
Yaser Alraddadi <yaseralraddadi@Mac.localdomain>
2024-10-15 01:59:58 +0300

    update

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -8,4 +8,39 @@ return {
     end,
     --
   },
+  {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'github/copilot.vim' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      -- See Configuration section for rest
+      window = {
+        layout = 'float',
+        relative = 'cursor',
+        width = 1,
+        height = 0.4,
+        row = 1,
+      },
+    },
+    -- See Commands section for default commands if you want to lazy load on them
+    --
+    -- keys = {
+    --   {
+    --     '<leader>ccq',
+    --     function()
+    --       local input = vim.fn.input 'Quick Chat: '
+    --       if input ~= '' then
+    --         -- require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+    --         require('CopilotChat').ask(input)
+    --       end
+    --     end,
+    --     desc = 'CopilotChat - Quick chat',
+    --   },
+    -- },
+  },
 }
