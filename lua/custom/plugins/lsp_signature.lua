9e9ec4ff6e9648ff1fbab89a382df0502e6e4f9c
Yaser Alraddadi <gam.phon@gmail.com>
2024-10-15 04:46:12 +0300

    update

---
+++ lua/custom/plugins/lsp_signature.lua
@@ -0,0 +1,10 @@
+return {
+  {
+    'ray-x/lsp_signature.nvim',
+    event = 'VeryLazy',
+    opts = {},
+    config = function(_, opts)
+      require('lsp_signature').setup(opts)
+    end,
+  },
+}
