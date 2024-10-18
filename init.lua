32cb89ddcfebb54eeeda22b423662cfaa258f984
Tobias <tobiasmuldager@gmail.com>
2024-10-18 20:12:31 +0200

    This is a test

--- init.lua
+++ init.lua
@@ -607,7 +607,7 @@ require('lazy').setup({
       local servers = {
         -- clangd = {},
         -- gopls = {},
-        -- pyright = {},
+        pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
