01ab2a45439126927ec18ec244afd5e8a2eb38a8
Éric NICOLAS (ccjmne) <ccjmne@gmail.com>
2024-10-20 22:34:07 +0200

    Disable linting autocmd for readonly buffers
    This should avoid linting in buffers outside of the user's control,
    having in mind especially the handy LSP pop-ups that describe your
    hovered symbol using markdown.
    
    Co-authored-by: Robin Gruyters <2082795+rgruyters@users.noreply.github.com>

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -47,7 +47,9 @@ return {
       vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
-          lint.try_lint()
+          if vim.opt_local.modifiable:get() then
+            lint.try_lint()
+          end
         end,
       })
     end,
