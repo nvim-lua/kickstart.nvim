f4db486f617dd38ed10bcfcae56df387e4155d61
Robin Gruyters <2082795+rgruyters@users.noreply.github.com>
2024-10-20 13:46:27 +0200

    fix(lsp): remove BufEnter event on linting
    When in use, linting will also be triggered when viewing lsp signature help
    (or lsp hover)

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -44,7 +44,7 @@ return {
       -- Create autocommand which carries out the actual linting
       -- on the specified events.
       local lint_augroup = vim.api.nvim_create_augroup('lint', { clear = true })
-      vim.api.nvim_create_autocmd({ 'BufEnter', 'BufWritePost', 'InsertLeave' }, {
+      vim.api.nvim_create_autocmd({ 'BufWritePost', 'InsertLeave' }, {
         group = lint_augroup,
         callback = function()
           lint.try_lint()
