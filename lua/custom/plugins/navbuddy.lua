c6fbc7d9c681fa2c313c5335b5d2f3b2a33dd559
Alexander Kabolov <alexander@kabolov.com>
2024-10-23 10:35:48 +0200

    adding navbuddy

---
+++ lua/custom/plugins/navbuddy.lua
@@ -0,0 +1,14 @@
+return {
+  'neovim/nvim-lspconfig',
+  dependencies = {
+    {
+      'SmiteshP/nvim-navbuddy',
+      dependencies = {
+        'SmiteshP/nvim-navic',
+        'MunifTanjim/nui.nvim',
+      },
+      opts = { lsp = { auto_attach = true } },
+    },
+  },
+  -- your lsp config or other stuff
+}
