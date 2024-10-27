cc4a25c5b355dc64cecf20e8d235dbfa97d5b849
jorgenwh <jorgenwhen@gmail.com>
2024-10-27 02:17:59 +0200

    a

--- lua/plugins/nvim_treesitter.lua
+++ lua/plugins/nvim_treesitter.lua
@@ -11,6 +11,7 @@ return { -- Highlight, edit, and navigate code
       --  If you are experiencing weird indenting issues, add the language to
       --  the list of additional_vim_regex_highlighting and disabled languages for indent.
       additional_vim_regex_highlighting = { 'ruby' },
+      disable = { 'text' },
     },
     indent = { enable = true, disable = { 'ruby' } },
   },
