69fc2ab15d62a520cae263c1041f64cb046c9d26
schistos <schistos@example.com>
2024-10-28 11:36:01 +0200

    update tsserver LSP to ts_ls
    https://github.com/neovim/nvim-lspconfig/pull/3232#issuecomment-2331025714
    tsserver was suddenly renamed to ts_ls.

--- init.lua
+++ init.lua
@@ -581,7 +581,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`tsserver`) will work just fine
-        tsserver = {},
+        ts_ls = {},
         html = {},
         cssls = {},
         clangd = {},
