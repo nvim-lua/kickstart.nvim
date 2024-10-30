272a44768f1a5b7557fad686a42a78c7bc5d5445
gloomy-lemon-debatable <91877885+gloomy-lemon-debatable@users.noreply.github.com>
2024-10-30 15:50:27 +0000

    Change diagnostic symbols if vim.g.have_nerd_font is true (#1195)
    * feat: Change diagnostic symbols if vim.g.have_nerd_font is true
    
    * feat: Comment out changes regarding diagnostic symbols so that only those who want to change them can do so
    
    ---------
    
    Co-authored-by: name <email>

--- init.lua
+++ init.lua
@@ -588,6 +588,15 @@ require('lazy').setup({
         end,
       })
 
+      -- Change diagnostic symbols in the sign column (gutter)
+      -- if vim.g.have_nerd_font then
+      --   local signs = { Error = '', Warn = '', Hint = '', Info = '' }
+      --   for type, icon in pairs(signs) do
+      --     local hl = 'DiagnosticSign' .. type
+      --     vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = hl })
+      --   end
+      -- end
+
       -- LSP servers and clients are able to communicate to each other what features they support.
       --  By default, Neovim doesn't support everything that is in the LSP specification.
       --  When you add nvim-cmp, luasnip, etc. Neovim now has *more* capabilities.
