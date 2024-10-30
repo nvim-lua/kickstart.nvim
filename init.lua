53310a4ddf9f53e75430108dbda6d7063a099c0f
Will Winder <wwinder.unh@gmail.com>
2024-10-30 13:33:27 -0400

    Remove two because there are more than two.

--- init.lua
+++ init.lua
@@ -908,7 +908,7 @@ require('lazy').setup({
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
-  -- The following two comments only work if you have downloaded the kickstart repo, not just copy pasted the
+  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
   -- place them in the correct locations.
 
