0d6852d0a49ebf3304bbf20a4f52d2008cbc8771
sam <110125971+samarth-na@users.noreply.github.com>
2024-10-30 21:19:16 +0530

    samarth-nagar fix: lazy help tag on line 931 (#1167)
    * samarth-nagar fix: lazy help tag on line 931
    
    found in issue #1152
    
    * fixed white space
    
    ---------
    
    Co-authored-by: sam <110125971+samarth-nagar@users.noreply.github.com>

--- init.lua
+++ init.lua
@@ -892,8 +892,12 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  --    For additional information, see `:help lazy.nvim-lazy.nvim-structuring-your-plugins`
-  { import = 'custom.plugins' },
+  -- { import = 'custom.plugins' },
+  --
+  -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
+  -- Or use telescope!
+  -- In normal mode type `<space>sh` then write `lazy.nvim-plugin`
+  -- you can continue same window with `<space>sr` which resumes last telescope search
 }, {
   ui = {
     -- If you are using a Nerd Font: set icons to an empty table which will use the
