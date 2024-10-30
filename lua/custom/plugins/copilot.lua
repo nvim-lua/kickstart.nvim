0363855037adeffbdbfade246ee14a245c6939f6
Yaser Alraddadi <gam.phon@gmail.com>
2024-10-30 03:06:21 +0300

    update

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -27,40 +27,87 @@ return {
   --     require('copilot_cmp').setup()
   --   end,
   -- },
-  {
-    'CopilotC-Nvim/CopilotChat.nvim',
-    branch = 'canary',
-    dependencies = {
-      { 'github/copilot.vim' }, -- or github/copilot.vim
-      -- { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
-      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
-    },
-    build = 'make tiktoken', -- Only on MacOS or Linux
-    opts = {
-      -- debug = true, -- Enable debugging
-      -- See Configuration section for rest
-      window = {
-        layout = 'float',
-        relative = 'cursor',
-        width = 1,
-        height = 0.4,
-        row = 1,
-      },
-    },
-    -- See Commands section for default commands if you want to lazy load on them
-    --
-    -- keys = {
-    --   {
-    --     '<leader>ccq',
-    --     function()
-    --       local input = vim.fn.input 'Quick Chat: '
-    --       if input ~= '' then
-    --         -- require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
-    --         require('CopilotChat').ask(input)
-    --       end
-    --     end,
-    --     desc = 'CopilotChat - Quick chat',
-    --   },
-    -- },
-  },
+  --
+  -- {
+  --   'CopilotC-Nvim/CopilotChat.nvim',
+  --   branch = 'canary',
+  --   dependencies = {
+  --     { 'github/copilot.vim' }, -- or github/copilot.vim
+  --     -- { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+  --     { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+  --   },
+  --   build = 'make tiktoken', -- Only on MacOS or Linux
+  --   opts = {
+  --     -- debug = true, -- Enable debugging
+  --     -- See Configuration section for rest
+  --     window = {
+  --       layout = 'float',
+  --       relative = 'cursor',
+  --       width = 1,
+  --       height = 0.4,
+  --       row = 1,
+  --     },
+  --   },
+  --   -- See Commands section for default commands if you want to lazy load on them
+  --   --
+  --   -- keys = {
+  --   --   {
+  --   --     '<leader>ccq',
+  --   --     function()
+  --   --       local input = vim.fn.input 'Quick Chat: '
+  --   --       if input ~= '' then
+  --   --         -- require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+  --   --         require('CopilotChat').ask(input)
+  --   --       end
+  --   --     end,
+  --   --     desc = 'CopilotChat - Quick chat',
+  --   --   },
+  --   -- },
+  -- },
+  -- {
+  --   'yetone/avante.nvim',
+  --   event = 'VeryLazy',
+  --   lazy = false,
+  --   version = false, -- set this if you want to always pull the latest change
+  --   opts = {
+  --     -- add any opts here
+  --   },
+  --   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
+  --   build = 'make',
+  --   -- build = "powershell -ExecutionPolicy Bypass -File Build.ps1 -BuildFromSource false" -- for windows
+  --   dependencies = {
+  --     'nvim-treesitter/nvim-treesitter',
+  --     'stevearc/dressing.nvim',
+  --     'nvim-lua/plenary.nvim',
+  --     'MunifTanjim/nui.nvim',
+  --     --- The below dependencies are optional,
+  --     'nvim-tree/nvim-web-devicons', -- or echasnovski/mini.icons
+  --     -- "zbirenbaum/copilot.lua", -- for providers='copilot'
+  --     {
+  --       -- support for image pasting
+  --       'HakonHarnes/img-clip.nvim',
+  --       event = 'VeryLazy',
+  --       opts = {
+  --         -- recommended settings
+  --         default = {
+  --           embed_image_as_base64 = false,
+  --           prompt_for_file_name = false,
+  --           drag_and_drop = {
+  --             insert_mode = true,
+  --           },
+  --           -- required for Windows users
+  --           use_absolute_path = true,
+  --         },
+  --       },
+  --     },
+  --     {
+  --       -- Make sure to set this up properly if you have lazy=true
+  --       'MeanderingProgrammer/render-markdown.nvim',
+  --       opts = {
+  --         file_types = { 'markdown', 'Avante' },
+  --       },
+  --       ft = { 'markdown', 'Avante' },
+  --     },
+  --   },
+  -- },
 }
