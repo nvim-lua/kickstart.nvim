c1de83cf4e7f90f513bc031c677fd093b81f7c0b
onlinemax <onlinepromax12@gmail.com>
2024-11-01 11:30:40 -0400

    Arch linux branch

--- init.lua
+++ init.lua
@@ -973,7 +973,6 @@ require('lazy').setup({
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
   require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
   --
@@ -1001,6 +1000,8 @@ require('lazy').setup({
     },
   },
 })
+require('java').setup()
+require('lspconfig').jdtls.setup {}
 
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
