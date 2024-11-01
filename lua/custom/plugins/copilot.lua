bda8e48b355d3f8f42dfb65600756a0b46155eb8
Yaser Alraddadi <gam.phon@gmail.com>
2024-11-01 18:56:45 +0300

    update
    Signed-off-by: Yaser Alraddadi <gam.phon@gmail.com>

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -119,9 +119,11 @@ return {
         -- Make sure to set this up properly if you have lazy=true
         'MeanderingProgrammer/render-markdown.nvim',
         opts = {
-          file_types = { 'markdown', 'Avante' },
+          -- file_types = { 'markdown', 'Avante' },
+          file_types = { 'Avante' },
         },
-        ft = { 'markdown', 'Avante' },
+        -- ft = { 'markdown', 'Avante' },
+        ft = { 'Avante' },
       },
     },
   },
