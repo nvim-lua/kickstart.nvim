35a75671303d0a5236f31a3e9aa8f64b28746b33
Lorran David <lorrantd1@gmail.com>
2024-11-03 15:27:44 -0300

    colorscheme vscode

--- lua/custom/plugins/colorschemes.lua
+++ lua/custom/plugins/colorschemes.lua
@@ -97,16 +97,11 @@ return {
   },
 
   {
-    'projekt0n/github-nvim-theme',
-    name = 'github-theme',
+    'Mofiqul/vscode.nvim',
     lazy = false, -- make sure we load this during startup if it is your main colorscheme
     priority = 1000, -- make sure to load this before all the other start plugins
     config = function()
-      require('github-theme').setup({
-        -- ...
-      })
-
-      vim.cmd('colorscheme github_dark_dimmed')
+      vim.cmd('colorscheme vscode')
     end,
     enabled = true,
   }
