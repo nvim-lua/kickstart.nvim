1f8b7465e61b3338f23895898bc79a5a2b81207c
shaoyu <fanshaoyu9@outlook.com>
2024-11-12 20:29:58 -0500

    Add lsp server for Typescript

--- init.lua
+++ init.lua
@@ -390,7 +390,7 @@ require('lazy').setup({
         --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
         --   },
         -- },
-        -- pickers = {}
+        picker = { hidden = true },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
@@ -627,7 +627,7 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        -- ts_ls = {},
+        ts_ls = {},
         --
 
         lua_ls = {
