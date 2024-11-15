04756b7991a3cd0d1951f9545909dd2c27efab18
Karolis Arbaciauskas <karolis.arbaciauskas@gmail.com>
2024-11-15 22:27:57 +0200

    Add telescope live grep args plugin

--- init.lua
+++ init.lua
@@ -357,6 +357,7 @@ require('lazy').setup({
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
       { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-telescope/telescope-live-grep-args.nvim' },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -400,15 +401,17 @@ require('lazy').setup({
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+      pcall(require('telescope').load_extension, 'live_grep_args')
 
       -- See `:help telescope.builtin`
+      local telescope = require 'telescope'
       local builtin = require 'telescope.builtin'
       vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
       vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
-      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sg', telescope.extensions.live_grep_args.live_grep_args, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -12,14 +12,62 @@ end
 
 return {
   {
+    'CopilotC-Nvim/CopilotChat.nvim',
+    branch = 'canary',
+    dependencies = {
+      { 'zbirenbaum/copilot.lua' }, -- or github/copilot.vim
+      { 'nvim-lua/plenary.nvim' }, -- for curl, log wrapper
+    },
+    build = 'make tiktoken', -- Only on MacOS or Linux
+    opts = {
+      debug = true, -- Enable debugging
+      context = 'buffer',
+    },
+    keys = {
+      {
+        '<leader>aa',
+        function()
+          local chat = require 'CopilotChat'
+          chat.toggle()
+        end,
+        desc = 'Copilot toggle chat',
+      },
+      {
+        '<leader>aq',
+        function()
+          local input = vim.fn.input 'Quick Chat: '
+          if input ~= '' then
+            require('CopilotChat').ask(input, { selection = require('CopilotChat.select').buffer })
+          end
+        end,
+        desc = 'Copilot quick chat',
+      },
+      {
+        '<leader>ax',
+        function()
+          local chat = require 'CopilotChat'
+          chat.reset()
+        end,
+        desc = 'Copilot chat reset',
+      },
+      {
+        '<leader>ap',
+        function()
+          local actions = require 'CopilotChat.actions'
+          require('CopilotChat.integrations.telescope').pick(actions.prompt_actions())
+        end,
+        desc = 'Copilot chat prompt actions',
+      },
+    },
+  },
+  {
     'zbirenbaum/copilot.lua',
     cmd = 'Copilot',
-    -- new
-    -- event = 'InsertEnter',
+    event = 'InsertEnter',
     build = ':Copilot auth',
     opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
+      -- suggestion = { enabled = false },
+      -- panel = { enabled = false },
       filetypes = {
         markdown = true,
         help = true,
@@ -37,16 +85,5 @@ return {
         copilot_cmp._on_insert_enter {}
       end, 'copilot')
     end,
-  },
-  {
-    'olimorris/codecompanion.nvim',
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-      'nvim-treesitter/nvim-treesitter',
-      'hrsh7th/nvim-cmp', -- Optional: For using slash commands and variables in the chat buffer
-      'nvim-telescope/telescope.nvim', -- Optional: For using slash commands
-      { 'stevearc/dressing.nvim', opts = {} }, -- Optional: Improves `vim.ui.select`
-    },
-    config = true,
   },
 }
