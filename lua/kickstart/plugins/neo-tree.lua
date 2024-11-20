f2d6d6e0f7fa6551e3e3460b77bbc993311c461d
Lorran David <lorrantd1@gmail.com>
2024-11-20 01:14:04 -0300

    show dotfiles neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -21,6 +21,11 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true, -- This is what you want: If you set this to `true`, all "hide" just mean "dimmed out"
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      }
     },
   },
 }
