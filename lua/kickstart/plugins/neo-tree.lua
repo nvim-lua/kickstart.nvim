402f40a433d9fffb0de3cf63af72386d8981e83f
nikolay <alexandroff.nikolay05@gmail.com>
2024-11-25 12:51:05 +0000

    fix: neotree

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -15,6 +15,11 @@ return {
   },
   opts = {
     filesystem = {
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = true,
+      },
       window = {
         mappings = {
           ['\\'] = 'close_window',
