d5b2ded7791b8c5a3ad50449177b1722e0f93b15
Thomas0642 <thomas@livmeds.com>
2024-11-26 14:37:29 +0100

    commit init lua

--- init.lua
+++ init.lua
@@ -411,7 +411,9 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
+      -- Navigation
+      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+      vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[p]revious [v]iew' })
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
