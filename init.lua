be8d53188515017a9a9d22d032772682fdc15ae8
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2024-11-30 11:33:22 -0600

    remove ruff-lsp

--- init.lua
+++ init.lua
@@ -474,10 +474,9 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         ruff = {},
-        ruff_lsp = {},
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
