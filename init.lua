1c0b22ccf0e9ebb96bd2921f669f2f3db99b0058
Geo Stanciu <geo.stanciu@outlook.com>
2024-12-02 16:50:32 +0200

    Update init.lua

--- init.lua
+++ init.lua
@@ -839,16 +839,16 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    --'folke/tokyonight.nvim',
-    'oxfist/night-owl.nvim',
+    'folke/tokyonight.nvim',
+    --'oxfist/night-owl.nvim',
     lazy = false,
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      --vim.cmd.colorscheme 'tokyonight-night'
-      vim.cmd.colorscheme 'night-owl'
+      vim.cmd.colorscheme 'tokyonight-night'
+      --vim.cmd.colorscheme 'night-owl'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
