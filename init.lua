eb71637c386da08f73ba9c726601b176fc0dcd4c
nicholasss <nicholaszingleman@gmail.com>
2024-12-03 15:17:54 -0500

    updated lazy-lock.json and make tabs appear as four spaces

--- init.lua
+++ init.lua
@@ -21,6 +21,10 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
+-- tabs appear as 4 spaces
+vim.opt.tabstop = 4
+vim.opt.shiftwidth = 4
+
 -- Make line numbers default
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
