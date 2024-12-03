4eddd086efaa6496ae7a319910250c71d4fc1a9b
Albert Yu <albert.yu@gmail.com>
2024-12-03 00:51:31 -0500

    Use supermaven, not copilot

---
+++ lua/custom/plugins/supermaven.lua
@@ -0,0 +1,9 @@
+-- supermaven
+-- https://github.com/supermaven-inc/supermaven-nvim
+
+return {
+  'supermaven-inc/supermaven-nvim',
+  config = function()
+    require('supermaven-nvim').setup {}
+  end,
+}
