fa00b60f988ae2c0c99e127760d5fd9f299036b8
Hendra <hendra2392@gmail.com>
2024-12-04 19:06:54 +0700

    Update

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -6,8 +6,18 @@ return {
     -- The following are optional:
     { 'MeanderingProgrammer/render-markdown.nvim', ft = { 'markdown', 'codecompanion' } },
   },
+  keys = {
+    { '<leader>cc', '<cmd>CodeCompanionChat<cr>', desc = 'Start chat' },
+  },
   config = function()
     require('codecompanion').setup {
+      adapters = {
+        anthropic = require('codecompanion.adapters').extend('anthropic', {
+          env = {
+            api_key = 'CODECOMP_ANTHROPIC_API_KEY',
+          },
+        }),
+      },
       strategies = {
         chat = {
           adapter = 'anthropic',
