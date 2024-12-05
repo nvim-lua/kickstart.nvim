77cca483354e8e267312f9071460ef2d7289fcec
unknown <bachourian@gmail.com>
2024-12-05 06:30:10 +0100

    add nerd fonts + nu tree sitter

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
@@ -859,6 +859,54 @@ require('lazy').setup({
         callback = function() vim.treesitter.start() end,
       })
     end,
+    build = ':TSUpdate',
+    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
+    opts = {
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'nu' },
+      -- Autoinstall languages that are not installed
+      auto_install = true,
+      highlight = {
+        enable = true,
+        -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
+        --  If you are experiencing weird indenting issues, add the language to
+        --  the list of additional_vim_regex_highlighting and disabled languages for indent.
+        additional_vim_regex_highlighting = { 'ruby' },
+      },
+      indent = { enable = true, disable = { 'ruby' } },
+      -- OPTIONAL!! These enable ts-specific textobjects.
+      -- So you can hit `yaf` to copy the closest function,
+      -- `dif` to clear the contet of the closest function,
+      -- or whatever keys you map to what query.
+      textobjects = {
+        select = {
+          enable = true,
+          keymaps = {
+            -- You can use the capture groups defined in textobjects.scm
+            -- For example:
+            -- Nushell only
+            ['aP'] = '@pipeline.outer',
+            ['iP'] = '@pipeline.inner',
+
+            -- supported in other languages as well
+            ['af'] = '@function.outer',
+            ['if'] = '@function.inner',
+            ['al'] = '@loop.outer',
+            ['il'] = '@loop.inner',
+            ['aC'] = '@conditional.outer',
+            ['iC'] = '@conditional.inner',
+            ['iS'] = '@statement.inner',
+            ['aS'] = '@statement.outer',
+          }, -- keymaps
+        }, -- select
+      }, -- textobjects
+    },
+    -- There are additional nvim-treesitter modules that you can use to interact
+    -- with nvim-treesitter. You should go explore a few and see what interests you:
+    --
+    --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
+    --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+    --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
