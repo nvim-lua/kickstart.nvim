209f4c2fc1c2d21cb8cc5c6d564fbfb3b98625ed
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-12-05 18:09:37 -0600

    disable some things but prep obsidian.vim

---
+++ lua/custom/plugins/obsidian-nvim.lua
@@ -0,0 +1,37 @@
+if true then
+  return {}
+end
+return {
+  'epwalsh/obsidian.nvim',
+  version = '*', -- recommended, use latest release instead of latest commit
+  lazy = true,
+  ft = 'markdown',
+  -- Replace the above line with this if you only want to load obsidian.nvim for markdown files in your vault:
+  -- event = {
+  --   -- If you want to use the home shortcut '~' here you need to call 'vim.fn.expand'.
+  --   -- E.g. "BufReadPre " .. vim.fn.expand "~" .. "/my-vault/*.md"
+  --   -- refer to `:h file-pattern` for more examples
+  --   "BufReadPre path/to/my-vault/*.md",
+  --   "BufNewFile path/to/my-vault/*.md",
+  -- },
+  dependencies = {
+    -- Required.
+    'nvim-lua/plenary.nvim',
+
+    -- see below for full list of optional dependencies 👇
+  },
+  opts = {
+    workspaces = {
+      {
+        name = 'daybook',
+        path = '~/workspace/obsidian/daybook',
+      },
+      {
+        name = 'obsidian-vault',
+        path = '~/workspace/obsidian/obsidian-vault',
+      },
+    },
+
+    -- see below for full list of options 👇
+  },
+}
--- lua/custom/plugins/pkl-neovim.lua
+++ lua/custom/plugins/pkl-neovim.lua
@@ -1,7 +1,12 @@
+if true then
+  return {}
+end
 return {
-    "https://github.com/apple/pkl-neovim",
-    lazy = true,
-    event = "BufReadPre *.pkl",
-    dependencies = {"nvim-treesitter/nvim-treesitter"},
-    build = function() vim.cmd("TSInstall! pkl") end,
+  'https://github.com/apple/pkl-neovim',
+  lazy = true,
+  event = 'BufReadPre *.pkl',
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
+  build = function()
+    vim.cmd 'TSInstall! pkl'
+  end,
 }
