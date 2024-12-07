abf1273892d609812a49e3ecf33f38f3ef5c65b3
Joshua Nussbaum <67082011+joshuanussbaum@users.noreply.github.com>
2024-12-07 08:59:42 -0600

    add snacks and catppuccin theme

---
+++ lua/custom/plugins/snacks-nvim.lua
@@ -0,0 +1,16 @@
+return {
+  'folke/snacks.nvim',
+  priority = 1000,
+  lazy = false,
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+    bigfile = { enabled = true },
+    dashboard = { enabled = true },
+    notifier = { enabled = true },
+    quickfile = { enabled = true },
+    statuscolumn = { enabled = true },
+    words = { enabled = true },
+  },
+}
