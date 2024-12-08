caf946341d1cb0cab30f9b6487512c300c81dd80
Siggy Gudbrandsson <sigurdur@sigginet.info>
2024-12-08 14:00:14 +0000

    Added comment plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -3,18 +3,29 @@
 --
 -- See the kickstart.nvim README for more information
 return {
-	{
-		"Exafunction/codeium.nvim",
-		dependencies = {
-			"nvim-lua/plenary.nvim",
-			"hrsh7th/nvim-cmp",
-		},
-		config = function()
-			require("codeium").setup({
-			})
-		end
-	},
+  {
+    "Exafunction/codeium.nvim",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "hrsh7th/nvim-cmp",
+    },
+    config = function()
+      require("codeium").setup({
+      })
+    end
+  },
+  -- add this to your lua/plugins.lua, lua/plugins/init.lua,  or the file you keep your other plugins:
+  {
+    'numToStr/Comment.nvim',
+    opts = {
+      -- add any options here
+    },
+    config = function ()
+      require('Comment').setup()
+    end
+  }
+
 }
 
 -- vim: ts=2 sts=2 sw=2 et
-
+--
