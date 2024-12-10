9de5d08a5a374609f91a65c4618d89c1235026b1
raf <bachourian@gmail.com>
2024-12-11 00:30:05 +0100

    add chezmoi syntax support

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -160,4 +160,13 @@ return {
       })
     end,
   },
+  {
+    'alker0/chezmoi.vim',
+    lazy = false,
+    init = function()
+      -- This option is required.
+      vim.g['chezmoi#use_tmp_buffer'] = true
+      -- add other options here if needed.
+    end,
+  },
 }
