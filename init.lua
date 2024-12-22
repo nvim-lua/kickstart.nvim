52d781c6b26483d20bd0316450b134758a671ff1
Schistos <schistos@example.com>
2024-12-22 16:56:37 +0200

    change colorscheme to matrix

--- init.lua
+++ init.lua
@@ -804,14 +804,18 @@ require('lazy').setup({
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
     -- 'folke/tokyonight.nvim',
     -- 'mathofprimes/nightvision-nvim',
-    'rebelot/kanagawa.nvim',
+    -- 'audibleblink/hackthebox.vim',
+    'iruzo/matrix-nvim',
+    -- 'rebelot/kanagawa.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
       -- vim.cmd.colorscheme 'tokyonight-storm'
-      vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'kanagawa-dragon'
+      -- vim.cmd.colorscheme 'hackthebox'
+      vim.cmd.colorscheme 'matrix'
       -- vim.cmd.colorscheme 'nightvision'
 
       -- You can configure highlights by doing something like:
