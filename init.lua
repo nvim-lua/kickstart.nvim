0cfee0d542f2235f5239524bfe464927d0f4df77
Oscar Pinochet <oscar.pinochet@usach.cl>
2024-12-31 15:01:30 -0300

    delete lazygit duplicate

--- init.lua
+++ init.lua
@@ -1088,26 +1088,6 @@ require('lazy').setup({
   --   end,
   -- },
   {
-    'kdheepak/lazygit.nvim',
-    lazy = true,
-    cmd = {
-      'LazyGit',
-      'LazyGitConfig',
-      'LazyGitCurrentFile',
-      'LazyGitFilter',
-      'LazyGitFilterCurrentFile',
-    },
-    -- optional for floating window border decoration
-    dependencies = {
-      'nvim-lua/plenary.nvim',
-    },
-    -- setting the keybinding for LazyGit with 'keys' is recommended in
-    -- order to load the plugin when the command is run for the first time
-    keys = {
-      { '<leader>lg', '<cmd>LazyGit<cr>', desc = 'LazyGit' },
-    },
-  },
-  {
     'maxmx03/fluoromachine.nvim',
     lazy = false,
     priority = 1000,
