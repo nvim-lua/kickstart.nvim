6117dda2477dc9465216a51de29078fbaff1564d
Chris Patti <feoh@feoh.org>
2025-01-07 11:35:26 -0500

    Remove the additional_vim_regex_highlighting clause which is discouraged:
    https://www.reddit.com/r/neovim/comments/yxjrkr/treesitter_syntax_highlighting_too_slow_on_large/

--- init.lua
+++ init.lua
@@ -912,9 +912,10 @@ require('lazy').setup({
         -- Some languages depend on vim's regex highlighting system (such as Ruby) for indent rules.
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
-        additional_vim_regex_highlighting = { 'ruby' },
+        -- additional_vim_regex_highlighting = { 'ruby' },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      -- indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
