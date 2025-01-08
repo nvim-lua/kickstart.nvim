4e4499d52283dfe45031f762885ce3b72d283d45
Lorran David <lorrantd1@gmail.com>
2025-01-08 11:46:03 -0300

    disable copilot-cmp

--- lua/custom/plugins/copilot.lua
+++ lua/custom/plugins/copilot.lua
@@ -6,14 +6,55 @@ return {
     build = ":Copilot auth",
     config = function()
       require("copilot").setup({
-        suggestion = { enabled = false },
-        panel = { enabled = false },
+        panel = {
+          enabled = true,
+          auto_refresh = false,
+          keymap = {
+            jump_prev = "[[",
+            jump_next = "]]",
+            accept = "<CR>",
+            refresh = "gr",
+            open = "<M-CR>"
+          },
+          layout = {
+            position = "bottom", -- | top | left | right | horizontal | vertical
+            ratio = 0.4
+          },
+        },
+        suggestion = {
+          enabled = true,
+          auto_trigger = true,
+          hide_during_completion = true,
+          debounce = 75,
+          keymap = {
+            accept = "<M-l>",
+            accept_word = false,
+            accept_line = false,
+            next = "<M-]>",
+            prev = "<M-[>",
+            dismiss = "<C-]>",
+          },
+        },
+        filetypes = {
+          yaml = false,
+          markdown = false,
+          help = false,
+          gitcommit = false,
+          gitrebase = false,
+          hgcommit = false,
+          svn = false,
+          cvs = false,
+          ["."] = false,
+        },
+        copilot_node_command = 'node', -- Node.js version must be > 18.x
+        server_opts_overrides = {},
       })
     end
   },
 
   {
     "zbirenbaum/copilot-cmp",
+    enabled = false,
     dependencies = { "zbirenbaum/copilot.lua", "hrsh7th/nvim-cmp" },
     config = function()
       require("copilot_cmp").setup()
