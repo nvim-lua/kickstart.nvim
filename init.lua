ace7e4760e65bbb851b3995954f10dcf5863ce57
Diorman Colmenares <229201+diorman@users.noreply.github.com>
2025-01-08 23:18:43 -0800

    Use luals 3rd library for luv

--- init.lua
+++ init.lua
@@ -447,11 +447,10 @@ require('lazy').setup({
     opts = {
       library = {
         -- Load luvit types when the `vim.uv` word is found
-        { path = 'luvit-meta/library', words = { 'vim%.uv' } },
+        { path = '${3rd}/luv/library', words = { 'vim%.uv' } },
       },
     },
   },
-  { 'Bilal2453/luvit-meta', lazy = true },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
