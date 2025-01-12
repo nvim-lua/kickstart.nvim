eef7772bd8df94622962a75cc974784417bf6798
Vojin <puricvojin@gmail.com>
2025-01-12 16:37:37 +0100

    added vim-visual-multi plugin

--- init.lua
+++ init.lua
@@ -922,6 +922,7 @@ require("lazy").setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require "kickstart.plugins.neo-tree",
+  require "custom.plugins.vim-visual-multi",
   -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
