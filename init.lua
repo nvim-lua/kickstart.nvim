7777fe5a34342bbb1e8ed931adddb6259959c551
Nhan Luu <62146587+nhld@users.noreply.github.com>
2025-01-13 16:30:28 +0700

    chore: remove redundant comment

--- init.lua
+++ init.lua
@@ -464,7 +464,6 @@ require('lazy').setup({
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
-      -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
       { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
