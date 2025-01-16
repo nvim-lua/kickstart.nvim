34233baa0e3223bd60bf3df25c8c6f0eeadb95ce
Razvan Grecu <razvan@stephanson.eu>
2025-01-16 19:25:25 +0200

    Create neotree.lua

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,10 @@
+return {
+    "nvim-neo-tree/neo-tree.nvim",
+    branch = "v3.x",
+    dependencies = {
+      "nvim-lua/plenary.nvim",
+      "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+      "MunifTanjim/nui.nvim",
+      -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+    }
+}
