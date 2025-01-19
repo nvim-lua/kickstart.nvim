8834de4589686e3bb24aafd7698fd3204f86ea53
Andrei Staicu <bleacheda@gmail.com>
2025-01-19 22:34:28 +0200

    remove whitespace

--- init.lua
+++ init.lua
@@ -250,7 +250,7 @@ require('lazy').setup({
   --
   -- Here is a more advanced example where we pass configuration
   -- options to `gitsigns.nvim`.
-  --  
+  --
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
     'lewis6991/gitsigns.nvim',
