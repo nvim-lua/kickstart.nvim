000fd72aefdac1e4599ac87409ec6399a0e5c9a2
0x4D5352 <67082011+0x4D5352@users.noreply.github.com>
2025-01-20 09:24:32 -0600

    update mini.nvim with suggested defaults

--- lua/custom/plugins/mini-nvim.lua
+++ lua/custom/plugins/mini-nvim.lua
@@ -1,1 +1,13 @@
-return { 'echasnovski/mini.nvim', version = false }
+return {
+  'echasnovski/mini.nvim',
+  version = false,
+  require('mini.ai').setup(),
+  require('mini.operators').setup(),
+  require('mini.surround').setup(),
+  require('mini.bracketed').setup(),
+  require('mini.files').setup(),
+  require('mini.pick').setup(),
+  -- require('mini.hues').setup(),
+  require('mini.icons').setup(),
+  require('mini.statusline').setup(),
+}
