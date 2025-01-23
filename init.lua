bfa7dac1953b91306d82a42bf5ba12fc33aa106e
Arpan Swaroop <arpan.swaroop@gmail.com>
2025-01-23 13:10:03 -0600

    ayo added a plugin

--- init.lua
+++ init.lua
@@ -849,14 +849,15 @@ require('lazy').setup({
     -- change the command in the config to whatever the name of that colorscheme is.
     --
     -- If you want to see what colorschemes are already installed, you can use `:Telescope colorscheme`.
-    'ellisonleao/gruvbox.nvim',
+    'rose-pine/neovim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    opts = { transparent_mode = false },
+    -- opts = { variant = 'auto', dark_variant = 'main' },
+
     init = function()
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'gruvbox'
+      vim.cmd.colorscheme 'rose-pine'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
