cd02e79c22e2ee450cc2f08e5eef8bf858b4ffc5
Eric Olerud <eric@olerud.com>
2025-01-22 21:36:30 -0500

    add Go LSP support

--- init.lua
+++ init.lua
@@ -501,7 +501,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         rust_analyzer = {},
         powershell_es = {},
