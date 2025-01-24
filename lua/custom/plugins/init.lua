14db8e5f36138e84f680dc73ca74a11626f74364
aryatsriv <aryatsrivastava06@gmail.com>
2025-01-25 00:44:42 +0530

    Update init.lua

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,47 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+return {
+  -- Other plugins...
+
+  -- Add nvim-tree plugin
+  {
+    'nvim-tree/nvim-tree.lua',
+    dependencies = { 'nvim-tree/nvim-web-devicons' }, -- For file icons
+    config = function()
+      require('nvim-tree').setup {
+        disable_netrw = true, -- Disable netrw
+        hijack_netrw = true, -- Hijack netrw for nvim-tree
+        view = {
+          side = 'left',
+          width = 30,
+        },
+        renderer = {
+          icons = {
+            show = {
+              file = true,
+              folder = true,
+              folder_arrow = true,
+              git = true,
+            },
+          },
+        },
+        diagnostics = {
+          enable = true, -- Show diagnostic info
+          icons = {
+            hint = '',
+            info = '',
+            warning = '',
+            error = '',
+          },
+        },
+        git = {
+          enable = true, -- Show git status
+        },
+        filters = {
+          dotfiles = false, -- Show hidden files
+        },
+      }
+    end,
+  },
+}
