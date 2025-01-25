8baae5ecf373a6a8fbe6d118c45d70ec8dbb9978
Alexander Kabolov <alexander@kabolov.com>
2025-01-25 18:49:21 +0100

    nord theme

--- init.lua
+++ init.lua
@@ -770,10 +770,17 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight'
+      -- vim.cmd.colorscheme 'tokyonight'
 
       -- You can configure highlights by doing something like:
       vim.cmd.hi 'Comment gui=none'
+    end,
+  },
+
+  {
+    'shaunsingh/nord.nvim',
+    init = function()
+      vim.cmd.colorscheme 'nord'
     end,
   },
 
