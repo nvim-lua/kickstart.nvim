56612daa5205b5a4810c9a7f10b484807bcc58b4
aryatsriv <aryatsrivastava06@gmail.com>
2025-01-29 18:33:56 +0530

    Update init.lua

--- init.lua
+++ init.lua
@@ -189,6 +189,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+--NvimTree Toggle
+vim.keymap.set('n', '<C-k><C-k>', ':NvimTreeToggle<CR>', { noremap = true, silent = true })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
