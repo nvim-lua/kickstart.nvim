90063935ad8fa8637ad4f99723688caa6a0e8f41
Oscar Pinochet <oscar.pinochet@usach.cl>
2025-02-03 00:21:44 -0300

    change yes completion from C-y to enter.

--- init.lua
+++ init.lua
@@ -1007,7 +1007,7 @@ require('lazy').setup({
           -- Accept ([y]es) the completion.
           --  This will auto-import if your LSP supports it.
           --  This will expand snippets if the LSP sent a snippet.
-          ['<C-y>'] = cmp.mapping.confirm { select = true },
+          ['<enter>'] = cmp.mapping.confirm { select = true },
 
           -- If you prefer more traditional completion keymaps,
           -- you can uncomment the following lines
