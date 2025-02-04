f89295166a324fd1ae9c3af14f0e94a1cbd31c6d
Abdullah Bodur <abdullahbodur.abbdr@gmail.com>
2025-02-04 08:39:45 +0100

    chore: enable relative number

--- init.lua
+++ init.lua
@@ -99,10 +99,10 @@ vim.g.have_nerd_font = false
 --  For more options, you can see `:help option-list`
 
 -- Make line numbers default
-vim.opt.number = true
+-- vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
