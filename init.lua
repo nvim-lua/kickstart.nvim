cccecc9a35b7bc3d8dd003753cd069bac68f97c1
Arpan Swaroop <arpan.swaroop@gmail.com>
2025-02-05 16:20:32 -0600

    yayyyy got vimtex to work properly now, opens up zathura

--- init.lua
+++ init.lua
@@ -88,7 +88,7 @@ P.S. You can delete this when you're done too. It's your config now! :)
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
 vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
+vim.g.maplocalleader = ','
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -44,6 +44,7 @@ return {
     init = function()
       -- VimTeX configuration goes here, e.g.
       vim.g.vimtex_view_method = 'zathura'
+      vim.g.vimtex_compiler_method = 'pdflatex'
     end,
   },
 }
