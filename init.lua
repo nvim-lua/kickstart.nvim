bc6fb494a06e2dd9b7bfdf2c423ad34687c177a1
Abdullah Bodur <abdullahbodur.abbdr@gmail.com>
2025-02-05 23:37:00 +0100

    chore: add java-nvim

--- init.lua
+++ init.lua
@@ -663,7 +663,8 @@ require('lazy').setup({
         'stylua', -- Used to format Lua code
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
-
+      require('java').setup()
+      require('lspconfig').jdtls.setup {}
       require('mason-lspconfig').setup {
         handlers = {
           function(server_name)
@@ -678,7 +679,9 @@ require('lazy').setup({
       }
     end,
   },
-
+  {
+    'nvim-java/nvim-java',
+  },
   { -- Autoformat
     'stevearc/conform.nvim',
     event = { 'BufWritePre' },
@@ -902,7 +905,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'java' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
