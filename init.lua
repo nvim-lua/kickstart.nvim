11fc2bea4ec0984518c1767d01a4a26fac569a05
Yaser Alraddadi <gam.phon@gmail.com>
2025-02-05 18:54:51 +0300

    update

--- init.lua
+++ init.lua
@@ -1118,6 +1118,13 @@ require('lazy').setup({
     --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
     --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
   },
+  --   'nu',
+  -- },
+  -- dependencies = {
+  --   -- Install official queries and filetype detection
+  --   -- alternatively, see section "Install official queries only"
+  --   { 'nushell/tree-sitter-nu' },
+  -- },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
