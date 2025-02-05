7e04c35b3f46f328c3b53a1e274375ecad5e902d
Arpan Swaroop <arpan.swaroop@gmail.com>
2025-02-05 16:14:59 -0600

    added vimtex, dont remember configuration arghhhhhh

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -37,4 +37,13 @@ return {
   {
     'mbbill/undotree',
   },
+  {
+    'lervag/vimtex',
+    lazy = false, -- we don't want to lazy load VimTeX
+    -- tag = "v2.15", -- uncomment to pin to a specific release
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_method = 'zathura'
+    end,
+  },
 }
