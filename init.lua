bfe28a0cbe87f7d8aa6de2e1f9da2114d707310a
ahmedsamyh <ahmedsamyh10@gmail.com>
2025-02-08 14:49:57 +0500

    [init.lua] Add autocmd for config.jsonc (waybar config)

--- init.lua
+++ init.lua
@@ -995,5 +995,12 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   end,
 })
 
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = 'config.jsonc',
+  callback = function()
+    vim.cmd '!killall -SIGUSR2 waybar'
+  end,
+})
+
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
