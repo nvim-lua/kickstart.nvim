c057743836608e163c058c46dd2467616b063bfd
bleacheda <60625523+bleacheda@users.noreply.github.com>
2025-02-15 05:32:50 +0200

    docs: clarify using opts = {} vs config = function() ... require('plu… (#1316)
    * docs: clarify using opts = {} vs config = function() ... require('plugin').setup({}) .. end
    
    The current documentation mentioning that using "require" is equivalent to using "opts" without detailing the use in the "config = function()" block seems inaccurate.
    Lower in the configuration the "config = function()" block is used without clarifying why it needed and what it does.
    This clarification may help new users understand the difference between the two, or how and where to place the "require" statement.
    
    * Update init.lua
    
    * remove whitespace

--- init.lua
+++ init.lua
@@ -234,12 +234,22 @@ require('lazy').setup({
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
   --
-  -- Use `opts = {}` to force a plugin to be loaded.
+  -- Use `opts = {}` to automatically pass options to a plugin's `setup()` function, forcing the plugin to be loaded.
   --
 
+  -- Alternatively, use `config = function() ... end` for full control over the configuration.
+  -- If you prefer to call `setup` explicitly, use:
+  --    {
+  --        'lewis6991/gitsigns.nvim',
+  --        config = function()
+  --            require('gitsigns').setup({
+  --                -- Your gitsigns configuration here
+  --            })
+  --        end,
+  --    }
+  --
   -- Here is a more advanced example where we pass configuration
-  -- options to `gitsigns.nvim`. This is equivalent to the following Lua:
-  --    require('gitsigns').setup({ ... })
+  -- options to `gitsigns.nvim`.
   --
   -- See `:help gitsigns` to understand what the configuration keys do
   { -- Adds git related signs to the gutter, as well as utilities for managing changes
