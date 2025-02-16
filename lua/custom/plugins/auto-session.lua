4ec26791f20b2ace1d5bb7d8ffaab90d65445945
Yaser Alraddadi <gam.phon@gmail.com>
2025-02-16 22:48:08 +0300

    update

--- lua/custom/plugins/auto-session.lua
+++ lua/custom/plugins/auto-session.lua
@@ -1,20 +1,20 @@
 return {
-  {
-    'rmagatti/auto-session',
-    lazy = false,
-    dependencies = {
-      'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
-    },
-    keys = {
-      -- Will use Telescope if installed or a vim.ui.select picker otherwise
-      { '<leader>wr', '<cmd>SessionSearch<CR>', desc = 'Session search' },
-      -- { '<leader>ws', '<cmd>SessionSave<CR>', desc = 'Save session' },
-      -- { '<leader>wa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
-    },
-    config = function()
-      require('auto-session').setup {
-        auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
-      }
-    end,
-  },
+  -- {
+  --   'rmagatti/auto-session',
+  --   lazy = false,
+  --   dependencies = {
+  --     'nvim-telescope/telescope.nvim', -- Only needed if you want to use sesssion lens
+  --   },
+  --   keys = {
+  --     -- Will use Telescope if installed or a vim.ui.select picker otherwise
+  --     { '<leader>wr', '<cmd>SessionSearch<CR>', desc = 'Session search' },
+  --     -- { '<leader>ws', '<cmd>SessionSave<CR>', desc = 'Save session' },
+  --     -- { '<leader>wa', '<cmd>SessionToggleAutoSave<CR>', desc = 'Toggle autosave' },
+  --   },
+  --   config = function()
+  --     require('auto-session').setup {
+  --       auto_session_suppress_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+  --     }
+  --   end,
+  -- },
 }
