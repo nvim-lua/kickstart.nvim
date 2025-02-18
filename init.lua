427b73325cd87d039c847bf476577afa50768647
Joaquín Guerra <joaquinguerratocino@gmail.com>
2025-02-18 01:10:48 +0100

    perf: load tokyonight.nvim in the intended way (#1360)
    Fixes #1357

--- init.lua
+++ init.lua
@@ -884,7 +884,14 @@ require('lazy').setup({
     --'folke/tokyonight.nvim',
     'ellisonleao/gruvbox.nvim',
     priority = 1000, -- Make sure to load this before all the other start plugins.
-    init = function()
+    config = function()
+      ---@diagnostic disable-next-line: missing-fields
+      require('tokyonight').setup {
+        styles = {
+          comments = { italic = false }, -- Disable italics in comments
+        },
+      }
+
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
