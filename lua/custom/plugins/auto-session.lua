8b7305f14bb80ce1eec9609180158872a2e274a1
Fabian Missbrenner <missbrenner@ccs-labs.org>
2025-02-19 10:32:56 +0100

    Use code minimap and store sessions

---
+++ lua/custom/plugins/auto-session.lua
@@ -0,0 +1,12 @@
+return {
+  'rmagatti/auto-session',
+  lazy = false,
+
+  ---enables autocomplete for opts
+  ---@module "auto-session"
+  ---@type AutoSession.Config
+  opts = {
+    suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+    -- log_level = 'debug',
+  }
+}
