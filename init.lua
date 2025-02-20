aa237307775d4b5848f497286ea506d7e2bf2733
Joshua Nussbaum <145164917+jnussbaumol@users.noreply.github.com>
2025-02-20 12:09:09 -0600

    update init

--- init.lua
+++ init.lua
@@ -393,7 +393,7 @@ require('lazy').setup({
       { 'j-hui/fidget.nvim', opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
-      'hrsh7th/cmp-nvim-lsp',
+      -- 'hrsh7th/cmp-nvim-lsp',
     },
     config = function()
       -- Brief aside: **What is LSP?**
