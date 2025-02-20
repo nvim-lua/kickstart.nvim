391833fcbf3791db3adf84bc7ba23b77f4c2dcc5
Joshua Nussbaum <145164917+jnussbaumol@users.noreply.github.com>
2025-02-20 08:40:33 -0600

    add venv selector plugin

---
+++ lua/custom/plugins/venv-selector.lua
@@ -0,0 +1,17 @@
+return {
+  'linux-cultist/venv-selector.nvim',
+  dependencies = {
+    'neovim/nvim-lspconfig',
+    'mfussenegger/nvim-dap',
+    'mfussenegger/nvim-dap-python', --optional
+    { 'nvim-telescope/telescope.nvim', branch = '0.1.x', dependencies = { 'nvim-lua/plenary.nvim' } },
+  },
+  lazy = false,
+  branch = 'regexp', -- This is the regexp branch, use this for the new version
+  config = function()
+    require('venv-selector').setup()
+  end,
+  keys = {
+    { ',v', '<cmd>VenvSelect<cr>' },
+  },
+}
