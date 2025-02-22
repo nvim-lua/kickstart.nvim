133b04bd3a9e17b8ce8e2e01973ee6073b77e838
Aoi Kurokawa <62386689+aoikurokawa@users.noreply.github.com>
2025-02-22 16:41:33 +0900

    Fix: enter (#3)

--- init.lua
+++ init.lua
@@ -867,7 +867,7 @@ require('lazy').setup({
 
            -- If you prefer more traditional completion keymaps,
            -- you can uncomment the following lines
-           --['<CR>'] = cmp.mapping.confirm { select = true },
+           ['<CR>'] = cmp.mapping.confirm { select = true },
            --['<Tab>'] = cmp.mapping.select_next_item(),
            --['<S-Tab>'] = cmp.mapping.select_prev_item(),
 
