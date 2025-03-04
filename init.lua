1a7651e4cb0af011e12de38f67b476c93fb67e95
ahmedsamyh <ahmedsamyh10@gmail.com>
2025-03-04 12:39:32 +0500

    [init.lua] Install presence.nvim (discord RPC)...
    - autocmd on BufWritePre xresources.

--- init.lua
+++ init.lua
@@ -228,6 +228,7 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+  'andweeb/presence.nvim',
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -968,6 +969,30 @@ require('lazy').setup({
   },
 })
 
+-- The setup config table shows all available config options with their default values:
+require('presence').setup {
+  -- General options
+  auto_update = true, -- Update activity based on autocmd events (if `false`, map or manually execute `:lua package.loaded.presence:update()`)
+  neovim_image_text = 'The One True Text Editor', -- Text displayed when hovered over the Neovim image
+  main_image = 'neovim', -- Main image display (either "neovim" or "file")
+  client_id = '793271441293967371', -- Use your own Discord application client id (not recommended)
+  log_level = nil, -- Log messages at or above this level (one of the following: "debug", "info", "warn", "error")
+  debounce_timeout = 10, -- Number of seconds to debounce events (or calls to `:lua package.loaded.presence:update(<filename>, true)`)
+  enable_line_number = false, -- Displays the current line number instead of the current project
+  blacklist = {}, -- A list of strings or Lua patterns that disable Rich Presence if the current file name, path, or workspace matches
+  buttons = true, -- Configure Rich Presence button(s), either a boolean to enable/disable, a static table (`{{ label = "<label>", url = "<url>" }, ...}`, or a function(buffer: string, repo_url: string|nil): table)
+  file_assets = {}, -- Custom file asset definitions keyed by file names and extensions (see default config at `lua/presence/file_assets.lua` for reference)
+  show_time = true, -- Show the timer
+
+  -- Rich Presence text options
+  editing_text = 'Editing %s', -- Format string rendered when an editable file is loaded in the buffer (either string or function(filename: string): string)
+  file_explorer_text = 'Browsing %s', -- Format string rendered when browsing a file explorer (either string or function(file_explorer_name: string): string)
+  git_commit_text = 'Committing changes', -- Format string rendered when committing changes in git (either string or function(filename: string): string)
+  plugin_manager_text = 'Managing plugins', -- Format string rendered when managing plugins (either string or function(plugin_manager_name: string): string)
+  reading_text = 'Reading %s', -- Format string rendered when a read-only or unmodifiable file is loaded in the buffer (either string or function(filename: string): string)
+  workspace_text = 'Working on %s', -- Format string rendered when in a git repository (either string or function(project_name: string|nil, filename: string): string)
+  line_number_text = 'Line %s out of %s', -- Format string rendered when `enable_line_number` is set to true (either string or function(line_number: number, line_count: number): string)
+}
 require('colorizer').setup()
 
 local lspconfig = require 'lspconfig'
@@ -975,6 +1000,8 @@ local lspconfig = require 'lspconfig'
 lspconfig['pyright'].setup {
   trace = 'verbose',
 }
+
+require('lspconfig').texlab.setup {}
 
 lspconfig['denols'].setup {
   trace = 'verbose',
@@ -1013,6 +1040,13 @@ vim.api.nvim_create_autocmd('BufWritePost', {
   pattern = '/home/momoyon/dotfiles/mako/.config/mako/config',
   callback = function()
     vim.cmd '!makoctl reload'
+  end,
+})
+
+vim.api.nvim_create_autocmd('BufWritePost', {
+  pattern = 'xresources',
+  callback = function()
+    vim.cmd '!xrdb -load %'
   end,
 })
 
