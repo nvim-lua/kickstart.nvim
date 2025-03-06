27382201d15a957fb98ec20a7152187c63064b5d
Lucas Garcia Rubio <lrubio@apoioprodesp.sp.gov.br>
2025-03-06 15:22:42 -0300

    adiciona java-debug-adapter em configurações do dap

--- init.lua
+++ init.lua
@@ -677,7 +677,6 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'java-debug-adapter',
         'java-test',
         'prettier',
         'eslint_d',
--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -94,7 +94,8 @@ return {
       -- online, please don't ask me how to install them :)
       ensure_installed = {
         -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        -- 'delve',
+        'java-debug-adapter',
       },
     }
 
