fcbf7c28735e7ed215013a8393c325286c171251
Karolis Arbaciauskas <karolis.arbaciauskas@gmail.com>
2025-03-06 22:31:53 +0200

    Add visual mode snippets/shorcuts

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -8,9 +8,18 @@ vim.api.nvim_create_user_command('DeleteOtherBuffers', function()
   require('snacks').bufdelete.other()
 end, { desc = 'Delete Other Buffers' })
 
+-- Resizing windows
 vim.keymap.set('n', '<A-h>', '<C-w>5<', { desc = 'Decrease window width' })
 vim.keymap.set('n', '<A-l>', '<C-w>5>', { desc = 'Increase window width' })
 vim.keymap.set('n', '<A-j>', '<C-w>1+', { desc = 'Increase window height' })
 vim.keymap.set('n', '<A-k>', '<C-w>1-', { desc = 'Decrease window height' })
+
+-- Move lines up and down
+vim.keymap.set('v', 'K', ":m '<-2<CR>gv=gv")
+vim.keymap.set('v', 'J', ":m '>+1<CR>gv=gv")
+
+-- Move lines left and right
+vim.keymap.set('v', '<', '<gv')
+vim.keymap.set('v', '>', '>gv')
 
 return {}
