7d205f9332d90b0acd55a4fc151d0250e003e396
Nicky Obreykov <nicky@areachica.se>
2025-03-08 12:06:27 +0100

    Add indent_line

--- lua/kickstart/plugins/indent_line.lua
+++ lua/kickstart/plugins/indent_line.lua
@@ -4,6 +4,15 @@ return {
     -- Enable `lukas-reineke/indent-blankline.nvim`
     -- See `:help ibl`
     main = 'ibl',
-    opts = {},
+    opts = {
+      indent = {
+        char = '│',
+      },
+      scope = {
+        enabled = false,
+        show_start = false,
+        show_end = false,
+      },
+    },
   },
 }
