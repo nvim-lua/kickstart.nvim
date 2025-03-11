019526c694d6cd1ea15b365a6a6e48961ed861ab
Tawfeeq <tawfeeqmeeri@gmail.com>
2025-03-11 12:17:10 +0100

    Added treesitter-textobjects

--- init.lua
+++ init.lua
@@ -1117,6 +1117,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
+    event = { 'BufReadPre', 'BufNewFile' },
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
@@ -1143,7 +1144,7 @@ require('lazy').setup({
         'markdown',
         'markdown_inline',
         'yaml',
-        'prisma'
+        'prisma',
       },
       -- Autoinstall languages that are not installed
       auto_install = true,
@@ -1172,8 +1173,8 @@ require('lazy').setup({
       -- with nvim-treesitter. You should go explore a few and see what interests you:
       --
       --    - Incremental selection: Included, see `:help nvim-treesitter-incremental-selection-mod`
-      --    - Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
-      --    - Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
+      --- Show your current context: https://github.com/nvim-treesitter/nvim-treesitter-context
+      --- Treesitter + textobjects: https://github.com/nvim-treesitter/nvim-treesitter-textobjects
     end,
   },
 
