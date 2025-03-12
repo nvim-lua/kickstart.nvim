5819f5559a7a416164c38f6ce35ce7066feca867
Crypto-Spartan <29098151+Crypto-Spartan@users.noreply.github.com>
2025-03-12 17:24:44 -0400

    feat: add `vim.opt.confirm = true` (#1384)

--- init.lua
+++ init.lua
@@ -156,6 +156,11 @@ vim.opt.cursorline = true
 -- Minimal number of screen lines to keep above and below the cursor.
 vim.opt.scrolloff = 10
 
+-- if performing an operation that would fail due to unsaved changes in the buffer (like `:q`),
+-- instead raise a dialog asking if you wish to save the current file(s)
+-- See `:help 'confirm'`
+vim.opt.confirm = true
+
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
 
