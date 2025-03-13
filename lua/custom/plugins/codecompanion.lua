406369e9659f0c61c439f278d406ac8ace6f1813
Hendra <hendra2392@gmail.com>
2025-03-13 22:01:25 +0700

    Update cc

--- lua/custom/plugins/codecompanion.lua
+++ lua/custom/plugins/codecompanion.lua
@@ -18,6 +18,17 @@ return {
       strategies = {
         chat = {
           adapter = 'anthropic',
+          slash_commands = {
+            ['file'] = {
+              -- Location to the slash command in CodeCompanion
+              callback = 'strategies.chat.slash_commands.file',
+              description = 'Select a file using FZF',
+              opts = {
+                provider = 'fzf_lua', -- Other options include 'default', 'mini_pick', 'fzf_lua', snacks
+                contains_code = true,
+              },
+            },
+          },
         },
         inline = {
           adapter = 'anthropic',
