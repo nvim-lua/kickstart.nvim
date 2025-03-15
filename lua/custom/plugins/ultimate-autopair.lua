1498ee4367e5ff30fcf0adada4c3b468623ef751
Tawfeeq <tawfeeqmeeri@gmail.com>
2025-03-15 14:54:18 +0100

    deleted completions

--- lua/custom/plugins/ultimate-autopair.lua
+++ lua/custom/plugins/ultimate-autopair.lua
@@ -1,8 +1,8 @@
 return {
-  'altermo/ultimate-autopair.nvim',
-  event = { 'InsertEnter', 'CmdlineEnter' },
-  branch = 'v0.6', --recommended as each new version will have breaking changes
-  opts = {
-    --Config goes here
-  },
+--   'altermo/ultimate-autopair.nvim',
+--   event = { 'InsertEnter', 'CmdlineEnter' },
+--   branch = 'v0.6', --recommended as each new version will have breaking changes
+--   opts = {
+--     --Config goes here
+--   },
 }
