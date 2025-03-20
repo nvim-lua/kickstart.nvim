65a4d0e7d0a3d3cdbf369fb9cf22ecd517b1e93e
MortenB22R <49730926+mortensb1@users.noreply.github.com>
2025-03-20 22:53:41 +0100

    p5

--- init.lua
+++ init.lua
@@ -194,6 +194,9 @@ vim.keymap.set('n', '<C-l>', '<C-w><C-l>', { desc = 'Move focus to the right win
 vim.keymap.set('n', '<C-j>', '<C-w><C-j>', { desc = 'Move focus to the lower window' })
 vim.keymap.set('n', '<C-k>', '<C-w><C-k>', { desc = 'Move focus to the upper window' })
 
+-- TODO: EGEN TEXT
+vim.keymap.set('n', '<leader>p', ':!start cmd.exe /K "live-server"<CR>', { desc = 'Live server Run' })
+
 -- [[ Basic Autocommands ]]
 --  See `:help lua-guide-autocommands`
 
