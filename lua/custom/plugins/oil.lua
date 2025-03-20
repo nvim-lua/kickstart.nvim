6df31f28065f03c7af890b3ac6388ce2ccb3eb88
Andrew <andrew@usemotion.com>
2025-03-20 08:48:36 -0700

    floating oil

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -8,6 +8,9 @@ return {
     delete_to_trash = true,
     skip_confirm_for_simple_edits = true,
 
+    float = {
+      max_width = 0.7,
+    },
     view_options = {
       show_hidden = true,
     },
@@ -17,6 +20,6 @@ return {
   -- Lazy loading is not recommended because it is very tricky to make it work correctly in all situations.
   lazy = false,
   init = function()
-    vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
+    vim.keymap.set('n', '-', '<CMD>Oil --float<CR>', { desc = 'Open parent directory' })
   end,
 }
