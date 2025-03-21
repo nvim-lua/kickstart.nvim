ceb56eb15a999d9021d8bbda4242dd011917f904
Aadi Rave <arave@purdue.edu>
2025-03-21 13:29:54 -0400

    added autosave, trouble etc.

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -332,4 +332,50 @@ return {
       vim.keymap.set('n', ',s', '<cmd> Symbols<CR>')
     end,
   },
+
+  -- autosave
+  {
+    'brianhuster/autosave.nvim',
+    event = 'InsertEnter',
+    opts = {}, -- Configuration here
+  },
+
+  -- diagnostics
+  {
+    'folke/trouble.nvim',
+    opts = {}, -- for default options, refer to the configuration section for custom setup.
+    cmd = 'Trouble',
+    keys = {
+      {
+        '<leader>xx',
+        '<cmd>Trouble diagnostics toggle<cr>',
+        desc = 'Diagnostics (Trouble)',
+      },
+      {
+        '<leader>xX',
+        '<cmd>Trouble diagnostics toggle filter.buf=0<cr>',
+        desc = 'Buffer Diagnostics (Trouble)',
+      },
+      {
+        '<leader>cs',
+        '<cmd>Trouble symbols toggle focus=false<cr>',
+        desc = 'Symbols (Trouble)',
+      },
+      {
+        '<leader>cl',
+        '<cmd>Trouble lsp toggle focus=false win.position=right<cr>',
+        desc = 'LSP Definitions / references / ... (Trouble)',
+      },
+      {
+        '<leader>xL',
+        '<cmd>Trouble loclist toggle<cr>',
+        desc = 'Location List (Trouble)',
+      },
+      {
+        '<leader>xQ',
+        '<cmd>Trouble qflist toggle<cr>',
+        desc = 'Quickfix List (Trouble)',
+      },
+    },
+  },
 }
