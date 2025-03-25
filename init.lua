4374b043adbf73e0af5093d4d106cf08f3fb40c8
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2025-03-26 01:13:16 +0800

    Copied snippets from sample nvim-java config for Kickstart.nvim

--- init.lua
+++ init.lua
@@ -840,7 +840,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'slint' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'slint', 'java' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       ignore_install = { 'latex' },
