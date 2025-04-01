7027051b4a565f5fd40bb7823f85a8f6629f1efd
orcensplitter <db71@tuta.io>
2025-04-01 20:21:10 +0200

    Meine Anpassungen an kickstart.nvim

---
+++ lua/custom/plugins/vimconfig.lua
@@ -0,0 +1,18 @@
+-- disable netrw
+vim.g.loaded_netrw = 1
+vim.g.loaded_netrwPlugin = 1
+
+-- Disable line wrap
+vim.opt.wrap = false
+
+-- No swap file
+vim.opt.swapfile = false
+
+-- Set shiftwidth and tabstop
+vim.opt.shiftwidth = 4
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+
+vim.opt.termguicolors = true
+
+return {}
