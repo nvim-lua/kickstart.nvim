c458068870791519a44660c4c7e75d4b546e8084
Alex Hebert <alex.hebert@lyteflo.com>
2025-04-01 15:39:07 -0400

    Update lint.lua

--- lua/kickstart/plugins/lint.lua
+++ lua/kickstart/plugins/lint.lua
@@ -7,6 +7,9 @@ return {
       local lint = require 'lint'
       lint.linters_by_ft = {
         markdown = { 'markdownlint' },
+        python = { 'ruff_format' },
+        json = { "jsonlint" },
+        go = {'golangci-lint'}
       }
 
       -- To allow other plugins to add linters to require('lint').linters_by_ft,
