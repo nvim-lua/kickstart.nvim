4bb4195a0141662298b2cfa890f092e37ceaf3f8
Alexandre Ignjatovic <alexandre.ignjatovic@gmail.com>
2025-04-02 08:00:44 +0200

    Add noselect to completion

--- init.lua
+++ init.lua
@@ -825,7 +825,7 @@ require('lazy').setup({
             luasnip.lsp_expand(args.body)
           end,
         },
-        completion = { completeopt = 'menu,menuone,noinsert' },
+        completion = { completeopt = 'menu,menuone,noinsert,noselect' },
 
         -- For an understanding of why these mappings were
         -- chosen, you will need to read `:help ins-completion`
