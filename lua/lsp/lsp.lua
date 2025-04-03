4cc7dd18371c2065b0ad1bf2f697e4dbe09670fc
Eric Olerud <eric@olerud.com>
2025-04-02 21:58:47 -0400

    disable extra lua warnings

--- lua/lsp/lsp.lua
+++ lua/lsp/lsp.lua
@@ -59,7 +59,7 @@ return {
               callSnippet = 'Replace',
             },
             -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-            -- diagnostics = { disable = { 'missing-fields' } },
+            diagnostics = { disable = { 'missing-fields' } },
           },
         },
       },
