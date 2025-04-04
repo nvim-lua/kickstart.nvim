e88815ad0871e3e8d85159f5c60ace7d8c5b7ae6
jensonjohnathon <johnathonreggentin@gmail.com>
2025-04-04 15:11:49 +0200

    Added LSP for Go and some Fromatters

--- init.lua
+++ init.lua
@@ -683,7 +683,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         -- clangd = {},
-        -- gopls = {},
+        gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
