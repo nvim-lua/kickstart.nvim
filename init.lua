a8cfa693b96dc19242202a80743cb1604e26218a
pateball <roger0@gmx.de>
2025-04-06 01:04:33 +0200

    search explorer shortcut

--- init.lua
+++ init.lua
@@ -436,7 +436,7 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
-
+      vim.keymap.set('n', '<leader>se', '<cmd>:Explore<CR>', { desc = '[S]earch [E]xplorer' })
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
         -- You can pass additional configuration to Telescope to change the theme, layout, etc.
