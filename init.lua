0ac5cb6802d7bd468fdf3ae64d475676d6c63645
Dmytro Onypko <vaporif@gmail.com>
2025-04-09 16:05:40 +0100

    Remove Telescope `0.1` branch lock (#1448)

--- init.lua
+++ init.lua
@@ -353,7 +353,6 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
