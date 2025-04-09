7327ec9ac832d1284fa594531bbd4f385692ceeb
Ruslan Rakhmanov <ruslan.rakhmanov.str@gmail.com>
2025-04-09 10:57:35 +0200

    Use Telescope version from master instead of 0.1.8

--- init.lua
+++ init.lua
@@ -359,7 +359,8 @@ require('lazy').setup({
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
     event = 'VimEnter',
-    branch = '0.1.x',
+    version = false,
+    -- tag = '0.1.8',
     dependencies = {
       'nvim-lua/plenary.nvim',
       { -- If encountering errors, see telescope-fzf-native README for installation instructions
