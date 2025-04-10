9229ef3cf4be2c3f2d3efa798d4c15b112c340cc
Albert Yu <albert.yu@navattic.com>
2025-04-10 16:56:53 -0500

    Use nightfox theme

--- init.lua
+++ init.lua
@@ -914,7 +914,14 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      -- vim.cmd.colorscheme 'tokyonight-storm'
+    end,
+  },
+  {
+    'EdenEast/nightfox.nvim',
+    priority = 1000,
+    config = function()
+      vim.cmd.colorscheme 'nightfox'
     end,
   },
 
