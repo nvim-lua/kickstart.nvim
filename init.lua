714f077cb0c42b56ef161ed76fd315e466e930c3
Tevin79 <wtevin2015@gmail.com>
2025-04-10 08:24:29 +0200

    refact: del some files

--- init.lua
+++ init.lua
@@ -4,7 +4,7 @@
 vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
--- Custom remaps
+-- ( Custom remaps
 vim.keymap.set('n', '!', '_') -- For macos only
 
 -- Center the view when moving
@@ -12,9 +12,11 @@ vim.keymap.set('n', '<C-d>', '<C-d>zz')
 vim.keymap.set('n', '<C-u>', '<C-u>zz')
 
 -- Access to file viewer easily
-vim.keymap.set('n', '<leader>pv', vim.cmd.Ex)
+vim.keymap.set('n', '<leader>pv', vim.cmd.Ex, { desc = '[P]roject files [V]iewer' })
 
--- End of custom remaps
+vim.api.nvim_set_hl(0, 'Normal', { bg = 'none' })
+vim.api.nvim_set_hl(0, 'NormalFloat', { bg = 'none' })
+-- End of custom remaps )
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = false
@@ -164,6 +166,12 @@ vim.opt.rtp:prepend(lazypath)
 --
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
+
+  {
+    'rose-pine/neovim',
+    name = 'rose-pine',
+  },
+
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
 
@@ -265,6 +273,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch' },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { '<leader>p', group = '[P]roject' },
       },
     },
   },
@@ -950,5 +959,88 @@ require('lazy').setup({
   },
 })
 
+require('rose-pine').setup {
+  variant = 'auto', -- auto, main, moon, or dawn
+  dark_variant = 'main', -- main, moon, or dawn
+  dim_inactive_windows = false,
+  extend_background_behind_borders = true,
+
+  enable = {
+    terminal = true,
+    legacy_highlights = true, -- Improve compatibility for previous versions of Neovim
+    migrations = true, -- Handle deprecated options automatically
+  },
+
+  styles = {
+    bold = true,
+    italic = true,
+    transparency = true,
+  },
+
+  groups = {
+    border = 'muted',
+    link = 'iris',
+    panel = 'surface',
+
+    error = 'love',
+    hint = 'iris',
+    info = 'foam',
+    note = 'pine',
+    todo = 'rose',
+    warn = 'gold',
+
+    git_add = 'foam',
+    git_change = 'rose',
+    git_delete = 'love',
+    git_dirty = 'rose',
+    git_ignore = 'muted',
+    git_merge = 'iris',
+    git_rename = 'pine',
+    git_stage = 'iris',
+    git_text = 'rose',
+    git_untracked = 'subtle',
+
+    h1 = 'iris',
+    h2 = 'foam',
+    h3 = 'rose',
+    h4 = 'gold',
+    h5 = 'pine',
+    h6 = 'foam',
+  },
+
+  palette = {
+    -- Override the builtin palette per variant
+    -- moon = {
+    --     base = '#18191a',
+    --     overlay = '#363738',
+    -- },
+  },
+
+  -- NOTE: Highlight groups are extended (merged) by default. Disable this
+  -- per group via `inherit = false`
+  highlight_groups = {
+    -- Comment = { fg = "foam" },
+    -- StatusLine = { fg = "love", bg = "love", blend = 15 },
+    -- VertSplit = { fg = "muted", bg = "muted" },
+    -- Visual = { fg = "base", bg = "text", inherit = false },
+  },
+
+  before_highlight = function(group, highlight, palette)
+    -- Disable all undercurls
+    -- if highlight.undercurl then
+    --     highlight.undercurl = false
+    -- end
+    --
+    -- Change palette colour
+    -- if highlight.fg == palette.pine then
+    --     highlight.fg = palette.foam
+    -- end
+  end,
+}
+
+vim.cmd 'colorscheme rose-pine'
+-- vim.cmd("colorscheme rose-pine-main")
+-- vim.cmd("colorscheme rose-pine-moon")
+-- vim.cmd("colorscheme rose-pine-dawn")
 -- The line beneath this is called `modeline`. See `:help modeline`
 -- vim: ts=2 sts=2 sw=2 et
--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,3 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
