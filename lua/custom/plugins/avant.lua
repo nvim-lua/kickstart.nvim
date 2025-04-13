698b646bedddc2ac97ef44baf8ef2d53fb388fee
Albert Yu <albert.yu@gmail.com>
2025-04-12 22:09:27 -0400

    Put max tokens back in

--- lua/custom/plugins/avant.lua
+++ lua/custom/plugins/avant.lua
@@ -10,7 +10,7 @@ return {
       endpoint = 'https://api.anthropic.com',
       model = 'claude-3-5-sonnet-20241022',
       temperature = 0,
-      -- max_tokens = 4096,
+      max_tokens = 4096,
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
