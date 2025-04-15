19269387176901e85d70b9a5c18cb3ba20af8998
pat-alt <patalt@patalt.org>
2025-04-15 10:30:52 +0200

    more useful plugins added: markdown render and todo comments

---
+++ lua/custom/plugins/todo-comments.lua
@@ -0,0 +1,9 @@
+return {
+  "folke/todo-comments.nvim",
+  dependencies = { "nvim-lua/plenary.nvim" },
+  opts = {
+    -- your configuration comes here
+    -- or leave it empty to use the default settings
+    -- refer to the configuration section below
+  }
+}
