01fd3b5a5ebada6455072e5cfd2db7384279c921
Sean Villars <github@seanvillars.com>
2025-04-16 11:32:45 -0500

    Updates

--- init.lua
+++ init.lua
@@ -1005,6 +1005,9 @@ require('lazy').setup({
   {
     'mg979/vim-visual-multi',
   },
+  {
+    'github/copilot.vim',
+  },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
   -- init.lua. If you want these files, they are in the repository, so you can just download them and
