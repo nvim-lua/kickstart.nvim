8240e2332033440dd34a075ae7c42afa129dfd30
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-17 23:00:04 +0200

    UPDATED init.lua to enable clang C++ LSP functionality

--- init.lua
+++ init.lua
@@ -663,7 +663,7 @@ require('lazy').setup({
       --  - settings (table): Override the default settings passed when initializing the server.
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
-        -- clangd = {},
+        clangd = {},
         -- gopls = {},
         -- pyright = {},
         -- rust_analyzer = {},
