8a94b47a08661594c7eee5409fbc07642a98e8bd
Ricman <ricardofreccero@hotmail.com>
2025-04-20 04:44:38 -0300

    Agregué el soporte en vimtex para poder usar gnuplots para gráficos mas potentes.

--- lua/custom/plugins/vimtex.lua
+++ lua/custom/plugins/vimtex.lua
@@ -1,6 +1,17 @@
 return {
   'lervag/vimtex',
   lazy = false,
+  config = function()
+    vim.g.vimtex_compiler_latexmk = {
+      options = {
+        '-shell-escape',
+        '-verbose',
+        '-file-line-error',
+        '-synctex=1',
+        '-interaction=nonstopmode',
+      },
+    }
+  end,
   init = function()
     vim.g.vimtex_view_general_viewer = 'C:/Users/ricar/AppData/Local/SumatraPDF/SumatraPDF.exe'
     vim.g.vimtex_view_general_options = '-reuse-instance -forward-search @tex @line @pdf'
