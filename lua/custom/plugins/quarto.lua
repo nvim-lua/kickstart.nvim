fecd57879991c2d5f2a106fc9f86c3787efc688c
pat-alt <patalt@patalt.org>
2025-04-22 09:08:31 +0200

    more on quarto code running

--- lua/custom/plugins/quarto.lua
+++ lua/custom/plugins/quarto.lua
@@ -11,6 +11,17 @@ return {
         default_method = 'slime',
       },
     },
+    config = function()
+      local runner = require 'quarto.runner'
+      vim.keymap.set('n', '<C-c>c', runner.run_cell, { desc = 'run cell', silent = true })
+      vim.keymap.set('n', '<C-c>a', runner.run_above, { desc = 'run cell and above', silent = true })
+      vim.keymap.set('n', '<C-c>A', runner.run_all, { desc = 'run all cells', silent = true })
+      vim.keymap.set('n', '<C-c>l', runner.run_line, { desc = 'run line', silent = true })
+      vim.keymap.set('v', '<C-c>r', runner.run_range, { desc = 'run visual range', silent = true })
+      -- vim.keymap.set('n', '<C-c>RA', function()
+      --   runner.run_all(true)
+      -- end, { desc = 'run all cells of all languages', silent = true })
+    end,
     dependencies = {
       'jmbuhr/otter.nvim',
       'nvim-treesitter/nvim-treesitter',
@@ -28,6 +39,23 @@ return {
         socket_name = vim.api.nvim_eval 'get(split($TMUX, ","), 0)',
         target_pane = '{bottom}',
       }
+    end,
+    config = function()
+      vim.g.slime_input_pid = false
+      vim.g.slime_suggest_default = true
+      vim.g.slime_menu_config = false
+      vim.g.slime_neovim_ignore_unlisted = true
+      vim.b.slime_cell_delimiter = '```'
+      local function mark_terminal()
+        local job_id = vim.b.terminal_job_id
+        vim.print('job_id: ' .. job_id)
+      end
+
+      local function set_terminal()
+        vim.fn.call('slime#config', {})
+      end
+      vim.keymap.set('n', '<leader>cm', mark_terminal, { desc = '[m]ark terminal' })
+      vim.keymap.set('n', '<leader>cs', set_terminal, { desc = '[s]et terminal' })
     end,
   },
 }
