fc3f20a29be3bd2fbdb8ebe0afc731ac0ae4401a
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-22 22:21:34 +0200

    UPDATED init.lua to enable rust analyzer LSP capabilities.

--- init.lua
+++ init.lua
@@ -666,7 +666,7 @@ require('lazy').setup({
         clangd = {},
         -- gopls = {},
         -- pyright = {},
-        -- rust_analyzer = {},
+        rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
         --
         -- Some languages (like typescript) have entire language plugins that can be useful:
