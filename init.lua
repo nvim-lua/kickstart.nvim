ee6c4368ae41e8cb5322926aff9f07bb83da7b1f
Nikolas (Daniel) Vincenti <165183152+NikVince@users.noreply.github.com>
2025-04-26 12:09:00 +0200

    UPDATED init.lua to support auto-formatting upon saving for Rust.

--- init.lua
+++ init.lua
@@ -789,7 +789,7 @@ require('lazy').setup({
         lua = { 'stylua' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
-        --
+        rust = { 'rustfmt' },
         -- You can use 'stop_after_first' to run the first available formatter from the list
         -- javascript = { "prettierd", "prettier", stop_after_first = true },
       },
