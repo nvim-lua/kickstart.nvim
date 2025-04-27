8fb5068d6942bd0bc6f0e1fd56dc09d04056324a
Sangho Lee <sangholee45@outlook.com>
2025-04-27 20:07:59 +0900

    fix: correct gitsigns mapping for <leader>tD
    The previous mapping <leader>tD was described as "[T]oggle git show [D]eleted"
    but was mapped to preview_hunk_inline, which didn't match the description.
    This commit updates the mapping to toggle_deleted, which fits the description.

--- lua/kickstart/plugins/gitsigns.lua
+++ lua/kickstart/plugins/gitsigns.lua
@@ -54,7 +54,7 @@ return {
         end, { desc = 'git [D]iff against last commit' })
         -- Toggles
         map('n', '<leader>tb', gitsigns.toggle_current_line_blame, { desc = '[T]oggle git show [b]lame line' })
-        map('n', '<leader>tD', gitsigns.preview_hunk_inline, { desc = '[T]oggle git show [D]eleted' })
+        map('n', '<leader>td', gitsigns.toggle_deleted, { desc = '[T]oggle git show [d]eleted' })
       end,
     },
   },
