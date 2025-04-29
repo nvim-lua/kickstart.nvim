a50a50293dbafad95132478ec1dc6b736a7a2966
Lepretre, Romain (ext) (SI GSW BELNAF GC EN) <romain.lepretre.ext@siemens.com>
2025-04-29 09:37:38 +0200

    :heavy_minus_sign: remove vim.surround as kickstart already installed mini.surround

--- init.lua
+++ init.lua
@@ -243,7 +243,6 @@ vim.opt.rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'tpope/vim-sleuth', -- Detect tabstop and shiftwidth automatically
-  'tpope/vim-surround',
   'catppuccin/nvim',
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
