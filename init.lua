b4bb1ea6920b783c5b6f86bd7fc433b444e9c79c
orip <oriori1703@gmail.com>
2025-04-30 00:48:00 +0300

    Add a comment about using vim.opt instead of vim.o

--- init.lua
+++ init.lua
@@ -144,6 +144,11 @@ vim.o.splitbelow = true
 -- Sets how neovim will display certain whitespace characters in the editor.
 --  See `:help 'list'`
 --  and `:help 'listchars'`
+--
+--  Notice listchars is set using `vim.opt` instead of `vim.o`.
+--  It is very similar to `vim.o` but offers an interface for conveniently interacting with tables.
+--   See `:help lua-options`
+--   and `:help lua-options-guide`
 vim.o.list = true
 vim.opt.listchars = { tab = '» ', trail = '·', nbsp = '␣' }
 
