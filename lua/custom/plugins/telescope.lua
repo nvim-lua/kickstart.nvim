00404dc3df57ec2b13b9fc6adb995c4811cdf753
dlond <dlond@me.com>
2025-05-09 13:38:59 +1200

    more bugs

--- lua/custom/plugins/telescope.lua
+++ lua/custom/plugins/telescope.lua
@@ -1,10 +1,10 @@
--- Telescope override configuration
-
 return {
+  -- ========================================
+  -- Telescope Override
+  -- ========================================
   {
     'nvim-telescope/telescope.nvim',
-    -- Ensure dependencies are loaded (lazy.nvim usually handles this if defined elsewhere)
-    -- Listing them here ensures they are available if Telescope is the first plugin requiring them.
+    -- Ensure dependencies are loaded
     dependencies = {
       'nvim-lua/plenary.nvim',
       {
@@ -24,17 +24,17 @@ return {
           find_command = { 'rg', '--files', '--hidden', '-g', '!.git' },
         },
       },
-      -- Ensure extensions are configured if kickstart doesn't handle it via opts
+      -- Configure extensions
       extensions = {
         ['ui-select'] = {
           require('telescope.themes').get_dropdown(),
         },
         -- Configuration for fzf-native extension
         fzf = {
-          fuzzy = true,
-          override_generic_sorter = true,
-          override_file_sorter = true,
-          case_mode = 'smart_case',
+          fuzzy = true, -- Enable fuzzy matching
+          override_generic_sorter = true, -- Override the generic sorter
+          override_file_sorter = true, -- Override the file sorter
+          case_mode = 'smart_case', -- Ignore case unless capitals are used
         },
       },
     },
@@ -44,6 +44,43 @@ return {
       -- Load extensions after setup
       pcall(require('telescope').load_extension, 'fzf')
       pcall(require('telescope').load_extension, 'ui-select')
+
+      -- *** ADD TELESCOPE KEYMAPS HERE ***
+      local builtin = require 'telescope.builtin'
+      -- Add the find_files keymap
+      vim.keymap.set('n', '<leader>sf', builtin.find_files, { desc = '[S]earch [F]iles' })
+
+      -- Add other Telescope keymaps from kickstart's init.lua (uncomment to enable)
+      vim.keymap.set('n', '<leader>sh', builtin.help_tags, { desc = '[S]earch [H]elp' })
+      vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
+      vim.keymap.set('n', '<leader>ss', builtin.builtin, { desc = '[S]earch [S]elect Telescope' })
+      vim.keymap.set('n', '<leader>sw', builtin.grep_string, { desc = '[S]earch current [W]ord' })
+      vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
+      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+
+      -- Fuzzy search in current buffer (corrected function body)
+      vim.keymap.set('n', '<leader>/', function()
+        builtin.current_buffer_fuzzy_find(require('telescope.themes').get_dropdown {
+          winblend = 10,
+          previewer = false,
+        })
+      end, { desc = '[/] Fuzzily search in current buffer' })
+
+      -- Search in open files (corrected function body)
+      vim.keymap.set('n', '<leader>s/', function()
+        builtin.live_grep {
+          grep_open_files = true,
+          prompt_title = 'Live Grep in Open Files',
+        }
+      end, { desc = '[S]earch [/] in Open Files' })
+
+      -- Search Neovim files (corrected function body)
+      vim.keymap.set('n', '<leader>sn', function()
+        builtin.find_files { cwd = vim.fn.stdpath 'config' }
+      end, { desc = '[S]earch [N]eovim files' })
     end,
   },
 }
