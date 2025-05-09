2639f27f8445468d228cdce518e303ef02c4ea28
dlond <dlond@me.com>
2025-05-09 12:19:20 +1200

    fixing keymaps

--- init.lua
+++ init.lua
@@ -219,6 +219,9 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   end,
 })
 
+require 'custom.options'
+require 'custom.keymaps'
+
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
 local lazypath = vim.fn.stdpath 'data' .. '/lazy/lazy.nvim'
