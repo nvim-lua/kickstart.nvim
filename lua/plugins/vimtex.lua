04638df2796486289d55e312d770485ad4fb9636
Diego Miguel M. Villamil <dmvillamil@up.edu.ph>
2025-05-11 03:45:57 +0800

    fixed vimtex.lua formatting

--- lua/plugins/vimtex.lua
+++ lua/plugins/vimtex.lua
@@ -1,11 +1,11 @@
 return {
-	{
-	    'lervag/vimtex',
-	    -- lazy = false,
-	    init = function()
-	      -- VimTeX configuration goes here, e.g.
-	      vim.g.vimtex_view_general_viewer = 'okular'
-	      vim.g.vimtex_compiler_method = 'latexmk'
-	    end,
-	}
+  {
+    'lervag/vimtex',
+    -- lazy = false,
+    init = function()
+      -- VimTeX configuration goes here, e.g.
+      vim.g.vimtex_view_general_viewer = 'okular'
+      vim.g.vimtex_compiler_method = 'latexmk'
+    end,
+  },
 }
