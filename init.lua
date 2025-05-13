590600ac7ff324f4fd4e090c63e898a13d5801a9
Hendrik Brinkmann <brinkmann@sipgate.de>
2025-05-13 13:12:08 +0200

    plugin: add toggleterm

--- init.lua
+++ init.lua
@@ -976,7 +976,7 @@ require('lazy').setup({
   --    This is the easiest way to modularize your config.
   --
   --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
-  -- { import = 'custom.plugins' },
+  { import = 'custom.plugins' },
   --
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
   -- Or use telescope!
---
+++ lua/custom/plugins/toggleterm.lua
@@ -0,0 +1,9 @@
+return {
+  {
+    'akinsho/toggleterm.nvim',
+    version = '*',
+    config = function()
+      require('toggleterm').setup()
+    end,
+  },
+}
