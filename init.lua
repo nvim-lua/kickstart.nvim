ea8714273198033dd249e998f1cfeb2b91845fff
Ye Thi Ha Htwe <yethihahtwe319@gmail.com>
2025-05-15 09:27:03 +0700

    Checkout to Linux branch

--- init.lua
+++ init.lua
@@ -907,7 +907,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-storm'
+      vim.cmd.colorscheme 'tokyonight-night'
     end,
   },
 
