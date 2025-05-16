6e636ad325953d1a0460de8c5869f7136a57ffcd
Daniel B Sherry <dbsherry890@gmail.com>
2025-05-16 10:30:46 -0500

    switching to Lazy

--- lua/user/telescope.lua
+++ lua/user/telescope.lua
@@ -82,17 +82,20 @@ telescope.setup {
     },
   },
   pickers = {
+    -- find_files = { theme = 'dropdown'},
     -- Default configuration for builtin pickers goes here:
     -- picker_name = {
     --   picker_config_key = value,
     --   ...
-    -- }
+    -- },
     -- Now the picker_config_key will be applied every time you call this
     -- builtin picker
     planets = {
       show_pluto = true,
+      show_moon = true,
     },
   },
+
   extensions = {
     media_files = {
       -- filetypes whitelist
