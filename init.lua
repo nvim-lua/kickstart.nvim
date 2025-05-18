7c31d2ccf8b35e2cb5a7a7c8ee29c39ec73747c5
Ronan Dalton <86718942+ronandalton@users.noreply.github.com>
2025-05-18 13:20:27 +1200

    fix(diagnostic): remove unnecessary config lines
    The following lines do nothing and can be removed:
    
     format = function(diagnostic)
       local diagnostic_message = {
         [vim.diagnostic.severity.ERROR] = diagnostic.message,
         [vim.diagnostic.severity.WARN] = diagnostic.message,
         [vim.diagnostic.severity.INFO] = diagnostic.message,
         [vim.diagnostic.severity.HINT] = diagnostic.message,
       }
       return diagnostic_message[diagnostic.severity]
     end,
    
    The following line causes diagnostics which aren't of severity level
    error to not have the specific text area underlined. This is less useful
    than the default of showing underlines for all diagnostic levels, so
    remove this line as well:
    
      underline = { severity = vim.diagnostic.severity.ERROR },

--- init.lua
+++ init.lua
@@ -631,7 +631,6 @@ require('lazy').setup({
       vim.diagnostic.config {
         severity_sort = true,
         float = { border = 'rounded', source = 'if_many' },
-        underline = { severity = vim.diagnostic.severity.ERROR },
         signs = vim.g.have_nerd_font and {
           text = {
             [vim.diagnostic.severity.ERROR] = '󰅚 ',
@@ -643,15 +642,6 @@ require('lazy').setup({
         virtual_text = {
           source = 'if_many',
           spacing = 2,
-          format = function(diagnostic)
-            local diagnostic_message = {
-              [vim.diagnostic.severity.ERROR] = diagnostic.message,
-              [vim.diagnostic.severity.WARN] = diagnostic.message,
-              [vim.diagnostic.severity.INFO] = diagnostic.message,
-              [vim.diagnostic.severity.HINT] = diagnostic.message,
-            }
-            return diagnostic_message[diagnostic.severity]
-          end,
         },
       }
 
