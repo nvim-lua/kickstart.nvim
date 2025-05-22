e3cd0554460afad14ca4cebea2dea9b6d6ed8e65
Gonzako <Gonzako123@gmail.com>
2025-05-22 16:27:10 +0200

    Fixed Guess-Indent not loading by default

--- init.lua
+++ init.lua
@@ -247,8 +247,11 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
 
+  {
+    'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+    opts = {},
+  },
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
