b49bd0131a51ae884a335aedccd915c32326849f
Pakin49 <philgus884@gmail.com>
2025-05-25 01:57:50 +0700

    .

--- init.lua
+++ init.lua
@@ -684,6 +684,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
+        hdl_checker = {},
         --gopls = {},
         pyright = {},
         -- rust_analyzer = {},
