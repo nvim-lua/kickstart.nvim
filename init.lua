59e6f7246ef26ccc10aac74c1bca941d0ed570da
Petr Sykora <petr.sykora@gmail.com>
2025-05-25 10:59:29 +0200

    fix: disable treesitter for html because it is crashing the neovim

--- init.lua
+++ init.lua
@@ -952,8 +952,9 @@ require('lazy').setup({
         --  If you are experiencing weird indenting issues, add the language to
         --  the list of additional_vim_regex_highlighting and disabled languages for indent.
         additional_vim_regex_highlighting = { 'ruby' },
+        disable = { "html" },
       },
-      indent = { enable = true, disable = { 'ruby' } },
+      indent = { enable = true, disable = { 'ruby', "html" } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
