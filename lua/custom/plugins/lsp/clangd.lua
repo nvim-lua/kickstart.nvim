c3b32d95c90e66269f5e1085b81ef0f2b69a981c
dlond <dlond@me.com>
2025-05-30 06:58:39 +1200

    cleanup

--- lua/custom/plugins/lsp/clangd.lua
+++ lua/custom/plugins/lsp/clangd.lua
@@ -114,7 +114,7 @@ function M.pick_commands_dir()
           local entry = require('telescope.actions.state').get_selected_entry()
           require('telescope.actions').close(prompt_bufnr)
           vim.defer_fn(function()
-            if entry[1] then
+            if entry then
               M.start_clangd(entry[1])
             end
           end, 100)
