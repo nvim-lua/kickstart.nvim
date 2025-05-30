84e58e64bc2cdbbb0704337d01e103621a627aad
peturparkur <peter@nagymathe.xyz>
2025-05-30 23:40:03 +0200

    nixd additional configuration

--- lua/utils/languages.lua
+++ lua/utils/languages.lua
@@ -68,10 +68,29 @@ local Languages = {
     },
   },
   ['nix'] = {
-    nixd = {},
-    ['nil_ls'] = {
-      alias = 'nil',
+    nixd = {
+      settings = {
+        nixd = {
+          nixpkgs = {
+            expr = 'import <nixpkgs> { }',
+          },
+          formatting = {
+            command = { 'nixfmt' },
+          },
+          options = {
+            nixos = {
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).nixosConfigurations.k-on.options',
+            },
+            home_manager = {
+              expr = '(builtins.getFlake ("git+file://" + toString ./.)).homeConfigurations."ruixi@k-on".options',
+            },
+          },
+        },
+      },
     },
+    -- ['nil_ls'] = {
+    --   alias = 'nil',
+    -- },
   },
   ['bash'] = {
     bashls = {
