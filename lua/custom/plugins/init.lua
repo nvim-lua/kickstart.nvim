98033761fda8a128e2fe42210ebbe33725819c92
Adam Beck <adam.beck@icf.com>
2025-06-02 08:15:11 -0400

    fzf-lua keybinding

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -4,6 +4,7 @@
 -- See the kickstart.nvim README for more information
 vim.keymap.set('n', '-', '<CMD>Oil<CR>', { desc = 'Open parent directory' })
 
+vim.g.copilot_enabled = false -- Disable Copilot by default
 vim.keymap.set('n', '<leader>cpd', ':Copilot disable<cr>', { silent = true, noremap = true }) -- Disable Copilot
 vim.keymap.set('n', '<leader>cpe', ':Copilot enable<cr>', { silent = true, noremap = true }) -- Enable Copilot
 
@@ -27,17 +28,29 @@ return {
     dependencies = { 'echasnovski/mini.icons' },
     opts = {},
     config = function()
-      require('fzf-lua').setup {}
+      require('fzf-lua').setup {
+        grep = {},
+      }
 
       local builtin = require 'fzf-lua'
 
       vim.keymap.set('n', '<leader>sf', builtin.files, { desc = '[S]earch [F]iles' })
+      vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>sw', builtin.grep_cword, { desc = '[S]earch current [W]ord' })
       vim.keymap.set('n', '<leader>sk', builtin.keymaps, { desc = '[S]earch [K]eymaps' })
       vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
       vim.keymap.set('n', '<leader>sg', builtin.live_grep, { desc = '[S]earch by [G]rep' })
       vim.keymap.set('n', '<leader>/', builtin.lgrep_curbuf, { desc = '[/] Fuzzily search in current buffer' })
+      vim.keymap.set('n', '<leader>sd', builtin.diagnostics_document, { desc = '[S]earch [D]iagnostics' })
+      vim.keymap.set('n', '<leader>sG', function()
+        builtin.live_grep {
+          file_ignore_patterns = {
+            '%.spec%.',
+            '%.test%.',
+          },
+        }
+      end, { desc = '[S]earch by [G]rep without Tests' })
     end,
   },
   'JoosepAlviste/nvim-ts-context-commentstring',
