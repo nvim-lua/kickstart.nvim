1a67b5a87cabd0248509c2361d14ced845b3073a
Ruslan Rakhmanov <ruslan.rakhmanov.str@gmail.com>
2025-06-06 21:58:19 +0200

    Added precognition

--- init.lua
+++ init.lua
@@ -895,7 +895,7 @@ require('lazy').setup({
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
       require('everforest').setup {
-        background = 'hard',
+        background = 'soft',
         ui_contrast = 'high',
       }
 
@@ -1083,6 +1083,37 @@ require('lazy').setup({
     'rcarriga/nvim-notify',
     lazy = false,
     opts = {},
+  },
+
+  {
+    'tris203/precognition.nvim',
+    --event = "VeryLazy",
+    opts = {
+      -- startVisible = true,
+      -- showBlankVirtLine = true,
+      -- highlightColor = { link = "Comment" },
+      -- hints = {
+      --      Caret = { text = "^", prio = 2 },
+      --      Dollar = { text = "$", prio = 1 },
+      --      MatchingPair = { text = "%", prio = 5 },
+      --      Zero = { text = "0", prio = 1 },
+      --      w = { text = "w", prio = 10 },
+      --      b = { text = "b", prio = 9 },
+      --      e = { text = "e", prio = 8 },
+      --      W = { text = "W", prio = 7 },
+      --      B = { text = "B", prio = 6 },
+      --      E = { text = "E", prio = 5 },
+      -- },
+      -- gutterHints = {
+      --     G = { text = "G", prio = 10 },
+      --     gg = { text = "gg", prio = 9 },
+      --     PrevParagraph = { text = "{", prio = 8 },
+      --     NextParagraph = { text = "}", prio = 8 },
+      -- },
+      -- disabled_fts = {
+      --     "startify",
+      -- },
+    },
   },
 
   -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
