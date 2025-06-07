63fe72a39c7edcb35e5a9402201b9ab81e5ff4b2
aryat <aryatsrivastava06@gmail.com>
2025-06-07 21:17:48 +0530

    file path issue fixed

--- init.lua
+++ init.lua
@@ -384,21 +384,20 @@ require('lazy').setup({
       -- [[ Configure Telescope ]]
       -- See `:help telescope` and `:help telescope.setup()`
       require('telescope').setup {
-        -- You can put your default mappings / updates / etc. in here
-        --  All the info you're looking for is in `:help telescope.setup()`
-        --
-       -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
-        -- pickers = {}
+        defaults = {
+          preview = {
+            hide_on_startup = false,
+          },
+          previewer = true,
+          file_ignore_patterns = {},
+          path_display = { "truncate" },
+        },
         extensions = {
           ['ui-select'] = {
             require('telescope.themes').get_dropdown(),
           },
-      },
-    }
+        },
+      }
 
       -- Enable Telescope extensions if they are installed
       pcall(require('telescope').load_extension, 'fzf')
