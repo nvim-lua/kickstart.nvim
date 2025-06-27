69f9e3d5b49a38205d7385180bc2a996bb01706b
Albert Yu <albert.yu@gmail.com>
2025-06-26 23:15:59 -0400

    Update avante config

--- lua/custom/plugins/avante.lua
+++ lua/custom/plugins/avante.lua
@@ -2,15 +2,22 @@ return {
   'yetone/avante.nvim',
   event = 'VeryLazy',
   version = false, -- Never set this value to "*"! Never!
+  ---@module 'avante'
+  ---@type avante.Config
   opts = {
     -- add any opts here
     -- for example
     provider = 'claude',
-    claude = {
-      endpoint = 'https://api.anthropic.com',
-      model = 'claude-3-5-sonnet-20241022',
-      temperature = 0,
-      max_tokens = 4096,
+    providers = {
+      claude = {
+        endpoint = 'https://api.anthropic.com',
+        model = 'claude-sonnet-4-20250514',
+        timeout = 30000,
+        extra_request_body = {
+          temperature = 0.75,
+          max_tokens = 20480,
+        },
+      },
     },
   },
   -- if you want to build from source then do `make BUILD_FROM_SOURCE=true`
