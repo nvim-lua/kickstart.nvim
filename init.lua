fab4b3ec593282158df0481786016f6826888c67
Joshua Gartmeier <joshua@gartmeier.dev>
2025-06-30 08:54:12 +0200

    fix: add empty opts to guess-indent plugin to ensure setup() is called
    The plugin requires opts to be defined to run its setup function which
    creates the :GuessIndent command and autocommands.

--- init.lua
+++ init.lua
@@ -247,7 +247,7 @@ rtp:prepend(lazypath)
 -- NOTE: Here is where you install your plugins.
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
-  'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
+  { 'NMAC427/guess-indent.nvim', opts = {} }, -- Detect tabstop and shiftwidth automatically
 
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
