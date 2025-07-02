c11ea3e41844d1f2190bdefb306871586a51a3fc
Batoor <123397108+batoorsayed@users.noreply.github.com>
2025-07-01 18:54:30 -0700

    add markdownlint to Mason auto-install

--- init.lua
+++ init.lua
@@ -716,6 +716,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
+        'markdownlint', -- Markdown linter
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
