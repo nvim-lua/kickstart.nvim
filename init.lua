9be57a4953b4e852cbe4556ae279e616b2997bce
Dominik <wezik@users.noreply.github.com>
2025-07-04 11:30:07 +0200

    Add keymap for accepting lsp completions

--- init.lua
+++ init.lua
@@ -858,6 +858,7 @@ require('lazy').setup({
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
         preset = 'default',
+        ['<C-k>'] = { 'accept' },
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
