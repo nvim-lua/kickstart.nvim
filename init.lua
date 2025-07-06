ebdfd6f45ad0ac59078c9f8811a6d578c5bb5981
Petr Sykora <petr.sykora@gmail.com>
2025-07-06 13:59:26 +0200

    feat: disable treesitter indent for yaml

--- init.lua
+++ init.lua
@@ -954,7 +954,7 @@ require('lazy').setup({
         additional_vim_regex_highlighting = { 'ruby' },
         disable = { "html" },
       },
-      indent = { enable = true, disable = { 'ruby', "html" } },
+      indent = { enable = true, disable = { 'ruby', "html", "yaml" } },
     },
     -- There are additional nvim-treesitter modules that you can use to interact
     -- with nvim-treesitter. You should go explore a few and see what interests you:
