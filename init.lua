60c3c79d36e455215260e6a76637ba28d3b1c0e9
Philipp Szechenyi <philipp.szechenyi@cgm.com>
2025-07-10 15:28:55 +0200

    explicitly enable telescope plugin, add some comments explainging why

--- init.lua
+++ init.lua
@@ -360,6 +360,15 @@ require('lazy').setup({
 
   { -- Fuzzy Finder (files, lsp, etc)
     'nvim-telescope/telescope.nvim',
+    -- To disable the default Telescope plugin and replace it with
+    -- another picker (like snacks), set enabled to false and
+    -- Enable your replacement picker by requiring it explicitly (e.g., 'kickstart.plugins.snacks')
+    -- By default, Telescope is included and acts as your picker for everything.
+
+    -- Note: When you customize your config for yourself,
+    -- it’s best to remove the Telescope plugin config entirely
+    -- instead of just disabling it here, to keep your config clean.
+    enabled = true,
     event = 'VimEnter',
     dependencies = {
       'nvim-lua/plenary.nvim',
