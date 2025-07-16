2bf0ff4c165ec43780c9900c2c115627089657df
Dennis Chan <dennisming2012@gmail.com>
2025-07-16 17:31:41 +0800

    add oil git status and remove claude code

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -3,6 +3,9 @@ return {
   ---@module 'oil'
   ---@type oil.SetupOpts
   opts = {
+    win_options = {
+      signcolumn = 'yes:2',
+    },
     default_file_explorer = true,
     view_options = {
       show_hidden = true,
@@ -12,4 +15,3 @@ return {
   dependencies = { { 'echasnovski/mini.icons', opts = {} } },
   lazy = false,
 }
-
