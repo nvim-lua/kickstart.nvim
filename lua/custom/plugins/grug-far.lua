2776e0b50155c82a80f58e6e760104cf6758909b
Dennis Chan <dennisming2012@gmail.com>
2025-07-16 17:43:44 +0800

    add grug-far

---
+++ lua/custom/plugins/grug-far.lua
@@ -0,0 +1,13 @@
+return {
+  'MagicDuck/grug-far.nvim',
+  -- Note (lazy loading): grug-far.lua defers all it's requires so it's lazy by default
+  -- additional lazy config to defer loading is not really needed...
+  config = function()
+    -- optional setup call to override plugin options
+    -- alternatively you can set options with vim.g.grug_far = { ... }
+    require('grug-far').setup {
+      -- options, see Configuration section below
+      -- there are no required options atm
+    }
+  end,
+}
