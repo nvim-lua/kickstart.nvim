c61d50d8b478acfe6b3790520144100d71ad0236
MN-nagy <mahmoudnagimohamed@gmail.com>
2025-07-21 01:33:07 +0300

    Refactor: updated colorcolum to a more appropriate value

--- init.lua
+++ init.lua
@@ -170,7 +170,7 @@ vim.o.confirm = true
 vim.o.wrap = false
 
 -- Highlight max chars per line
--- vim.o.colorcolumn = '100'
+-- vim.o.colorcolumn = '120'
 
 -- [[ Basic Keymaps ]]
 --  See `:help vim.keymap.set()`
@@ -253,16 +253,6 @@ vim.api.nvim_create_autocmd('BufWritePre', {
     end
   end,
 })
-
--- disable automatic comment on newline
--- vim.api.nvim_create_autocmd('FileType', {
---   desc = 'Disable automatic comment on newline',
---   group = vim.api.nvim_create_augroup('kickstart-disable-auto-comment', { clear = true }),
---   pattern = '*',
---   callback = function()
---     vim.opt_local.formatoptions:remove { 'c', 'r', 'o' }
---   end,
--- })
 
 -- [[ Install `lazy.nvim` plugin manager ]]
 --    See `:help lazy.nvim.txt` or https://github.com/folke/lazy.nvim for more info
