49a344c061903aaf240b05b034ad5a991e7f5a08
Test User <test@example.com>
2025-07-25 13:51:10 +0300

    search in buffers: sort buffers by access order

--- init.lua
+++ init.lua
@@ -435,7 +435,10 @@ require('lazy').setup({
       vim.keymap.set('n', '<leader>sd', builtin.diagnostics, { desc = '[S]earch [D]iagnostics' })
       vim.keymap.set('n', '<leader>sr', builtin.resume, { desc = '[S]earch [R]esume' })
       vim.keymap.set('n', '<leader>s.', builtin.oldfiles, { desc = '[S]earch Recent Files ("." for repeat)' })
-      vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
+        vim.keymap.set('n', '<leader><leader>', function()
+          builtin.buffers { sort_mru = true }
+        end, { desc = '[ ] Find existing buffers' }) 
+      -- vim.keymap.set('n', '<leader><leader>', builtin.buffers, { desc = '[ ] Find existing buffers' })
 
       -- Slightly advanced example of overriding default behavior and theme
       vim.keymap.set('n', '<leader>/', function()
