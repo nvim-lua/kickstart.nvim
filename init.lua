4bd0d717395b835fd2470b868749230717764790
nicholas.difilippo7 <nicholas.difilippo7@gmail.com>
2025-07-25 20:48:05 -0500

    Adding fugitive plugin

--- init.lua
+++ init.lua
@@ -163,7 +163,7 @@ rtp:prepend(lazypath)
 require('lazy').setup({
   -- NOTE: Plugins can be added with a link (or for a github repo: 'owner/repo' link).
   'NMAC427/guess-indent.nvim', -- Detect tabstop and shiftwidth automatically
-
+  'tpope/vim-fugitive', -- the premier Vim plugin for Git
   -- NOTE: Plugins can also be added by using a table,
   -- with the first argument being the link and the following
   -- keys can be used to configure plugin behavior/loading/etc.
