c1120083540d366627d11a4a7bb053ae42d5b5fb
jensonjohnathon <johnathonreggentin@gmail.com>
2025-07-31 11:14:36 +0200

    Fixed Config needs nightly for plugins currently running fine on:
    NVIM v0.12.0-dev-914+gb79ff967ac
    Build type: RelWithDebInfo

--- init.lua
+++ init.lua
@@ -495,8 +495,8 @@ require('lazy').setup({
       -- Automatically install LSPs and related tools to stdpath for Neovim
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
-      { 'mason-org/mason.nvim', opts = {} },
-      'mason-org/mason-lspconfig.nvim',
+      { 'williamboman/mason.nvim', opts = {} },
+      'williamboman/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
