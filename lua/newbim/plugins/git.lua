d580ce71e9b7329437fb5ba359314115efc9deeb
Ilia Levikov <fonych1@gmail.com>
2025-07-31 12:57:35 +1200

    add gbrowse

--- lua/newbim/plugins/git.lua
+++ lua/newbim/plugins/git.lua
@@ -1,6 +1,13 @@
 return {
-  'tpope/vim-fugitive',
-  config = function()
-    vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
-  end,
+  {
+    'tpope/vim-fugitive',
+    config = function()
+      vim.keymap.set('n', '<leader>gs', vim.cmd.Git)
+      vim.keymap.set('n', '<leader>gg', ':GBrowse<CR>', { desc = 'GitHub Browse (Gbrowse)' })
+    end,
+  },
+  {
+    'tpope/vim-rhubarb', -- Enables :Gbrowse for GitHub remotes
+    event = 'VeryLazy',
+  },
 }
--- lua/newbim/plugins/markdown.lua
+++ lua/newbim/plugins/markdown.lua
@@ -1,6 +1,6 @@
 return {
   'MeanderingProgrammer/render-markdown.nvim',
-  dependencies = { 'nvim-treesitter/nvim-treesitter', 'echasnovsky/mini.nvim' },
+  dependencies = { 'nvim-treesitter/nvim-treesitter' },
   ---@module 'render-markdown'
   ---@type render.md.UserConfig
   opts = {},
