60d0f1b953b12565ce0749926e05424a22b41f88
christiangun <christian@tailorstore.com>
2025-08-01 22:45:24 +0200

    Add Kanagawa theme

---
+++ lua/custom/plugins/kanagawa.lua
@@ -0,0 +1,29 @@
+return {
+  'rebelot/kanagawa.nvim',
+  config = function()
+    require('kanagawa').setup {
+      compile = false, -- enable compiling the colorscheme
+      undercurl = true, -- enable undercurls
+      commentStyle = { italic = true },
+      functionStyle = {},
+      keywordStyle = { italic = true },
+      statementStyle = { bold = true },
+      typeStyle = {},
+      transparent = false, -- do not set background color
+      dimInactive = false, -- dim inactive window `:h hl-NormalNC`
+      terminalColors = true, -- define vim.g.terminal_color_{0,17}
+      colors = { -- add/modify theme and palette colors
+        palette = {},
+        theme = { wave = {}, lotus = {}, dragon = {}, all = {} },
+      },
+      overrides = function(colors) -- add/modify highlights
+        return {}
+      end,
+      theme = 'wave', -- Load "wave" theme
+      background = { -- map the value of 'background' option to a theme
+        dark = 'wave', -- try "dragon" !
+        light = 'lotus',
+      },
+    }
+  end,
+}
