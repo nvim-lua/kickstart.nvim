29954b7d4447d7efcf2b1eb8a8db810472aa9bec
christiangun <christian@tailorstore.com>
2025-08-01 22:47:09 +0200

    Remove some set configurations

--- lua/custom/set.lua
+++ lua/custom/set.lua
@@ -1,26 +1,5 @@
 -- We do some overrides for Kickstart configs here
 -- to avoid conflicts from the upstream in the future.
--- require('conform').format_on_save = false
-
--- require('treesitter.configs').opts.ensure_installed = {
---   'bash',
---   'c',
---   'diff',
---   'html',
---   'lua',
---   'luadoc',
---   'markdown',
---   'markdown_inline',
---   'query',
---   'vim',
---   'vimdoc',
---   'php',
---   'javascript',
---   'html',
---   'css',
--- }
-
-vim.api.nvim_create_user_command('Logs', ':e /tmp/ts_d', {})
 
 vim.api.nvim_create_user_command('Fixtabs', ':set tabstop=4', {})
 
