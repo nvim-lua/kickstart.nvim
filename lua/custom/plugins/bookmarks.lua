e0db90c49bf88bb467da9aeab803dc4561d31074
christiangun <christian@tailorstore.com>
2025-08-01 22:45:49 +0200

    Bookmark keybinds

--- lua/custom/plugins/bookmarks.lua
+++ lua/custom/plugins/bookmarks.lua
@@ -6,17 +6,16 @@ return {
         local bm = require 'bookmarks'
         local map = vim.keymap.set
         local telescope = require 'telescope'
-        map('n', 'mm', bm.bookmark_toggle) -- add or remove bookmark at current line
-        map('n', 'mi', bm.bookmark_ann) -- add or edit mark annotation at current line
-        map('n', 'mc', bm.bookmark_clean) -- clean all marks in local buffer
-        map('n', 'mn', bm.bookmark_next) -- jump to next mark in local buffer
-        map('n', 'mp', bm.bookmark_prev) -- jump to previous mark in local buffer
+        map('n', '<leader>mm', bm.bookmark_toggle, { desc = 'Toggle bookmark' }) -- add or remove bookmark at current line
+        map('n', '<leader>mi', bm.bookmark_ann, { desc = 'Annotate bookmark' }) -- add or edit mark annotation at current line
+        map('n', '<leader>mc', bm.bookmark_clean, { desc = 'Clean marks in buffer' }) -- clean all marks in local buffer
+        map('n', '<leader>mn', bm.bookmark_next, { desc = 'Next bookmark in buffer' }) -- jump to next mark in local buffer
+        map('n', '<leader>mp', bm.bookmark_prev, { desc = 'Previous bookmark in buffer' }) -- jump to previous mark in local buffer
         -- map('n', 'ml', bm.bookmark_list) -- show marked file list in quickfix window
-        map('n', 'ml', telescope.extensions.bookmarks.list) -- show marked file list in quickfix window
-        map('n', 'mx', bm.bookmark_clear_all) -- removes all bookmarks
+        map('n', '<leader>ml', telescope.extensions.bookmarks.list, { desc = 'List bookmarks' }) -- show marked file list in quickfix window
+        map('n', '<leader>mx', bm.bookmark_clear_all, { desc = 'Clear all bookmarks' }) -- removes all bookmarks
       end,
-
-      pcall(require('telescope').load_extension, 'bookmarks'),
     }
+    pcall(require('telescope').load_extension, 'bookmarks')
   end,
 }
