0c39e25d18352ef0c96f5148a77a043e28ad8b76
christiangun <christian@tailorstore.com>
2025-08-01 22:45:08 +0200

    Enable gitsigns

--- init.lua
+++ init.lua
@@ -934,7 +934,7 @@ require('lazy').setup({
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
+  require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
 
   -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
   --    This is the easiest way to modularize your config.
