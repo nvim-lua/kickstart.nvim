7810e2af8d27d8bf90b927be3803c3b667652ed8
Ruslan Matveev <rmatveev@lululemon.com>
2025-08-05 16:10:51 -0400

    Enabled nerd-fonts

--- init.lua
+++ init.lua
@@ -91,7 +91,7 @@ vim.g.mapleader = ' '
 vim.g.maplocalleader = ' '
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
-vim.g.have_nerd_font = false
+vim.g.have_nerd_font = true
 
 -- [[ Setting options ]]
 -- See `:help vim.o`
