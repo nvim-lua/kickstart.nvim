52f23168fc40071b40d4b62416ae092ef96f9f82
alice-vu-163 <thamvh@arrowhitech.com>
2025-08-08 09:06:09 +0700

    add python

--- init.lua
+++ init.lua
@@ -1297,7 +1297,7 @@ require('lazy').setup({
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
-      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'xml' },
+      ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc', 'xml','python' },
       -- Autoinstall languages that are not installed
       auto_install = true,
       highlight = {
