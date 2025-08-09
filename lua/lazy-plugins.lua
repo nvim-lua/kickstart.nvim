b4e17309083916939ad9ff7f288be0c1fe670cde
a-robson <andrew.d.robson@gmail.com>
2025-08-09 14:56:55 +0100

    modularise conform

--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -62,53 +62,14 @@ require('lazy').setup({
 
     require 'kickstart.plugins.lspconfig',
 
+    require 'kickstart.plugins.conform',
+
     -- NOTE: Plugins can specify dependencies.
     --
     -- The dependencies are proper plugin specifications as well - anything
     -- you do for a plugin at the top level, you can do for a dependency.
     --
     -- Use the `dependencies` key to specify the dependencies of a particular plugin
-
-    { -- Autoformat
-      'stevearc/conform.nvim',
-      event = { 'BufWritePre' },
-      cmd = { 'ConformInfo' },
-      keys = {
-        {
-          '<leader>f',
-          function()
-            require('conform').format { async = true, lsp_format = 'fallback' }
-          end,
-          mode = '',
-          desc = '[F]ormat buffer',
-        },
-      },
-      opts = {
-        notify_on_error = false,
-        format_on_save = function(bufnr)
-          -- Disable "format_on_save lsp_fallback" for languages that don't
-          -- have a well standardized coding style. You can add additional
-          -- languages here or re-enable it for the disabled ones.
-          local disable_filetypes = { c = true, cpp = true }
-          if disable_filetypes[vim.bo[bufnr].filetype] then
-            return nil
-          else
-            return {
-              timeout_ms = 500,
-              lsp_format = 'fallback',
-            }
-          end
-        end,
-        formatters_by_ft = {
-          lua = { 'stylua' },
-          -- Conform can also run multiple formatters sequentially
-          -- python = { "isort", "black" },
-          --
-          -- You can use 'stop_after_first' to run the first available formatter from the list
-          -- javascript = { "prettierd", "prettier", stop_after_first = true },
-        },
-      },
-    },
 
     { -- Autocompletion
       'saghen/blink.cmp',
