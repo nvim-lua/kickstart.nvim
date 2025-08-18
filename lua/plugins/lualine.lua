11272260db88e57da718d8aed425719ce1ba648c
Walter Jenkins <walter@transit.chat>
2025-08-18 11:45:29 -0500

    update project path

--- lua/plugins/lualine.lua
+++ lua/plugins/lualine.lua
@@ -56,7 +56,7 @@ return {
     local filename = {
       'filename',
       file_status = true, -- displays file status (readonly status, modified status)
-      path = 0,           -- 0 = just filename, 1 = relative path, 2 = absolute path
+      path = 1,           -- 0 = just filename, 1 = relative path, 2 = absolute path
     }
 
     local hide_in_width = function()
