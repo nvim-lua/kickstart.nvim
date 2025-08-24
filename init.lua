6002925188899484c9e54fe40de86edebb8fcdfa
tannerlp <t@tmoney.io>
2025-08-24 09:35:58 -0400

    add go lsp back

--- init.lua
+++ init.lua
@@ -601,7 +601,7 @@ require('lazy').setup({
       --        For example, to see the options for `lua_ls`, you could go to: https://luals.github.io/wiki/settings/
       local servers = {
         clangd = {},
-        -- gopls = {},
+        gopls = {},
         pyright = {}, -- Need to install via npm first
         rust_analyzer = {},
         -- ... etc. See `:help lspconfig-all` for a list of all the pre-configured LSPs
