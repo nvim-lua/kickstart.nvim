c1909b1cd0b79de9627c569cec17e727db319342
Evgeni Chasnovski <evgeni.chasnovski@gmail.com>
2025-08-29 14:52:31 +0300

    fix: adjust after 'mini.nvim' transfer to 'nvim-mini' org

--- init.lua
+++ init.lua
@@ -902,7 +902,7 @@ require('lazy').setup({
   { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } },
 
   { -- Collection of various small independent plugins/modules
-    'echasnovski/mini.nvim',
+    'nvim-mini/mini.nvim',
     config = function()
       -- Better Around/Inside textobjects
       --
@@ -935,7 +935,7 @@ require('lazy').setup({
       end
 
       -- ... and there is more!
-      --  Check out: https://github.com/echasnovski/mini.nvim
+      --  Check out: https://github.com/nvim-mini/mini.nvim
     end,
   },
   { -- Highlight, edit, and navigate code
