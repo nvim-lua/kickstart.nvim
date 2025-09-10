a9751b6c2d7e7765dd3b764a49466a24d08847d8
Jason Stentz <jbot801@gmail.com>
2025-09-09 21:58:30 -0400

    adding rust finder

--- init.lua
+++ init.lua
@@ -790,7 +790,7 @@ require('lazy').setup({
       -- the rust implementation via `'prefer_rust_with_warning'`
       --
       -- See :h blink-cmp-config-fuzzy for more information
-      fuzzy = { implementation = 'lua' },
+      fuzzy = { implementation = 'rust' },
 
       -- Shows a signature help window while you type arguments for a function
       signature = { enabled = true },
