80b44e09ddb4ed9cb6fe5dc33f6507f9180beb38
jensonjohnathon <johnathonreggentin@gmail.com>
2025-09-15 15:25:28 +0200

    Snippets

---
+++ LuaSnip/Readme.md
@@ -0,0 +1,10 @@
+# Example LuaSnip directory structure
+${HOME}/.config/nvim/LuaSnip/
+├── all.lua
+├── markdown.lua
+├── python.lua
+└── tex
+    ├── delimiters.lua
+    ├── environments.lua
+    ├── fonts.lua
+    └── math.lua
--- init.lua
+++ init.lua
@@ -302,6 +302,14 @@ require('lazy').setup({
       require('luasnip.loaders.from_vscode').lazy_load()
       require('luasnip.loaders.from_lua').load { paths = '~/.config/nvim/LuaSnip/' }
 
+      require('luasnip').config.set_config { -- Setting LuaSnip config
+
+        -- Enable autotriggered snippets
+        enable_autosnippets = true,
+
+        -- Use Tab (or some other key if you prefer) to trigger visual selection
+        store_selection_keys = '<Tab>',
+      }
       -- einfache Keymaps
       vim.keymap.set({ 'i' }, '<C-k>', function()
         luasnip.expand()
