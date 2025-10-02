f420d02066fd43866014fad8dd2efe8ad47b6c1b
Julien Raynal <julien@Juliens-MacBook-Pro.local>
2025-10-02 15:37:16 +0200

    Removed tokyonight

--- init.lua
+++ init.lua
@@ -905,13 +905,6 @@ require('lazy').setup({
     name = 'catppuccin',
     priority = 1000, -- Make sure to load this before all the other start plugins.
     config = function()
-      ---@diagnostic disable-next-line: missing-fields
-      require('tokyonight').setup {
-        styles = {
-          comments = { italic = false }, -- Disable italics in comments
-        },
-      }
-
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
