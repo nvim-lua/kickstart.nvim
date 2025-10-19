9cc437bea1bb95c5866e2220044f0907e83b3a90
40DEEA <40deea@gmail.com>
2025-10-19 16:44:19 -0700

    Enable indent_line

--- init.lua
+++ init.lua
@@ -974,7 +974,7 @@ require('lazy').setup({
   --  Uncomment any of the lines below to enable them (you will need to restart nvim).
   --
   -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.indent_line',
   -- require 'kickstart.plugins.lint',
   -- require 'kickstart.plugins.autopairs',
   -- require 'kickstart.plugins.neo-tree',
