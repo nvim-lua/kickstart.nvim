af97caf178258ffc1a2e88209f059c8f529a16ef
cavelazquez8 <cavelazquez8@gmail.com>
2025-10-28 23:11:51 -0700

    fix previous

--- lua/custom/plugins/wiki.lua
+++ lua/custom/plugins/wiki.lua
@@ -16,7 +16,7 @@ return {
         -- Custom Markdown to HTML converter
         --   custom_wiki2html = "~/.vim/autoload/vimwiki/convert.py",
         -- Auto update diary index
-        index = 'MOCs/main-moc',
+        index = 'mocs/main-moc',
         auto_diary_index = 1,
         links_space_char = '-', -- Replace spaces with hyphens in links
       },
