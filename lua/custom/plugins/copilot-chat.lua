220a47a19e4337b04fd28b2a449f2bdd0e10a8c5
40DEEA <40deea@gmail.com>
2025-10-30 16:11:15 -0700

    Add copilot chat

---
+++ lua/custom/plugins/copilot-chat.lua
@@ -0,0 +1,13 @@
+--Copilot chat
+return {
+  {
+    "CopilotC-Nvim/CopilotChat.nvim",
+    dependencies = {
+      { "nvim-lua/plenary.nvim", branch = "master" },
+    },
+    build = "make tiktoken",
+    opts = {
+      -- See Configuration section for options
+    },
+  },
+}
