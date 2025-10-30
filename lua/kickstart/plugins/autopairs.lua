1bd4a54f28a50333cfe684096f2d209860168d8c
Phil Nguyen <philnguyen@Mac.lan>
2025-10-30 11:34:21 +0700

    update config

--- lua/kickstart/plugins/autopairs.lua
+++ lua/kickstart/plugins/autopairs.lua
@@ -26,9 +26,6 @@ return {
       },
     })
 
-    -- Integration with nvim-cmp
-    local cmp_autopairs = require('nvim-autopairs.completion.cmp')
-    local cmp = require('cmp')
-    cmp.event:on('confirm_done', cmp_autopairs.on_confirm_done())
+
   end,
 }
