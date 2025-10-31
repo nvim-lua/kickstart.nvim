686c298c4433c52754d3d2127dfde229de0e2da9
Anup Sebastian <anupjsebastian@Anups-Air.attlocal.net>
2025-10-31 09:15:20 -0500

    fix: post yank text highlight time

--- init.lua
+++ init.lua
@@ -280,7 +280,7 @@ vim.api.nvim_create_autocmd('TextYankPost', {
   desc = 'Highlight when yanking (copying) text',
   group = vim.api.nvim_create_augroup('kickstart-highlight-yank', { clear = true }),
   callback = function()
-    vim.hl.on_yank()
+    vim.hl.on_yank({higroup = 'IncSearch', timeout = 300})
   end,
 })
 
