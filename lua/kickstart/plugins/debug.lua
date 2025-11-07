ebd12e7ab19b76718c58dfc5de966d01abade9f3
Thomas Lazarus <lazarustda@gmail.com>
2025-11-06 22:22:45 -0600

    Setups python debugger

--- lua/kickstart/plugins/debug.lua
+++ lua/kickstart/plugins/debug.lua
@@ -12,7 +12,6 @@ return {
     'jay-babu/mason-nvim-dap.nvim',
 
     -- Add your own debuggers here
-    'leoluz/nvim-dap-go',
     'mfussenegger/nvim-dap-python',
   },
   keys = {
@@ -72,25 +71,16 @@ return {
     local dap = require 'dap'
     local dapui = require 'dapui'
 
-    require('dap-python').setup { 'uv' }
-    require('dap-python').test_runner = 'pytest'
-
     require('mason-nvim-dap').setup {
-      -- Makes a best effort to setup the various debuggers with
-      -- reasonable debug configurations
       automatic_installation = true,
-
-      -- You can provide additional configuration to the handlers,
-      -- see mason-nvim-dap README for more information
       handlers = {},
-
-      -- You'll need to check that you have the required things installed
-      -- online, please don't ask me how to install them :)
       ensure_installed = {
-        -- Update this to ensure that you have the debuggers for the langs you want
-        'delve',
+        'python',
       },
     }
+
+    require('dap-python').setup 'uv'
+    require('dap-python').test_runner = 'pytest'
 
     -- Dap UI setup
     -- For more information, see |:help nvim-dap-ui|
@@ -129,14 +119,5 @@ return {
     dap.listeners.after.event_initialized['dapui_config'] = dapui.open
     dap.listeners.before.event_terminated['dapui_config'] = dapui.close
     dap.listeners.before.event_exited['dapui_config'] = dapui.close
-
-    -- Install golang specific config
-    require('dap-go').setup {
-      delve = {
-        -- On Windows delve must be run attached or it crashes.
-        -- See https://github.com/leoluz/nvim-dap-go/blob/main/README.md#configuring
-        detached = vim.fn.has 'win32' == 0,
-      },
-    }
   end,
 }
