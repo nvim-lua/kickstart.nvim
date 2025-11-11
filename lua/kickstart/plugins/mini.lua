37bbcda810332712102f002a1c3f6f94ecb418e6
Thomas Lazarus <lazarustda@gmail.com>
2025-11-10 20:02:54 -0600

    Adds mini.comment

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -16,6 +16,8 @@ return { -- Collection of various small independent plugins/modules
     -- - sr)'  - [S]urround [R]eplace [)] [']
     require('mini.surround').setup()
 
+    require('mini.comment').setup()
+
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
