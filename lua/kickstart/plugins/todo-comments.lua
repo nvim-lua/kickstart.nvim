9348a948f8bb43f140cf6da0d5b2baf30c5bbc77
Thomas Lazarus <lazarustda@gmail.com>
2025-11-10 19:56:01 -0600

    Change todo comments config

--- lua/kickstart/plugins/todo-comments.lua
+++ lua/kickstart/plugins/todo-comments.lua
@@ -1,3 +1,3 @@
-return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = { signs = false } }
+return { 'folke/todo-comments.nvim', event = 'VimEnter', dependencies = { 'nvim-lua/plenary.nvim' }, opts = {} }
 
 -- vim: ts=2 sts=2 sw=2 et
