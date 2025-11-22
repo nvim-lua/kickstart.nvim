0b62cac365c1513fa92f57ce5fd21ace5aea1f0f
Jason Stentz <jbot801@gmail.com>
2025-11-22 18:41:43 -0500

    adding ability to paste without clobbering buffer

--- init.lua
+++ init.lua
@@ -24,6 +24,9 @@ vim.o.mouse = 'a'
 -- Don't show the mode, since it's already in the status line
 vim.o.showmode = false
 
+-- Allow pasting without clobbering the buffer
+vim.keymap.set('x', '<leader>p', '"_dP')
+
 -- Sync clipboard between OS and Neovim.
 --  Schedule the setting after `UiEnter` because it can increase startup-time.
 --  Remove this option if you want your OS clipboard to remain independent.
