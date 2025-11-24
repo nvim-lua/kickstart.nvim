3f5a91e7922d52c575ac2e958fe1763138050b9f
Stian Tøsse <stian.tosse@tripletex.no>
2025-11-24 09:00:39 +0100

    use prettier as formatting

--- init.lua
+++ init.lua
@@ -581,11 +581,10 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        tsx = { 'prettier' },
+        typescriptreact = { 'prettier' },
       },
     },
   },
