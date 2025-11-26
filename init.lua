b8d5f55163f064997607b70e30477b1c42c59083
scutta <scuttari.pietro@gmail.com>
2025-11-26 20:23:31 +0100

    Second change

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = false
 vim.opt.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.opt.relativenumber = true
+vim.opt.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.opt.mouse = 'a'
@@ -662,8 +662,8 @@ require('lazy').setup({
           end,
         },
       }
-      local lspconfig = require 'lspconfig'
-      lspconfig.gopls.setup {}
+      -- local lspconfig = require 'lspconfig'
+      -- lspconfig.gopls.setup {}
     end,
   },
 
