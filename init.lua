9a7240b92604fe62fe96e49cde7024a449bac497
Test User <test@example.com>
2025-12-04 19:31:31 +0200

    show the actual line number

--- init.lua
+++ init.lua
@@ -98,10 +98,7 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
--- vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
+vim.o.number = true
 vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
