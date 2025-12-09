168b40ba9c58cb63038f6d2513bc5202b7fdbc0c
Nikita Avgustanov <gh@nik-avg.org>
2025-12-09 12:22:00 +0400

    Install yamlls for yaml support

--- init.lua
+++ init.lua
@@ -728,6 +728,7 @@ require('lazy').setup({
       require('mason-lspconfig').setup {
         ensure_installed = {
           'gopls',
+          'yamlls',
         }, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
         automatic_installation = false,
         handlers = {
