d8c4721bbc79f4a214e50e168c96544c65c7f601
Temporary vm <mariosranag@gmail.com>
2025-12-13 13:48:52 +0200

    Tries to figure out what is should be tracked

--- init.lua
+++ init.lua
@@ -705,7 +705,7 @@ require('lazy').setup({
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
         'stylua', -- Used to format Lua code
-        'fish-lsp', -- Used for fish shell scripts.
+        'fish_lsp', -- Used for fish shell scripts.
       })
       require('mason-tool-installer').setup { ensure_installed = ensure_installed }
 
