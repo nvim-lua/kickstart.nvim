e827cd7aa1fc669a7b2892f8a31d6c86f0dd26c1
DonMatano <mwakimamatano@gmail.com>
2025-12-14 20:13:02 +0300

    add total file lines in status

--- init.lua
+++ init.lua
@@ -893,7 +893,9 @@ require('lazy').setup({
       -- default behavior. For example, here we set the section for
       -- cursor location to LINE:COLUMN
       ---@diagnostic disable-next-line: duplicate-set-field
-      statusline.section_location = function() return '%2l:%-2v' end
+      statusline.section_location = function()
+        return '%2l:%-2v - %L'
+      end
 
       -- ... and there is more!
       --  Check out: https://github.com/nvim-mini/mini.nvim
