d0541d916761c5d35560d4576b7ed743aaef0e7f
kdave5 <130889305+kdave5@users.noreply.github.com>
2025-12-19 03:25:09 -0600

    Update init.lua
    changed treesitter to master for temp fix

--- init.lua
+++ init.lua
@@ -940,6 +940,7 @@ require('lazy').setup({
   },
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
+    branch = "master",
     build = ':TSUpdate',
     main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
