5e4c84ff0313f0a199496b3584be7ff2107b82fb
Subhendu Karmakar <subhendukarmakar61@gmail.com>
2025-12-29 23:33:09 +0530

    Fix main module path for nvim-treesitter
    failed to run 'config' for nvim-treesitter error fixed....

--- init.lua
+++ init.lua
@@ -941,7 +941,7 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
+    main = 'nvim-treesitter.config', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
