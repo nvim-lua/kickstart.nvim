62933b1a7f6cbf59d51701c6290942939cda3f94
Thomas Lazarus <lazarustda@gmail.com>
2026-01-03 11:15:10 -0600

    Adds mini.surround

--- lua/kickstart/plugins/mini.lua
+++ lua/kickstart/plugins/mini.lua
@@ -18,11 +18,12 @@ return { -- Collection of various small independent plugins/modules
 
     require('mini.comment').setup()
 
+    require('mini.pairs').setup()
+
     -- Simple and easy statusline.
     --  You could remove this setup call if you don't like it,
     --  and try some other statusline plugin
     local statusline = require 'mini.statusline'
-    -- set use_icons to true if you have a Nerd Font
     statusline.setup { use_icons = vim.g.have_nerd_font }
 
     -- You can configure sections in the statusline by overriding their
--- lua/lazy-plugins.lua
+++ lua/lazy-plugins.lua
@@ -32,22 +32,6 @@ require('lazy').setup({
   require 'kickstart.plugins.snacks',
 }, {
   ui = {
-    -- If you are using a Nerd Font: set icons to an empty table which will use the
-    -- default lazy.nvim defined Nerd Font icons, otherwise define a unicode icons table
-    icons = vim.g.have_nerd_font and {} or {
-      cmd = '⌘',
-      config = '🛠',
-      event = '📅',
-      ft = '📂',
-      init = '⚙',
-      keys = '🗝',
-      plugin = '🔌',
-      runtime = '💻',
-      require = '🌙',
-      source = '📄',
-      start = '🚀',
-      task = '📌',
-      lazy = '💤 ',
-    },
+    icons = {},
   },
 })
