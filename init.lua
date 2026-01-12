0ec19008b89941594e42919e1860f6012ee1afe3
Roland <rolandybyr@gmail.com>
2026-01-12 06:32:02 -0500

    Fix nvim-treesitter lazy.nvim config by removing invalid 'main' field

--- init.lua
+++ init.lua
@@ -941,7 +941,6 @@ require('lazy').setup({
   { -- Highlight, edit, and navigate code
     'nvim-treesitter/nvim-treesitter',
     build = ':TSUpdate',
-    main = 'nvim-treesitter.configs', -- Sets main module to use for opts
     -- [[ Configure Treesitter ]] See `:help nvim-treesitter`
     opts = {
       ensure_installed = { 'bash', 'c', 'diff', 'html', 'lua', 'luadoc', 'markdown', 'markdown_inline', 'query', 'vim', 'vimdoc' },
