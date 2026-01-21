a2d6f60b4c797b70ee595f8336a149c77b6a8f78
tprostguestlogix <tprost@guestlogix.com>
2026-01-21 18:00:51 -0500

    simpelr config for 0.12

--- init.lua
+++ init.lua
@@ -1,22 +1,5 @@
--- Minimal Neovim Configuration
--- Clean slate for starting from scratch
-
--- Basic vim settings
-vim.g.mapleader = ' '
-vim.g.maplocalleader = ' '
-
-vim.opt.number = true
-vim.opt.relativenumber = true
-vim.opt.mouse = 'a'
-vim.opt.breakindent = true
-vim.opt.undofile = true
-vim.opt.ignorecase = true
-vim.opt.smartcase = true
-vim.opt.signcolumn = 'yes'
-vim.opt.updatetime = 250
-vim.opt.timeoutlen = 300
-vim.opt.completeopt = 'menuone,noselect'
-vim.opt.termguicolors = true
-
--- Basic keymaps
-vim.keymap.set({ 'n', 'v' }, '<Space>', '<Nop>', { silent = true })
+require('configs')
+require('keymaps')
+require('autocmds')
+require('plugins')
+require('lsp')
