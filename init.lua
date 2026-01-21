20035ad333f29f9374b3e874922a3c0c9a45c513
jujekebab <tofuredbull@gmail.com>
2026-01-21 13:56:06 +0900

    show only currnet line number

--- init.lua
+++ init.lua
@@ -39,11 +39,9 @@ vim.g.have_nerd_font = true
 -- NOTE: You can change these options as you wish!
 --  For more options, you can see `:help option-list`
 
--- Make line numbers default
+-- Only show line number on current line
 vim.o.number = true
--- You can also add relative line numbers, to help with jumping.
---  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.statuscolumn = '%{v:relnum == 0 ? v:lnum : ""}'
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
@@ -635,8 +633,9 @@ require('lazy').setup({
               completion = {
                 callSnippet = 'Replace',
               },
-              -- You can toggle below to ignore Lua_LS's noisy `missing-fields` warnings
-              -- diagnostics = { disable = { 'missing-fields' } },
+              diagnostics = {
+                globals = { 'vim' },
+              },
             },
           },
         },
