8638f8b57dc02c571ee89d33312cf286423a44dc
Reathe <bachourian@gmail.com>
2026-01-29 14:58:30 +0100

    show hidden dotfiles by default

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -20,6 +20,11 @@ return {
           ['\\'] = 'close_window',
         },
       },
+      filtered_items = {
+        visible = true,
+        hide_dotfiles = false,
+        hide_gitignored = false,
+      },
     },
   },
 }
