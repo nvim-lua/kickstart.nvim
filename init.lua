75f37c95d37638d8dab2137a4b49972da90b46c0
Marcus <marcushoks@gmail.com>
2026-01-29 11:31:29 +0800

    Add telescope mapping to delete buffers

--- init.lua
+++ init.lua
@@ -438,11 +438,16 @@ require('lazy').setup({
         -- You can put your default mappings / updates / etc. in here
         --  All the info you're looking for is in `:help telescope.setup()`
         --
-        -- defaults = {
-        --   mappings = {
-        --     i = { ['<c-enter>'] = 'to_fuzzy_refine' },
-        --   },
-        -- },
+        defaults = {
+          mappings = {
+            i = {
+              ['<C-w>'] = require('telescope.actions').delete_buffer,
+            },
+            n = {
+              ['<C-w>'] = require('telescope.actions').delete_buffer,
+            },
+          },
+        },
         pickers = {
           colorscheme = { enable_preview = true },
         },
