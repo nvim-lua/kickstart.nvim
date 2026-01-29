e577be0e7f03087afb4f7e4b2a12a0c2a36a5d15
Jason Stentz <jbot801@gmail.com>
2026-01-28 21:29:05 -0500

    adding claudecode plugin

---
+++ lua/custom/plugins/claudecode.lua
@@ -0,0 +1,24 @@
+return {
+  'coder/claudecode.nvim',
+  dependencies = { 'folke/snacks.nvim' },
+  config = true,
+  keys = {
+    { '<leader>a', nil, desc = 'AI/Claude Code' },
+    { '<leader>ac', '<cmd>ClaudeCode<cr>', desc = 'Toggle Claude' },
+    { '<leader>af', '<cmd>ClaudeCodeFocus<cr>', desc = 'Focus Claude' },
+    { '<leader>ar', '<cmd>ClaudeCode --resume<cr>', desc = 'Resume Claude' },
+    { '<leader>aC', '<cmd>ClaudeCode --continue<cr>', desc = 'Continue Claude' },
+    { '<leader>am', '<cmd>ClaudeCodeSelectModel<cr>', desc = 'Select Claude model' },
+    { '<leader>ab', '<cmd>ClaudeCodeAdd %<cr>', desc = 'Add current buffer' },
+    { '<leader>as', '<cmd>ClaudeCodeSend<cr>', mode = 'v', desc = 'Send to Claude' },
+    {
+      '<leader>as',
+      '<cmd>ClaudeCodeTreeAdd<cr>',
+      desc = 'Add file',
+      ft = { 'NvimTree', 'neo-tree', 'oil', 'minifiles', 'netrw' },
+    },
+    -- Diff management
+    { '<leader>aa', '<cmd>ClaudeCodeDiffAccept<cr>', desc = 'Accept diff' },
+    { '<leader>ad', '<cmd>ClaudeCodeDiffDeny<cr>', desc = 'Deny diff' },
+  },
+}
