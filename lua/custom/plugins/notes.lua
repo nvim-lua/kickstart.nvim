359162d269ba3d620db6980c251d7c71aafd2022
name <you@example.com>
2026-02-07 18:52:29 +0000

    add render markdown

--- lua/custom/plugins/notes.lua
+++ lua/custom/plugins/notes.lua
@@ -15,8 +15,14 @@ return {
     dependencies = { 'nvim-telescope/telescope.nvim' },
   },
   {
-    --'MeanderingProgrammer/render-makrdown.nvim',
-    --dependecies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' },
-    --@module 'render-markdown'
+    {
+      'MeanderingProgrammer/render-markdown.nvim',
+      dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.nvim' }, -- if you use the mini.nvim suite
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-mini/mini.icons' },        -- if you use standalone mini plugins
+      -- dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' }, -- if you prefer nvim-web-devicons
+      ---@module 'render-markdown'
+      ---@type render.md.UserConfig
+      opts = {},
+    },
   },
 }
