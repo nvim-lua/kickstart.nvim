c2a02f734b8d86272f0533a994145f81815e95cc
Nick Burt <nbur4556@gmail.com>
2026-02-08 22:45:50 -0600

    remove not found typescript language server

--- init.lua
+++ init.lua
@@ -604,7 +604,8 @@ require('lazy').setup({
         --    https://github.com/pmizio/typescript-tools.nvim
         --
         -- But for many setups, the LSP (`ts_ls`) will work just fine
-        ts_ls = {},
+        -- FIX: Can not find ts_ls
+        -- ts_ls = {},
       }
 
       -- Ensure the servers and tools above are installed
