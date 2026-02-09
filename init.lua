40d036454a069af79666f54880151984fd3dda14
Nick Burt <nbur4556@gmail.com>
2026-02-08 23:05:59 -0600

    install and set default theme to catppuccin

--- init.lua
+++ init.lua
@@ -792,6 +792,8 @@ require('lazy').setup({
     },
   },
 
+  { 'catppuccin/nvim', name = 'catppuccin', priority = 1000 },
+
   { -- You can easily change to a different colorscheme.
     -- Change the name of the colorscheme plugin below, and then
     -- change the command in the config to whatever the name of that colorscheme is.
@@ -810,7 +812,7 @@ require('lazy').setup({
       -- Load the colorscheme here.
       -- Like many other themes, this one has different styles, and you could load
       -- any other, such as 'tokyonight-storm', 'tokyonight-moon', or 'tokyonight-day'.
-      vim.cmd.colorscheme 'tokyonight-night'
+      vim.cmd.colorscheme 'catppuccin-mocha'
     end,
   },
 
