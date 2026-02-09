f4cf583044714a3b9e3129565d830b1e05af19ca
Nick Burt <nbur4556@gmail.com>
2026-02-08 22:50:46 -0600

    enable prettier for javascript files

--- init.lua
+++ init.lua
@@ -693,7 +693,7 @@ require('lazy').setup({
         -- python = { "isort", "black" },
         --
         -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettierd', 'prettier', stop_after_first = true },
       },
     },
   },
