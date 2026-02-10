44ec0be42f37e472240cd5d9409653332179f379
Nick Burt <nbur4556@gmail.com>
2026-02-09 20:07:27 -0600

    load snacks types

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -2,7 +2,9 @@ return {
   'folke/snacks.nvim',
   priority = 1000,
   lazy = false,
+  ---@type snacks.Config
   opts = {
+    --TODO: Use this for a bit and compare to mini dashboard
     dashboard = { enabled = true },
   },
 }
