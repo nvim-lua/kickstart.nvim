b14cb685d777b4007093f326be48580a18ee0191
Nick Burt <nbur4556@gmail.com>
2026-02-09 20:41:11 -0600

    install and configure keybind for lazygit

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -5,6 +5,12 @@ return {
   ---@type snacks.Config
   opts = {
     --TODO: Use this for a bit and compare to mini dashboard
+    --TODO: design a style and featuers for my dashboard
     dashboard = { enabled = true },
+    lazygit = { enabled = true },
+  },
+  keys = {
+    { '<leader>=', function() Snacks.dashboard.open() end, desc = 'Dashboard' },
+    { '<leader>gl', function() Snacks.lazygit.open() end, desc = 'Open Lazy Git' },
   },
 }
