111cea00ab27ec9cd5b9356fd47d001a02048505
Jason Stentz <jbot801@gmail.com>
2026-02-18 00:39:56 -0500

    fixing lua

--- init.lua
+++ init.lua
@@ -551,6 +551,10 @@ require("lazy").setup({
 				lua_ls = {
 					settings = {
 						Lua = {
+							workspace = {
+								library = vim.api.nvim_get_runtime_file("", true),
+								checkThirdParty = false,
+							},
 							completion = {
 								callSnippet = "Replace",
 							},
