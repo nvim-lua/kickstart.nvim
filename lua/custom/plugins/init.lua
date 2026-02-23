4891019be72a8b844a57cd7a5ca39597a2494447
Reathe <bachourian@gmail.com>
2026-02-23 15:31:44 +0100

    formatting change

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -27,108 +27,78 @@ return {
     keys = {
       {
         '<leader>.',
-        function()
-          Snacks.scratch()
-        end,
+        function() Snacks.scratch() end,
         desc = 'Toggle Scratch Buffer',
       },
       {
         '<leader>S',
-        function()
-          Snacks.scratch.select()
-        end,
+        function() Snacks.scratch.select() end,
         desc = 'Select Scratch Buffer',
       },
       {
         '<leader>n',
-        function()
-          Snacks.notifier.show_history()
-        end,
+        function() Snacks.notifier.show_history() end,
         desc = 'Notification History',
       },
       {
         '<leader>bd',
-        function()
-          Snacks.bufdelete()
-        end,
+        function() Snacks.bufdelete() end,
         desc = 'Delete Buffer',
       },
       {
         '<leader>cR',
-        function()
-          Snacks.rename.rename_file()
-        end,
+        function() Snacks.rename.rename_file() end,
         desc = 'Rename File',
       },
       {
         '<leader>gB',
-        function()
-          Snacks.gitbrowse()
-        end,
+        function() Snacks.gitbrowse() end,
         desc = 'Git Browse',
       },
       {
         '<leader>gb',
-        function()
-          Snacks.git.blame_line()
-        end,
+        function() Snacks.git.blame_line() end,
         desc = 'Git Blame Line',
       },
       {
         '<leader>gf',
-        function()
-          Snacks.lazygit.log_file()
-        end,
+        function() Snacks.lazygit.log_file() end,
         desc = 'Lazygit Current File History',
       },
       {
         '<leader>gg',
-        function()
-          Snacks.lazygit()
-        end,
+        function() Snacks.lazygit() end,
         desc = 'Lazygit',
       },
       {
         '<leader>gl',
-        function()
-          Snacks.lazygit.log()
-        end,
+        function() Snacks.lazygit.log() end,
         desc = 'Lazygit Log (cwd)',
       },
       {
         '<leader>un',
-        function()
-          Snacks.notifier.hide()
-        end,
+        function() Snacks.notifier.hide() end,
         desc = 'Dismiss All Notifications',
       },
       {
         '<c-/>',
-        function()
-          Snacks.terminal()
-        end,
+        function() Snacks.terminal() end,
         desc = 'Toggle Terminal',
       },
       {
         '<c-_>',
-        function()
-          Snacks.terminal()
-        end,
+        function() Snacks.terminal() end,
         desc = 'which_key_ignore',
       },
       {
         ']]',
-        function()
-          Snacks.words.jump(vim.v.count1)
-        end,
+        function() Snacks.words.jump(vim.v.count1) end,
         desc = 'Next Reference',
         mode = { 'n', 't' },
       },
       {
         '[[',
-        function()
-          Snacks.words.jump(-vim.v.count1)
-        end,
+        function() Snacks.words.jump(-vim.v.count1) end,
         desc = 'Prev Reference',
         mode = { 'n', 't' },
       },
@@ -138,12 +108,8 @@ return {
         pattern = 'VeryLazy',
         callback = function()
           -- Setup some globals for debugging (lazy-loaded)
-          _G.dd = function(...)
-            Snacks.debug.inspect(...)
-          end
-          _G.bt = function()
-            Snacks.debug.backtrace()
-          end
+          _G.dd = function(...) Snacks.debug.inspect(...) end
+          _G.bt = function() Snacks.debug.backtrace() end
           vim.print = _G.dd -- Override print to use snacks for `:=` command
 
           -- Create some toggle mappings
