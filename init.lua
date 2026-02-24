e8378d3b79165e6d1c5be126ef2d373ecadcd8ee
William Abecassis <wabecassis@bravenpartners.com>
2026-02-24 09:44:31 -0800

    Rename lua_ls to lua-language-server
    Fix server

--- init.lua
+++ init.lua
@@ -614,7 +614,7 @@ require('lazy').setup({
       -- You can press `g?` for help in this menu.
       local ensure_installed = vim.tbl_keys(servers or {})
       vim.list_extend(ensure_installed, {
-        'lua_ls', -- Lua Language server
+        'lua-language-server', -- Lua Language server
         'stylua', -- Used to format Lua code
         -- You can add other tools here that you want Mason to install
       })
