19390c9d865d817b5200457775da34c533409e1d
Reathe <bachourian@gmail.com>
2026-02-26 03:51:50 +0100

    add auto-session

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -135,4 +135,16 @@ return {
       -- add other options here if needed.
     end,
   },
+  {
+    'rmagatti/auto-session',
+    lazy = false,
+
+    ---enables autocomplete for opts
+    ---@module "auto-session"
+    ---@type AutoSession.Config
+    opts = {
+      suppressed_dirs = { '~/', '~/Projects', '~/Downloads', '/' },
+      -- log_level = 'debug',
+    },
+  },
 }
