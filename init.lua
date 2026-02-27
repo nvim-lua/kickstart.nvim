80b1ee1789d19b3c949ab50ba959f412cfc5e54b
Ori Perry <oriori1703@gmail.com>
2026-02-27 21:04:07 +0200

    Add 'gr' group to which-keys
    Adds 'gr' to which-keys documentation, so users can see that LSP actions are grouped after 'gr' key binds.
    
    Co-authored-by: thiago-negri <evohunz@gmail.com>

--- init.lua
+++ init.lua
@@ -319,6 +319,7 @@ require('lazy').setup({
         { '<leader>s', group = '[S]earch', mode = { 'n', 'v' } },
         { '<leader>t', group = '[T]oggle' },
         { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
+        { 'gr', group = 'LSP Actions', mode = { 'n' } },
       },
     },
   },
