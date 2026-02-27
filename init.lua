1ba17893188d9bdbe483e35b77ce3678b6cd30c3
Ori Perry <oriori1703@gmail.com>
2026-02-27 19:41:45 +0200

    Re-add mason-lspconfig

--- init.lua
+++ init.lua
@@ -481,6 +481,8 @@ require('lazy').setup({
       -- Mason must be loaded before its dependents so we need to set it up here.
       -- NOTE: `opts = {}` is the same as calling `require('mason').setup({})`
       { 'mason-org/mason.nvim', opts = {} },
+      -- Maps LSP server names between nvim-lspconfig and Mason package names.
+      'mason-org/mason-lspconfig.nvim',
       'WhoIsSethDaniel/mason-tool-installer.nvim',
 
       -- Useful status updates for LSP.
