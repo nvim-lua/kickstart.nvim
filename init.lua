86f1ba26f275d7e5cfb0a6d4652f9ecc02a1492d
Ori Perry <oriori1703@gmail.com>
2026-02-28 11:31:21 +0200

    Improve undofile comment

--- init.lua
+++ init.lua
@@ -119,7 +119,7 @@ vim.schedule(function() vim.o.clipboard = 'unnamedplus' end)
 -- Enable break indent
 vim.o.breakindent = true
 
--- Save undo history
+-- Enable undo/redo changes even after closing and reopening a file
 vim.o.undofile = true
 
 -- Case-insensitive searching UNLESS \C or one or more capital letters in the search term
