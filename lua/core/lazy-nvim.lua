9471bfdedcfbafe96975fd69b71b9211c4283b8c
Abdulrahman Sheikho <abdulrahman.j.sheikho@gmail.com>
2026-03-08 08:07:22 +0300

    merge: lua/kickstart/plugins with lua/plugins

--- lua/core/lazy-nvim.lua
+++ lua/core/lazy-nvim.lua
@@ -64,26 +64,17 @@ require('lazy').setup({
   --
   -- Use the `dependencies` key to specify the dependencies of a particular plugin
 
-  -- The following comments only work if you have downloaded the kickstart repo, not just copy pasted the
-  -- init.lua. If you want these files, they are in the repository, so you can just download them and
-  -- place them in the correct locations.
-
   -- NOTE: Next step on your Neovim journey: Add/Configure additional plugins for Kickstart
   --
-  --  Here are some example plugins that I've included in the Kickstart repository.
-  --  Uncomment any of the lines below to enable them (you will need to restart nvim).
+  --  You can do that by requiring each file as:
   --
-  -- require 'kickstart.plugins.debug',
-  -- require 'kickstart.plugins.indent_line',
-  -- require 'kickstart.plugins.lint',
-  -- require 'kickstart.plugins.autopairs',
-  -- require 'kickstart.plugins.neo-tree',
-  -- require 'kickstart.plugins.gitsigns', -- adds gitsigns recommend keymaps
-
-  -- NOTE: The import below can automatically add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
-  --    This is the easiest way to modularize your config.
+  -- require 'plugins.gitsigns'
   --
-  --  Uncomment the following line and add your plugins to `lua/custom/plugins/*.lua` to get going.
+  --  Or by using lazy.nvim special `import` field to automatically import your
+  --  own plugins, configuration, etc from `lua/custom/plugins/*.lua`
+  --
+  --  This is the easiest way to modularize your config.
+  --
   { import = 'plugins' },
 
   -- For additional information with loading, sourcing and examples see `:help lazy.nvim-🔌-plugin-spec`
---
+++ lua/plugins/indent-blankline.lua
@@ -0,0 +1,13 @@
+-- Add indentation guides even on blank lines
+
+---@module 'lazy'
+---@type LazySpec
+return {
+  'lukas-reineke/indent-blankline.nvim',
+  -- Enable `lukas-reineke/indent-blankline.nvim`
+  -- See `:help ibl`
+  main = 'ibl',
+  ---@module 'ibl'
+  ---@type ibl.config
+  opts = {},
+}
