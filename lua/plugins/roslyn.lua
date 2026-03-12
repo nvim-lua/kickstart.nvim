0a3145a39d1ed9ebfd28a6ca1b1c25ed0b559621
Abdulrahman Sheikho <abdulrahman.j.sheikho@gmail.com>
2026-03-12 19:31:42 +0300

    add-plugin: roslyn.nvim - to work with C#

---
+++ lua/plugins/roslyn.lua
@@ -0,0 +1,9 @@
+return {
+  'seblyng/roslyn.nvim',
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  ft = { 'csharp' },
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+}
