ebc82857dc1e43b3adf18ce3f6c700290d0ac166
Nick Burt <nbur4556@gmail.com>
2026-03-14 22:12:39 -0500

    initial roslyn configuration

---
+++ lua/custom/plugins/roslyn.lua
@@ -0,0 +1,8 @@
+return {
+  'seblyng/roslyn.nvim',
+  ---@module 'roslyn.config'
+  ---@type RoslynNvimConfig
+  opts = {
+    -- your configuration comes here; leave empty for default settings
+  },
+}
