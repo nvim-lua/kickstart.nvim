32aa7dcbd1aaaec2f7090aa5006b00f41eb792da
hlstwizard <h.l.s.t@163.com>
2026-03-16 15:48:18 +0800

    Add json fmt

--- init.lua
+++ init.lua
@@ -697,6 +697,7 @@ require('lazy').setup({
       end,
       formatters_by_ft = {
         lua = { 'stylua' },
+        json = { 'prettier' },
         yaml = { 'prettier' },
         -- Conform can also run multiple formatters sequentially
         -- python = { "isort", "black" },
