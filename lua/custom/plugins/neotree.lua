4cee729300f36562c9baded09e38df3326ac7187
hlstwizard <h.l.s.t@163.com>
2026-03-17 20:33:34 +0800

    Try different file explore and neotree wins

---
+++ lua/custom/plugins/neotree.lua
@@ -0,0 +1,12 @@
+return {
+  {
+    'nvim-neo-tree/neo-tree.nvim',
+    branch = 'v3.x',
+    dependencies = {
+      'nvim-lua/plenary.nvim',
+      'MunifTanjim/nui.nvim',
+      'nvim-tree/nvim-web-devicons', -- optional, but recommended
+    },
+    lazy = false, -- neo-tree will lazily load itself
+  },
+}
