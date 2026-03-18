bbdb00f57ec01748434b813785a9e8840e103350
Jose Carvajal <josecarcar1@gmail.com>
2026-03-18 17:00:01 -0600

    Update catppuccin

--- lua/custom/plugins/theme.lua
+++ lua/custom/plugins/theme.lua
@@ -5,7 +5,7 @@ return {
     priority = 1000,
     config = function()
       require('catppuccin').setup()
-      vim.cmd.colorscheme 'catppuccin'
+      vim.cmd.colorscheme 'catppuccin-nvim'
     end,
   },
   { -- You can easily change to a different colorscheme.
