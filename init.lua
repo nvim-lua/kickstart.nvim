a3d1a092dbe1694f0ec94acf2db0c3e73cf568f7
Jesper Liljegren <jesper.liljegren@gmail.com>
2026-03-23 10:08:11 +0100

    feat: enable relative line numbers in Neovim

--- init.lua
+++ init.lua
@@ -102,7 +102,7 @@ vim.g.have_nerd_font = true
 vim.o.number = true
 -- You can also add relative line numbers, to help with jumping.
 --  Experiment for yourself to see if you like it!
--- vim.o.relativenumber = true
+vim.o.relativenumber = true
 
 -- Enable mouse mode, can be useful for resizing splits for example!
 vim.o.mouse = 'a'
