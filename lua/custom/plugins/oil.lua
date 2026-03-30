c65b95682c3aaf37b628d1f11144591df4c14f2d
Jose Carvajal <josecarcar1@gmail.com>
2026-03-30 10:02:26 -0600

    Add keymap to save file and trigger oil in one go

--- lua/custom/plugins/oil.lua
+++ lua/custom/plugins/oil.lua
@@ -6,4 +6,12 @@ return {
   dependencies = { { 'nvim-mini/mini.icons', opts = {} } },
   -- dependencies = { "nvim-tree/nvim-web-devicons" }, -- use if you prefer nvim-web-devicons
   lazy = false,
+  config = function(_, opts)
+    require('oil').setup(opts)
+    -- add keymap to save file and trigger oil afterwards
+    vim.keymap.set('n', '<A-o>', function()
+      vim.cmd 'w'
+      vim.cmd 'Oil'
+    end, {})
+  end,
 }
