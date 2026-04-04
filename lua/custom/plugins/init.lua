4bb0089b394ffb47444d69ee427a7eb0608e17bf
Joe Sinopoli <3653367+joe-sinopoli@users.noreply.github.com>
2026-04-04 00:40:27 -0400

    Add render-markdown.nvim for inline markdown rendering

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,4 +2,12 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-return {}
+-- All .lua files in this directory will be automatically loaded by lazy.nvim
+return {
+  {
+    'MeanderingProgrammer/render-markdown.nvim',
+    dependencies = { 'nvim-treesitter/nvim-treesitter', 'nvim-tree/nvim-web-devicons' },
+    ft = { 'markdown' },
+    opts = {},
+  },
+}
