5cd972fd06ebf2aa54d248a24722866d57ea93a3
Nick Burt <nbur4556@gmail.com>
2026-04-11 19:32:58 -0500

    comment for remote sshfs plugin usage

--- lua/custom/plugins/remote-sshfs.lua
+++ lua/custom/plugins/remote-sshfs.lua
@@ -1,3 +1,4 @@
+-- Usage: :RemoteSSHFSConnect <user>@<ipaddress>:/path/to/file
 return {
   'nosduco/remote-sshfs.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
