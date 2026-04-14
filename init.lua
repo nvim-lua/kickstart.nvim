47f7a55f7b32335fa451a29d0c45d0f51a1163d3
Brian Henderson <brianjhenderson@gmail.com>
2026-04-14 21:14:52 +0200

    refactor: extract blink.cmp preset to custom plugin

--- init.lua
+++ init.lua
@@ -798,7 +798,7 @@ require('lazy').setup({
         -- <c-k>: Toggle signature help
         --
         -- See :h blink-cmp-config-keymap for defining your own keymap
-        preset = 'super-tab',
+        preset = 'default',
 
         -- For more advanced Luasnip keymaps (e.g. selecting choice nodes, expansion) see:
         --    https://github.com/L3MON4D3/LuaSnip?tab=readme-ov-file#keymaps
---
+++ lua/custom/plugins/blink-cmp.lua
@@ -0,0 +1,6 @@
+return {
+  'saghen/blink.cmp',
+  opts = {
+    keymap = { preset = 'super-tab' },
+  },
+}
