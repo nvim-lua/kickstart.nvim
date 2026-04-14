2d05423ac20cdf4c6fa2199b7f163194111ecc94
Brian Henderson <brianjhenderson@gmail.com>
2026-04-14 21:23:18 +0200

    refactor: extract kickstart optional plugin enables

---
+++ lua/custom/plugins/kickstart-enable.lua
@@ -0,0 +1,8 @@
+return {
+  require 'kickstart.plugins.debug',
+  require 'kickstart.plugins.indent_line',
+  require 'kickstart.plugins.lint',
+  require 'kickstart.plugins.autopairs',
+  require 'kickstart.plugins.neo-tree',
+  require 'kickstart.plugins.gitsigns',
+}
