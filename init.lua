9f74599d9f1c46549bcc85de4fffe75e926bb199
orip <oriori1703@gmail.com>
2026-04-21 13:10:16 +0300

    Enable vim.loader for faster loading time

--- init.lua
+++ init.lua
@@ -84,6 +84,9 @@ I hope you enjoy your Neovim journey,
 P.S. You can delete this when you're done too. It's your config now! :)
 --]]
 
+-- Enables faster startup time by caching your compiled Lua modules.
+vim.loader.enable()
+
 -- Set <space> as the leader key
 -- See `:help mapleader`
 --  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
