85d4b8dfc5fbf3f2b036146998a2fc1648946b4e
Nick Burt <nbur4556@gmail.com>
2026-04-28 19:32:38 -0500

    add documentation comments for using remote sshfs

--- lua/custom/plugins/remote-sshfs.lua
+++ lua/custom/plugins/remote-sshfs.lua
@@ -1,6 +1,10 @@
--- Usage: :RemoteSSHFSConnect <user>@<ipaddress>:/path/to/file
 return {
   'nosduco/remote-sshfs.nvim',
   dependencies = { 'nvim-telescope/telescope.nvim', 'nvim-lua/plenary.nvim' },
   opts = {},
 }
+
+-- Usage: :RemoteSSHFSConnect <user>@<ipaddress>:/path/to/file
+-- Sometimes you have to force close, and that causes permission failed on reconnect.
+-- When that happens DO NOT DELETE THE DIRECTORY under .sshfd. That will delete everything that mountpoint is connected to *at the connection*
+-- Instead safely unmount with `umount -l ~/.sshfd/[MOUNT_POINT]`
