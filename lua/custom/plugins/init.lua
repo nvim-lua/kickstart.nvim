a232e1641d3cfa6ebb91a11c7867d8af1e4d3912
Kontrol <kontrol@localhost>
2026-06-12 02:09:45 -0300

    Local plugin

--- lua/custom/plugins/init.lua
+++ lua/custom/plugins/init.lua
@@ -2,5 +2,10 @@
 --  I promise not to create any merge conflicts in this directory :)
 --
 -- See the kickstart.nvim README for more information
-
-return {}
+local plugins_dir = vim.fs.joinpath(vim.fn.stdpath 'config', 'lua', 'custom', 'plugins')
+for file_name, type in vim.fs.dir(plugins_dir, { follow = true }) do
+  if (type == 'file' or type == 'link') and file_name:match '%.lua$' and file_name ~= 'init.lua' then
+    local module = file_name:gsub('%.lua$', '')
+    require('custom.plugins.' .. module)
+  end
+end
