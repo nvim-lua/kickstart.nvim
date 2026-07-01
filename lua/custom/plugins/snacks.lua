36247a0bad6f789483f3ed6ef8c1a0a1160652eb
Nick Burt <nbur4556@gmail.com>
2026-07-01 13:34:28 -0500

    define snacks dashboard sections

--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -26,14 +26,20 @@
 --FIX: postmerge-20260701: Dashboard not working
 vim.pack.add { 'https://github.com/folke/snacks.nvim' }
 require('snacks').setup {
-  -- dashboard = { enabled = true },
+  dashboard = {
+    enabled = true,
+    sections = {
+      { section = 'header' },
+      { section = 'keys', gap = 1, padding = 1 },
+    },
+  },
   gitbrowse = { enabled = true },
   lazygit = { enabled = true },
   scratch = { enabled = true },
   terminal = { enabled = true },
 }
 
--- vim.keymap.set('n', '<leader>=', function() Snacks.dashboard.open() end, { desc = 'Dashboard' })
+vim.keymap.set('n', '<leader>=', function() Snacks.dashboard.open() end, { desc = 'Dashboard' })
 vim.keymap.set('n', '<leader>gb', function() Snacks.gitbrowse.open() end, { desc = 'Open [G]it Repository in [B]rowser' })
 vim.keymap.set('n', '<leader>gl', function() Snacks.lazygit.open() end, { desc = 'Open [L]azy [G]it' })
 vim.keymap.set('n', '<leader>no', function() Snacks.scratch() end, { desc = '[O]pen [N]otepad' })
