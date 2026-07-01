dae771e520c9a6b0cd65a3bbe57b1fbb266505e5
Nick Burt <nbur4556@gmail.com>
2026-07-01 13:08:39 -0500

    configure snacks with vim pack

--- doc/smoketest.md
+++ doc/smoketest.md
@@ -7,8 +7,15 @@ A manual smoketest checklist to ensure features are working:
 ### Custom
 - [ ] Oil
     - Shortcut `<leader>-` should open Oil
+- [ ] Snacks
+    - Should load dashboard at start, and `<leader>=` should open dashboard
+    - `<leader>gb` should open the remote git repository in a browser
+    - `<leader>gl` should open lazy git
+    - `<leader>no` should open a scratchpad
+    - `<leader>ns` browses existing notes in scratchpad
+    - `<leader><C-t>` should open a terminal
 - [ ] VimTmuxNavigator
-    - Should be able to navigate between nvim and tmux panes using `<CTRL>j`, `<CTRL>k`, `<CTRL>h`, `<CTRL>l` for each respective direction
+    - Should be able to navigate between nvim and tmux panes using `<C-j>`, `<C-k>`, `<C-h>`, `<C-l>` for each respective direction
 
 ### Themes
 - [ ] Catppuccin Theme
--- lua/custom/plugins/snacks.lua
+++ lua/custom/plugins/snacks.lua
@@ -1,24 +1,41 @@
-return {
-  'folke/snacks.nvim',
-  priority = 1000,
-  lazy = false,
-  ---@type snacks.Config
-  opts = {
-    --TODO: Use this for a bit and compare to mini dashboard
-    --TODO: design a style and featuers for my dashboard (add opening parent directory with Oil)
-    dashboard = { enabled = true },
-    gitbrowse = { enabled = true },
-    lazygit = { enabled = true },
-    --TODO: determine a method for deleting scratches easily
-    scratch = { enabled = true },
-    terminal = { enabled = true },
-  },
-  keys = {
-    { '<leader>=', function() Snacks.dashboard.open() end, desc = 'Dashboard' },
-    { '<leader>gb', function() Snacks.gitbrowse.open() end, desc = 'Open Git Repository in Browser' },
-    { '<leader>gl', function() Snacks.lazygit.open() end, desc = 'Open Lazy Git' },
-    { '<leader>no', function() Snacks.scratch() end, desc = 'Open Notepad' },
-    { '<leader>ns', function() Snacks.scratch.select() end, desc = 'Select Note' },
-    { '<leader><C-t>', function() Snacks.terminal() end, desc = 'Open Terminal' },
-  },
+-- return {
+--   'folke/snacks.nvim',
+--   priority = 1000,
+--   lazy = false,
+--   ---@type snacks.Config
+--   opts = {
+--     dashboard = { enabled = true },
+--     gitbrowse = { enabled = true },
+--     lazygit = { enabled = true },
+--     scratch = { enabled = true },
+--     terminal = { enabled = true },
+--   },
+--   keys = {
+--     { '<leader>=', function() Snacks.dashboard.open() end, desc = 'Dashboard' },
+--     { '<leader>gb', function() Snacks.gitbrowse.open() end, desc = 'Open Git Repository in Browser' },
+--     { '<leader>gl', function() Snacks.lazygit.open() end, desc = 'Open Lazy Git' },
+--     { '<leader>no', function() Snacks.scratch() end, desc = 'Open Notepad' },
+--     { '<leader>ns', function() Snacks.scratch.select() end, desc = 'Select Note' },
+--     { '<leader><C-t>', function() Snacks.terminal() end, desc = 'Open Terminal' },
+--   },
+-- }
+
+--TODO: design a style and features for my dashboard (add opening parent directory with Oil)
+--TODO: determine a method for deleting scratches easily
+
+--FIX: postmerge-20260701: Dashboard not working
+vim.pack.add { 'https://github.com/folke/snacks.nvim' }
+require('snacks').setup {
+  -- dashboard = { enabled = true },
+  gitbrowse = { enabled = true },
+  lazygit = { enabled = true },
+  scratch = { enabled = true },
+  terminal = { enabled = true },
 }
+
+-- vim.keymap.set('n', '<leader>=', function() Snacks.dashboard.open() end, { desc = 'Dashboard' })
+vim.keymap.set('n', '<leader>gb', function() Snacks.gitbrowse.open() end, { desc = 'Open [G]it Repository in [B]rowser' })
+vim.keymap.set('n', '<leader>gl', function() Snacks.lazygit.open() end, { desc = 'Open [L]azy [G]it' })
+vim.keymap.set('n', '<leader>no', function() Snacks.scratch() end, { desc = '[O]pen [N]otepad' })
+vim.keymap.set('n', '<leader>ns', function() Snacks.scratch.select() end, { desc = '[S]elect [N]ote' })
+vim.keymap.set('n', '<leader><C-t>', function() Snacks.terminal() end, { desc = 'Open [T]erminal' })
