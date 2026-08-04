099de58276cb135943ed6f32ca97cab4d1911735
Torben Baumann <torben.baumann@x2e.de>
2026-08-04 14:38:40 +0200

    add formater for python

--- lua/kickstart/plugins/conform.lua
+++ lua/kickstart/plugins/conform.lua
@@ -31,8 +31,7 @@ return {
     end,
     formatters_by_ft = {
       lua = { "stylua" },
-      -- Conform can also run multiple formatters sequentially
-      -- python = { "isort", "black" },
+      python = { "isort", "black" },
       --
       -- You can use 'stop_after_first' to run the first available formatter from the list
       -- javascript = { "prettierd", "prettier", stop_after_first = true },
