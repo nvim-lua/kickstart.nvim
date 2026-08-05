c2205d89eb1f5eccf4030927e99e99e3db3afe5d
Torben Baumann <torben.baumann@x2e.de>
2026-08-05 11:58:26 +0200

    edit file specific indentation

--- after/ftplugin/json.lua
+++ after/ftplugin/json.lua
@@ -1,10 +1,10 @@
 -- Size of tab
-vim.o.tabstop = 2
+vim.o.tabstop = 4
 
 -- How far the tab key or auto new line indent shifts you in
 -- shiftwidth / tabstop = num of actual tabs (\t)
 -- the remainder gets filled in with spaces
-vim.o.shiftwidth = 2
+vim.o.shiftwidth = 4
 
 -- Don't replace tab with spaces
 vim.o.expandtab = false
