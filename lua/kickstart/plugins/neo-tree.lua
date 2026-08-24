786d123ed1fb5a2ba91f0067e56aa9592ba9da10
SplinterSword <sparshj2003@gmail.com>
2026-08-24 10:00:34 +0530

    feat: made it so that all the dotfiles and gitignore files are shown in neotree plugin

--- lua/kickstart/plugins/neo-tree.lua
+++ lua/kickstart/plugins/neo-tree.lua
@@ -11,6 +11,11 @@ vim.keymap.set('n', '\\', '<Cmd>Neotree reveal<CR>', { desc = 'NeoTree reveal', 
 
 require('neo-tree').setup {
   filesystem = {
+    filtered_items = {
+      visible = true,
+      hide_dotfiles = true,
+      hide_gitignored = true,
+    },
     window = {
       mappings = {
         ['\\'] = 'close_window',
