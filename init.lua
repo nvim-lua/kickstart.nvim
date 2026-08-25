a751df8421926a68ba5d683128146226e4cbd232
SplinterSword <sparshj2003@gmail.com>
2026-08-25 19:29:22 +0530

    feat: disabled Neotree

--- init.lua
+++ init.lua
@@ -981,7 +981,7 @@ do
   require 'kickstart.plugins.indent_line'
   require 'kickstart.plugins.lint'
   require 'kickstart.plugins.autopairs'
-  require 'kickstart.plugins.neo-tree'
+  -- require 'kickstart.plugins.neo-tree'
   require 'kickstart.plugins.gitsigns' -- adds gitsigns recommended keymaps
 
   -- NOTE: You can add your own plugins, configuration, etc from `lua/custom/plugins/*.lua`
